913c59a3121cc59ba783a933aa58fb9005681f00
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-07-17 12:27:02 +0800

    Initial code for CSharpGL.

---
+++ CSharpGL/GL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// OpenGL API.
+    /// </summary>
+    public partial class GL
+    {
+    }
+}
