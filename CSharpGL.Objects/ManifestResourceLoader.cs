6a8e4d7808a41e5818b33fbcf329767b8d72e344
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-07-24 04:15:10 +0800

    添加GLSLShader给GLCanvas

---
+++ CSharpGL.Objects/ManifestResourceLoader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace CSharpGL.Objects
+{
+    /// <summary>
+    /// A small helper class to load manifest resource files.
+    /// </summary>
+    public static class ManifestResourceLoader
+    {
+        /// <summary>
+        /// Loads the named manifest resource as a text string.
+        /// </summary>
+        /// <param name="textFileName">Name of the text file.</param>
+        /// <returns>The contents of the manifest resource.</returns>
+        public static string LoadTextFile(string textFileName)
+        {
+            Assembly executingAssembly;
+            string location;
+            GetLocation(textFileName, out executingAssembly, out location);
+
+            using (var stream = executingAssembly.GetManifestResourceStream(location))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private static void GetLocation(string textFileName, out Assembly executingAssembly, out string location)
+        {
+            StackTrace stack = new StackTrace();
+            StackFrame frame = stack.GetFrame(1);
+            MethodBase method = frame.GetMethod();
+            Type type = method.ReflectedType;
+            executingAssembly = type.Assembly; //Assembly.GetExecutingAssembly();
+            string pathToDots = textFileName.Replace("\\", ".");
+            location = string.Format("{0}.{1}", executingAssembly.GetName().Name, pathToDots);
+        }
+
+        public static Bitmap LoadBitmap(string filename)
+        {
+            Assembly executingAssembly;
+            string location;
+            GetLocation(filename, out executingAssembly, out location);
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(location))
+            {
+                Image image = Bitmap.FromStream(stream);
+                Bitmap bmp = image as Bitmap;
+                return bmp;
+            }
+        }
+    }
+}
