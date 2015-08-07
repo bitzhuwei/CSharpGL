94696bb69164b428b13c0e074c53d528e671473c
祝威 <bitzhuwei.cnblogs@qq.com>
2015-08-07 14:14:35 +0800

    TTF2Bmps尝试为TTF生成生成一个大的纹理。

--- TTF2Bmps/FormMain.Designer.cs
+++ TTF2Bmps/FormMain.Designer.cs
@@ -39,15 +39,19 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.numFontHeight = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFirstChar = new System.Windows.Forms.TextBox();
+            this.txtLastChar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numFontHeight)).BeginInit();
             this.SuspendLayout();
-            //
+            // 
             // openTTFFileDlg
-            //
+            // 
             this.openTTFFileDlg.Filter = "(字体文件 *.ttf)|*.ttf";
-            //
+            // 
             // btnBrowseTTFFile
-            //
+            // 
             this.btnBrowseTTFFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnBrowseTTFFile.Location = new System.Drawing.Point(644, 12);
             this.btnBrowseTTFFile.Margin = new System.Windows.Forms.Padding(4);
@@ -57,10 +61,10 @@
             this.btnBrowseTTFFile.Text = "Browse...";
             this.btnBrowseTTFFile.UseVisualStyleBackColor = true;
             this.btnBrowseTTFFile.Click += new System.EventHandler(this.btnBrowseTTFFile_Click);
-            //
+            // 
             // txtTTFFullname
-            //
-            this.txtTTFFullname.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            // 
+            this.txtTTFFullname.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtTTFFullname.Location = new System.Drawing.Point(103, 15);
             this.txtTTFFullname.Margin = new System.Windows.Forms.Padding(4);
@@ -69,9 +73,9 @@
             this.txtTTFFullname.Size = new System.Drawing.Size(532, 25);
             this.txtTTFFullname.TabIndex = 1;
             this.txtTTFFullname.Text = "C:\\Users\\威\\Documents\\ARIALUNI.TTF";
-            //
+            // 
             // label1
-            //
+            // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(16, 21);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -79,9 +83,9 @@
             this.label1.Size = new System.Drawing.Size(79, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "TTF file:";
-            //
+            // 
             // btnBrowseFolder
-            //
+            // 
             this.btnBrowseFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnBrowseFolder.Location = new System.Drawing.Point(644, 46);
             this.btnBrowseFolder.Margin = new System.Windows.Forms.Padding(4);
@@ -91,10 +95,10 @@
             this.btnBrowseFolder.Text = "Browse...";
             this.btnBrowseFolder.UseVisualStyleBackColor = true;
             this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
-            //
+            // 
             // txtDestFolder
-            //
-            this.txtDestFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            // 
+            this.txtDestFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtDestFolder.Location = new System.Drawing.Point(103, 49);
             this.txtDestFolder.Margin = new System.Windows.Forms.Padding(4);
@@ -103,9 +107,9 @@
             this.txtDestFolder.Size = new System.Drawing.Size(532, 25);
             this.txtDestFolder.TabIndex = 1;
             this.txtDestFolder.Text = "C:\\Users\\威\\Desktop\\ttf2bmps";
-            //
+            // 
             // label2
-            //
+            // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(16, 55);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -113,9 +117,9 @@
             this.label2.Size = new System.Drawing.Size(71, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "save to:";
-            //
+            // 
             // btnStart
-            //
+            // 
             this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnStart.Location = new System.Drawing.Point(644, 122);
             this.btnStart.Margin = new System.Windows.Forms.Padding(4);
@@ -125,9 +129,9 @@
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            //
+            // 
             // label3
-            //
+            // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(16, 88);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -135,9 +139,9 @@
             this.label3.Size = new System.Drawing.Size(63, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "height:";
-            //
+            // 
             // numFontHeight
-            //
+            // 
             this.numFontHeight.Location = new System.Drawing.Point(103, 82);
             this.numFontHeight.Minimum = new decimal(new int[] {
             24,
@@ -152,12 +156,58 @@
             0,
             0,
             0});
-            //
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(211, 88);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "first char:";
+            // 
+            // txtFirstChar
+            // 
+            this.txtFirstChar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFirstChar.Location = new System.Drawing.Point(314, 82);
+            this.txtFirstChar.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFirstChar.Name = "txtFirstChar";
+            this.txtFirstChar.Size = new System.Drawing.Size(57, 25);
+            this.txtFirstChar.TabIndex = 4;
+            this.txtFirstChar.Text = "!";
+            // 
+            // txtLastChar
+            // 
+            this.txtLastChar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLastChar.Location = new System.Drawing.Point(474, 82);
+            this.txtLastChar.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLastChar.Name = "txtLastChar";
+            this.txtLastChar.Size = new System.Drawing.Size(57, 25);
+            this.txtLastChar.TabIndex = 4;
+            this.txtLastChar.Text = "~";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(379, 88);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "last char:";
+            // 
             // FormMain
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(760, 165);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLastChar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFirstChar);
             this.Controls.Add(this.numFontHeight);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -189,5 +239,9 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown numFontHeight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFirstChar;
+        private System.Windows.Forms.TextBox txtLastChar;
+        private System.Windows.Forms.Label label5;
     }
 }
