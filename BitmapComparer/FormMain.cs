2064a78b0551705651290f3b83d7cfda5c17b59c
祝威 <bitzhuwei.cnblogs@qq.com>
2015-08-08 01:05:06 +0800

    整理代码。

--- BitmapComparer/FormMain.cs
+++ BitmapComparer/FormMain.cs
@@ -51,50 +51,64 @@ namespace BitmapComparer
                 return;
             }
 
-            string name1 = this.txtBitmapFullname1.Text;
-
-            string name2 = this.txtBitmapFullname2.Text;
-
-            Bitmap bmp1 = new Bitmap(name1);
-            Bitmap bmp2 = new Bitmap(name2);
-            if (bmp1.Width != bmp2.Width || bmp1.Height != bmp2.Height)
+            Bitmap bmp1 = null;
+            Bitmap bmp2 = null;
+            try
             {
-                MessageBox.Show("not the same size!");
-            }
-            else
-            {
+                string name1 = this.txtBitmapFullname1.Text;
+                string name2 = this.txtBitmapFullname2.Text;
 
-                FileInfo bmpFile1Info = new FileInfo(this.txtBitmapFullname1.Text);
-                string outputFile = string.Format("{0}\\{1}.txt", bmpFile1Info.DirectoryName, bmpFile1Info.Name);
-                using (StreamWriter sw = new StreamWriter(outputFile))
+                bmp1 = new Bitmap(name1);
+                bmp2 = new Bitmap(name2);
+
+                if (bmp1.Width != bmp2.Width || bmp1.Height != bmp2.Height)
                 {
-                    sw.WriteLine("comparer:");
-                    sw.WriteLine(name1);
-                    sw.WriteLine(name2);
-                    int diffCount = 0;
-                    for (int row = 0; row < bmp1.Height; row++)
+                    MessageBox.Show("not the same size!");
+                }
+                else
+                {
+
+                    FileInfo bmpFile1Info = new FileInfo(this.txtBitmapFullname1.Text);
+                    string outputFile = string.Format("{0}\\{1}.txt", bmpFile1Info.DirectoryName, bmpFile1Info.Name);
+                    using (StreamWriter sw = new StreamWriter(outputFile))
                     {
-                        for (int col = 0; col < bmp1.Width; col++)
+                        sw.WriteLine("comparer:");
+                        sw.WriteLine(name1);
+                        sw.WriteLine(name2);
+                        int diffCount = 0;
+                        for (int row = 0; row < bmp1.Height; row++)
                         {
-                            Color c1 = bmp1.GetPixel(col, row);
-                            Color c2 = bmp2.GetPixel(col, row);
-                            if (c1.R != c2.R || c1.G != c2.G || c1.B != c2.B || c1.A != c2.A)
+                            for (int col = 0; col < bmp1.Width; col++)
                             {
-                                sw.WriteLine(string.Format("@ ({0}, {1}) diff: {2} vs {3}",
-                                    row, col, c1, c2));
-                                diffCount++;
+                                Color c1 = bmp1.GetPixel(col, row);
+                                Color c2 = bmp2.GetPixel(col, row);
+                                if (c1.R != c2.R || c1.G != c2.G || c1.B != c2.B || c1.A != c2.A)
+                                {
+                                    sw.WriteLine(string.Format("@ ({0}, {1}) diff: {2} vs {3}",
+                                        row, col, c1, c2));
+                                    diffCount++;
+                                }
                             }
                         }
+
+                        sw.WriteLine(string.Format("{0} diffs.", diffCount));
                     }
 
-                    sw.WriteLine(string.Format("{0} diffs.", diffCount));
+                    Process.Start("explorer", outputFile);
                 }
-
-                Process.Start("explorer", outputFile);
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if (bmp1 != null)
+                { bmp1.Dispose(); }
+                if (bmp2 != null)
+                { bmp2.Dispose(); }
 
-            bmp1.Dispose();
-            bmp2.Dispose();
+            }
         }
 
 
--- CSharpGL.Objects/Texts/TTFTexture.cs
+++ CSharpGL.Objects/Texts/TTFTexture.cs
@@ -192,13 +192,12 @@ namespace CSharpGL.Objects.Texts
                     {
                         if (cInfo.width > 0 && cInfo.height > 0)
                         {
-                            //System.Drawing.Imaging.PixelFormat format = System.Drawing.Imaging.PixelFormat.Format32bppArgb;
-                            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(cInfo.width, cInfo.height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
-                            System.Drawing.Imaging.BitmapData bmpData = bitmap.LockBits(
-                                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-                                 System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                            System.Drawing.Imaging.PixelFormat format = System.Drawing.Imaging.PixelFormat.Format32bppRgb;
+                            System.Drawing.Imaging.ImageLockMode lockMode = System.Drawing.Imaging.ImageLockMode.WriteOnly;
+                            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(cInfo.width, cInfo.height, format);
+                            Rectangle bitmapRect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                            System.Drawing.Imaging.BitmapData bmpData = bitmap.LockBits(bitmapRect, lockMode, format);
 
-                            // Declare an array to hold the bytes of the bitmap.
                             int length = Math.Abs(bmpData.Stride) * bitmap.Height;
                             byte[] bitmapBytes = new byte[length];
                             for (int row = 0; row < cInfo.height; row++)
@@ -213,98 +212,10 @@ namespace CSharpGL.Objects.Texts
                                 }
                             }
 
-                            // Copy the RGB values back to the bitmap
                             System.Runtime.InteropServices.Marshal.Copy(bitmapBytes, 0, bmpData.Scan0, length);
 
-                            // Unlock the bits.
                             bitmap.UnlockBits(bmpData);
-                            //TmpTest(byteBitmap, cInfo);
-                            //foreach (var item in Enum.GetValues(typeof(System.Drawing.Imaging.PixelFormat)))
-                            //{
-                            //    try
-                            //    {
-                            //        System.Drawing.Imaging.PixelFormat format = (System.Drawing.Imaging.PixelFormat)item;
-                            //        System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(cInfo.width, cInfo.height,
-                            //            System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-                            //        System.Drawing.Imaging.BitmapData bmpData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-                            //             System.Drawing.Imaging.ImageLockMode.WriteOnly, format);
-                            //        // Get the address of the first line.
-                            //        IntPtr ptr = bmpData.Scan0;
 
-                            //        // Declare an array to hold the bytes of the bitmap.
-                            //        int bytes = Math.Abs(bmpData.Stride) * bitmap.Height;
-                            //        byte[] rgbValues = new byte[bytes];
-
-                            //        // Copy the RGB values into the array.
-                            //        System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-                            //        // Set every third value to 255. A 24bpp bitmap will look red.
-                            //        for (int counter = 2; counter < rgbValues.Length; counter += 3)
-                            //        {
-                            //            rgbValues[counter] = 255;
-                            //        }
-                            //        for (int tmpRow = 0; tmpRow < cInfo.height; ++tmpRow)
-                            //        {
-                            //            for (int tmpWidth = 0; tmpWidth < cInfo.width; ++tmpWidth)
-                            //            {
-                            //                byte color = byteBitmap[tmpRow * cInfo.width + tmpWidth];
-                            //                //bitmap.SetPixel(tmpWidth, tmpRow, Color.FromArgb(color, color, color));
-                            //                rgbValues[tmpRow * cInfo.width + tmpWidth + 0] = color;
-                            //                rgbValues[tmpRow * cInfo.width + tmpWidth + 1] = color;
-                            //                rgbValues[tmpRow * cInfo.width + tmpWidth + 2] = color;
-                            //                rgbValues[tmpRow * cInfo.width + tmpWidth + 3] = color;
-                            //            }
-                            //        }
-                            //        // Copy the RGB values back to the bitmap
-                            //        System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-
-                            //        // Unlock the bits.
-                            //        bitmap.UnlockBits(bmpData);
-                            //        bitmap.Save(item + ".bmp");
-                            //    }
-                            //    catch (Exception e)
-                            //    {
-                            //        Console.WriteLine(e);
-                            //    }
-                            //}
-
-                            //for (int tmpRow = 0; tmpRow < cInfo.height; ++tmpRow)
-                            //{
-                            //    for (int tmpWidth = 0; tmpWidth < cInfo.width; ++tmpWidth)
-                            //    {
-                            //        byte color = byteBitmap[tmpRow * cInfo.width + tmpWidth];
-                            //        bitmap.SetPixel(tmpWidth, tmpRow, Color.FromArgb(color, color, color));
-                            //    }
-                            //}
-
-                            //System.Drawing.Imaging.ImageFormat[] formats = new System.Drawing.Imaging.ImageFormat[] {
-                            //    System.Drawing.Imaging.ImageFormat.Bmp,
-                            //    System.Drawing.Imaging.ImageFormat.Emf,
-                            //    System.Drawing.Imaging.ImageFormat.Exif,
-                            //    System.Drawing.Imaging.ImageFormat.Gif,
-                            //    System.Drawing.Imaging.ImageFormat.Icon,
-                            //    System.Drawing.Imaging.ImageFormat.Jpeg,
-                            //    System.Drawing.Imaging.ImageFormat.MemoryBmp,
-                            //    System.Drawing.Imaging.ImageFormat.Png,
-                            //    System.Drawing.Imaging.ImageFormat.Tiff,
-                            //    System.Drawing.Imaging.ImageFormat.Wmf,
-                            //};
-                            //foreach (var format in formats)
-                            //{
-                            //    try
-                            //    {
-                            //        using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
-                            //        {
-                            //            bitmap.Save(ms, format);
-                            //            byte[] bytes = ms.GetBuffer();
-                            //            bytes = ms.ToArray();
-                            //        }
-                            //    }
-                            //    catch (Exception e)
-                            //    {
-                            //        string str = e.ToString();
-                            //    }
-                            //}
                             int baseLine = fontHeight * 3 / 4;
                             graphics.DrawImage(bitmap, cInfo.xoffset, cInfo.yoffset + baseLine - glyph.obj.top);
                         }
