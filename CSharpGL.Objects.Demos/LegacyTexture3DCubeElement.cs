f311d66bb6863e1739856c7db7a7f75dc94b526b
祝威 <bitzhuwei.cnblogs@qq.com>
2015-08-21 18:05:05 +0800

    提取CSharpGL.Objects.Demos项目。

---
+++ CSharpGL.Objects.Demos/LegacyTexture3DCubeElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL.Objects.Demos
+{
+    /// <summary>
+    /// 演示Texture3D的用法和效果。
+    /// </summary>
+    class LegacyTexture3DCubeElement : SceneElementBase
+    {
+        protected override void DoInitialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoRender(RenderModes renderMode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
