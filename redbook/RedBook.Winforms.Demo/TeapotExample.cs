cc0f4b88c4d74ee3e2d60f77f0659b9ce07f6602
祝威 <bitzhuwei.cnblogs@qq.com>
2015-08-29 01:39:45 +0800

    准备TeapotExample

---
+++ redbook/RedBook.Winforms.Demo/TeapotExample.cs
@@ -0,0 +1,22 @@
+﻿using CSharpGL.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBook.Winforms.Demo
+{
+    class TeapotExample : SceneElementBase
+    {
+        protected override void DoInitialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoRender(RenderEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
