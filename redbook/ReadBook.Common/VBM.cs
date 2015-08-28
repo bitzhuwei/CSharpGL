d28f6c4bd6f61a04be95161a22b0ab475d5bae2c
祝威 <bitzhuwei.cnblogs@qq.com>
2015-08-28 20:16:42 +0800

    添加redbook相关项目。准备把redbook里的部分或全部demo转换为c#版。

---
+++ redbook/ReadBook.Common/VBM.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadBook.Common
+{
+    public class VBM
+    {
+        public const int VBM_FLAG_HAS_VERTICES = 0x00000001;
+        public const int VBM_FLAG_HAS_INDICES = 0x00000002;
+        public const int VBM_FLAG_HAS_FRAMES = 0x00000004;
+        public const int VBM_FLAG_HAS_MATERIALS = 0x00000008;
+    }
+
+    public struct VBM_HEADER
+    {
+        public uint magic;
+        public uint size;
+        public string name;//char name[64];
+        public uint num_attribs;
+        public uint num_frames;
+        public uint num_chunks;
+        public uint num_vertices;
+        public uint num_indices;
+        public uint indexype;
+        public uint num_materials;
+        public uint flags;
+    }
+
+    public struct VBM_ATTRIB_HEADER
+    {
+        public string name; //char name[64];
+        public uint type;
+        public uint components;
+        public uint flags;
+    }
+
+    public struct VBM_FRAME_HEADER
+    {
+        public uint first;
+        public uint count;
+        public uint flags;
+    }
+
+    public struct VBM_RENDER_CHUNK
+    {
+        public uint material_index;
+        public uint first;
+        public uint count;
+    }
+
+    public struct VBM_VEC4F
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+    }
+
+    public struct VBM_VEC3F
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    public struct VBM_VEC2F
+    {
+        public float x;
+        public float y;
+    }
+
+    public struct VBM_MATERIAL
+    {
+        /// <summary>
+        /// Name of material
+        /// </summary>
+        public string name;//char name[32]; 
+        /// <summary>
+        /// Ambient color
+        /// </summary>
+        public VBM_VEC3F ambient;
+        /// <summary>
+        /// Diffuse color
+        /// </summary>
+        public VBM_VEC3F diffuse;
+        /// <summary>
+        /// Specular color
+        /// </summary>
+        public VBM_VEC3F specular;
+        /// <summary>
+        /// Specular exponent
+        /// </summary>
+        public VBM_VEC3F specular_exp;
+        /// <summary>
+        /// Shininess
+        /// </summary>
+        public float shininess;
+        /// <summary>
+        /// Alpha (transparency)
+        /// </summary>
+        public float alpha;
+        /// <summary>
+        /// Transmissivity
+        /// </summary>
+        public VBM_VEC3F transmission;
+        /// <summary>
+        /// Index of refraction (optical density)
+        /// </summary>
+        public float ior;
+        /// <summary>
+        /// Ambient map (texture)
+        /// </summary>
+        public string ambient_map;   // char ambient_map[64];                   
+        /// <summary>
+        /// Diffuse map (texture)
+        /// </summary>
+        public string diffuse_map;   // char diffuse_map[64];   
+        /// <summary>
+        /// Specular map (texture)
+        /// </summary>
+
+        public string specular_map;  // char specular_map[64];                  
+        /// <summary>
+        /// Normal map (texture)
+        /// </summary>
+        public string normal_map;    // char normal_map[64];                    
+    }
+}
---
+++ redbook/ReadBook.Common/VBObject.cs
@@ -0,0 +1,113 @@
+﻿using CSharpGL;
+using CSharpGL.Maths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadBook.Common
+{
+    public class VBObject
+    {
+
+
+        #region simple functions
+
+        public uint GetVertexCount(uint frame = 0)
+        {
+            return frame < m_header.num_frames ? m_frame[frame].count : 0;
+        }
+
+        public uint GetAttributeCount()
+        {
+            return m_header.num_attribs;
+        }
+
+        public string GetAttributeName(uint index)
+        {
+            return index < m_header.num_attribs ? m_attrib[index].name : string.Empty;
+        }
+
+        public uint GetFrameCount()
+        {
+            return m_header.num_frames;
+        }
+
+        public uint GetMaterialCount()
+        {
+            return m_header.num_materials;
+        }
+
+        public string GetMaterialName(uint material_index)
+        {
+            return m_material[material_index].name;
+        }
+
+        public vec3 GetMaterialAmbient(uint material_index)
+        {
+            return new vec3(m_material[material_index].ambient.x, m_material[material_index].ambient.y, m_material[material_index].ambient.z);
+        }
+
+        public vec3 GetMaterialDiffuse(uint material_index)
+        {
+            return new vec3(m_material[material_index].diffuse.x, m_material[material_index].diffuse.y, m_material[material_index].diffuse.z);
+        }
+
+        public string GetMaterialDiffuseMapName(uint material_index)
+        {
+            return m_material[material_index].diffuse_map;
+        }
+
+        public string GetMaterialSpecularMapName(uint material_index)
+        {
+            return m_material[material_index].specular_map;
+        }
+
+        public string GetMaterialNormalMapName(uint material_index)
+        {
+            return m_material[material_index].normal_map;
+        }
+
+        public void SetMaterialDiffuseTexture(uint material_index, uint texname)
+        {
+            m_material_textures[material_index].diffuse = texname;
+        }
+
+        public void SetMaterialSpecularTexture(uint material_index, uint texname)
+        {
+            m_material_textures[material_index].specular = texname;
+        }
+
+        public void SetMaterialNormalTexture(uint material_index, uint texname)
+        {
+            m_material_textures[material_index].normal = texname;
+        }
+
+        public void BindVertexArray()
+        {
+            GL.BindVertexArray(m_vao);
+        }
+
+        #endregion simple functions
+
+        protected uint m_vao;
+        protected uint m_attribute_buffer;
+        protected uint m_index_buffer;
+
+        protected VBM_HEADER m_header;
+        protected VBM_ATTRIB_HEADER[] m_attrib;
+        protected VBM_FRAME_HEADER[] m_frame;
+        protected VBM_MATERIAL[] m_material;
+        protected VBM_RENDER_CHUNK m_chunks;
+
+        protected struct material_texture
+        {
+            public uint diffuse;
+            public uint specular;
+            public uint normal;
+        };
+
+        protected material_texture[] m_material_textures;
+    }
+}
