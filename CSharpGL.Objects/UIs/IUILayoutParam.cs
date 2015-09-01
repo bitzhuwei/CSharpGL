a9b8600ba4235264aa6884ce0e8224ae0a7a1482
祝威 <bitzhuwei.cnblogs@qq.com>
2015-09-01 15:03:44 +0800

    整理XML注释

--- CSharpGL.Objects/UIs/IUILayoutParam.cs
+++ CSharpGL.Objects/UIs/IUILayoutParam.cs
@@ -11,7 +11,7 @@ namespace CSharpGL.Objects.UIs
     {
 
         /// <summary>
-        /// the edges of the OpenGLControl to which a SimpleUIRect is bound and determines how it is resized with its parent.
+        /// the edges of the <see cref="GLCanvas"/> to which a UI’s rect is bound and determines how it is resized with its parent.
         /// <para>something like AnchorStyles.Left | AnchorStyles.Bottom.</para>
         /// </summary>
         public System.Windows.Forms.AnchorStyles Anchor;
@@ -20,13 +20,6 @@ namespace CSharpGL.Objects.UIs
         /// Gets or sets the space between viewport and SimpleRect.
         /// </summary>
         public System.Windows.Forms.Padding Margin;
-
-        ///// <summary>
-        ///// Left bottom point's location on view port.
-        ///// <para>This works when <see cref="OpenGLUIRect.Anchor"/>.Left &amp; <see cref="OpenGLUIRect.Anchor"/>.Right is <see cref="OpenGLUIRect.Anchor"/>.None.
-        ///// or <see cref="OpenGLUIRect.Anchor"/>.Top &amp; <see cref="OpenGLUIRect.Anchor"/>.Bottom is <see cref="OpenGLUIRect.Anchor"/>.None.</para>
-        ///// </summary>
-        // System.Drawing.Point Location { get; set; }
 
         /// <summary>
         /// Stores width when <see cref="OpenGLUIRect.Anchor"/>.Left &amp; <see cref="OpenGLUIRect.Anchor"/>.Right is <see cref="OpenGLUIRect.Anchor"/>.None.
