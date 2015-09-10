1cd3ca7e998f6367b46dabda39a3638e2f68d585
祝威 <bitzhuwei.cnblogs@qq.com>
2015-09-10 20:24:16 +0800

    MappingFilenameChunk应获取贴图文件名。

--- Functions/CSharpGL.FileParser._3DSParser/Chunks/MappingFilenameChunk.cs
+++ Functions/CSharpGL.FileParser._3DSParser/Chunks/MappingFilenameChunk.cs
@@ -1,18 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharpGL.FileParser._3DSParser.Chunks
 {
-    class MappingFilenameChunk : PercentageChunk
+    class MappingFilenameChunk : ChunkBase
     {
-        public ushort Useless { get { return this.percentage; } }
+        public string TextureFilename;
 
         public override string ToString()
         {
-            return string.Format("{0}, Useless: {1}", base.ToString(), this.Useless);
+            return string.Format("{0}, TextureFilename: {1}", base.ToString(), this.TextureFilename);
+        }
+
+        internal override void Process(ParsingContext context)
+        {
+            var reader = context.reader;
+            var chunk = this;
+
+            string name;
+            {
+                StringBuilder builder = new StringBuilder();
+
+                byte b = reader.ReadByte();
+                uint idx = 0;
+                while (b != 0)
+                {
+                    builder.Append((char)b);
+                    b = reader.ReadByte();
+                    idx++;
+                }
+                chunk.BytesRead += idx + 1;
+
+                name = builder.ToString();
+            }
+
+            this.TextureFilename = Path.Combine(context.base_dir, name);
         }
     }
 }
