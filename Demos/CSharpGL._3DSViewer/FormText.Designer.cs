23689282d998f07018eaba1734167809f5c84f54
祝威 <bitzhuwei.cnblogs@qq.com>
2015-09-11 13:10:43 +0800

    整理代码

--- Demos/CSharpGL._3DSViewer/FormText.Designer.cs
+++ Demos/CSharpGL._3DSViewer/FormText.Designer.cs
@@ -40,6 +40,7 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.textBox1.Size = new System.Drawing.Size(734, 601);
             this.textBox1.TabIndex = 0;
             // 
