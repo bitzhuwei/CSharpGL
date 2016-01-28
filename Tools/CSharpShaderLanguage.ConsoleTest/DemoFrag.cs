5efa4d5ee6250b7185ca7efcc9e00983a4b22abd
祝威 <bitzhuwei.cnblogs@qq.com>
2016-01-28 16:07:34 +0800

    fix

---
+++ Tools/CSharpShaderLanguage.ConsoleTest/DemoFrag.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CSharpShaderLanguage.ConsoleTest
+{
+    class DemoFrag
+    {
+    }
+}
---
+++ Tools/CSharpShaderLanguage.ConsoleTest/DemoVert.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpShaderLanguage.ConsoleTest
+{
+    class DemoVert
+    {
+        public void DemoOuput()
+        {
+            Console.WriteLine("Hello! This is from DemoVert!");
+        }
+    }
+}
