4dc8de4e6d7f425959369a2ec7477c455d80b186
祝威 <bitzhuwei.cnblogs@qq.com>
2016-02-19 16:05:13 +0800

    创建objects

---
+++ Functions/CSharpGL.Objects.Controllers/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSharpGL.Objects.Cameras;
+
+namespace CSharpGL.Objects.Controllers
+{
+    public partial class CameraController : UserControl
+    {
+        public CameraController(Camera camera)
+        {
+            InitializeComponent();
+
+            this.camera = camera;
+        }
+
+        public Camera camera { get; private set; }
+    }
+}
---
+++ Functions/CSharpGL.Objects.Controllers/PerspectiveCameraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSharpGL.Objects.Cameras;
+
+namespace CSharpGL.Objects.Controllers
+{
+    public partial class PerspectiveCameraController : UserControl
+    {
+        public PerspectiveCameraController(IPerspectiveViewCamera camera)
+        {
+            InitializeComponent();
+
+            this.camera = camera;
+        }
+
+        public IPerspectiveViewCamera camera { get; private set; }
+    }
+}
