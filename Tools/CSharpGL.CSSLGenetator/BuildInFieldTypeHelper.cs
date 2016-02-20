13e11e709411a164e27a82143a34e407b0731c8d
祝威 <bitzhuwei.cnblogs@qq.com>
2016-02-20 15:41:28 +0800

    ==>

---
+++ Tools/CSharpGL.CSSLGenetator/BuildInFieldTypeHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL.CSSLGenetator
+{
+    static class BuildInFieldTypeHelper
+    {
+        static List<IntermediateStructure> buildInFieldTypeList = new List<IntermediateStructure>();
+        
+        static BuildInFieldTypeHelper()
+        {
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "int" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "float" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "vec2" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "vec3" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "vec4" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "mat2" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "mat3" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "mat4" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "sampler1" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "sampler2" });
+            buildInFieldTypeList.Add(new IntermediateStructure() { Name = "sampler3" });
+        }
+
+        public static IEnumerable<IntermediateStructure> GetBuildInTypeList()
+        {
+            return buildInFieldTypeList;
+        }
+
+    }
+}
---
+++ Tools/CSharpGL.CSSLGenetator/FormAddIntermediateStructure.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpGL.CSSLGenetator
+{
+    public partial class FormAddIntermediateStructure : Form
+    {
+
+        public IntermediateStructure Result { get; private set; }
+
+        private CSSLTemplate template;
+
+        public FormAddIntermediateStructure(CSSLTemplate template)
+        {
+            InitializeComponent();
+
+            this.template = template;
+        }
+
+        private void FormAddVertexShaderField_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            var result = new IntermediateStructure()
+            {
+                Name = this.txtName.Text,
+            };
+            foreach (var item in this.lstFIeld.Items)
+            {
+                result.FieldList.Add(item as StructureField);
+            }
+            this.Result = result;
+
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        private void addAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dlg = new FormAddStructureField(this.template);
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StructureField field = dlg.Result;
+                this.lstFIeld.Items.Add(field);
+            }
+        }
+    }
+}
