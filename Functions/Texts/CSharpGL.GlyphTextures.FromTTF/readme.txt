896e9fd6a008c615fb077f515e57a67ae3321b98
祝威 <bitzhuwei.cnblogs@qq.com>
2016-02-21 17:56:53 +0800

    fix

--- Functions/Texts/CSharpGL.GlyphTextures.FromTTF/readme.txt
+++ Functions/Texts/CSharpGL.GlyphTextures.FromTTF/readme.txt
@@ -1,3 +1,4 @@
 ﻿本项目用于将*.ttf/*.ttc字体文件转换为[*.png+*.xml]保存的字形信息转换为OpenGL中的贴图。
+这是生产原材料的方法。
 参考资料：（https://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Text_Rendering_01）
 freetype.2.5.4ForStudy.dll的源码来自这里：（https://github.com/bitzhuwei/FreeType2.5.4ForStudy）
---
+++ Functions/Texts/CSharpGL.Texts/StringModel.cs
@@ -0,0 +1,35 @@
+﻿using CSharpGL.Objects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL.Texts
+{
+    /// <summary>
+    /// 用于渲染一段文字
+    /// </summary>
+    public class StringModel : IModel
+    {
+        public Objects.VertexBuffers.BufferRenderer GetPositionBufferRenderer(string varNameInShader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Objects.VertexBuffers.BufferRenderer GetColorBufferRenderer(string varNameInShader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Objects.VertexBuffers.BufferRenderer GetNormalBufferRenderer(string varNameInShader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Objects.VertexBuffers.BufferRenderer GetIndexes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
