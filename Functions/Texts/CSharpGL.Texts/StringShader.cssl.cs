98a1961da99fb53d2fce954a662c37a062e98762
祝威 <bitzhuwei.cnblogs@qq.com>
2016-02-21 18:49:41 +0800

    --

--- Functions/Texts/CSharpGL.Texts/StringShader.cssl.cs
+++ Functions/Texts/CSharpGL.Texts/StringShader.cssl.cs
@@ -1,12 +1,11 @@
 // 用法：使用CSSL2GLSL.exe编译此文件，即可获得对应的vertex shader, geometry shader, fragment shader。
 // 此文件中的类型不应被直接调用，发布release时可以去掉。
+#if DEBUG
+
 namespace CSharpShadingLanguage.StringShader
 {
     // 不可将此文件中的代码复制到其他文件内（如果包含了其他的using ...;，那么CSSL2GLSL.exe就无法正常编译这些代码了。）
     using CSharpShadingLanguage;
-
-#if DEBUG
-
 
     /// <summary>
     /// 一个<see cref="StringShaderVert"/>对应一个(vertex shader+fragment shader+..shader)组成的shader program。
@@ -67,6 +66,7 @@ namespace CSharpShadingLanguage.StringShader
         }
     }
 
-#endif
 }
+
+#endif
 
