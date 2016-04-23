6381cc8c620c8bff3353adce858bda59f477b82e
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-24 01:52:58 +0800

    LinesRecognizer

---
+++ CSharpGL/OpenGLObjects/ColorCodedPicking/PrimivitiveRecognizers/LinesRecognizer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL
+{
+    class LinesRecognizer : PrimitiveRecognizer
+    {
+        public override List<RecognizedPrimitiveIndex> Recognize(
+            uint lastVertexId, IntPtr pointer, int length)
+        {
+            var lastIndexIdList = new List<RecognizedPrimitiveIndex>();
+            unsafe
+            {
+                var array = (uint*)pointer.ToPointer();
+                for (uint i = 1; i < length; i += 2)
+                {
+                    if (array[i - 0] == lastVertexId)
+                    {
+                        var item = new RecognizedPrimitiveIndex(lastVertexId, i - 0);
+                        item.IndexIdList.Add(array[i - 1]);
+                        item.IndexIdList.Add(array[i - 0]);
+                        lastIndexIdList.Add(item);
+                    }
+                }
+            }
+
+            return lastIndexIdList;
+        }
+
+        public override List<RecognizedPrimitiveIndex> Recognize(
+            uint lastVertexId, IntPtr pointer, int length, uint primitiveRestartIndex)
+        {
+            var lastIndexIdList = new List<RecognizedPrimitiveIndex>();
+            unsafe
+            {
+                var array = (uint*)pointer.ToPointer();
+                uint i = 0;
+                while (i + 1 < length)
+                {
+                    if (array[i] == primitiveRestartIndex)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        if (array[i + 1] == lastVertexId)
+                        {
+                            var item = new RecognizedPrimitiveIndex(lastVertexId, i + 1);
+                            item.IndexIdList.Add(array[i + 0]);
+                            item.IndexIdList.Add(array[i + 1]);
+                            lastIndexIdList.Add(item);
+                        }
+
+                        i += 2;
+                    }
+                }
+            }
+
+            return lastIndexIdList;
+        }
+
+    }
+}
