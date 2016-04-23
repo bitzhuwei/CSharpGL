3d9495f58550654f8108decc9f689ae29082b70e
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-23 15:04:52 +0800

    PickingShaderHelper

--- CSharpGL/OpenGLObjects/ColorCodedPicking/PickingShaderHelper.cs
+++ CSharpGL/OpenGLObjects/ColorCodedPicking/PickingShaderHelper.cs
@@ -9,16 +9,23 @@ namespace CSharpGL
     public static class PickingShaderHelper //: IDisposable
     {
 
+        static ShaderProgram pickingShaderProgram = null;
+
         public static ShaderProgram GetPickingShaderProgram()
         {
-            ShaderCode[] shaders = new ShaderCode[2];
-            shaders[0] = new ShaderCode(GetShaderSource(ShaderType.VertexShader), ShaderType.VertexShader);
-            shaders[1] = new ShaderCode(GetShaderSource(ShaderType.FragmentShader), ShaderType.FragmentShader);
+            if (pickingShaderProgram == null)
+            {
+                ShaderCode[] shaders = new ShaderCode[2];
+                shaders[0] = new ShaderCode(GetShaderSource(ShaderType.VertexShader), ShaderType.VertexShader);
+                shaders[1] = new ShaderCode(GetShaderSource(ShaderType.FragmentShader), ShaderType.FragmentShader);
 
-            var shaderProgram = new ShaderProgram();
-            shaderProgram.Create((from item in shaders select item.CreateShader()).ToArray());
+                var shaderProgram = new ShaderProgram();
+                shaderProgram.Create((from item in shaders select item.CreateShader()).ToArray());
 
-            return shaderProgram;
+                pickingShaderProgram = shaderProgram;
+            }
+
+            return pickingShaderProgram;
         }
 
         /// <summary>
