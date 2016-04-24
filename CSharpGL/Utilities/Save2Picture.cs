e8f1908fe342d8809bf2b3adbb72823a456cf6b0
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-24 17:01:17 +0800

    Save2Picture

--- CSharpGL/Utilities/Save2Picture.cs
+++ CSharpGL/Utilities/Save2Picture.cs
@@ -35,7 +35,6 @@ namespace CSharpGL
     /// </summary>
     public static class Save2PictureHelper
     {
-        static Random random = new Random();
         /// <summary>
         /// 把OpenGL渲染的内容保存到图片文件。
         /// </summary>
@@ -46,50 +45,16 @@ namespace CSharpGL
         /// <param name="filename"></param>
         public static void Save2Picture(int x, int y, int width, int height, string filename)
         {
-            var pdata = new UnmanagedArray<Pixel>(width * height);
-            GL.ReadPixels(x, y, width, height, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, pdata.Header);
-            //{
-            //    var bitmap = new Bitmap(width, height);
-            //    int index = 0;
-            //    for (int j = height - 1; j >= 0; j--)
-            //    {
-            //        for (int i = 0; i < width; i++)
-            //        {
-            //            Pixel v = pdata[index++];
-            //            Color c = v.ToColor();
-            //            bitmap.SetPixel(i, j, c);
-            //        }
-            //    }
+            System.Drawing.Imaging.PixelFormat format = System.Drawing.Imaging.PixelFormat.Format32bppArgb;
+            System.Drawing.Imaging.ImageLockMode lockMode = System.Drawing.Imaging.ImageLockMode.WriteOnly;
+            var bitmap = new Bitmap(width, height, format);
+            Rectangle bitmapRect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            System.Drawing.Imaging.BitmapData bmpData = bitmap.LockBits(bitmapRect, lockMode, format);
+            GL.ReadPixels(x, y, width, height, GL.GL_BGRA, GL.GL_UNSIGNED_BYTE, bmpData.Scan0);
+            bitmap.UnlockBits(bmpData);
+            bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
 
-            //    bitmap.Save(filename);
-            //}
-            {
-                System.Drawing.Imaging.PixelFormat format = System.Drawing.Imaging.PixelFormat.Format32bppArgb;
-                System.Drawing.Imaging.ImageLockMode lockMode = System.Drawing.Imaging.ImageLockMode.WriteOnly;
-                var bitmap = new Bitmap(width, height, format);
-                Rectangle bitmapRect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
-                System.Drawing.Imaging.BitmapData bmpData = bitmap.LockBits(bitmapRect, lockMode, format);
-                int length = Math.Abs(bmpData.Stride) * bitmap.Height;
-                byte[] bitmapBytes = new byte[length];
-                int index = 0;
-                for (int j = height - 1; j >= 0; j--)
-                {
-                    for (int i = 0; i < width; i++)
-                    {
-                        Pixel v = pdata[index++];
-                        bitmapBytes[j * bmpData.Stride + i * 4 + 0] = v.b;
-                        bitmapBytes[j * bmpData.Stride + i * 4 + 1] = v.g;
-                        bitmapBytes[j * bmpData.Stride + i * 4 + 2] = v.r;
-                        bitmapBytes[j * bmpData.Stride + i * 4 + 3] = v.a;
-                    }
-                }
-
-                System.Runtime.InteropServices.Marshal.Copy(bitmapBytes, 0, bmpData.Scan0, length);
-
-                bitmap.UnlockBits(bmpData);
-
-                bitmap.Save(filename);
-            }
+            bitmap.Save(filename);
         }
     }
 }
