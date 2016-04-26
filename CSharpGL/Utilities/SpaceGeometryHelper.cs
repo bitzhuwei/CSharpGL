71b0ab7c77b96659a11899e84641e0fe4e40415e
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-26 21:47:35 +0800

    InTheSameLine

--- CSharpGL/Utilities/SpaceGeometryHelper.cs
+++ CSharpGL/Utilities/SpaceGeometryHelper.cs
@@ -19,23 +19,21 @@ namespace CSharpGL
         /// <returns></returns>
         public static bool InTheSameLine(this vec3 walker, vec3 start, vec3 end, out float percent, float allowableError = 0.001f)
         {
-            percent = 0;
             vec3 v1 = walker - start;
             vec3 v2 = end - start;
-            float x = v1.x / v2.x;
-            float y = v1.y / v2.y;
-            float z = v1.z / v2.z;
-            float average = x / 3 + y / 3 + z / 3;
-            if (Math.Abs(x - average) <= allowableError
-                && Math.Abs(y - average) <= allowableError
-                && Math.Abs(z - average) <= allowableError)
+            var values = new float[] { v1.x / v2.x, v1.y / v2.y, v1.z / v2.z, };
+            float average, variance;
+            values.AverageVariance(out average, out variance);
+            if (variance <= allowableError)
             {
                 percent = average;
                 return true;
             }
-
-
-            return false;
+            else
+            {
+                percent = 0;
+                return false;
+            }
         }
     }
 }
--- CSharpGL/Utilities/StatisticsHelper.cs
+++ CSharpGL/Utilities/StatisticsHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpGL.Utilities
+namespace CSharpGL
 {
     public static class StatisticsHelper
     {
