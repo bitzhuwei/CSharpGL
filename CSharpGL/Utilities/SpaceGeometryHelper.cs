ad5a6e9353566a426194f6d040701c9d7a6e8ec5
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-26 23:29:00 +0800

    InTheSameLine

--- CSharpGL/Utilities/SpaceGeometryHelper.cs
+++ CSharpGL/Utilities/SpaceGeometryHelper.cs
@@ -17,13 +17,29 @@ namespace CSharpGL
         /// <param name="end"></param>
         /// <param name="percent">如果三点共线，<paramref name="percent"/>代表(<paramref name="walker"/> - <paramref name="start"/>)/(<paramref name="end"/> - <paramref name="start"/>)</param>
         /// <returns></returns>
-        public static bool InTheSameLine(this vec3 walker, vec3 start, vec3 end, out float percent, float allowableError = 0.001f)
+        public static bool InTheSameLine(this vec3 walker, vec3 start, vec3 end, out float percent, float allowableError = 0.01f)
         {
             vec3 v1 = walker - start;
             vec3 v2 = end - start;
-            var values = new float[] { v1.x / v2.x, v1.y / v2.y, v1.z / v2.z, };
+            var values = new List<float>();
+            for (int i = 0; i < 3; i++)
+            {
+                if (v2[i] == 0.0f)
+                {
+                    if (v1[i] != 0.0f)
+                    {
+                        percent = 0;
+                        return false;
+                    }
+                }
+                else
+                {
+                    values.Add(v1[i] / v2[i]);
+                }
+            }
+
             float average, variance;
-            values.AverageVariance(out average, out variance);
+            values.ToArray().AverageVariance(out average, out variance);
             if (variance <= allowableError)
             {
                 percent = average;
