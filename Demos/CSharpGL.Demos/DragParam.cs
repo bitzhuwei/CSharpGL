de9489800b20b471ca2d22f3475c3abf8653b8ad
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-27 01:42:12 +0800

    DragParam

---
+++ Demos/CSharpGL.Demos/DragParam.cs
@@ -0,0 +1,36 @@
+﻿using GLM;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL.Demos
+{
+
+    class DragParam
+    {
+
+        public PickedGeometry pickedGeometry;
+        public mat4 projectionMatrix;
+        public mat4 viewMatrix;
+        public Point lastMousePositionOnScreen;
+        public vec4 viewport;
+
+        public DragParam(PickedGeometry pickedGeometry, mat4 projectionMatrix, mat4 viewMatrix, Point lastMousePositionOnScreen)
+        {
+            this.pickedGeometry = pickedGeometry;
+            this.projectionMatrix = projectionMatrix;
+            this.viewMatrix = viewMatrix;
+            this.lastMousePositionOnScreen = lastMousePositionOnScreen;
+            var viewport = new int[4]; GL.GetInteger(GetTarget.Viewport, viewport);
+            this.viewport = new vec4(viewport[0], viewport[1], viewport[2], viewport[3]);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Last Mouse Position On Screen: [{0}]", this.lastMousePositionOnScreen);
+        }
+    }
+}
