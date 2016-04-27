ca108a54038b6ddb605d9ac947f225085c7e767b
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-27 19:18:01 +0800

    HighlightModernRenderer

--- CSharpGL/OpenGLObjects/ModernRendering/HighlightModernRenderer.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/HighlightModernRenderer.cs
@@ -21,10 +21,10 @@ namespace CSharpGL
         /// <param name="propertyNameMap">关联<see cref="VertexBufferPtr"/>和<see cref="ShaderCode"/>中的属性</param>
         /// <param name="positionNameInIBufferable">描述顶点位置信息的buffer的名字</param>
         ///<param name="switches"></param>
-        internal HighlightModernRenderer(IBufferable bufferable,
+        public HighlightModernRenderer(IBufferable bufferable,
             string positionNameInIBufferable,
             params GLSwitch[] switches)
-            : base(bufferable, PickingShaderHelper.GetPickingShaderCode(),
+            : base(bufferable, HighlightShaderHelper.GetHighlightShaderCode(),
                 new PropertyNameMap("in_Position", positionNameInIBufferable),
                 positionNameInIBufferable, switches)
         {
@@ -32,8 +32,13 @@ namespace CSharpGL
             //another way: uniform.SetValue(new vec4(1, 1, 1, 1));
             uniform.Value = new vec4(1, 1, 1, 1);
             this.UniformVariables.Add(uniform);
-            var glSwitch = new PolygonModeSwitch(PolygonModes.Lines);
-            this.SwitchList.Add(glSwitch);
+            this.UniformVariables.Add(this.pickingMVP);
+            var polygonModeSwitch = new PolygonModeSwitch(PolygonModes.Lines);
+            this.SwitchList.Add(polygonModeSwitch);
+            var lineWidthSwitch = new LineWidthSwitch(10.0f);
+            this.SwitchList.Add(lineWidthSwitch);
+            var pointSizeSwitch = new PointSizeSwitch(10.0f);
+            this.SwitchList.Add(pointSizeSwitch);
         }
 
     }
