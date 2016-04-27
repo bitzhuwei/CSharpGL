50e69a8c17d6f0919ad34572d4090feaba33a484
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-27 17:56:28 +0800

    HighlightedPickableRenderer

---
+++ Demos/CSharpGL.Demos/HighlightedPickableRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL.Demos
+{
+    class HighlightedPickableRenderer : RendererBase
+    {
+
+        public HighlightedPickableRenderer(HighlightModernRenderer highlighter, 
+            PickableModernRenderer pickableRenderer)
+        {
+            this.Highlighter = highlighter;
+            this.PickableRenderer = pickableRenderer;
+        }
+
+        protected override void DoInitialize()
+        {
+            this.Highlighter.Initialize();
+            this.PickableRenderer.Initialize();
+        }
+
+        protected override void DoRender(RenderEventArgs e)
+        {
+            this.Highlighter.Render(e);
+            this.PickableRenderer.Render(e);
+        }
+
+        protected override void DisposeUnmanagedResources()
+        {
+            this.Highlighter.Dispose();
+            this.PickableRenderer.Dispose();
+        }
+
+        public HighlightModernRenderer Highlighter { get; private set; }
+
+        public PickableModernRenderer PickableRenderer { get; private set; }
+
+    }
+}
