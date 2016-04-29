b7c2259bf28560f308812765d99c0f366c347bc3
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-29 19:52:39 +0800

    GLSwithListEditor

--- CSharpGL/UITypeEditors/GLSwithListEditor.cs
+++ CSharpGL/UITypeEditors/GLSwithListEditor.cs
@@ -15,13 +15,13 @@ namespace CSharpGL
 
         public override UITypeEditorEditStyle GetEditStyle(System.ComponentModel.ITypeDescriptorContext context)
         {
-            //指定为模式窗体属性编辑器类型 
+            // 指定为模式窗体属性编辑器类型 
             return UITypeEditorEditStyle.Modal;
         }
 
         public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
         {
-            //打开属性编辑器修改数据 
+            // 打开属性编辑器修改数据 
             var frmGLSwitchListEditor = new FormGLSwitchListEditor(value as List<GLSwitch>);
             if (frmGLSwitchListEditor.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
