385bcc551a2f0dba4b051d72f84abbc9c40a1aa3
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-30 02:03:40 +0800

    Axis

--- CSharpGL/OpenGLObjects/ModernRendering/IBufferables/Axis.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/IBufferables/Axis.cs
@@ -14,6 +14,7 @@ namespace CSharpGL
     {
         private AxisModel model;
 
+        public Axis(uint partCount = 4, float radius = 1.0f)
         {
             this.model = new AxisModel(partCount, radius);
         }
