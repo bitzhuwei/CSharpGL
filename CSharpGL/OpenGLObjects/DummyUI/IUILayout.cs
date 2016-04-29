9085de1654ffb92afa2b47735d96442882951839
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-29 18:18:49 +0800

    UIModernRenderer

---
+++ CSharpGL/OpenGLObjects/DummyUI/IUILayout.cs
@@ -0,0 +1,56 @@
+﻿using GLM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 实现在OpenGL窗口中的UI布局
+    /// </summary>
+    public interface IUILayout
+    {
+        IUILayoutParam Param { get; set; }
+
+    }
+
+    public struct IUILayoutParam
+    {
+
+        /// <summary>
+        /// the edges of the <see cref="GLCanvas"/> to which a UI’s rect is bound and determines how it is resized with its parent.
+        /// <para>something like AnchorStyles.Left | AnchorStyles.Bottom.</para>
+        /// </summary>
+        public System.Windows.Forms.AnchorStyles Anchor;
+
+        /// <summary>
+        /// Gets or sets the space between viewport and SimpleRect.
+        /// </summary>
+        public System.Windows.Forms.Padding Margin;
+
+        /// <summary>
+        /// Stores width when <see cref="OpenGLUIRect.Anchor"/>.Left &amp; <see cref="OpenGLUIRect.Anchor"/>.Right is <see cref="OpenGLUIRect.Anchor"/>.None.
+        /// <para> and height when <see cref="OpenGLUIRect.Anchor"/>.Top &amp; <see cref="OpenGLUIRect.Anchor"/>.Bottom is <see cref="OpenGLUIRect.Anchor"/>.None.</para>
+        /// </summary>
+        public System.Drawing.Size Size;
+
+        public int zNear;
+
+        public int zFar;
+
+        public IUILayoutParam(AnchorStyles anchorStyle, Padding padding, System.Drawing.Size size,
+            int zNear = -1000, int zFar = 1000)
+        {
+            // TODO: Complete member initialization
+            this.Anchor = anchorStyle;
+            this.Margin = padding;
+            this.Size = size;
+            this.zNear = zNear;
+            this.zFar = zFar;
+        }
+
+    }
+}
---
+++ CSharpGL/OpenGLObjects/DummyUI/UIModernRenderer.cs
@@ -0,0 +1,50 @@
+﻿using GLM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 支持"拾取"的渲染器
+    /// </summary>
+    public partial class UIModernRenderer : RendererBase, IUILayout
+    {
+        private ModernRenderer modernRenderer;
+        public IUILayoutParam Param { get; set; }
+
+        /// <summary>
+        /// 支持"拾取"的渲染器
+        /// </summary>
+        /// <param name="bufferable">一种渲染方式</param>
+        /// <param name="shaderCodes">各种类型的shader代码</param>
+        /// <param name="propertyNameMap">关联<see cref="PropertyBufferPtr"/>和<see cref="shaderCode"/>中的属性</param>
+        /// <param name="positionNameInIBufferable">描述顶点位置信息的buffer的名字</param>
+        ///<param name="switches"></param>
+        public UIModernRenderer(IUILayoutParam param,
+            ModernRenderer modernRenderer)
+        {
+            this.Param = param;
+            this.modernRenderer = modernRenderer;
+        }
+
+
+        protected override void DoInitialize()
+        {
+            this.modernRenderer.Initialize();
+        }
+
+        protected override void DoRender(RenderEventArgs e)
+        {
+            this.modernRenderer.Render(e);
+        }
+
+        protected override void DisposeUnmanagedResources()
+        {
+            this.modernRenderer.Dispose();
+        }
+    }
+}
