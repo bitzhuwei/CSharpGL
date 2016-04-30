de20d729b8ee44851bd179e852b71d6550e5e84e
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-30 14:19:37 +0800

    comment

--- CSharpGL/OpenGLObjects/UILayout/UIModernRenderer.cs
+++ CSharpGL/OpenGLObjects/UILayout/UIModernRenderer.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace CSharpGL
 {
     /// <summary>
-    /// 支持"拾取"的渲染器
+    /// 支持UI布局的渲染器
     /// </summary>
     public partial class UIModernRenderer : RendererBase, IUILayout
     {
@@ -21,13 +21,14 @@ namespace CSharpGL
         }
 
         /// <summary>
-        /// 支持"拾取"的渲染器
+        /// 支持UI布局的渲染器
         /// </summary>
-        /// <param name="bufferable">一种渲染方式</param>
-        /// <param name="shaderCodes">各种类型的shader代码</param>
-        /// <param name="propertyNameMap">关联<see cref="PropertyBufferPtr"/>和<see cref="shaderCode"/>中的属性</param>
-        /// <param name="positionNameInIBufferable">描述顶点位置信息的buffer的名字</param>
-        ///<param name="switches"></param>
+        /// <param name="modernRenderer">要渲染的对象</param>
+        /// <param name="Anchor">绑定到窗口的哪些边？</param>
+        /// <param name="Margin">到绑定边的距离</param>
+        /// <param name="Size">UI大小</param>
+        /// <param name="zNear"></param>
+        /// <param name="zFar"></param>
         public UIModernRenderer(
             ModernRenderer modernRenderer,
             System.Windows.Forms.AnchorStyles Anchor,
