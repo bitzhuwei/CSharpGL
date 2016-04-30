5f7d881a0539432f3158c3e79cb620a6f803123f
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-30 17:13:18 +0800

    picking a point, line, triangle, quad, or polygon from specified VBO?

---
+++ CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/PickingPrimitiveType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// picking a point, line, triangle, quad, or polygon from specified VBO?
+    /// </summary>
+    public enum PickingPrimitiveType
+    {
+        /// <summary>
+        /// picking a point from specified VBO
+        /// </summary>
+        Point,
+        /// <summary>
+        /// picking a line from specified VBO
+        /// </summary>
+        Line,
+        /// <summary>
+        /// picking a triangle from specified VBO
+        /// </summary>
+        Triangle,
+        /// <summary>
+        /// picking a quad from specified VBO
+        /// </summary>
+        Quad,
+        /// <summary>
+        /// picking a polygon from specified VBO
+        /// </summary>
+        Polygon,
+    }
+}
