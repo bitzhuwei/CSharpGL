43573cf6aa28f73c13ba485aac8327b922faa83a
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-01 08:30:49 +0800

    ZeroIndexLineInPolygonSearcher

--- CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexLineInPolygonSearcher.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexLineInPolygonSearcher.cs
@@ -7,9 +7,14 @@ namespace CSharpGL
 {
     class ZeroIndexLineInPolygonSearcher : ZeroIndexLineSearcher
     {
-        internal override uint[] Search(RenderEventArgs e, int x, int y, int canvasWidth, int canvasHeight, uint lastVertexId, CSharpGL.ZeroIndexModernRenderer zeroIndexModernRenderer)
+        internal override uint[] Search(RenderEventArgs e,
+            int x, int y, int canvasWidth, int canvasHeight,
+            uint lastVertexId, CSharpGL.ZeroIndexModernRenderer zeroIndexModernRenderer)
         {
-            throw new NotImplementedException();
+            if (0 < lastVertexId)
+            { return new uint[] { lastVertexId - 1, lastVertexId, }; }
+            else
+            { return new uint[] { (uint)(zeroIndexModernRenderer.Length - 1), lastVertexId, }; }
         }
     }
 }
