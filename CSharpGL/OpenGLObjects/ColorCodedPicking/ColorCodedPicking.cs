da2bf5200e336f9a5b049f430b48a22aeb05c617
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-01 23:39:45 +0800

    using (var codedColor = new UnmanagedArray<byte>(4))

--- CSharpGL/OpenGLObjects/ColorCodedPicking/ColorCodedPicking.cs
+++ CSharpGL/OpenGLObjects/ColorCodedPicking/ColorCodedPicking.cs
@@ -101,7 +101,7 @@ namespace CSharpGL
             return result;
         }
 
-        private static void Render4Picking(RenderEventArgs arg, IColorCodedPicking[] pickableElements)
+        public static void Render4Picking(RenderEventArgs arg, IColorCodedPicking[] pickableElements)
         {
             // 暂存clear color
             var originalClearColor = new float[4];
@@ -158,29 +158,31 @@ namespace CSharpGL
             uint stageVertexId = uint.MaxValue;
             // get coded color.
             //byte[] codedColor = new byte[4];
-            UnmanagedArray<byte> codedColor = new UnmanagedArray<byte>(4);
-            GL.ReadPixels(x, canvasHeight - y - 1, 1, 1, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, codedColor.Header);
-            if (!
-                // This is when (x, y) is on background and no primitive is picked.
-                (codedColor[0] == byte.MaxValue && codedColor[1] == byte.MaxValue
-                && codedColor[2] == byte.MaxValue && codedColor[3] == byte.MaxValue))
+            using (var codedColor = new UnmanagedArray<byte>(4))
             {
-                /* // This is how is vertexID coded into color in vertex shader.
-                 * 	int objectID = gl_VertexID;
-                    codedColor = vec4(
-                        float(objectID & 0xFF), 
-                        float((objectID >> 8) & 0xFF), 
-                        float((objectID >> 16) & 0xFF), 
-                        float((objectID >> 24) & 0xFF));
-                 */
-                // get vertexID from coded color.
-                // the vertexID is the last vertex that constructs the primitive.
-                // see http://www.cnblogs.com/bitzhuwei/p/modern-opengl-picking-primitive-in-VBO-2.html
-                uint shiftedR = (uint)codedColor[0];
-                uint shiftedG = (uint)codedColor[1] << 8;
-                uint shiftedB = (uint)codedColor[2] << 16;
-                uint shiftedA = (uint)codedColor[3] << 24;
-                stageVertexId = shiftedR + shiftedG + shiftedB + shiftedA;
+                GL.ReadPixels(x, canvasHeight - y - 1, 1, 1, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, codedColor.Header);
+                if (!
+                    // This is when (x, y) is on background and no primitive is picked.
+                    (codedColor[0] == byte.MaxValue && codedColor[1] == byte.MaxValue
+                    && codedColor[2] == byte.MaxValue && codedColor[3] == byte.MaxValue))
+                {
+                    /* // This is how is vertexID coded into color in vertex shader.
+                     * 	int objectID = gl_VertexID;
+                        codedColor = vec4(
+                            float(objectID & 0xFF), 
+                            float((objectID >> 8) & 0xFF), 
+                            float((objectID >> 16) & 0xFF), 
+                            float((objectID >> 24) & 0xFF));
+                     */
+                    // get vertexID from coded color.
+                    // the vertexID is the last vertex that constructs the primitive.
+                    // see http://www.cnblogs.com/bitzhuwei/p/modern-opengl-picking-primitive-in-VBO-2.html
+                    uint shiftedR = (uint)codedColor[0];
+                    uint shiftedG = (uint)codedColor[1] << 8;
+                    uint shiftedB = (uint)codedColor[2] << 16;
+                    uint shiftedA = (uint)codedColor[3] << 24;
+                    stageVertexId = shiftedR + shiftedG + shiftedB + shiftedA;
+                }
             }
 
             return stageVertexId;
