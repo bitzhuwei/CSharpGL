44805106cce0358ee89726d54d8a4b8542b6adf3
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-01 08:37:23 +0800

    ZeroIndexLineInQuadSearcher

--- CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexLineInQuadSearcher.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexLineInQuadSearcher.cs
@@ -7,9 +7,14 @@ namespace CSharpGL
 {
     class ZeroIndexLineInQuadSearcher : ZeroIndexLineSearcher
     {
-        internal override uint[] Search(RenderEventArgs e, int x, int y, int canvasWidth, int canvasHeight, uint lastVertexId, CSharpGL.ZeroIndexModernRenderer zeroIndexModernRenderer)
+        internal override uint[] Search(RenderEventArgs e,
+            int x, int y, int canvasWidth, int canvasHeight,
+            uint lastVertexId, CSharpGL.ZeroIndexModernRenderer zeroIndexModernRenderer)
         {
-            throw new NotImplementedException();
+            if (lastVertexId % 4 == 0)
+            { return new uint[] { lastVertexId + 4 - 1, lastVertexId, }; }
+            else
+            { return new uint[] { lastVertexId - 1, lastVertexId, }; }
         }
     }
 }
