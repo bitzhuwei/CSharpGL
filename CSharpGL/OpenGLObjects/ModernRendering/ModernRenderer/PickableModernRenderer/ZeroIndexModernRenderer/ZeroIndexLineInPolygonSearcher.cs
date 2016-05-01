60f9f06efda72d95228148a82fe3e52f2ac735f6
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-01 09:27:28 +0800

    ZeroIndexLineInPolygonSearcher

--- CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexLineInPolygonSearcher.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexLineInPolygonSearcher.cs
@@ -11,10 +11,22 @@ namespace CSharpGL
             int x, int y, int canvasWidth, int canvasHeight,
             uint lastVertexId, CSharpGL.ZeroIndexModernRenderer zeroIndexModernRenderer)
         {
-            if (0 < lastVertexId)
-            { return new uint[] { lastVertexId - 1, lastVertexId, }; }
+            var zeroIndexBufferPtr = zeroIndexModernRenderer.GetIndexBufferPtr() as ZeroIndexBufferPtr;
+            ZeroIndexBufferPtr indexBufferPtr = null;
+            using (var buffer = new ZeroIndexBuffer(DrawMode.LineLoop,
+                zeroIndexBufferPtr.FirstVertex, zeroIndexBufferPtr.VertexCount))
+            {
+                indexBufferPtr = buffer.GetBufferPtr() as ZeroIndexBufferPtr;
+            }
+            zeroIndexModernRenderer.Render4Picking(e, indexBufferPtr);
+            uint id = zeroIndexModernRenderer.ReadPixel(x, y, canvasHeight);
+
+            indexBufferPtr.Dispose();
+
+            if (id == zeroIndexBufferPtr.FirstVertex)
+            { return new uint[] { (uint)(zeroIndexBufferPtr.FirstVertex + zeroIndexBufferPtr.VertexCount - 1), id, }; }
             else
-            { return new uint[] { (uint)(zeroIndexModernRenderer.Length - 1), lastVertexId, }; }
+            { return new uint[] { id - 1, id, }; }
         }
     }
 }
