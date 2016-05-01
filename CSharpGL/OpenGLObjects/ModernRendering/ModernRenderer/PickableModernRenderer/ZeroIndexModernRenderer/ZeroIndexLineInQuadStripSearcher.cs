987d29833c80de394db33cd5dbb1a0037ab48136
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-01 10:47:28 +0800

    ZeroIndexLineInQuadStripSearcher

--- CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexLineInQuadStripSearcher.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexLineInQuadStripSearcher.cs
@@ -11,7 +11,31 @@ namespace CSharpGL
             int x, int y, int canvasWidth, int canvasHeight,
             uint lastVertexId, ZeroIndexModernRenderer modernRenderer)
         {
-            throw new NotImplementedException();
+            OneIndexBufferPtr indexBufferPtr = null;
+            using (var buffer = new OneIndexBuffer<uint>(DrawMode.Lines, BufferUsage.StaticDraw))
+            {
+                buffer.Alloc(6);
+                buffer[0] = lastVertexId - 0; buffer[1] = lastVertexId - 2;
+                buffer[2] = lastVertexId - 2; buffer[3] = lastVertexId - 3;
+                buffer[4] = lastVertexId - 3; buffer[5] = lastVertexId - 1;
+                buffer[6] = lastVertexId - 1; buffer[7] = lastVertexId - 0;
+                indexBufferPtr = buffer.GetBufferPtr() as OneIndexBufferPtr;
+            }
+
+            modernRenderer.Render4Picking(e, indexBufferPtr);
+            uint id = modernRenderer.ReadPixel(x, y, canvasHeight);
+
+            indexBufferPtr.Dispose();
+            if (id + 2 == lastVertexId)
+            { return new uint[] { lastVertexId - 0, lastVertexId - 2, }; }
+            else if (id + 3 == lastVertexId)
+            { return new uint[] { lastVertexId - 2, lastVertexId - 3 }; }
+            else if (id + 1 == lastVertexId)
+            { return new uint[] { lastVertexId - 3, lastVertexId - 1, }; }
+            else if (id + 0 == lastVertexId)
+            { return new uint[] { lastVertexId - 1, lastVertexId - 0, }; }
+            else
+            { throw new Exception("This should not happen!"); }
         }
     }
 }
