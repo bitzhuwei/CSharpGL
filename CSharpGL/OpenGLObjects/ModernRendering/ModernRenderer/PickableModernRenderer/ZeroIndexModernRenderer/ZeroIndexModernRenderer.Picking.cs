77bcdc9a9523fe31bd6b5e9b594864cb31361c54
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-02 17:03:41 +0800

    GetLineSearcher

--- CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexModernRenderer.Picking.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/ModernRenderer/PickableModernRenderer/ZeroIndexModernRenderer/ZeroIndexModernRenderer.Picking.cs
@@ -9,31 +9,6 @@ namespace CSharpGL
 {
     public partial class ZeroIndexModernRenderer : PickableModernRenderer
     {
-        static Dictionary<DrawMode, ZeroIndexLineSearcher> lineSearchDict;
-
-        static ZeroIndexLineSearcher GetLineSearcher(DrawMode mode)
-        {
-            if (lineSearchDict == null)
-            {
-                var dict = new Dictionary<DrawMode, ZeroIndexLineSearcher>();
-                dict.Add(DrawMode.Triangles, new ZeroIndexLineInTriangleSearcher());
-                dict.Add(DrawMode.TrianglesAdjacency, new ZeroIndexLineInTrianglesAdjacencySearcher());
-                dict.Add(DrawMode.TriangleStrip, new ZeroIndexLineInTriangleStripSearcher());
-                dict.Add(DrawMode.TriangleStripAdjacency, new ZeroIndexLineInTriangleStripAdjacencySearcher());
-                dict.Add(DrawMode.TriangleFan, new ZeroIndexLineInTriangleFanSearcher());
-                dict.Add(DrawMode.Quads, new ZeroIndexLineInQuadSearcher());
-                dict.Add(DrawMode.QuadStrip, new ZeroIndexLineInQuadStripSearcher());
-                dict.Add(DrawMode.Polygon, new ZeroIndexLineInPolygonSearcher());
-
-                lineSearchDict = dict;
-            }
-
-            ZeroIndexLineSearcher result = null;
-            if (lineSearchDict.TryGetValue(mode, out result))
-            { return result; }
-            else
-            { return null; }
-        }
 
         public override PickedGeometry Pick(
             RenderEventArgs arg,
