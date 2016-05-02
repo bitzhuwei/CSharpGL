45c5c30560fe5360f71e31d7a337f28c99198b8d
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-02 14:38:53 +0800

    11 points is the best test case

--- CSharpGL/OpenGLObjects/ModernRendering/IBufferables/BigDipperModel.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/IBufferables/BigDipperModel.cs
@@ -17,7 +17,7 @@ namespace CSharpGL
 
         static BigDipperModel()
         {
-            positions = new vec3[8];
+            positions = new vec3[11];
             positions[0] = new vec3(0, 2, 0);
             positions[1] = new vec3(1, 2, 0);
             positions[2] = new vec3(2, 1.5f, 0);
@@ -26,9 +26,12 @@ namespace CSharpGL
             positions[5] = new vec3(4.5f, 0, 0);
             positions[6] = new vec3(5, 1, 0);
             positions[7] = new vec3(5.9f, 4.1f, 0);
+            positions[8] = new vec3(6.1f, 4.2f, 0);
+            positions[9] = new vec3(6.2f, 3.9f, 0);
+            positions[10] = new vec3(6.4f, 3.9f, 0);
             positions = positions.Move2Center();
 
-            colors = new vec3[8];
+            colors = new vec3[11];
             colors[0] = new vec3(1, 0, 0);
             colors[1] = new vec3(1, 0.5f, 0);
             colors[2] = new vec3(1, 1, 0);
@@ -37,6 +40,9 @@ namespace CSharpGL
             colors[5] = new vec3(0, 0, 1);
             colors[6] = new vec3(0.5f, 0, 1);
             colors[7] = new vec3(1.0f, 215.0f / 255.0f, 0.0f);
+            colors[8] = new vec3(1.0f * 0.9f, 215.0f / 255.0f * 0.9f, 0.0f);
+            colors[9] = new vec3(1.0f * 0.8f, 215.0f / 255.0f * 0.8f, 0.0f);
+            colors[10] = new vec3(1.0f * 0.7f, 215.0f / 255.0f * 0.7f, 0.0f);
 
         }
     }
