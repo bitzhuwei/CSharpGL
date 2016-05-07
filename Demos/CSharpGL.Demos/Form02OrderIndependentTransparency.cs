88fbd832d280723bff1339d8037cc04788cc904a
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-07 12:05:30 +0800

    Form02OrderIndependentTransparency

--- Demos/CSharpGL.Demos/Form02OrderIndependentTransparency.cs
+++ Demos/CSharpGL.Demos/Form02OrderIndependentTransparency.cs
@@ -18,10 +18,57 @@ namespace CSharpGL.Demos
         private OrderIndependentTransparencyRenderer OITRenderer;
         private FormProperyGrid formPropertyGrid;
 
+
         public Form02OrderIndependentTransparency()
         {
             InitializeComponent();
-            
+
+            this.glCanvas1.OpenGLDraw += glCanvas1_OpenGLDraw;
+            this.glCanvas1.MouseDown += glCanvas1_MouseDown;
+            this.glCanvas1.MouseMove += glCanvas1_MouseMove;
+            this.glCanvas1.MouseUp += glCanvas1_MouseUp;
+            this.glCanvas1.MouseWheel += glCanvas1_MouseWheel;
+
+            // 天蓝色背景
+            GL.ClearColor(0x87 / 255.0f, 0xce / 255.0f, 0xeb / 255.0f, 0xff / 255.0f);
+        }
+
+        private void glCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            GL.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+
+            RenderEventArgs arg = new RenderEventArgs(RenderModes.Render, this.glCanvas1.ClientRectangle, this.camera);
+            IRenderable renderer = this.OITRenderer;
+            if (renderer != null)
+            { renderer.Render(arg); }
+
+            // Cross cursor shows where the mouse is.
+            GL.DrawText(this.lastMousePosition.X - offset.X,
+                this.glCanvas1.Height - (this.lastMousePosition.Y + offset.Y) - 1,
+                Color.Red, "Courier New", crossCursorSize, "+");
+        }
+
+
+        private const float crossCursorSize = 40.0f;
+
+        private static readonly Point offset = new Point(13, 13);
+
+
+        void glCanvas1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            ICamera camera = this.camera;
+            if (camera != null)
+            {
+                camera.MouseWheel(e.Delta);
+            }
+        }
+
+        private void glCanvas1_Resize(object sender, EventArgs e)
+        {
+            if (camera != null)
+            {
+                camera.Resize(this.glCanvas1.Width, this.glCanvas1.Height);
+            }
         }
 
     }
