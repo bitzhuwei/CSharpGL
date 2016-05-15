87d33ef5d7e02db7aa66f6865b3164c2b93bbf1b
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-15 19:23:31 +0800

    Press 'o' to select image. Press 'c' to select display mode.

--- Demos/CSharpGL.Demos/Form06ImageProcessing/Form06ImageProcessing.Designer.cs
+++ Demos/CSharpGL.Demos/Form06ImageProcessing/Form06ImageProcessing.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.glCanvas1 = new CSharpGL.Windows.GLCanvas();
             this.openTextureDlg = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,12 +39,12 @@
             this.glCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.glCanvas1.Location = new System.Drawing.Point(13, 13);
+            this.glCanvas1.Location = new System.Drawing.Point(13, 33);
             this.glCanvas1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.glCanvas1.Name = "glCanvas1";
             this.glCanvas1.OpenGLVersion = CSharpGL.Windows.GLVersion.OpenGL2_1;
             this.glCanvas1.RenderTrigger = CSharpGL.Windows.RenderTriggers.TimerBased;
-            this.glCanvas1.Size = new System.Drawing.Size(757, 522);
+            this.glCanvas1.Size = new System.Drawing.Size(757, 502);
             this.glCanvas1.TabIndex = 0;
             this.glCanvas1.TimerTriggerInterval = 20;
             this.glCanvas1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.glCanvas1_KeyPress);
@@ -52,17 +53,29 @@
             // 
             this.openTextureDlg.Filter = "*.bmp|*.bmp|*.jpg|*.jpg|*.png|*.png";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(609, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press \'o\' to select image. Press \'c\' to select display mode.";
+            // 
             // Form06ImageProcessing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(783, 548);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.glCanvas1);
             this.Name = "Form06ImageProcessing";
             this.Text = "Form06ImageProcessing";
             this.Load += new System.EventHandler(this.Form02OrderIndependentTransparency_Load);
             ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +83,6 @@
 
         private Windows.GLCanvas glCanvas1;
         private System.Windows.Forms.OpenFileDialog openTextureDlg;
+        private System.Windows.Forms.Label label1;
     }
 }
