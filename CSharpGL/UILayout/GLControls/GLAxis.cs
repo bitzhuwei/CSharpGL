e5938c6fef5978f5efb09d4aac6671b901ff2d19
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-24 21:03:03 +0800

    opengl UI for Axis

--- CSharpGL/UILayout/GLControls/GLAxis.cs
+++ CSharpGL/UILayout/GLControls/GLAxis.cs
@@ -7,15 +7,26 @@ using System.Threading.Tasks;
 
 namespace CSharpGL
 {
+    /// <summary>
+    /// opengl UI for Axis
+    /// </summary>
     public class GLAxis : UIRenderer
     {
-        //private PickableRenderer renderer;
 
+        /// <summary>
+        /// opengl UI for Axis
+        /// </summary>
+        /// <param name="anchor"></param>
+        /// <param name="margin"></param>
+        /// <param name="size"></param>
+        /// <param name="zNear"></param>
+        /// <param name="zFar"></param>
         public GLAxis(
             System.Windows.Forms.AnchorStyles anchor, System.Windows.Forms.Padding margin,
             System.Drawing.Size size, int zNear, int zFar)
             : base(null, anchor, margin, size, zNear, zFar)
         {
+            this.Name = "GLAxis";
             var shaderCodes = new ShaderCode[2];
             shaderCodes[0] = new ShaderCode(ManifestResourceLoader.LoadTextFile(
 @"UILayout.GLControls.GLAxis.vert"), ShaderType.VertexShader);
@@ -25,7 +36,6 @@ namespace CSharpGL
             map.Add("in_Position", "position");
             map.Add("in_Color", "color");
             PickableRenderer renderer = (new Axis()).GetRenderer(shaderCodes, map, "position");
-            renderer.Name = "GLAxis";
           
             this.Renderer = renderer;
         }
