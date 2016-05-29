ba35dd1c5fd813a9be674967036db8793cf8953e
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-29 15:01:12 +0800

    RaycastModel

--- Demos/CSharpGL.Demos/10RaycastVolumeRender/RaycastVolumeRender.cs
+++ Demos/CSharpGL.Demos/10RaycastVolumeRender/RaycastVolumeRender.cs
@@ -29,6 +29,7 @@ namespace CSharpGL.Demos
 
         protected override void DoInitialize()
         {
+
             throw new NotImplementedException();
         }
 
@@ -40,6 +41,100 @@ namespace CSharpGL.Demos
         protected override void DisposeUnmanagedResources()
         {
             throw new NotImplementedException();
+        }
+
+        class RaycastModel : IBufferable
+        {
+            public const string strPosition = "position";
+            Dictionary<string, PropertyBufferPtr> propertyBufferPtrDict = new Dictionary<string, PropertyBufferPtr>();
+            // draw the six faces of the boundbox by drawwing triangles
+            // draw it contra-clockwise
+            // front: 1 5 7 3
+            // back:  0 2 6 4
+            // left： 0 1 3 2
+            // right: 7 5 4 6    
+            // up:    2 3 7 6
+            // down:  1 0 4 5
+            static readonly float[] vertices = 
+            {
+				0.0f, 0.0f, 0.0f,
+				0.0f, 0.0f, 1.0f,
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 1.0f,
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 1.0f,
+				1.0f, 1.0f, 0.0f,
+				1.0f, 1.0f, 1.0f,
+            };
+            static readonly uint[] indices = 
+            {
+				1,5,7,
+				7,3,1,
+				0,2,6,
+				6,4,0,
+				0,1,3,
+				3,2,0,
+				7,5,4,
+				4,6,7,
+				2,3,7,
+				7,6,2,
+				1,0,4,
+				4,5,1,
+            };
+
+            public PropertyBufferPtr GetProperty(string bufferName, string varNameInShader)
+            {
+                if (bufferName == strPosition)
+                {
+                    if (!propertyBufferPtrDict.ContainsKey(bufferName))
+                    {
+                        using (var buffer = new PropertyBuffer<float>(varNameInShader, 3, OpenGL.GL_FLOAT, BufferUsage.StaticDraw))
+                        {
+                            buffer.Alloc(vertices.Length);
+                            unsafe
+                            {
+                                var array = (float*)buffer.FirstElement();
+                                for (int i = 0; i < vertices.Length; i++)
+                                {
+                                    array[i] = vertices[i];
+                                }
+                            }
+                            propertyBufferPtrDict.Add(bufferName, buffer.GetBufferPtr() as PropertyBufferPtr);
+                        }
+                    }
+                    return propertyBufferPtrDict[bufferName];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            public IndexBufferPtr GetIndex()
+            {
+                if (indexBufferPtr == null)
+                {
+                    using (var buffer = new OneIndexBuffer<uint>(DrawMode.Triangles, BufferUsage.StaticDraw))
+                    {
+                        buffer.Alloc(indices.Length);
+                        unsafe
+                        {
+                            var array = (uint*)buffer.FirstElement();
+                            for (int i = 0; i < indices.Length; i++)
+                            {
+                                array[i] = indices[i];
+                            }
+                        }
+
+                        indexBufferPtr = buffer.GetBufferPtr() as IndexBufferPtr;
+                    }
+                }
+
+                return indexBufferPtr;
+            }
+
+            IndexBufferPtr indexBufferPtr = null;
+
         }
     }
 }
