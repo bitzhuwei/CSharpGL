1cf10f6dcbe43817637d04ce37b88a91ebf1cdf0
祝威 <bitzhuwei.cnblogs@qq.com>
2016-05-30 01:09:11 +0800

    RaycastVolumeRender

--- Demos/CSharpGL.Demos/10RaycastVolumeRender/RaycastVolumeRender.cs
+++ Demos/CSharpGL.Demos/10RaycastVolumeRender/RaycastVolumeRender.cs
@@ -16,14 +16,15 @@ namespace CSharpGL.Demos
     {
         private Renderer backfaceRenderer;
         private Renderer raycastRenderer;
-        private uint[] tff1DTexObj = new uint[1];
-        private uint[] face2DTexObj = new uint[1];
+        private uint[] transferFunc1DTexObj = new uint[1];
+        private uint[] backface2DTexObj = new uint[1];
         private uint[] vol3DTexObj = new uint[1];
         private uint[] frameBuffer = new uint[1];
 
         private DepthTestSwitch depthTest;
 
         private static readonly IBufferable model = new RaycastModel();
+        private float g_stepSize = 0.001f;
 
         protected override void DoInitialize()
         {
@@ -38,6 +39,24 @@ namespace CSharpGL.Demos
             initFrameBuffer(viewport[2], viewport[3]);
 
             this.depthTest = new DepthTestSwitch();
+
+            RaycastingSetupUniforms();
+        }
+
+        private void RaycastingSetupUniforms()
+        {
+            // setting uniforms such as
+            // ScreenSize 
+            // StepSize
+            // TransferFunc
+            // ExitPoints i.e. the backface, the backface hold the ExitPoints of ray casting
+            // VolumeTex the texture that hold the volume data i.e. head256.raw
+            int[] viewport = OpenGL.GetViewport();
+            this.raycastRenderer.SetUniform("ScreenSize", new vec2(viewport[2], viewport[3]));
+            this.raycastRenderer.SetUniform("StepSize", g_stepSize);
+            this.raycastRenderer.SetUniform("TransferFunc", new samplerValue(BindTextureTarget.Texture1D, transferFunc1DTexObj[0], OpenGL.GL_TEXTURE0));
+            this.raycastRenderer.SetUniform("ExitPoints", new samplerValue(BindTextureTarget.Texture2D, backface2DTexObj[0], OpenGL.GL_TEXTURE1));
+            this.raycastRenderer.SetUniform("VolumeTex", new samplerValue(BindTextureTarget.Texture3D, vol3DTexObj[0], OpenGL.GL_TEXTURE2));
         }
 
         private void initFrameBuffer(int texWidth, int texHeight)
@@ -51,7 +70,7 @@ namespace CSharpGL.Demos
             // attach the texture and the depth buffer to the framebuffer
             OpenGL.GetDelegateFor<OpenGL.glGenFramebuffersEXT>()(1, frameBuffer);
             OpenGL.GetDelegateFor<OpenGL.glBindFramebufferEXT>()(OpenGL.GL_FRAMEBUFFER_EXT, frameBuffer[0]);
-            OpenGL.GetDelegateFor<OpenGL.glFramebufferTexture2DEXT>()(OpenGL.GL_FRAMEBUFFER_EXT, OpenGL.GL_COLOR_ATTACHMENT0_EXT, OpenGL.GL_TEXTURE_2D, face2DTexObj[0], 0);
+            OpenGL.GetDelegateFor<OpenGL.glFramebufferTexture2DEXT>()(OpenGL.GL_FRAMEBUFFER_EXT, OpenGL.GL_COLOR_ATTACHMENT0_EXT, OpenGL.GL_TEXTURE_2D, backface2DTexObj[0], 0);
             OpenGL.GetDelegateFor<OpenGL.glFramebufferRenderbufferEXT>()(OpenGL.GL_FRAMEBUFFER_EXT, OpenGL.GL_DEPTH_ATTACHMENT_EXT, OpenGL.GL_RENDERBUFFER, depthBuffer[0]);
             checkFramebufferStatus();
             //OpenGL.Enable(GL_DEPTH_TEST); 
@@ -105,8 +124,8 @@ namespace CSharpGL.Demos
 
         private void initFace2DTex(int width, int height)
         {
-            OpenGL.GenTextures(1, face2DTexObj);
-            OpenGL.BindTexture(OpenGL.GL_TEXTURE_2D, face2DTexObj[0]);
+            OpenGL.GenTextures(1, backface2DTexObj);
+            OpenGL.BindTexture(OpenGL.GL_TEXTURE_2D, backface2DTexObj[0]);
             OpenGL.TexParameteri(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_WRAP_S, (int)OpenGL.GL_REPEAT);
             OpenGL.TexParameteri(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_WRAP_T, (int)OpenGL.GL_REPEAT);
             OpenGL.TexParameteri(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MIN_FILTER, (int)OpenGL.GL_NEAREST);
@@ -123,8 +142,8 @@ namespace CSharpGL.Demos
             {
                 tff = br.ReadBytes((int)fs.Length);
             }
-            OpenGL.GenTextures(1, tff1DTexObj);
-            OpenGL.BindTexture(OpenGL.GL_TEXTURE_1D, tff1DTexObj[0]);
+            OpenGL.GenTextures(1, transferFunc1DTexObj);
+            OpenGL.BindTexture(OpenGL.GL_TEXTURE_1D, transferFunc1DTexObj[0]);
             OpenGL.TexParameteri(OpenGL.GL_TEXTURE_1D, OpenGL.GL_TEXTURE_WRAP_S, (int)OpenGL.GL_REPEAT);
             OpenGL.TexParameteri(OpenGL.GL_TEXTURE_1D, OpenGL.GL_TEXTURE_MIN_FILTER, (int)OpenGL.GL_NEAREST);
             OpenGL.TexParameteri(OpenGL.GL_TEXTURE_1D, OpenGL.GL_TEXTURE_MAG_FILTER, (int)OpenGL.GL_NEAREST);
@@ -142,6 +161,7 @@ namespace CSharpGL.Demos
             map.Add("color", "color");
             this.raycastRenderer = new Renderer(model, shaderCodes, map);
             this.raycastRenderer.Initialize();
+            this.backfaceRenderer.SwitchList.Add(new CullFaceSwitch(CullFaceMode.Back, true));
         }
 
         private void InitBackfaceRenderer()
@@ -154,19 +174,51 @@ namespace CSharpGL.Demos
             map.Add("color", "color");
             this.backfaceRenderer = new Renderer(model, shaderCodes, map);
             this.backfaceRenderer.Initialize();
+            this.backfaceRenderer.SwitchList.Add(new CullFaceSwitch(CullFaceMode.Front, true));
         }
 
         protected override void DoRender(RenderEventArgs arg)
         {
-            throw new NotImplementedException();
+            this.depthTest.On();
+            // render to texture
+            OpenGL.GetDelegateFor<OpenGL.glBindFramebufferEXT>()(OpenGL.GL_DRAW_FRAMEBUFFER, frameBuffer[0]);
+            this.backfaceRenderer.Render(arg);
+            OpenGL.GetDelegateFor<OpenGL.glBindFramebufferEXT>()(OpenGL.GL_DRAW_FRAMEBUFFER, 0);
+
+            this.raycastRenderer.Render(arg);
+            // need or need not to resume the state of only one active texture unit?
+            // glActiveTexture(GL_TEXTURE1);
+            // glBindTexture(GL_TEXTURE_2D, 0);
+            // glDisable(GL_TEXTURE_2D);
+            // glActiveTexture(GL_TEXTURE2);
+            // glBindTexture(GL_TEXTURE_3D, 0);    
+            // glDisable(GL_TEXTURE_3D);
+            // glActiveTexture(GL_TEXTURE0);
+            // glBindTexture(GL_TEXTURE_1D, 0);    
+            // glDisable(GL_TEXTURE_1D);
+            // glActiveTexture(GL_TEXTURE0);
+
+            // // for test the first pass
+            // glBindFramebuffer(GL_READ_FRAMEBUFFER, g_frameBuffer);
+            // checkFramebufferStatus();
+            // glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+            // glViewport(0, 0, g_winWidth, g_winHeight);
+            // glClearColor(0.0, 0.0, 1.0, 1.0);
+            // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            // GL_ERROR();
+            // glBlitFramebuffer(0, 0, g_winWidth, g_winHeight,0, 0,
+            // 		      g_winWidth, g_winHeight, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+            // glBindFramebuffer(GL_FRAMEBUFFER, 0);
+            // GL_ERROR();
+            this.depthTest.Off();
         }
 
         protected override void DisposeUnmanagedResources()
         {
             this.backfaceRenderer.Dispose();
             this.raycastRenderer.Dispose();
-            OpenGL.DeleteTextures(1, tff1DTexObj);
-            OpenGL.DeleteTextures(1, face2DTexObj);
+            OpenGL.DeleteTextures(1, transferFunc1DTexObj);
+            OpenGL.DeleteTextures(1, backface2DTexObj);
             OpenGL.DeleteTextures(1, vol3DTexObj);
             OpenGL.DeleteTextures(1, frameBuffer);
         }
