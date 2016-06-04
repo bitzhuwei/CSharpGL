0529acc09853e12f07881cb2b1b39ed865458f08
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-04 14:08:43 +0800

    BlendFactorHelper

---
+++ Infrastructure/CSharpGL.TestHelpers/BlendFactorHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL.TestHelpers
+{
+    public class BlendFactorHelper
+    {
+        private int currentSource;
+        private int currentDest;
+
+        private static BlendingSourceFactor[] sourceFactors;
+        private static BlendingDestinationFactor[] destFactors;
+
+        static BlendFactorHelper()
+        {
+            {
+                Array sources = Enum.GetValues(typeof(BlendingSourceFactor));
+                sourceFactors = new BlendingSourceFactor[sources.Length];
+                int i = 0;
+                foreach (var item in sources)
+                {
+                    sourceFactors[i++] = (BlendingSourceFactor)item;
+                }
+            }
+
+            {
+                Array dests = Enum.GetValues(typeof(BlendingDestinationFactor));
+                destFactors = new BlendingDestinationFactor[dests.Length];
+                int i = 0;
+                foreach (var item in dests)
+                {
+                    destFactors[i++] = (BlendingDestinationFactor)item;
+                }
+            }
+        }
+
+        public void GetNext(out BlendingSourceFactor source, out BlendingDestinationFactor dest)
+        {
+            source = sourceFactors[currentSource];
+            dest = destFactors[currentDest];
+            currentDest++;
+            if (currentDest >= destFactors.Length)
+            {
+                currentDest = 0;
+                currentSource++;
+                if (currentSource >= sourceFactors.Length)
+                {
+                    currentSource = 0;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("glBlendFunc({0}, {1});", sourceFactors[currentSource], destFactors[currentDest]);
+        }
+    }
+}
