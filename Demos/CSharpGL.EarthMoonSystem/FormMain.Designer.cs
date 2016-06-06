4afb16c04ac08b3841ad998f4ad4a0fb43470969
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-06 22:56:04 +0800

    start earth-moon-system

---
+++ Demos/CSharpGL.EarthMoonSystem/FormMain.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace CSharpGL.EarthMoonSystem
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glCanvas1 = new CSharpGL.GLCanvas();
+            ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glCanvas1
+            // 
+            this.glCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.glCanvas1.Location = new System.Drawing.Point(13, 13);
+            this.glCanvas1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glCanvas1.Name = "glCanvas1";
+            this.glCanvas1.OpenGLVersion = CSharpGL.GLVersion.OpenGL2_1;
+            this.glCanvas1.RenderTrigger = CSharpGL.RenderTriggers.TimerBased;
+            this.glCanvas1.Size = new System.Drawing.Size(838, 505);
+            this.glCanvas1.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 531);
+            this.Controls.Add(this.glCanvas1);
+            this.Name = "FormMain";
+            this.Text = "Earth-Moon System by bitzhuwei";
+            ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GLCanvas glCanvas1;
+    }
+}
+
