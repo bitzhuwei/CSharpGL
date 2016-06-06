d821b8e1f777010dc8a769c966a5f38cd45152d5
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-06 20:57:09 +0800

    Form13EarthMoonSystem

---
+++ Demos/CSharpGL.Demos/13EarthMoonSystem/Form13EarthMoonSystem.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CSharpGL.Demos
+{
+    partial class Form13EarthMoonSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openTextureDlg = new System.Windows.Forms.OpenFileDialog();
+            this.glCanvas1 = new CSharpGL.GLCanvas();
+            ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openTextureDlg
+            // 
+            this.openTextureDlg.Filter = "Image File(*.TTF;*.OTF)|*.TTF;*.OTF";
+            // 
+            // glCanvas1
+            // 
+            this.glCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.glCanvas1.Location = new System.Drawing.Point(13, 13);
+            this.glCanvas1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glCanvas1.Name = "glCanvas1";
+            this.glCanvas1.OpenGLVersion = CSharpGL.GLVersion.OpenGL2_1;
+            this.glCanvas1.RenderTrigger = CSharpGL.RenderTriggers.TimerBased;
+            this.glCanvas1.Size = new System.Drawing.Size(757, 522);
+            this.glCanvas1.TabIndex = 0;
+            this.glCanvas1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.glCanvas1_KeyPress);
+            // 
+            // Form13EarthMoonSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 548);
+            this.Controls.Add(this.glCanvas1);
+            this.Name = "Form13EarthMoonSystem";
+            this.Text = "Form09TextBoxRenderer";
+            this.Load += new System.EventHandler(this.Form02OrderIndependentTransparency_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GLCanvas glCanvas1;
+        private System.Windows.Forms.OpenFileDialog openTextureDlg;
+    }
+}
---
+++ Demos/CSharpGL.Demos/13EarthMoonSystem/Form13EarthMoonSystem.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpGL.Demos
+{
+    public partial class Form13EarthMoonSystem : Form
+    {
+
+        private Camera camera;
+        private SatelliteRotator rotator;
+
+
+        public Form13EarthMoonSystem()
+        {
+            InitializeComponent();
+
+            this.glCanvas1.OpenGLDraw += glCanvas1_OpenGLDraw;
+            this.glCanvas1.MouseDown += glCanvas1_MouseDown;
+            this.glCanvas1.MouseMove += glCanvas1_MouseMove;
+            this.glCanvas1.MouseUp += glCanvas1_MouseUp;
+            this.glCanvas1.MouseWheel += glCanvas1_MouseWheel;
+            this.glCanvas1.Resize += glCanvas1_Resize;
+
+            OpenGL.ClearColor(0x87 / 255.0f, 0xce / 255.0f, 0xeb / 255.0f, 0xff / 255.0f);
+        }
+
+        private void glCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            OpenGL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_STENCIL_BUFFER_BIT);
+
+            RenderEventArgs arg = new RenderEventArgs(RenderModes.Render, this.glCanvas1.ClientRectangle, this.camera);
+
+            UIRenderersDraw(arg);
+
+            // Cross cursor shows where the mouse is.
+            OpenGL.DrawText(this.lastMousePosition.X - offset.X,
+                this.glCanvas1.Height - (this.lastMousePosition.Y + offset.Y) - 1,
+                Color.Red, "Courier New", crossCursorSize, "o");
+        }
+
+        private const float crossCursorSize = 40.0f;
+
+        private Point offset = new Point(13, 11);
+        private void UIRenderersDraw(RenderEventArgs arg)
+        {
+            GLControl uiRoot = this.uiRoot;
+            if (uiRoot != null)
+            {
+                uiRoot.Layout();
+                mat4 projection, view, model;
+                {
+                    projection = glAxis.GetOrthoProjection();
+                    vec3 position = (this.camera.Position - this.camera.Target).normalize();
+                    view = glm.lookAt(position, new vec3(0, 0, 0), camera.UpVector);
+                    float length = Math.Max(glAxis.Size.Width, glAxis.Size.Height) / 2;
+                    model = glm.scale(mat4.identity(),
+                        new vec3(length, length, length));
+                    glAxis.Renderer.SetUniform("projectionMatrix", projection);
+                    glAxis.Renderer.SetUniform("viewMatrix", view);
+                    glAxis.Renderer.SetUniform("modelMatrix", model);
+
+                    glAxis.Render(arg);
+                }
+            }
+        }
+        void glCanvas1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            ICamera camera = this.camera;
+            if (camera != null)
+            {
+                camera.MouseWheel(e.Delta);
+            }
+        }
+
+        private void glCanvas1_Resize(object sender, EventArgs e)
+        {
+            if (camera != null)
+            {
+                camera.Resize(this.glCanvas1.Width, this.glCanvas1.Height);
+            }
+
+            this.uiRoot.Size = this.glCanvas1.Size;
+        }
+
+
+        private void glCanvas1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+    }
+
+}
