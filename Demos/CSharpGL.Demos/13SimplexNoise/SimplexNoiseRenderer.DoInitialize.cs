2fe10188b736901abbd7e5cbc4bdbe5afb55fc21
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-10 21:14:25 +0800

    DoInitialize

--- Demos/CSharpGL.Demos/13SimplexNoise/SimplexNoiseRenderer.DoInitialize.cs
+++ Demos/CSharpGL.Demos/13SimplexNoise/SimplexNoiseRenderer.DoInitialize.cs
@@ -7,12 +7,33 @@ using System.Threading.Tasks;
 
 namespace CSharpGL.Demos
 {
-    partial class SimplexNoiseRenderer 
+    partial class SimplexNoiseRenderer
     {
+        private uint[] permTextureID = new uint[1];
+        private uint[] simplexTextureID = new uint[1];
+        private uint[] gradTextureID = new uint[1];
+
+        float time;
+        DateTime lastTime;
 
         protected override void DoInitialize()
         {
             base.DoInitialize();
+
+            initPermTexture(this.permTextureID);
+            initSimplexTexture(this.simplexTextureID);
+            initGradTexture(this.gradTextureID);
+
+            lastTime = DateTime.Now;
+            time = 0;
+            this.SetUniform("time", time);
+            this.SetUniform("permTexture", new samplerValue(
+                BindTextureTarget.Texture2D, permTextureID[0], OpenGL.GL_TEXTURE0));
+            this.SetUniform("simplexTexture", new samplerValue(
+                BindTextureTarget.Texture1D, simplexTextureID[0], OpenGL.GL_TEXTURE1));
+            this.SetUniform("gradTexture", new samplerValue(
+                BindTextureTarget.Texture2D, gradTextureID[0], OpenGL.GL_TEXTURE2));
+
         }
 
     }
