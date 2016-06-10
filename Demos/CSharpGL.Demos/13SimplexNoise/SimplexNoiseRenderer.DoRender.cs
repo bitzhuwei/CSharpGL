1243f7380fb73e2723b43ce7b08959a2bd96be8f
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-10 23:06:45 +0800

    seems all done for simplex-noise demo

--- Demos/CSharpGL.Demos/13SimplexNoise/SimplexNoiseRenderer.DoRender.cs
+++ Demos/CSharpGL.Demos/13SimplexNoise/SimplexNoiseRenderer.DoRender.cs
@@ -17,7 +17,14 @@ namespace CSharpGL.Demos
             time = (float)now.Subtract(this.lastTime).TotalMilliseconds;
             this.lastTime = now;
             this.SetUniform("time", time);
-           
+
+            mat4 projection = arg.Camera.GetProjectionMat4();
+            mat4 view = arg.Camera.GetViewMat4();
+            mat4 model = mat4.identity();
+            this.SetUniform("projectionMatrix", projection);
+            this.SetUniform("viewMatrix", view);
+            this.SetUniform("modelMatrix", model); 
+
             base.DoRender(arg);
         }
     }
--- Demos/CSharpGL.Demos/13SimplexNoise/SimplexNoiseRenderer.Static.cs
+++ Demos/CSharpGL.Demos/13SimplexNoise/SimplexNoiseRenderer.Static.cs
@@ -16,8 +16,8 @@ namespace CSharpGL.Demos
             new ShaderCode(File.ReadAllText(@"13SimplexNoise\SimplexNoise.frag"), ShaderType.FragmentShader),
         };
         static readonly PropertyNameMap staticPropertyNameMap = new PropertyNameMap(
-            new string[] { "in_Position", "in_Color", },
-            new string[] { Sphere.strPosition, Sphere.strColor });
+            new string[] { "in_Position", },
+            new string[] { Sphere.strPosition, });
 
         static readonly int[] perm = new int[256] {151,160,137,91,90,15,
             131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
