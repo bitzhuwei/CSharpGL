454cbce907d5667477460b40ef0311c65aa980a0
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-10 23:59:03 +0800

    control property

--- Demos/CSharpGL.Demos/13SimplexNoise/SimplexNoiseRenderer.DoRender.cs
+++ Demos/CSharpGL.Demos/13SimplexNoise/SimplexNoiseRenderer.DoRender.cs
@@ -9,14 +9,28 @@ namespace CSharpGL.Demos
 {
     partial class SimplexNoiseRenderer 
     {
+        private float timeSpeed = 1.0f;
+        public float TimeSpeed
+        {
+            get { return timeSpeed; }
+            set { timeSpeed = value; }
+        }
+
+        private float partsFactor = 5.0f;
+        public float PartsFactor
+        {
+            get { return partsFactor; }
+            set { partsFactor = value; }
+        }
 
         protected override void DoRender(RenderEventArgs arg)
         {
             // setup uniforms
             var now = DateTime.Now;
-            time = (float)now.Subtract(this.lastTime).TotalMilliseconds;
+            float time = (float)now.Subtract(this.lastTime).TotalMilliseconds;
             this.lastTime = now;
-            this.SetUniform("time", time);
+            this.SetUniform("time", time * timeSpeed);
+            this.SetUniform("partsFactor", partsFactor);
 
             mat4 projection = arg.Camera.GetProjectionMat4();
             mat4 view = arg.Camera.GetViewMat4();
