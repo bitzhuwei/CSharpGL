e2f08cdba5294eaa81f80a445e761bb62b583ee4
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-11 22:23:20 +0800

    radians/degrees

---
+++ CSharpGL/CSSL/BuildInFunctions/AngleAndTrigonometry.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL.CSSL
+{
+    /// <summary>
+    /// 所有CSSL都共有的内容。
+    /// </summary>
+    public abstract partial class CSShaderCode
+    {
+        /// <summary>
+        /// Converts degrees to radians and returns the
+        /// result, i.e., result = π/180 * degrees.
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        public static float radians(double degrees) { return 0.0f; }
+        /// <summary>
+        /// Converts degrees to radians and returns the
+        /// result, i.e., result = π/180 * degrees.
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        public static vec2 radians(vec2 degrees) { return null; }
+        /// <summary>
+        /// Converts degrees to radians and returns the
+        /// result, i.e., result = π/180 * degrees.
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        public static vec3 radians(vec3 degrees) { return null; }
+        /// <summary>
+        /// Converts degrees to radians and returns the
+        /// result, i.e., result = π/180 * degrees.
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        public static vec4 radians(vec4 degrees) { return null; }
+
+        /// <summary>
+        /// Converts radians to degrees and returns the
+        /// result, i.e., result = 180 / π * radians.
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
+        public static float degrees(double radians) { return 0.0f; }
+        /// <summary>
+        /// Converts radians to degrees and returns the
+        /// result, i.e., result = 180 / π * radians.
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
+        public static vec2 degrees(vec2 radians) { return null; }
+        /// <summary>
+        /// Converts radians to degrees and returns the
+        /// result, i.e., result = 180 / π * radians.
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
+        public static vec3 degrees(vec3 radians) { return null; }
+        /// <summary>
+        /// Converts radians to degrees and returns the
+        /// result, i.e., result = 180 / π * radians.
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
+        public static vec4 degrees(vec4 radians) { return null; }
+
+
+    }
+}
