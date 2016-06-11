28574a884dd6659a76a27d87c39e5eb686aad81d
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-11 23:18:41 +0800

    FragmentProcessingFunctions

--- CSharpGL/CSSL/BuildInFunctions/FragmentProcessingFunctions.cs
+++ CSharpGL/CSSL/BuildInFunctions/FragmentProcessingFunctions.cs
@@ -11,6 +11,89 @@ namespace CSharpGL.CSSL
     /// </summary>
     public abstract partial class FragmentCSShaderCode
     {
-        //TODO: add build in functions
+
+        /// <summary>
+        /// Returns the derivative in x for the input argument p
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static float dFdx(double p) { return 0.0f; }
+        /// <summary>
+        /// Returns the derivative in x for the input argument p
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec2 dFdx(vec2 p) { return null; }
+        /// <summary>
+        /// Returns the derivative in x for the input argument p
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec3 dFdx(vec3 p) { return null; }
+        /// <summary>
+        /// Returns the derivative in x for the input argument p
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec4 dFdx(vec4 p) { return null; }
+
+        /// <summary>
+        /// Returns the derivative in y for the input argument p
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static float dFdy(double p) { return 0.0f; }
+        /// <summary>
+        /// Returns the derivative in y for the input argument p
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec2 dFdy(vec2 p) { return null; }
+        /// <summary>
+        /// Returns the derivative in y for the input argument p
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec3 dFdy(vec3 p) { return null; }
+        /// <summary>
+        /// Returns the derivative in y for the input argument p
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec4 dFdy(vec4 p) { return null; }
+
+        /// <summary>
+        /// Returns the sum of the absolute derivative in x and y for
+        /// the input argument p, i.e.,
+        /// return = abs( dFdx( p ) ) + abs( dFdy( p ) );
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static float fwidth(double p) { return 0.0f; }
+        /// <summary>
+        /// Returns the sum of the absolute derivative in x and y for
+        /// the input argument p, i.e.,
+        /// return = abs( dFdx( p ) ) + abs( dFdy( p ) );
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec2 fwidth(vec2 p) { return null; }
+        /// <summary>
+        /// Returns the sum of the absolute derivative in x and y for
+        /// the input argument p, i.e.,
+        /// return = abs( dFdx( p ) ) + abs( dFdy( p ) );
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec3 fwidth(vec3 p) { return null; }
+        /// <summary>
+        /// Returns the sum of the absolute derivative in x and y for
+        /// the input argument p, i.e.,
+        /// return = abs( dFdx( p ) ) + abs( dFdy( p ) );
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static vec4 fwidth(vec4 p) { return null; }
+
     }
 }
