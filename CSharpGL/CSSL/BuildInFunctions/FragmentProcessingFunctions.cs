aea053bc425ceee93f87bfd336797d8ad74ee2a4
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-11 23:08:10 +0800

    add todo labels

--- CSharpGL/CSSL/BuildInFunctions/FragmentProcessingFunctions.cs
+++ CSharpGL/CSSL/BuildInFunctions/FragmentProcessingFunctions.cs
@@ -11,5 +11,6 @@ namespace CSharpGL.CSSL
     /// </summary>
     public abstract partial class FragmentCSShaderCode
     {
+        //TODO: add build in functions
     }
 }
--- CSharpGL/CSSL/BuildInFunctions/MatrixFunctions.cs
+++ CSharpGL/CSSL/BuildInFunctions/MatrixFunctions.cs
@@ -11,5 +11,6 @@ namespace CSharpGL.CSSL
     /// </summary>
     public abstract partial class CSShaderCode
     {
+        //TODO: add build in functions
     }
 }
