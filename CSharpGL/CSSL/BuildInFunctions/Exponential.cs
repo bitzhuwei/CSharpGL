302673b979c9d7fa26d5ed37736f26821651d370
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-11 22:48:34 +0800

    exp

--- CSharpGL/CSSL/BuildInFunctions/Exponential.cs
+++ CSharpGL/CSSL/BuildInFunctions/Exponential.cs
@@ -12,7 +12,7 @@ namespace CSharpGL.CSSL
     public abstract partial class CSShaderCode
     {
         /// <summary>
-        /// Returns x raised to the y power, i.e., xy. Results
+        /// Returns x raised to the y power, i.e., x^y. Results
         /// are undefined if x < 0. Results are undefined if
         /// x = 0 and y = 0.
         /// </summary>
@@ -21,7 +21,7 @@ namespace CSharpGL.CSSL
         /// <returns></returns>
         public static float pow(double x, double y) { return 0.0f; }
         /// <summary>
-        /// Returns x raised to the y power, i.e., xy. Results
+        /// Returns x raised to the y power, i.e., x^y. Results
         /// are undefined if x < 0. Results are undefined if
         /// x = 0 and y = 0.
         /// </summary>
@@ -30,7 +30,7 @@ namespace CSharpGL.CSSL
         /// <returns></returns>
         public static vec2 pow(vec2 x, vec2 y) { return null; }
         /// <summary>
-        /// Returns x raised to the y power, i.e., xy. Results
+        /// Returns x raised to the y power, i.e., x^y. Results
         /// are undefined if x < 0. Results are undefined if
         /// x = 0 and y = 0.
         /// </summary>
@@ -39,7 +39,7 @@ namespace CSharpGL.CSSL
         /// <returns></returns>
         public static vec3 pow(vec3 x, vec3 y) { return null; }
         /// <summary>
-        /// Returns x raised to the y power, i.e., xy. Results
+        /// Returns x raised to the y power, i.e., x^y. Results
         /// are undefined if x < 0. Results are undefined if
         /// x = 0 and y = 0.
         /// </summary>
@@ -48,6 +48,30 @@ namespace CSharpGL.CSSL
         /// <returns></returns>
         public static vec4 pow(vec4 x, vec4 y) { return null; }
 
+        /// <summary>
+        /// Returns the natural exponentiation of x, i.e., e^x.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static float exp(double x) { return 0.0f; }
+        /// <summary>
+        /// Returns the natural exponentiation of x, i.e., e^x.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static vec2 exp(vec2 x) { return null; }
+        /// <summary>
+        /// Returns the natural exponentiation of x, i.e., e^x.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static vec3 exp(vec3 x) { return null; }
+        /// <summary>
+        /// Returns the natural exponentiation of x, i.e., e^x.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static vec4 exp(vec4 x) { return null; }
 
     }
 }
