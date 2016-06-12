7e46b35827b425cafa6563f16021e17d4c1e2181
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-12 10:57:21 +0800

    fix

--- Demos/CSharpGL.Demos/08AnalyzedBillboard/Form08AnalyzedBillboard.Designer.cs
+++ Demos/CSharpGL.Demos/08AnalyzedBillboard/Form08AnalyzedBillboard.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.glCanvas1 = new CSharpGL.GLCanvas();
-            this.label1 = new System.Windows.Forms.Label();
             this.openTextureDlg = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).BeginInit();
             this.SuspendLayout();
@@ -39,23 +38,13 @@
             this.glCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.glCanvas1.Location = new System.Drawing.Point(13, 33);
+            this.glCanvas1.Location = new System.Drawing.Point(13, 13);
             this.glCanvas1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.glCanvas1.Name = "glCanvas1";
             this.glCanvas1.OpenGLVersion = CSharpGL.GLVersion.OpenGL2_1;
             this.glCanvas1.RenderTrigger = CSharpGL.RenderTriggers.TimerBased;
-            this.glCanvas1.Size = new System.Drawing.Size(757, 502);
+            this.glCanvas1.Size = new System.Drawing.Size(757, 522);
             this.glCanvas1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 12F);
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(269, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Press \'o\' to select image.";
             // 
             // openTextureDlg
             // 
@@ -66,21 +55,18 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(783, 548);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.glCanvas1);
             this.Name = "Form08AnalyzedBillboard";
             this.Text = "Form08AnalyzedBillboard";
             this.Load += new System.EventHandler(this.Form02OrderIndependentTransparency_Load);
             ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private GLCanvas glCanvas1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.OpenFileDialog openTextureDlg;
     }
 }
