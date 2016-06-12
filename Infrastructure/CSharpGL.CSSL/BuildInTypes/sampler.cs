d0818888ab0cf6984bfb0cc6df80d963a9310140
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-12 19:05:47 +0800

    texture access functions

--- Infrastructure/CSharpGL.CSSL/BuildInTypes/sampler.cs
+++ Infrastructure/CSharpGL.CSSL/BuildInTypes/sampler.cs
@@ -9,21 +9,140 @@ namespace CSharpGL.CSSL
     /// 专用于CSSL。不可用于数学计算。
     /// <para>Specially designed for CSSL. Not for glm.</para>
     /// </summary>
-    public class sampler1D
-    {
-    }
+    public class sampler1D { }
     /// <summary>
     /// 专用于CSSL。不可用于数学计算。
     /// <para>Specially designed for CSSL. Not for glm.</para>
     /// </summary>
-    public class sampler2D
-    {
-    }
+    public class sampler2D { }
     /// <summary>
     /// 专用于CSSL。不可用于数学计算。
     /// <para>Specially designed for CSSL. Not for glm.</para>
     /// </summary>
-    public class sampler3D
-    {
-    }
+    public class sampler3D { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// <para>Specially designed for CSSL. Not for glm.</para>
+    /// </summary>
+    public class isampler1D { }
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// <para>Specially designed for CSSL. Not for glm.</para>
+    /// </summary>
+    public class isampler2D { }
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// <para>Specially designed for CSSL. Not for glm.</para>
+    /// </summary>
+    public class isampler3D { }
+
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// <para>Specially designed for CSSL. Not for glm.</para>
+    /// </summary>
+    public class usampler1D { }
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// <para>Specially designed for CSSL. Not for glm.</para>
+    /// </summary>
+    public class usampler2D { }
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// <para>Specially designed for CSSL. Not for glm.</para>
+    /// </summary>
+    public class usampler3D { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class sampler2DRect { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class isampler2DRect { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class usampler2DRect { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class samplerCube { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class isamplerCube { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class usamplerCube { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class sampler1DArray { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class sampler2DArray { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class isampler1DArray { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class usampler1DArray { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class isampler2DArray { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class usampler2DArray { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class samplerBuffer { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class isamplerBuffer { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class usamplerBuffer { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class sampler1DShadow { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class sampler2DShadow { }
+
+    /// <summary>
+    /// 专用于CSSL。不可用于数学计算。
+    /// </summary>
+    public class sampler2DRectShadow { }
+
+
 }
