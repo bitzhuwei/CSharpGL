316a4a99bdc07542ec68679f9541476e8c02d2b5
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-14 19:43:22 +0800

    clear

---
+++ CSharpGL/FontResource/FontResource.FromTTF.Glyph.cs
@@ -0,0 +1,81 @@
+﻿using SharpFont;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 字形贴图及其UV。
+    /// </summary>
+    public sealed partial class FontResource
+    {
+
+        private static unsafe Bitmap GetGlyphBitmap(Surface surface)
+        {
+            if (surface.Width > 0 && surface.Height > 0)
+            {
+                var bitmap = new Bitmap(surface.Width, surface.Height, PixelFormat.Format24bppRgb);
+                var bitmapData = bitmap.LockBits(new Rectangle(0, 0, surface.Width, surface.Height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+                for (int y = 0; y < surface.Height; y++)
+                {
+                    var dest = (byte*)bitmapData.Scan0 + y * bitmapData.Stride;
+                    var src = (byte*)surface.Bits + y * surface.Pitch;
+
+                    for (int x = 0; x < surface.Width; x++)
+                    {
+                        var b = *src++;
+                        *dest++ = b;
+                        *dest++ = b;
+                        *dest++ = b;
+                    }
+                }
+
+                bitmap.UnlockBits(bitmapData);
+                return bitmap;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static unsafe bool RenderGlyph(FontFace typeface, char c, int pixelSize, out Surface surface, out Glyph glyph)
+        {
+            bool result = false;
+
+            glyph = typeface.GetGlyph(c, pixelSize);
+            if (glyph != null)
+            {
+                surface = new Surface
+                {
+                    Bits = Marshal.AllocHGlobal(glyph.RenderWidth * glyph.RenderHeight),
+                    Width = glyph.RenderWidth,
+                    Height = glyph.RenderHeight,
+                    Pitch = glyph.RenderWidth
+                };
+
+                var stuff = (byte*)surface.Bits;
+                // todo: this is not needed?
+                for (int i = 0; i < surface.Width * surface.Height; i++)
+                    *stuff++ = 0;
+
+                glyph.RenderTo(surface);
+
+                result = true;
+            }
+            else
+            {
+                surface = new Surface();
+            }
+
+            return result;
+        }
+
+    }
+}
