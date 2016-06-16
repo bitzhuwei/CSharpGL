6ae85cd01c4abd9e9afcc1341166fc929c05a811
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-16 20:51:06 +0800

    impl tansform component and renderer

--- CSharpGL/Scenes/SceneObjectRenderer.cs
+++ CSharpGL/Scenes/SceneObjectRenderer.cs
@@ -7,9 +7,73 @@ namespace CSharpGL
 {
     public class SceneObjectRenderer : Renderer
     {
-        public SceneObjectRenderer(IBufferable bufferable, ShaderCode[] shaderCodes,
+        const string projection = "projection";
+        const string view = "view";
+        const string model = "model";
+        //uint projectionLocation;
+        //uint viewLocation;
+        //uint modelLocation;
+
+        public SceneObject Owner { get; set; }
+
+        public SceneObjectRenderer(SceneObject owner,
+            IBufferable bufferable, ShaderCode[] shaderCodes,
             PropertyNameMap propertyNameMap, params GLSwitch[] switches)
             : base(bufferable, shaderCodes, propertyNameMap, switches)
-        { }
+        {
+            this.Owner = owner;
+        }
+
+        //protected override void DoInitialize()
+        //{
+        //    base.DoInitialize();
+
+        //    int location;
+        //    location = this.shaderProgram.GetUniformLocation(projection);
+        //    if (location < 0)
+        //    { throw new Exception(string.Format("No uniform found for the name [{0}]", projection)); }
+        //    else
+        //    { this.projectionLocation = (uint)location; }
+        //    location = this.shaderProgram.GetUniformLocation(view);
+        //    if (location < 0)
+        //    { throw new Exception(string.Format("No uniform found for the name [{0}]", view)); }
+        //    else
+        //    { this.viewLocation = (uint)location; }
+        //    location = this.shaderProgram.GetUniformLocation(model);
+        //    if (location < 0)
+        //    { throw new Exception(string.Format("No uniform found for the name [{0}]", model)); }
+        //    else
+        //    { this.modelLocation = (uint)location; }
+
+        //}
+
+        protected override void DoRender(RenderEventArgs arg)
+        {
+            SceneObject owner = this.Owner;
+            if (owner != null)
+            {
+                mat4 projectionMatrix = arg.Camera.GetProjectionMat4();
+                mat4 viewMatrix = arg.Camera.GetViewMat4();
+                mat4 modelMatrix;
+
+                SceneObject parent = owner.Parent;
+                if (parent != null)
+                {
+                    modelMatrix = owner.Transform.GetMatrix() * parent.Transform.GetMatrix();
+                }
+                else
+                {
+                    modelMatrix = owner.Transform.GetMatrix();
+                }
+
+                {
+                    this.SetUniform(projection, projectionMatrix);
+                    this.SetUniform(view, viewMatrix);
+                    this.SetUniform(model, modelMatrix);
+                }
+
+                base.DoRender(arg);
+            }
+        }
     }
 }
--- CSharpGL/Scenes/TransformComponent.cs
+++ CSharpGL/Scenes/TransformComponent.cs
@@ -7,9 +7,34 @@ namespace CSharpGL
     /// </summary>
     public partial class TransformComponent : Component
     {
+        public vec3 Position { get; set; }
+        public vec3 Rotation { get; set; }
+        public vec3 Scale { get; set; }
+
         public TransformComponent(SceneObject bindingObject = null)
             : base(bindingObject)
         {
+            this.Scale = new vec3(1, 1, 1);
+        }
+
+        static readonly vec3 xAxis = new vec3(1, 0, 0);
+        static readonly vec3 yAxis = new vec3(0, 1, 0);
+        static readonly vec3 zAxis = new vec3(0, 0, 1);
+        /// <summary>
+        /// Get model matrix.
+        /// </summary>
+        /// <returns></returns>
+        public mat4 GetMatrix()
+        {
+            mat4 matrix;
+
+            matrix = glm.rotate(this.Rotation.x, xAxis);
+            matrix = glm.rotate(this.Rotation.y, yAxis);
+            matrix = glm.rotate(this.Rotation.z, zAxis);
+            matrix = glm.scale(matrix, this.Scale);
+            matrix = glm.translate(matrix, this.Position);
+
+            return matrix;
         }
     }
 }
