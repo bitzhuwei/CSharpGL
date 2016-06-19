ac0f356e09d7c67631481037e80edf2d5e878f16
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-19 17:09:51 +0800

    SatelliteManipulater

--- CSharpGL/OpenGLObjects/Camera/SatelliteManipulater.cs
+++ CSharpGL/OpenGLObjects/Camera/SatelliteManipulater.cs
@@ -17,14 +17,156 @@ namespace CSharpGL
     public class SatelliteManipulater : CameraManipulater
     {
 
+        private ICamera camera;
+        private Control canvas;
+
+        private MouseEventHandler mouseDownEvent;
+        private MouseEventHandler mouseMoveEvent;
+        private MouseEventHandler mouseUpEvent;
+        private MouseEventHandler mouseWheelEvent;
+
+        private Point downPosition = new Point();
+        private Size bound = new Size();
+        private bool mouseDownFlag = false;
+        private vec3 up;
+        private vec3 back;
+        private vec3 right;
+
+        public float HorizontalRotationFactor { get; set; }
+
+        public float VerticalRotationFactor { get; set; }
+
+        public SatelliteManipulater()
+        {
+            this.HorizontalRotationFactor = 4;
+            this.VerticalRotationFactor = 4;
+            this.mouseDownEvent = new MouseEventHandler(this.canvas_MouseDown);
+            this.mouseMoveEvent = new MouseEventHandler(this.canvas_MouseMove);
+            this.mouseUpEvent = new MouseEventHandler(this.canvas_MouseUp);
+            this.mouseWheelEvent = new MouseEventHandler(this.canvas_MouseWheel);
+        }
+
         public override void Bind(ICamera camera, Control canvas)
         {
-            throw new NotImplementedException();
+            if (camera == null || canvas == null) { throw new ArgumentNullException(); }
+
+            this.camera = camera;
+            this.canvas = canvas;
+
+            canvas.MouseDown += this.mouseDownEvent;
+            canvas.MouseMove += this.mouseMoveEvent;
+            canvas.MouseUp += this.mouseUpEvent;
+            canvas.MouseWheel += this.mouseWheelEvent;
         }
 
         public override void Unbind()
         {
-            throw new NotImplementedException();
+            this.camera = null;
+            if (this.canvas != null && (!this.canvas.IsDisposed))
+            {
+                this.canvas.MouseDown -= this.mouseDownEvent;
+                this.canvas.MouseMove -= this.mouseMoveEvent;
+                this.canvas.MouseUp -= this.mouseUpEvent;
+                this.canvas.MouseWheel -= this.mouseWheelEvent;
+            }
         }
+
+
+        public override string ToString()
+        {
+            return string.Format("back:{0}|{3:0.00},up:{1}|{4:0.00},right:{2}|{5:0.00}",
+                back, up, right, back.Magnitude(), up.Magnitude(), right.Magnitude());
+        }
+
+
+        private void canvas_MouseWheel(object sender, MouseEventArgs e)
+        {
+            this.camera.MouseWheel(e.Delta);
+        }
+
+        public void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.mouseDownFlag = false;
+        }
+
+        public void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.mouseDownFlag)
+            {
+                IViewCamera camera = this.camera;
+                if (camera == null) { return; }
+
+                vec3 back = this.back;
+                vec3 right = this.right;
+                vec3 up = this.up;
+                Size bound = this.bound;
+                Point downPosition = this.downPosition;
+                {
+                    float deltaX = -this.HorizontalRotationFactor * (e.X - downPosition.X) / bound.Width;
+                    float cos = (float)Math.Cos(deltaX);
+                    float sin = (float)Math.Sin(deltaX);
+                    vec3 newBack = new vec3(
+                        back.x * cos + right.x * sin,
+                        back.y * cos + right.y * sin,
+                        back.z * cos + right.z * sin);
+                    back = newBack;
+                    right = up.cross(back);
+                    back = back.normalize();
+                    right = right.normalize();
+                }
+                {
+                    float deltaY = this.VerticalRotationFactor * (e.Y - downPosition.Y) / bound.Height;
+                    float cos = (float)Math.Cos(deltaY);
+                    float sin = (float)Math.Sin(deltaY);
+                    vec3 newBack = new vec3(
+                        back.x * cos + up.x * sin,
+                        back.y * cos + up.y * sin,
+                        back.z * cos + up.z * sin);
+                    back = newBack;
+                    up = back.cross(right);
+                    back = back.normalize();
+                    up = up.normalize();
+                }
+
+                camera.Position = camera.Target +
+                    back * (float)((camera.Position - camera.Target).Magnitude());
+                camera.UpVector = up;
+                this.back = back;
+                this.right = right;
+                this.up = up;
+                this.downPosition = e.Location;
+            }
+        }
+
+        public void SetBounds(int width, int height)
+        {
+            this.bound.Width = width;
+            this.bound.Height = height;
+        }
+
+        public void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.downPosition = e.Location;
+            var control = sender as Control;
+            this.SetBounds(control.Width, control.Height);
+            this.mouseDownFlag = true;
+            PrepareCamera();
+        }
+
+        private void PrepareCamera()
+        {
+            var camera = this.camera;
+            if (camera != null)
+            {
+                vec3 back = camera.Position - camera.Target;
+                vec3 right = camera.UpVector.cross(back);
+                vec3 up = back.cross(right);
+
+                this.back = back.normalize();
+                this.right = right.normalize();
+                this.up = up.normalize();
+            }
+        }
+
     }
 }
