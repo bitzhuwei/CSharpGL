b75f76551f28dde92058f8166bd509f4f1e0603c
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-19 21:55:58 +0800

    HexahedronGrid

---
+++ Demos/GridViewer/Models/HexahedronGrid.cs
@@ -0,0 +1,22 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewer.Models
+{
+    class HexahedronGrid : IBufferable
+    {
+        public PropertyBufferPtr GetProperty(string bufferName, string varNameInShader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IndexBufferPtr GetIndex()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
