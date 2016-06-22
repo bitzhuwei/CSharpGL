0bf4e27440fb633f123221aede280f579def429d
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-22 21:23:58 +0800

    PositionHelper.Move2Center() supports vec2

--- CSharpGL/Utilities/PositionHelper.cs
+++ CSharpGL/Utilities/PositionHelper.cs
@@ -9,6 +9,55 @@ namespace CSharpGL
     public static class PositionHelper
     {
         /// <summary>
+        /// Move positions where around (0, 0)
+        /// </summary>
+        /// <param name="positions"></param>
+        /// <returns></returns>
+        public static void Move2Center(this vec2[] positions)
+        {
+            if (positions.Length == 0) { return; }
+
+            vec2 min = positions[0], max = positions[0];
+            for (int i = 1; i < positions.Length; i++)
+            {
+                vec2 value = positions[i];
+                if (value.x < min.x) { min.x = value.x; }
+                if (value.y < min.y) { min.y = value.y; }
+                if (max.x < value.x) { max.x = value.x; }
+                if (max.y < value.y) { max.y = value.y; }
+            }
+            vec2 mid = max / 2 + min / 2;
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = positions[i] - mid;
+            }
+        }
+
+        /// <summary>
+        /// Move positions where around (0, 0, 0)
+        /// </summary>
+        /// <param name="positions"></param>
+        /// <returns></returns>
+        public static void Move2Center(this IList<vec2> positions)
+        {
+            if (positions.Count == 0) { return; }
+
+            vec2 min = positions[0], max = positions[0];
+            for (int i = 1; i < positions.Count; i++)
+            {
+                vec2 value = positions[i];
+                if (value.x < min.x) { min.x = value.x; }
+                if (value.y < min.y) { min.y = value.y; }
+                if (max.x < value.x) { max.x = value.x; }
+                if (max.y < value.y) { max.y = value.y; }
+            }
+            vec2 mid = max / 2 + min / 2;
+            for (int i = 0; i < positions.Count; i++)
+            {
+                positions[i] = positions[i] - mid;
+            }
+        }
+        /// <summary>
         /// Move positions where around (0, 0, 0)
         /// </summary>
         /// <param name="positions"></param>
