d316fb6555e75f02261cf5bd72ccd639e6ef3d33
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-23 00:01:42 +0800

    ColorCodedBarRenderer

---
+++ Demos/GridViewer/Renderers/ColorCodedBarRenderer.cs
@@ -0,0 +1,42 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewer
+{
+    class ColorCodedBarRenderer : RendererBase
+    {
+        public CodedColor[] CodedColors { get; private set; }
+
+        public Renderer RectRenderer { get; private set; }
+
+        public Renderer LineRenderer { get; private set; }
+
+        public Renderer[] ValueRenderers { get; private set; }
+
+        public ColorCodedBarRenderer(CodedColor[] codedColors)
+        {
+            this.CodedColors = codedColors;
+            this.ValueRenderers = new Renderer[codedColors.Length];
+        }
+
+        protected override void DoInitialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoRender(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DisposeUnmanagedResources()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
