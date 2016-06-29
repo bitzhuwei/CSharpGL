b6a602bae68943ddea7fb1541cb529c5ba3887cc
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-29 10:05:12 +0800

    clean

--- CSharpGL/OpenGLObjects/Manipulaters/CameraManipulaters/CameraManipulater.cs
+++ CSharpGL/OpenGLObjects/Manipulaters/CameraManipulaters/CameraManipulater.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace CSharpGL
 {
+    //TODO: post a blog about two ways of using CameraManipulater.
     /// <summary>
     /// Manipulate camera.
     /// </summary>
