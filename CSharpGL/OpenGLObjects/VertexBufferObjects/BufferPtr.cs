76a84d8d00cd4cd139f0a7f5c6682323368ccd16
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-04 14:52:36 +0800

    clean

--- CSharpGL/OpenGLObjects/VertexBufferObjects/BufferPtr.cs
+++ CSharpGL/OpenGLObjects/VertexBufferObjects/BufferPtr.cs
@@ -8,7 +8,7 @@ namespace CSharpGL
 {
     /// <summary>
     /// 将VBO上传到GPU后，就得到VBO的指针。CPU内存中的VBO数据就可以释放掉了。
-    /// VBO's pointer got from Buffer's Upload2GPU() method. It's totally OK to free memory of unmanaged array stored in this buffer object now.
+    /// VBO's pointer got from Buffer's Upload2GPU() method.
     /// </summary>
     public abstract class BufferPtr : IDisposable
     {
