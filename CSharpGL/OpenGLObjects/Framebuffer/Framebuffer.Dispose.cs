2b2415abe1ba82864b2a7b2bd9de0494f45a181b
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-04 12:52:29 +0800

    clean

---
+++ CSharpGL/OpenGLObjects/Framebuffer/Framebuffer.Dispose.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace CSharpGL
+{
+    //TODO: IDisposable
+    public partial class Framebuffer : IDisposable
+    {
+        
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Framebuffer()
+        {
+            this.Dispose(false);
+        }
+
+        private bool disposedValue = false;
+
+        private void Dispose(bool disposing)
+        {
+            if (this.disposedValue == false)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources.
+
+                }
+
+                // Dispose unmanaged resources.
+                glDeleteFramebuffers(1, this.framebufferId);
+            }
+
+            this.disposedValue = true;
+        }
+    }
+}
---
+++ CSharpGL/OpenGLObjects/Framebuffer/FramebufferTexture.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 专为FrameBuffer服务的Texture类型。
+    /// </summary>
+    public class FramebufferTexture
+    {
+        private uint[] m_texture_id = new uint[1];
+        public uint TextureId { get { return m_texture_id[0]; } }
+
+        public uint TextureTarget { get; private set; }
+
+        private uint m_internalfmt;
+        private uint m_fmt;
+        private bool m_mipmap;
+        private bool m_interpol;
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public void setFormat(uint internalfmt, int width, int height, uint format,
+            bool mipmap, bool interpol)
+        {
+            Width = width;
+            Height = height;
+            m_internalfmt = internalfmt;
+            m_fmt = format;
+            m_mipmap = mipmap;
+            m_interpol = interpol;
+            TextureTarget = OpenGL.GL_TEXTURE_2D;
+
+            OpenGL.GenTextures(1, m_texture_id);
+            OpenGL.BindTexture(TextureTarget, m_texture_id[0]);
+            OpenGL.TexParameterf(TextureTarget, OpenGL.GL_TEXTURE_WRAP_S, OpenGL.GL_CLAMP_TO_EDGE);
+            OpenGL.TexParameterf(TextureTarget, OpenGL.GL_TEXTURE_WRAP_T, OpenGL.GL_CLAMP_TO_EDGE);
+            if (mipmap)
+            {
+                OpenGL.TexParameterf(TextureTarget, OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_LINEAR);
+                OpenGL.TexParameterf(TextureTarget, OpenGL.GL_TEXTURE_MIN_FILTER, OpenGL.GL_LINEAR_MIPMAP_LINEAR);
+                OpenGL.TexParameterf(TextureTarget, OpenGL.GL_GENERATE_MIPMAP, OpenGL.GL_TRUE);
+            }
+            else
+            {
+                if (interpol)
+                {
+                    OpenGL.TexParameterf(TextureTarget, OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_LINEAR);
+                    OpenGL.TexParameterf(TextureTarget, OpenGL.GL_TEXTURE_MIN_FILTER, OpenGL.GL_LINEAR);
+                }
+                else
+                {
+                    OpenGL.TexParameterf(TextureTarget, OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_NEAREST);
+                    OpenGL.TexParameterf(TextureTarget, OpenGL.GL_TEXTURE_MIN_FILTER, OpenGL.GL_NEAREST);
+                }
+                OpenGL.TexParameteri(TextureTarget, OpenGL.GL_GENERATE_MIPMAP, (int)OpenGL.GL_FALSE);
+            }
+            OpenGL.TexImage2D(TextureTarget, 0, internalfmt, width, height, 0,
+                format, OpenGL.GL_UNSIGNED_BYTE, IntPtr.Zero);
+            OpenGL.BindTexture(TextureTarget, 0);
+        }
+
+        internal void resize(int width, int height)
+        {
+            OpenGL.DeleteTextures(1, m_texture_id);
+            setFormat(m_internalfmt, width, height, m_fmt, m_mipmap, m_interpol);
+        }
+
+    }
+}
