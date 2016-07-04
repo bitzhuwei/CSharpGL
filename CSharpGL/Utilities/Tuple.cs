c49d14889ca4f9d2715fab42f3921063da404317
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-04 12:34:18 +0800

    clean

--- CSharpGL/Utilities/Tuple.cs
+++ CSharpGL/Utilities/Tuple.cs
@@ -8,94 +8,54 @@ namespace CSharpGL
 
     public class Tuple<T1>
     {
-        private readonly T1 m_Item1;
+        //private readonly T1 m_Item1;
 
-        public T1 Item1
-        {
-            get
-            {
-                return this.m_Item1;
-            }
-        }
+        public T1 Item1 { get; private set; }
 
         public Tuple(T1 item1)
         {
-            this.m_Item1 = item1;
+            this.Item1 = item1;
         }
 
     }
 
-
     public class Tuple<T1, T2>
     {
-        private readonly T1 m_Item1;
+        //private readonly T1 m_Item1;
 
-        private readonly T2 m_Item2;
+        //private readonly T2 m_Item2;
 
-        public T1 Item1
-        {
-            get
-            {
-                return this.m_Item1;
-            }
-        }
+        public T1 Item1 { get; private set; }
 
-
-        public T2 Item2
-        {
-            get
-            {
-                return this.m_Item2;
-            }
-        }
+        public T2 Item2 { get; private set; }
 
         public Tuple(T1 item1, T2 item2)
         {
-            this.m_Item1 = item1;
-            this.m_Item2 = item2;
+            this.Item1 = item1;
+            this.Item2 = item2;
         }
 
     }
 
     public class Tuple<T1, T2, T3>
     {
-        private readonly T1 m_Item1;
+        //private readonly T1 m_Item1;
 
-        private readonly T2 m_Item2;
+        //private readonly T2 m_Item2;
 
-        private readonly T3 m_Item3;
+        //private readonly T3 m_Item3;
 
-        public T1 Item1
-        {
-            get
-            {
-                return this.m_Item1;
-            }
-        }
+        public T1 Item1 { get; private set; }
 
+        public T2 Item2 { get; private set; }
 
-        public T2 Item2
-        {
-            get
-            {
-                return this.m_Item2;
-            }
-        }
-
-
-        public T3 Item3
-        {
-            get
-            {
-                return this.m_Item3;
-            }
-        }
+        public T3 Item3 { get; private set; }
 
         public Tuple(T1 item1, T2 item2, T3 item3)
         {
-            this.m_Item1 = item1;
-            this.m_Item2 = item2;
-            this.m_Item3 = item3;
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
         }
 
     }
@@ -103,52 +63,28 @@ namespace CSharpGL
 
     public class Tuple<T1, T2, T3, T4>
     {
-        private readonly T1 m_Item1;
+        //private readonly T1 m_Item1;
 
-        private readonly T2 m_Item2;
+        //private readonly T2 m_Item2;
 
-        private readonly T3 m_Item3;
+        //private readonly T3 m_Item3;
 
-        private readonly T4 m_Item4;
+        //private readonly T4 m_Item4;
 
-        public T1 Item1
-        {
-            get
-            {
-                return this.m_Item1;
-            }
-        }
+        public T1 Item1 { get; private set; }
 
-        public T2 Item2
-        {
-            get
-            {
-                return this.m_Item2;
-            }
-        }
+        public T2 Item2 { get; private set; }
 
-        public T3 Item3
-        {
-            get
-            {
-                return this.m_Item3;
-            }
-        }
+        public T3 Item3 { get; private set; }
 
-        public T4 Item4
-        {
-            get
-            {
-                return this.m_Item4;
-            }
-        }
+        public T4 Item4 { get; private set; }
 
         public Tuple(T1 item1, T2 item2, T3 item3, T4 item4)
         {
-            this.m_Item1 = item1;
-            this.m_Item2 = item2;
-            this.m_Item3 = item3;
-            this.m_Item4 = item4;
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+            this.Item4 = item4;
         }
 
     }
@@ -156,149 +92,71 @@ namespace CSharpGL
 
     public class Tuple<T1, T2, T3, T4, T5>
     {
-        private readonly T1 m_Item1;
+        //private readonly T1 m_Item1;
 
-        private readonly T2 m_Item2;
+        //private readonly T2 m_Item2;
 
-        private readonly T3 m_Item3;
+        //private readonly T3 m_Item3;
 
-        private readonly T4 m_Item4;
+        //private readonly T4 m_Item4;
 
-        private readonly T5 m_Item5;
+        //private readonly T5 m_Item5;
 
-        public T1 Item1
-        {
-            get
-            {
-                return this.m_Item1;
-            }
-        }
+        public T1 Item1 { get; private set; }
 
-        public T2 Item2
-        {
-            get
-            {
-                return this.m_Item2;
-            }
-        }
+        public T2 Item2 { get; private set; }
 
-        public T3 Item3
-        {
-            get
-            {
-                return this.m_Item3;
-            }
-        }
+        public T3 Item3 { get; private set; }
 
-        public T4 Item4
-        {
-            get
-            {
-                return this.m_Item4;
-            }
-        }
+        public T4 Item4 { get; private set; }
 
-
-        public T5 Item5
-        {
-            get
-            {
-                return this.m_Item5;
-            }
-        }
+        public T5 Item5 { get; private set; }
 
         public Tuple(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5)
         {
-            this.m_Item1 = item1;
-            this.m_Item2 = item2;
-            this.m_Item3 = item3;
-            this.m_Item4 = item4;
-            this.m_Item5 = item5;
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+            this.Item4 = item4;
+            this.Item5 = item5;
         }
     }
 
     public class Tuple<T1, T2, T3, T4, T5, T6>
     {
-        private readonly T1 m_Item1;
+        //private readonly T1 m_Item1;
 
-        private readonly T2 m_Item2;
+        //private readonly T2 m_Item2;
 
-        private readonly T3 m_Item3;
+        //private readonly T3 m_Item3;
 
-        private readonly T4 m_Item4;
+        //private readonly T4 m_Item4;
 
-        private readonly T5 m_Item5;
+        //private readonly T5 m_Item5;
 
-        private readonly T6 m_Item6;
-
-
-        public T1 Item1
-        {
-
-            get
-            {
-                return this.m_Item1;
-            }
-        }
+        //private readonly T6 m_Item6;
 
 
-        public T2 Item2
-        {
+        public T1 Item1 { get; private set; }
 
-            get
-            {
-                return this.m_Item2;
-            }
-        }
+        public T2 Item2 { get; private set; }
 
+        public T3 Item3 { get; private set; }
 
-        public T3 Item3
-        {
+        public T4 Item4 { get; private set; }
 
-            get
-            {
-                return this.m_Item3;
-            }
-        }
+        public T5 Item5 { get; private set; }
 
-
-        public T4 Item4
-        {
-
-            get
-            {
-                return this.m_Item4;
-            }
-        }
-
-
-        public T5 Item5
-        {
-
-            get
-            {
-                return this.m_Item5;
-            }
-        }
-
-
-        public T6 Item6
-        {
-
-            get
-            {
-                return this.m_Item6;
-            }
-        }
+        public T6 Item6 { get; private set; }
 
         public Tuple(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6)
         {
-            this.m_Item1 = item1;
-            this.m_Item2 = item2;
-            this.m_Item3 = item3;
-            this.m_Item4 = item4;
-            this.m_Item5 = item5;
-            this.m_Item6 = item6;
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+            this.Item4 = item4;
+            this.Item5 = item5;
+            this.Item6 = item6;
         }
 
     }
@@ -306,99 +164,44 @@ namespace CSharpGL
 
     public class Tuple<T1, T2, T3, T4, T5, T6, T7>
     {
-        private readonly T1 m_Item1;
+        //private readonly T1 m_Item1;
 
-        private readonly T2 m_Item2;
+        //private readonly T2 m_Item2;
 
-        private readonly T3 m_Item3;
+        //private readonly T3 m_Item3;
 
-        private readonly T4 m_Item4;
+        //private readonly T4 m_Item4;
 
-        private readonly T5 m_Item5;
+        //private readonly T5 m_Item5;
 
-        private readonly T6 m_Item6;
+        //private readonly T6 m_Item6;
 
-        private readonly T7 m_Item7;
-
-
-        public T1 Item1
-        {
-
-            get
-            {
-                return this.m_Item1;
-            }
-        }
+        //private readonly T7 m_Item7;
 
 
-        public T2 Item2
-        {
+        public T1 Item1 { get; private set; }
 
-            get
-            {
-                return this.m_Item2;
-            }
-        }
+        public T2 Item2 { get; private set; }
 
+        public T3 Item3 { get; private set; }
 
-        public T3 Item3
-        {
+        public T4 Item4 { get; private set; }
 
-            get
-            {
-                return this.m_Item3;
-            }
-        }
+        public T5 Item5 { get; private set; }
 
+        public T6 Item6 { get; private set; }
 
-        public T4 Item4
-        {
-
-            get
-            {
-                return this.m_Item4;
-            }
-        }
-
-
-        public T5 Item5
-        {
-
-            get
-            {
-                return this.m_Item5;
-            }
-        }
-
-
-        public T6 Item6
-        {
-
-            get
-            {
-                return this.m_Item6;
-            }
-        }
-
-
-        public T7 Item7
-        {
-
-            get
-            {
-                return this.m_Item7;
-            }
-        }
+        public T7 Item7 { get; private set; }
 
         public Tuple(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7)
         {
-            this.m_Item1 = item1;
-            this.m_Item2 = item2;
-            this.m_Item3 = item3;
-            this.m_Item4 = item4;
-            this.m_Item5 = item5;
-            this.m_Item6 = item6;
-            this.m_Item7 = item7;
+            this.Item1 = item1;
+            this.Item2 = item2;
+            this.Item3 = item3;
+            this.Item4 = item4;
+            this.Item5 = item5;
+            this.Item6 = item6;
+            this.Item7 = item7;
         }
 
     }
