1a2ef4f0ee1474f4668e717906445ef8460d9cab
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-04 16:04:54 +0800

    clean

--- Demos/GridViewer/Models/CatesianGrid/CatesianGrid.Index.cs
+++ Demos/GridViewer/Models/CatesianGrid/CatesianGrid.Index.cs
@@ -54,4 +54,34 @@ namespace GridViewer
         }
 
     }
+
+    /// <summary>
+    /// 描述用OpenGL.GL_QUAD_STRIP渲染六面体时的三个面+一个PrimitiveRestart索引。
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct HalfHexahedronIndex
+    {
+        /// <summary>
+        /// 第0个顶点的索引值
+        /// </summary>
+        public uint dot0;
+        /// <summary>
+        /// 第1个顶点的索引值
+        /// </summary>
+        public uint dot1;
+        /// <summary>
+        /// 第2个顶点的索引值
+        /// </summary>
+        public uint dot2;
+        public uint dot3;
+        public uint dot4;
+        public uint dot5;
+        public uint dot6;
+        public uint dot7;
+
+        /// <summary>
+        /// 请始终给此变量赋值uint.MaxValue
+        /// </summary>
+        public uint restartIndex;
+    }
 }
