0147c0995b3d9d8316e66489868a55e33c63eddf
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-09 00:19:12 +0800

    use glm instead of glu.perspective and lookat

--- CSharpGL/WinformControls/GLCanvasHelper.cs
+++ CSharpGL/WinformControls/GLCanvasHelper.cs
@@ -20,7 +20,7 @@ namespace CSharpGL
         //{
         //    pyramidVAOElement.Render(new RenderEventArgs(RenderModes.Render, this.camera));
         //}
-
+        static readonly mat4 viewMatrix = glm.lookAt(new vec3(-2, 2, -2), new vec3(0, 0, 0), new vec3(0, 1, 0));
         public static void ResizeGL(double width, double height)
         {
             //  Set the projection matrix.
@@ -28,9 +28,10 @@ namespace CSharpGL
 
             //  Load the identity.
             OpenGL.LoadIdentity();
-
             ////  Create a perspective transformation.
             //OpenGL.gluPerspective(60.0f, width / height, 0.01, 100.0);
+            mat4 projectionMatrix = glm.perspective(glm.radians(60.0f), (float)(width / height), 0.01f, 100.0f);
+            OpenGL.MultMatrixf((projectionMatrix * viewMatrix).to_array());
 
             ////  Use the 'look at' helper function to position and aim the camera.
             //OpenGL.gluLookAt(-2, 2, -2, 0, 0, 0, 0, 1, 0);
