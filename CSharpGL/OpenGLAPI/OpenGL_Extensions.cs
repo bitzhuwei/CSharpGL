a2c2547fc085a2a794ae9e408c4c0cb882d0d737
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-08 23:13:29 +0800

    clean

--- CSharpGL/OpenGLAPI/OpenGL_Extensions.cs
+++ CSharpGL/OpenGLAPI/OpenGL_Extensions.cs
@@ -53,41 +53,149 @@ namespace CSharpGL
         public delegate void glResetMinmax(uint target);
 
         //  Constants
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_BYTE_3_3_2 = 0x8032;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_SHORT_4_4_4_4 = 0x8033;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_SHORT_5_5_5_1 = 0x8034;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_INT_8_8_8_8 = 0x8035;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_INT_10_10_10_2 = 0x8036;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_TEXTURE_BINDING_3D = 0x806A;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_PACK_SKIP_IMAGES = 0x806B;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_PACK_IMAGE_HEIGHT = 0x806C;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNPACK_SKIP_IMAGES = 0x806D;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNPACK_IMAGE_HEIGHT = 0x806E;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_TEXTURE_3D = 0x806F;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_PROXY_TEXTURE_3D = 0x8070;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_TEXTURE_DEPTH = 0x8071;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_TEXTURE_WRAP_R = 0x8072;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_MAX_3D_TEXTURE_SIZE = 0x8073;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_BYTE_2_3_3_REV = 0x8362;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_SHORT_5_6_5 = 0x8363;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_SHORT_5_6_5_REV = 0x8364;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_SHORT_4_4_4_4_REV = 0x8365;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_SHORT_1_5_5_5_REV = 0x8366;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_INT_8_8_8_8_REV = 0x8367;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_UNSIGNED_INT_2_10_10_10_REV = 0x8368;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_BGR = 0x80E0;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_BGRA = 0x80E1;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_MAX_ELEMENTS_VERTICES = 0x80E8;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_MAX_ELEMENTS_INDICES = 0x80E9;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_CLAMP_TO_EDGE = 0x812F;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_TEXTURE_MIN_LOD = 0x813A;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_TEXTURE_MAX_LOD = 0x813B;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_TEXTURE_BASE_LEVEL = 0x813C;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_TEXTURE_MAX_LEVEL = 0x813D;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_SMOOTH_POINT_SIZE_RANGE = 0x0B12;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_SMOOTH_POINT_SIZE_GRANULARITY = 0x0B13;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_SMOOTH_LINE_WIDTH_RANGE = 0x0B22;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_SMOOTH_LINE_WIDTH_GRANULARITY = 0x0B23;
+        /// <summary>
+        /// 
+        /// </summary>
         public const uint GL_ALIASED_LINE_WIDTH_RANGE = 0x846E;
 
         #endregion
@@ -95,52 +203,340 @@ namespace CSharpGL
         #region OpenGL 1.3
 
         //  Delegates
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="texture"></param>
         public delegate void glActiveTexture(uint texture);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="invert"></param>
         public delegate void glSampleCoverage(float value, bool invert);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="level"></param>
+        /// <param name="internalformat"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="depth"></param>
+        /// <param name="border"></param>
+        /// <param name="imageSize"></param>
+        /// <param name="data"></param>
         public delegate void glCompressedTexImage3D(uint target, int level, uint internalformat, int width, int height, int depth, int border, int imageSize, IntPtr data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="level"></param>
+        /// <param name="internalformat"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="border"></param>
+        /// <param name="imageSize"></param>
+        /// <param name="data"></param>
         public delegate void glCompressedTexImage2D(uint target, int level, uint internalformat, int width, int height, int border, int imageSize, IntPtr data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="level"></param>
+        /// <param name="internalformat"></param>
+        /// <param name="width"></param>
+        /// <param name="border"></param>
+        /// <param name="imageSize"></param>
+        /// <param name="data"></param>
         public delegate void glCompressedTexImage1D(uint target, int level, uint internalformat, int width, int border, int imageSize, IntPtr data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="level"></param>
+        /// <param name="xoffset"></param>
+        /// <param name="yoffset"></param>
+        /// <param name="zoffset"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="depth"></param>
+        /// <param name="format"></param>
+        /// <param name="imageSize"></param>
+        /// <param name="data"></param>
         public delegate void glCompressedTexSubImage3D(uint target, int level, int xoffset, int yoffset, int zoffset, int width, int height, int depth, uint format, int imageSize, IntPtr data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="level"></param>
+        /// <param name="xoffset"></param>
+        /// <param name="yoffset"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="format"></param>
+        /// <param name="imageSize"></param>
+        /// <param name="data"></param>
         public delegate void glCompressedTexSubImage2D(uint target, int level, int xoffset, int yoffset, int width, int height, uint format, int imageSize, IntPtr data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="level"></param>
+        /// <param name="xoffset"></param>
+        /// <param name="width"></param>
+        /// <param name="format"></param>
+        /// <param name="imageSize"></param>
+        /// <param name="data"></param>
         public delegate void glCompressedTexSubImage1D(uint target, int level, int xoffset, int width, uint format, int imageSize, IntPtr data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="level"></param>
+        /// <param name="img"></param>
         public delegate void glGetCompressedTexImage(uint target, int level, IntPtr img);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="texture"></param>
         public delegate void glClientActiveTexture(uint texture);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
         public delegate void glMultiTexCoord1d(uint target, double s);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord1dv(uint target, double[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
         public delegate void glMultiTexCoord1f(uint target, float s);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord1fv(uint target, float[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
         public delegate void glMultiTexCoord1i(uint target, int s);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord1iv(uint target, int[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
         public delegate void glMultiTexCoord1s(uint target, short s);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord1sv(uint target, short[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
         public delegate void glMultiTexCoord2d(uint target, double s, double t);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord2dv(uint target, double[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
         public delegate void glMultiTexCoord2f(uint target, float s, float t);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord2fv(uint target, float[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
         public delegate void glMultiTexCoord2i(uint target, int s, int t);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord2iv(uint target, int[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
         public delegate void glMultiTexCoord2s(uint target, short s, short t);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord2sv(uint target, short[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="r"></param>
         public delegate void glMultiTexCoord3d(uint target, double s, double t, double r);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord3dv(uint target, double[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="r"></param>
         public delegate void glMultiTexCoord3f(uint target, float s, float t, float r);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord3fv(uint target, float[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="r"></param>
         public delegate void glMultiTexCoord3i(uint target, int s, int t, int r);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord3iv(uint target, int[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="r"></param>
         public delegate void glMultiTexCoord3s(uint target, short s, short t, short r);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord3sv(uint target, short[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="r"></param>
+        /// <param name="q"></param>
         public delegate void glMultiTexCoord4d(uint target, double s, double t, double r, double q);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord4dv(uint target, double[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="r"></param>
+        /// <param name="q"></param>
         public delegate void glMultiTexCoord4f(uint target, float s, float t, float r, float q);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord4fv(uint target, float[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="r"></param>
+        /// <param name="q"></param>
         public delegate void glMultiTexCoord4i(uint target, int s, int t, int r, int q);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord4iv(uint target, int[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="r"></param>
+        /// <param name="q"></param>
         public delegate void glMultiTexCoord4s(uint target, short s, short t, short r, short q);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="v"></param>
         public delegate void glMultiTexCoord4sv(uint target, short[] v);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="m"></param>
         public delegate void glLoadTransposeMatrixf(float[] m);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="m"></param>
         public delegate void glLoadTransposeMatrixd(double[] m);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="m"></param>
         public delegate void glMultTransposeMatrixf(float[] m);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="m"></param>
         public delegate void glMultTransposeMatrixd(double[] m);
 
         //  Constants
