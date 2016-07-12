6533eb5e34559574cb2598da3ac8638b276fc070
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-12 18:39:55 +0800

    clean

--- CSharpGL/OpenGLAPI/OpenGL_Consts.cs
+++ CSharpGL/OpenGLAPI/OpenGL_Consts.cs
@@ -408,31 +408,31 @@ namespace CSharpGL
         /// <summary>
         /// 
         /// </summary>
-        public const uint GL_NO_ERROR = 0;
+        internal const uint GL_NO_ERROR = 0;
         /// <summary>
         /// 
         /// </summary>
-        public const uint GL_INVALID_ENUM = 0x0500;
+        internal const uint GL_INVALID_ENUM = 0x0500;
         /// <summary>
         /// 
         /// </summary>
-        public const uint GL_INVALID_VALUE = 0x0501;
+        internal const uint GL_INVALID_VALUE = 0x0501;
         /// <summary>
         /// 
         /// </summary>
-        public const uint GL_INVALID_OPERATION = 0x0502;
+        internal const uint GL_INVALID_OPERATION = 0x0502;
         /// <summary>
         /// 
         /// </summary>
-        public const uint GL_STACK_OVERFLOW = 0x0503;
+        internal const uint GL_STACK_OVERFLOW = 0x0503;
         /// <summary>
         /// 
         /// </summary>
-        public const uint GL_STACK_UNDERFLOW = 0x0504;
+        internal const uint GL_STACK_UNDERFLOW = 0x0504;
         /// <summary>
         /// 
         /// </summary>
-        public const uint GL_OUT_OF_MEMORY = 0x0505;
+        internal const uint GL_OUT_OF_MEMORY = 0x0505;
 
         //   FeedBackMode
         /// <summary>
