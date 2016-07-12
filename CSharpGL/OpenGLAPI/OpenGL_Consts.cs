a67f6355c60d1ca1a439ede968237717556375d5
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-13 01:23:37 +0800

    clean AttribMask

--- CSharpGL/OpenGLAPI/OpenGL_Consts.cs
+++ CSharpGL/OpenGLAPI/OpenGL_Consts.cs
@@ -72,91 +72,91 @@ namespace CSharpGL
         /// </summary>
         public const uint GL_ALWAYS = 0x0207;
 
-        //  AttribMask
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_CURRENT_BIT = 0x00000001;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_POINT_BIT = 0x00000002;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_LINE_BIT = 0x00000004;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_POLYGON_BIT = 0x00000008;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_POLYGON_STIPPLE_BIT = 0x00000010;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_PIXEL_MODE_BIT = 0x00000020;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_LIGHTING_BIT = 0x00000040;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_FOG_BIT = 0x00000080;
+        ////  AttribMask
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_CURRENT_BIT = 0x00000001;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_POINT_BIT = 0x00000002;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_LINE_BIT = 0x00000004;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_POLYGON_BIT = 0x00000008;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_POLYGON_STIPPLE_BIT = 0x00000010;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_PIXEL_MODE_BIT = 0x00000020;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_LIGHTING_BIT = 0x00000040;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_FOG_BIT = 0x00000080;
         /// <summary>
         /// 
         /// </summary>
         public const uint GL_DEPTH_BUFFER_BIT = 0x00000100;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_ACCUM_BUFFER_BIT = 0x00000200;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_ACCUM_BUFFER_BIT = 0x00000200;
         /// <summary>
         /// 
         /// </summary>
         public const uint GL_STENCIL_BUFFER_BIT = 0x00000400;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_VIEWPORT_BIT = 0x00000800;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_TRANSFORM_BIT = 0x00001000;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_ENABLE_BIT = 0x00002000;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_VIEWPORT_BIT = 0x00000800;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_TRANSFORM_BIT = 0x00001000;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_ENABLE_BIT = 0x00002000;
         /// <summary>
         /// 
         /// </summary>
         public const uint GL_COLOR_BUFFER_BIT = 0x00004000;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_HINT_BIT = 0x00008000;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_EVAL_BIT = 0x00010000;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_LIST_BIT = 0x00020000;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_TEXTURE_BIT = 0x00040000;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_SCISSOR_BIT = 0x00080000;
-        /// <summary>
-        /// 
-        /// </summary>
-        public const uint GL_ALL_ATTRIB_BITS = 0x000fffff;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_HINT_BIT = 0x00008000;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_EVAL_BIT = 0x00010000;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_LIST_BIT = 0x00020000;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_TEXTURE_BIT = 0x00040000;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_SCISSOR_BIT = 0x00080000;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_ALL_ATTRIB_BITS = 0x000fffff;
 
         //  BeginMode
         /// <summary>
