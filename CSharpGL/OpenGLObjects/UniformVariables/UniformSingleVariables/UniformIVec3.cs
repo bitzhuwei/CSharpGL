ea1c0786cac84bc9e739a55e3c9f75ffa4f9edf7
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-15 22:17:53 +0800

    uniform variable of type ivec3/4

---
+++ CSharpGL/OpenGLObjects/UniformVariables/UniformSingleVariables/UniformIVec3.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// uniform vec2 variable;
+    /// </summary>
+    public class UniformIVec3 : UniformSingleVariable<ivec3>
+    {
+
+        /// <summary>
+        /// uniform vec2 variable;
+        /// </summary>
+        /// <param name="varName"></param>
+        public UniformIVec3(string varName) : base(varName) { }
+        /// <summary>
+        /// uniform vec2 variable;
+        /// </summary>
+        /// <param name="varName"></param>
+        /// <param name="value"></param>
+        public UniformIVec3(string varName, ivec3 value) : base(varName, value) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="program"></param>
+        public override void SetUniform(ShaderProgram program)
+        {
+            this.Location = program.SetUniform(VarName, value.x, value.y, value.z);
+        }
+    }
+}
