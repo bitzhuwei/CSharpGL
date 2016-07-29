bf57a3c037ccc8aa3458b75cee26218b05072f5d
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-29 10:42:06 +0800

    clean

--- CSharpGL/NewFontResource/FontBitmapHelper.cs
+++ CSharpGL/NewFontResource/FontBitmapHelper.cs
@@ -20,8 +20,6 @@ namespace CSharpGL.NewFontResource
         /// <returns></returns>
         public static FontBitmap GetFontBitmap(this Font font, string charSet)
         {
-            InitStandardWidths();
-
             var fontBitmap = new FontBitmap();
             fontBitmap.font = font;
 
@@ -54,33 +52,20 @@ namespace CSharpGL.NewFontResource
         private static void GetGlyphSizes(FontBitmap fontBitmap, string charSet)
         {
             float fontSize = fontBitmap.font.Size;
-            int maxWidth = GetMaxWidth(fontSize, charSet.Length);
 
-            using (var bitmap = new Bitmap(maxWidth, maxWidth, PixelFormat.Format24bppRgb))
+            using (var bitmap = new Bitmap(1, 1, PixelFormat.Format24bppRgb))
             {
-                float currentX = 0, currentY = 0;
                 using (Graphics graphics = Graphics.FromImage(bitmap))
                 {
                     foreach (char c in charSet)
                     {
                         SizeF size = graphics.MeasureString(c.ToString(), fontBitmap.font);
-                        if (currentX + xInterval + size.Width >= maxWidth)
-                        {
-                            currentX = 0;
-                            currentY += yInterval + fontSize;
-                            if (currentY + yInterval + fontSize >= maxWidth)
-                            { throw new Exception("Texture Size not big enough for required characters."); }
-                        }
-                        const int a = 5;
-                        const int b = 8;
-                        var info = new GlyphInfo(currentX, currentY, size.Width + xInterval, yInterval + fontSize - 1);
+                        var info = new GlyphInfo(0, 0, size.Width, size.Height);
                         fontBitmap.glyphInfoDictionary.Add(c, info);
-                        currentX += xInterval + size.Width;
                     }
                 }
             }
         }
-        static int[] standardWidths;
 
         private static Bitmap ShortenBitmap(Bitmap bitmap, int width, int height)
         {
@@ -90,43 +75,6 @@ namespace CSharpGL.NewFontResource
             g.Dispose();
             //finalBitmap.Save("Test.bmp");
             return finalBitmap;
-        }
-
-        private static int GetMaxWidth(float pixelSize, int count)
-        {
-            if (count < 1) { throw new ArgumentException(); }
-            float maxWidth = (float)(Math.Sqrt(count) * pixelSize);
-            if (maxWidth < pixelSize)
-            {
-                maxWidth = pixelSize;
-            }
-            for (int i = 0; i < standardWidths.Length; i++)
-            {
-                if (maxWidth <= standardWidths[i])
-                {
-                    maxWidth = standardWidths[i];
-                    break;
-                }
-            }
-            return (int)Math.Ceiling(maxWidth);
-        }
-
-        private static void InitStandardWidths()
-        {
-            if (standardWidths == null)
-            {
-                var maxTextureSize = new int[1];
-                OpenGL.GetInteger(GetTarget.MaxTextureSize, maxTextureSize);
-                if (maxTextureSize[0] == 0) { maxTextureSize[0] = (int)Math.Pow(2, 14); }
-                int i = 2;
-                var widths = new List<int>();
-                while (Math.Pow(2, i) <= maxTextureSize[0])
-                {
-                    widths.Add((int)Math.Pow(2, i));
-                    i++;
-                }
-                standardWidths = widths.ToArray();
-            }
         }
 
         const int xInterval = 1;
