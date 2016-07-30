bf72d310f2f4c544c506e738be27cf4447675582
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-31 01:09:45 +0800

    ColorPaletteRenderer

---
+++ Demos/GridViewer/Renderers/ColorPaletteRenderer/ColorPaletteRenderer.cs
@@ -0,0 +1,23 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewer
+{
+    class ColorPaletteRenderer : RendererBase
+    {
+
+        protected override void DoInitialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoRender(RenderEventArg arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
