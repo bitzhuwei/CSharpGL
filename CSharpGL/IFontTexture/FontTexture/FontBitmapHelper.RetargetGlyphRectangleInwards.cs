f0c923180da71a1d8990936d3afc3fe4e2244b75
祝威 <bitzhuwei.cnblogs@qq.com>
2016-07-31 13:47:45 +0800

    clean

---
+++ CSharpGL/IFontTexture/FontTexture/FontBitmapHelper.RetargetGlyphRectangleInwards.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// helper class.
+    /// </summary>
+    public static partial class FontBitmapHelper
+    {
+
+        /// <summary>
+        /// Returns try if the given pixel is empty (i.e. black)
+        /// </summary>
+        /// <param name="bitmapData"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private static unsafe bool IsEmptyPixel(BitmapData bitmapData, int x, int y)
+        {
+            var addr = (byte*)(bitmapData.Scan0) + bitmapData.Stride * y + x * 3;
+            return (*addr == 0 && *(addr + 1) == 0 && *(addr + 2) == 0);
+        }
+
+        /// <summary>
+        /// shrink glyph's width to fit in exactly.
+        /// </summary>
+        /// <param name="bitmapData"></param>
+        /// <param name="glyph"></param>
+        private static void RetargetGlyphRectangleInwards(BitmapData bitmapData, GlyphInfo glyph)
+        {
+            int startX, endX;
+
+            {
+                bool done = false;
+                for (startX = glyph.xoffset; startX < bitmapData.Width; startX++)
+                {
+                    for (int j = glyph.yoffset; j < glyph.yoffset + glyph.height; j++)
+                    {
+                        if (!IsEmptyPixel(bitmapData, startX, j))
+                        {
+                            done = true;
+                            break;
+                        }
+                    }
+                    if (done) { break; }
+                }
+            }
+            {
+                bool done = false;
+                for (endX = glyph.xoffset + glyph.width - 1; endX >= 0; endX--)
+                {
+                    for (int j = glyph.yoffset; j < glyph.yoffset + glyph.height; j++)
+                    {
+                        if (!IsEmptyPixel(bitmapData, endX, j))
+                        {
+                            done = true;
+                            break;
+                        }
+                    }
+                    if (done) { break; }
+                }
+            }
+
+            if (endX < startX)
+            {
+                //startX = endX = glyph.xoffset;
+                glyph.width = 0;
+            }
+            else
+            {
+                glyph.xoffset = startX;
+                glyph.width = endX - startX + 1;
+            }
+        }
+
+    }
+}
--- CSharpGL/IFontTexture/FontTexture/FontBitmapHelper.cs
+++ CSharpGL/IFontTexture/FontTexture/FontBitmapHelper.cs
@@ -10,7 +10,7 @@ namespace CSharpGL
     /// <summary>
     /// helper class.
     /// </summary>
-    public static class FontBitmapHelper
+    public static partial class FontBitmapHelper
     {
         /// <summary>
         /// bigger interval means less mix. Maybe 1 is enough for font with size of 64.
@@ -138,70 +138,6 @@ namespace CSharpGL
             }
 
             bitmap.UnlockBits(bitmapData);
-        }
-
-        /// <summary>
-        /// Returns try if the given pixel is empty (i.e. black)
-        /// </summary>
-        /// <param name="bitmapData"></param>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        private static unsafe bool IsEmptyPixel(BitmapData bitmapData, int x, int y)
-        {
-            var addr = (byte*)(bitmapData.Scan0) + bitmapData.Stride * y + x * 3;
-            return (*addr == 0 && *(addr + 1) == 0 && *(addr + 2) == 0);
-        }
-
-        /// <summary>
-        /// shrink glyph's width to fit in exactly.
-        /// </summary>
-        /// <param name="bitmapData"></param>
-        /// <param name="glyph"></param>
-        private static void RetargetGlyphRectangleInwards(BitmapData bitmapData, GlyphInfo glyph)
-        {
-            int startX, endX;
-
-            {
-                bool done = false;
-                for (startX = glyph.xoffset; startX < bitmapData.Width; startX++)
-                {
-                    for (int j = glyph.yoffset; j < glyph.yoffset + glyph.height; j++)
-                    {
-                        if (!IsEmptyPixel(bitmapData, startX, j))
-                        {
-                            done = true;
-                            break;
-                        }
-                    }
-                    if (done) { break; }
-                }
-            }
-            {
-                bool done = false;
-                for (endX = glyph.xoffset + glyph.width - 1; endX >= 0; endX--)
-                {
-                    for (int j = glyph.yoffset; j < glyph.yoffset + glyph.height; j++)
-                    {
-                        if (!IsEmptyPixel(bitmapData, endX, j))
-                        {
-                            done = true;
-                            break;
-                        }
-                    }
-                    if (done) { break; }
-                }
-            }
-
-            if (endX < startX)
-            {
-                //startX = endX = glyph.xoffset;
-                glyph.width = 0;
-            }
-            else
-            {
-                glyph.xoffset = startX;
-                glyph.width = endX - startX + 1;
-            }
         }
 
         /// <summary>
