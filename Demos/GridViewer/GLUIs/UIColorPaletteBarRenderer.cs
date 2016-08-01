e45d6c41142ec2964bf98f5d5fca5678dd10823b
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-01 16:49:30 +0800

    UIColorPaletteBarRenderer

---
+++ Demos/GridViewer/GLUIs/UIColorPaletteBarRenderer.cs
@@ -0,0 +1,30 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewer
+{
+    class UIColorPaletteBarRenderer : UIRenderer
+    {
+
+        /// <summary>
+        /// opengl UI for Axis
+        /// </summary>
+        /// <param name="anchor"></param>
+        /// <param name="margin"></param>
+        /// <param name="size"></param>
+        /// <param name="zNear"></param>
+        /// <param name="zFar"></param>
+        public UIColorPaletteBarRenderer(
+            System.Windows.Forms.AnchorStyles anchor, System.Windows.Forms.Padding margin,
+            System.Drawing.Size size, int zNear, int zFar)
+            : base(anchor, margin, size, zNear, zFar)
+        {
+            this.SwitchList.Add(new ClearColorSwitch());
+        }
+
+    }
+}
