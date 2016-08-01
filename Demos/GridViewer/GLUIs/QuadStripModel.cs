5dba2068e02c3789f88cb5ea9ced4eec0d5f0473
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-01 22:33:34 +0800

    QuadStripModel

---
+++ Demos/GridViewer/GLUIs/QuadStripModel.cs
@@ -0,0 +1,129 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewer.GLUIs
+{
+    /// <summary>
+    ///  /|\ y
+    ///   |
+    ///   |
+    ///   |
+    ///   ---------------&gt; x
+    /// (0, 0)
+    /// 0    2    4    6    8    10
+    /// --------------------------
+    /// {    {    {    {    {    {
+    /// {    {    {    {    {    {
+    /// {    {    {    {    {    {
+    /// {    {    {    {    {    {
+    /// {    {    {    {    {    {
+    /// {    {    {    {    {    {
+    /// {    {    {    {    {    {
+    /// --------------------------
+    /// 1    3    5    7    9    11
+    /// side length is 1.
+    /// </summary>
+    class QuadStripModel : IBufferable
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string position = "position";
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string color = "color";
+        private PropertyBufferPtr positionBufferPtr;
+        private PropertyBufferPtr colorBufferPtr;
+
+        public QuadStripModel(int quadCount)
+        {
+            this.quadCount = quadCount;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bufferName"></param>
+        /// <param name="varNameInShader"></param>
+        /// <returns></returns>
+        public PropertyBufferPtr GetProperty(string bufferName, string varNameInShader)
+        {
+            if (bufferName == position)
+            {
+                if (positionBufferPtr == null)
+                {
+                    using (var buffer = new PropertyBuffer<vec3>(
+                        varNameInShader, 3, OpenGL.GL_FLOAT, BufferUsage.StaticDraw))
+                    {
+                        buffer.Create((this.quadCount + 1) * 2);
+                        unsafe
+                        {
+                            var array = (vec3*)buffer.Header.ToPointer();
+                            for (int i = 0; i < (this.quadCount + 1); i++)
+                            {
+                                array[i * 2 + 0] = new vec3(-0.5f + (float)i / (float)(this.quadCount + 1) * 0.5f, 1, 0);
+                                array[i * 2 + 1] = new vec3(-0.5f + (float)i / (float)(this.quadCount + 1) * 0.5f, -1, 0);
+                            }
+                        }
+
+                        positionBufferPtr = buffer.GetBufferPtr() as PropertyBufferPtr;
+                    }
+                }
+                return positionBufferPtr;
+            }
+            else if (bufferName == color)
+            {
+                if (colorBufferPtr == null)
+                {
+                    using (var buffer = new PropertyBuffer<vec3>(
+                        varNameInShader, 3, OpenGL.GL_FLOAT, BufferUsage.StaticDraw))
+                    {
+                        buffer.Create((this.quadCount + 1) * 2);
+                        unsafe
+                        {
+                            var array = (vec3*)buffer.Header.ToPointer();
+                            for (int i = 0; i < (this.quadCount + 1); i++)
+                            {
+                                array[i * 2 + 0] = new vec3(-0.5f + (float)i / (float)(this.quadCount + 1) * 0.5f, 1, 0);
+                                array[i * 2 + 1] = new vec3(-0.5f + (float)i / (float)(this.quadCount + 1) * 0.5f, 1, 0);
+                            }
+                        }
+
+                        colorBufferPtr = buffer.GetBufferPtr() as PropertyBufferPtr;
+                    }
+                }
+                return colorBufferPtr;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IndexBufferPtr GetIndex()
+        {
+            if (indexBufferPtr == null)
+            {
+                using (var buffer = new ZeroIndexBuffer(
+                    DrawMode.QuadStrip, 0, (this.quadCount + 1) * 2))
+                {
+                    indexBufferPtr = buffer.GetBufferPtr() as IndexBufferPtr;
+                }
+            }
+
+            return indexBufferPtr;
+        }
+
+        private IndexBufferPtr indexBufferPtr = null;
+        private int quadCount;
+    }
+}
