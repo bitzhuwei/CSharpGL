cd8482a4f8d56a725238d482412baf495a8faf55
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-03 17:06:35 +0800

    clean

--- CSharpGL/_Foundations/OpenGLObjects/Textures/TexureUpdater.cs
+++ CSharpGL/_Foundations/OpenGLObjects/Textures/TexureUpdater.cs
@@ -41,21 +41,10 @@ namespace CSharpGL
         /// <returns></returns>
         public bool UpdateTexture(Bitmap bitmap)
         {
-            // generate texture.
             //  Lock the image bits (so that we can pass them to OGL).
             BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
                 ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-            //GL.ActiveTexture(GL.GL_TEXTURE0);
-            //OpenGL.GetDelegateFor<OpenGL.glActiveTexture>()(OpenGL.GL_TEXTURE0);
             OpenGL.BindTexture(this.Target, this.Id);
-            /* We require 1 byte alignment when uploading texture data */
-            //GL.PixelStorei(GL.GL_UNPACK_ALIGNMENT, 1);
-            /* Clamping to edges is important to prevent artifacts when scaling */
-            //OpenGL.TexParameteri(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_WRAP_S, (int)OpenGL.GL_CLAMP_TO_EDGE);
-            //OpenGL.TexParameteri(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_WRAP_T, (int)OpenGL.GL_CLAMP_TO_EDGE);
-            ///* Linear filtering usually looks best for text */
-            //OpenGL.TexParameteri(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MIN_FILTER, (int)OpenGL.GL_LINEAR);
-            //OpenGL.TexParameteri(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MAG_FILTER, (int)OpenGL.GL_LINEAR);
             OpenGL.TexImage2D(this.Target, 0, (int)OpenGL.GL_RGBA,
                 bitmap.Width, bitmap.Height, 0, OpenGL.GL_BGRA, OpenGL.GL_UNSIGNED_BYTE,
                 bitmapData.Scan0);
