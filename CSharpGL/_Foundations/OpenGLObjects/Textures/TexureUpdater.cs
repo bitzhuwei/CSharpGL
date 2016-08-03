c325b5f5d73400c59ba490aa84ee74c9c5c0212b
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-03 10:13:09 +0800

    TexureUpdater

--- CSharpGL/_Foundations/OpenGLObjects/Textures/TexureUpdater.cs
+++ CSharpGL/_Foundations/OpenGLObjects/Textures/TexureUpdater.cs
@@ -12,10 +12,12 @@ namespace CSharpGL
     /// </summary>
     public class TexureUpdater
     {
-        /// <summary>
-        /// OpenGL.GL_TEXTURE0 etc.
-        /// </summary>
-        public uint ActiveTexture { get; set; }
+        public uint Id { get; private set; }
+
+        ///// <summary>
+        ///// OpenGL.GL_TEXTURE0 etc.
+        ///// </summary>
+        //public uint ActiveTexture { get; set; }
 
         /// <summary>
         /// OpenGL.GL_TEXTURE_2D etc.
@@ -25,9 +27,10 @@ namespace CSharpGL
         /// <summary>
         /// build texture.
         /// </summary>
-        public TexureUpdater()
+        public TexureUpdater(uint id)
         {
-            this.ActiveTexture = OpenGL.GL_TEXTURE0;
+            this.Id = id;
+            //this.ActiveTexture = OpenGL.GL_TEXTURE0;
             this.Target = OpenGL.GL_TEXTURE_2D;
         }
 
@@ -38,34 +41,19 @@ namespace CSharpGL
         /// <returns></returns>
         public bool UpdateTexture(Bitmap bitmap)
         {
-            throw new NotImplementedException();
-            //var ids = new uint[1];
-            //// generate texture.
-            ////  Lock the image bits (so that we can pass them to OGL).
-            //BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-            //    ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-            ////GL.ActiveTexture(GL.GL_TEXTURE0);
+            //  Lock the image bits (so that we can pass them to OGL).
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            //GL.ActiveTexture(GL.GL_TEXTURE0);
             //OpenGL.GetDelegateFor<OpenGL.glActiveTexture>()(this.ActiveTexture);
-            //OpenGL.GenTextures(1, ids);
-            //OpenGL.BindTexture(this.Target, ids[0]);
-            ///* We require 1 byte alignment when uploading texture data */
-            ////GL.PixelStorei(GL.GL_UNPACK_ALIGNMENT, 1);
-            ///* Clamping to edges is important to prevent artifacts when scaling */
-            //OpenGL.TexParameteri(this.Target, OpenGL.GL_TEXTURE_WRAP_S, (int)OpenGL.GL_CLAMP_TO_EDGE);
-            //OpenGL.TexParameteri(this.Target, OpenGL.GL_TEXTURE_WRAP_T, (int)OpenGL.GL_CLAMP_TO_EDGE);
-            ///* Linear filtering usually looks best for text */
-            //OpenGL.TexParameteri(this.Target, OpenGL.GL_TEXTURE_MIN_FILTER, (int)OpenGL.GL_LINEAR);
-            //OpenGL.TexParameteri(this.Target, OpenGL.GL_TEXTURE_MAG_FILTER, (int)OpenGL.GL_LINEAR);
-            //OpenGL.TexImage2D(this.Target, 0, (int)OpenGL.GL_RGBA,
-            //    bitmap.Width, bitmap.Height, 0, OpenGL.GL_BGRA, OpenGL.GL_UNSIGNED_BYTE,
-            //    bitmapData.Scan0);
-            ////  Unlock the image.
-            //bitmap.UnlockBits(bitmapData);
-            //OpenGL.BindTexture(this.Target, 0);
+            OpenGL.BindTexture(this.Target, this.Id);
+            OpenGL.TexSubImage2D(this.Target, 0, 0, 0, bitmap.Width, bitmap.Height, OpenGL.GL_RGBA, OpenGL.GL_UNSIGNED_BYTE, bitmapData.Scan0);
+            OpenGL.BindTexture(OpenGL.GL_TEXTURE_2D, 0);
+            bitmap.UnlockBits(bitmapData);
+            OpenGL.BindTexture(this.Target, 0);
 
-            //var result = new TextureInfo();
-            //result.Id = ids[0];
-            //return result;
+            return true;
         }
+
     }
 }
