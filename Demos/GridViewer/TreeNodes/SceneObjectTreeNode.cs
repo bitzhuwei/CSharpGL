1ce632360dfaedbb2495c415a74caa3c21c3c2f7
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-05 11:32:24 +0800

    special tree node type for scene object and script component.

---
+++ Demos/GridViewer/TreeNodes/SceneObjectTreeNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridViewer
+{
+    public partial class SceneObjectTreeNode : TreeNode
+    {
+        public SceneObjectTreeNode()
+        {
+        }
+    }
+}
