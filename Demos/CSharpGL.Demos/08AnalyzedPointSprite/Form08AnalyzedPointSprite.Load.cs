a92125d86aaeb7fc87be80a53fbc5223d9cb6e33
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-08 16:54:29 +0800

    use Scene in demo 08

--- Demos/CSharpGL.Demos/08AnalyzedPointSprite/Form08AnalyzedPointSprite.Load.cs
+++ Demos/CSharpGL.Demos/08AnalyzedPointSprite/Form08AnalyzedPointSprite.Load.cs
@@ -13,10 +13,7 @@ namespace CSharpGL.Demos
 {
     public partial class Form08AnalyzedPointSprite : Form
     {
-        private FormProperyGrid formPropertyGrid;
-        private UIRoot uiRoot;
-        private UIAxis uiAxis;
-        private UICursor uiCursor;
+        private Scene scene;
 
         private void Form_Load(object sender, EventArgs e)
         {
@@ -26,39 +23,25 @@ namespace CSharpGL.Demos
                     CameraType.Perspecitive, this.glCanvas1.Width, this.glCanvas1.Height);
                 var rotator = new SatelliteManipulater();
                 rotator.Bind(camera, this.glCanvas1);
-                this.camera = camera;
-                this.rotator = rotator;
+                this.scene = new Scene(camera);
+                this.glCanvas1.Resize += this.scene.Resize;
             }
             {
                 var renderer = new AnalyzedPointSpriteRenderer(10000);
                 renderer.Initialize();
-                this.renderer = renderer;
-            }
-            {
-                var uiRoot = new UIRoot();
-                uiRoot.Initialize();
-                this.uiRoot = uiRoot;
+                var obj = new SceneObject();
+                obj.Renderer = new RendererBaseComponent(renderer);
+                this.scene.ObjectList.Add(obj);
             }
             {
                 var uiAxis = new UIAxis(AnchorStyles.Left | AnchorStyles.Bottom,
                     new Padding(3, 3, 3, 3), new Size(128, 128), -100, 100);
                 uiAxis.Initialize();
-                this.uiAxis = uiAxis;
-                uiRoot.Children.Add(uiAxis);
+                this.scene.UIRoot.Children.Add(uiAxis);
             }
             {
-                var uiCursor = UICursor.CreateDefault();
-                uiCursor.Initialize();
-                //uiCursor.SwitchList.Add(new ClearColorSwitch());
-                uiRoot.Children.Add(uiCursor);
-                this.uiCursor = uiCursor;
-                var frmPropertyGrid = new FormProperyGrid(uiCursor);
+                var frmPropertyGrid = new FormProperyGrid(this.scene);
                 frmPropertyGrid.Show();
-            }
-            {
-                var frmPropertyGrid = new FormProperyGrid(this.renderer);
-                frmPropertyGrid.Show();
-                this.formPropertyGrid = frmPropertyGrid;
             }
         }
     }
--- Demos/CSharpGL.Demos/08AnalyzedPointSprite/Form08AnalyzedPointSprite.cs
+++ Demos/CSharpGL.Demos/08AnalyzedPointSprite/Form08AnalyzedPointSprite.cs
@@ -13,21 +13,11 @@ namespace CSharpGL.Demos
     public partial class Form08AnalyzedPointSprite : Form
     {
 
-        private Camera camera;
-        private SatelliteManipulater rotator;
-        private AnalyzedPointSpriteRenderer renderer;
-
-
         public Form08AnalyzedPointSprite()
         {
             InitializeComponent();
 
             this.glCanvas1.OpenGLDraw += glCanvas1_OpenGLDraw;
-            //this.glCanvas1.MouseDown += glCanvas1_MouseDown;
-            //this.glCanvas1.MouseMove += glCanvas1_MouseMove;
-            //this.glCanvas1.MouseUp += glCanvas1_MouseUp;
-            //this.glCanvas1.MouseWheel += glCanvas1_MouseWheel;
-            this.glCanvas1.Resize += glCanvas1_Resize;
 
             Application.Idle += Application_Idle;
             OpenGL.ClearColor(0, 0, 0, 0);
@@ -42,56 +32,7 @@ namespace CSharpGL.Demos
         {
             OpenGL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_STENCIL_BUFFER_BIT);
 
-            RenderEventArg arg = new RenderEventArg(RenderModes.Render, this.glCanvas1.ClientRectangle, this.camera);
-            {
-                renderer.Render(arg);
-            }
-            {
-                Point mousePosition = this.glCanvas1.PointToClient(Control.MousePosition);
-                this.uiCursor.UpdatePosition(mousePosition);
-
-                UIRenderersDraw(arg);
-            }
-
-            //Point mousePosition = this.glCanvas1.PointToClient(Control.MousePosition);
-            // Cross cursor shows where the mouse is.
-            //OpenGL.DrawText(mousePosition.X - offset.X,
-            //    this.glCanvas1.Height - (mousePosition.Y + offset.Y) - 1,
-            //    Color.Red, "Courier New", crossCursorSize, "o");
-        }
-
-
-        private const float crossCursorSize = 40.0f;
-
-        private Point offset = new Point(13, 11);
-        private void UIRenderersDraw(RenderEventArg arg)
-        {
-            UIRoot uiRoot = this.uiRoot;
-            if (uiRoot != null)
-            {
-                uiRoot.Render(arg);
-            }
-        }
-        //void glCanvas1_MouseWheel(object sender, MouseEventArgs e)
-        //{
-        //    ICamera camera = this.camera;
-        //    if (camera != null)
-        //    {
-        //        camera.MouseWheel(e.Delta);
-        //    }
-        //}
-
-        private void glCanvas1_Resize(object sender, EventArgs e)
-        {
-            if (camera != null)
-            {
-                camera.Resize(this.glCanvas1.Width, this.glCanvas1.Height);
-            }
-
-            if (this.uiRoot != null)
-            {
-                this.uiRoot.Size = this.glCanvas1.Size;
-            }
+            this.scene.Render(RenderModes.Render, this.glCanvas1.ClientRectangle, this.glCanvas1.PointToClient(Control.MousePosition));
         }
 
     }
