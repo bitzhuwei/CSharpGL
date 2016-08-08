8dc4c8ff09d50c782ecd0d3e599fb6294d9d46a7
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-08 15:10:03 +0800

    use Scene in demo 04.

--- Demos/CSharpGL.Demos/04SimpleCompute/Form04SimpleCompute.Load.cs
+++ Demos/CSharpGL.Demos/04SimpleCompute/Form04SimpleCompute.Load.cs
@@ -13,8 +13,7 @@ namespace CSharpGL.Demos
 {
     public partial class Form04SimpleCompute : Form
     {
-        private FormProperyGrid formPropertyGrid;
-
+        private Scene scene;
 
         private void Form_Load(object sender, EventArgs e)
         {
@@ -24,17 +23,17 @@ namespace CSharpGL.Demos
                     CameraType.Perspecitive, this.glCanvas1.Width, this.glCanvas1.Height);
                 var rotator = new SatelliteManipulater();
                 rotator.Bind(camera, this.glCanvas1);
-                this.camera = camera;
+                this.scene = new Scene(camera);
+                this.glCanvas1.Resize += this.scene.Resize;
             }
             {
                 var renderer = new SimpleComputeRenderer();
                 renderer.Initialize();
-                this.renderer = renderer;
-            }
-            {
-                var frmPropertyGrid = new FormProperyGrid(this.renderer);
+                var obj = new SceneObject();
+                obj.Renderer = new RendererBaseComponent(renderer);
+                this.scene.ObjectList.Add(obj);
+                var frmPropertyGrid = new FormProperyGrid(this.scene);
                 frmPropertyGrid.Show();
-                this.formPropertyGrid = frmPropertyGrid;
             }
         }
     }
--- Demos/CSharpGL.Demos/04SimpleCompute/Form04SimpleCompute.cs
+++ Demos/CSharpGL.Demos/04SimpleCompute/Form04SimpleCompute.cs
@@ -13,19 +13,11 @@ namespace CSharpGL.Demos
     public partial class Form04SimpleCompute : Form
     {
 
-        private Camera camera;
-        private RendererBase renderer;
-
         public Form04SimpleCompute()
         {
             InitializeComponent();
 
             this.glCanvas1.OpenGLDraw += glCanvas1_OpenGLDraw;
-            //this.glCanvas1.MouseDown += glCanvas1_MouseDown;
-            //this.glCanvas1.MouseMove += glCanvas1_MouseMove;
-            //this.glCanvas1.MouseUp += glCanvas1_MouseUp;
-            //this.glCanvas1.MouseWheel += glCanvas1_MouseWheel;
-            this.glCanvas1.Resize += glCanvas1_Resize;
 
             Application.Idle += Application_Idle;
             // 天蓝色背景
@@ -41,28 +33,7 @@ namespace CSharpGL.Demos
         {
             OpenGL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_STENCIL_BUFFER_BIT);
 
-            RenderEventArg arg = new RenderEventArg(RenderModes.Render, this.glCanvas1.ClientRectangle, this.camera);
-            IRenderable renderer = this.renderer;
-            if (renderer != null)
-            {
-                renderer.Render(arg);
-            }
-
-            Point mousePosition = this.glCanvas1.PointToClient(Control.MousePosition);
-            // Cross cursor shows where the mouse is.
-            OpenGL.DrawText(mousePosition.X - offset.X,
-                this.glCanvas1.Height - (mousePosition.Y + offset.Y) - 1,
-                Color.Red, "Courier New", crossCursorSize, "o");
-        }
-
-
-        private const float crossCursorSize = 40.0f;
-
-        private Point offset = new Point(13, 11);
-
-        private void glCanvas1_Resize(object sender, EventArgs e)
-        {
-            camera.Resize(this.glCanvas1.Width, this.glCanvas1.Height);
+            this.scene.Render(RenderModes.Render, this.glCanvas1.ClientRectangle, this.glCanvas1.PointToClient(Control.MousePosition));
         }
 
     }
