e337a25e2a757ce0a18628b70e2d4bbd4116b06b
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-10 10:55:33 +0800

    add rpoject for generating Renderers.

---
+++ Infrastructure/RendererGenerator/FormMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RendererGenerator
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
