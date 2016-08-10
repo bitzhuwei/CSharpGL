973406a81028fb18753c521c583072e19b645252
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-10 22:38:07 +0800

    fix: updates the other to rotate in demo 03

--- Demos/CSharpGL.Demos/03OrderDependentTransparency/Form03OrderDependentTransparency.MouseDownMoveUp.cs
+++ Demos/CSharpGL.Demos/03OrderDependentTransparency/Form03OrderDependentTransparency.MouseDownMoveUp.cs
@@ -20,12 +20,14 @@ namespace CSharpGL.Demos
         {
             this.lastMousePosition = e.Location;
 
-            // operate camera
-            rotator.canvas_MouseDown(sender, e);
-
             if (sender == this.glCanvas1)
             {
                 form02.glCanvas1_MouseDown(sender, e);
+            }
+            else
+            {
+                // operate camera
+                rotator.canvas_MouseDown(sender, e);
             }
         }
 
@@ -33,27 +35,31 @@ namespace CSharpGL.Demos
         {
             if (lastMousePosition == e.Location) { return; }
 
-            // operate camera
-            rotator.canvas_MouseMove(sender, e);
-
-            this.lastMousePosition = new Point(e.X, e.Y);
+            this.lastMousePosition = e.Location;
 
             if (sender == this.glCanvas1)
             {
                 form02.glCanvas1_MouseMove(sender, e);
             }
+            else
+            {
+                // operate camera
+                rotator.canvas_MouseMove(sender, e);
+            }
         }
 
         internal void glCanvas1_MouseUp(object sender, MouseEventArgs e)
         {
-            // operate camera
-            rotator.canvas_MouseUp(sender, e);
-
             this.lastMousePosition = e.Location;
 
             if (sender == this.glCanvas1)
             {
                 form02.glCanvas1_MouseUp(sender, e);
+            }
+            else
+            {
+                // operate camera
+                rotator.canvas_MouseUp(sender, e);
             }
         }
 
