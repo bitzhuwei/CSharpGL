5c2b9e425de8e2d610b45e935f2e10d8d1f52e96
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-12 13:48:40 +0800

    add readme.

--- CSharpGL/0Foundations/OpenGLObjects/Texture/Texture.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Texture/Texture.cs
@@ -59,8 +59,8 @@ namespace CSharpGL
                 OpenGL.GetDelegateFor<OpenGL.glActiveTexture>()(OpenGL.GL_TEXTURE0);
                 OpenGL.GenTextures(1, id);
                 OpenGL.BindTexture(this.Target, id[0]);
-                this.SamplerBuilder.Build(this.Target);
                 this.ImageBuilder.Build(this.Target);
+                this.SamplerBuilder.Build(this.Target);
                 OpenGL.BindTexture(this.Target, 0);
                 this.initialized = true;
             }
---
+++ CSharpGL/0Foundations/OpenGLObjects/Texture/readme.md
@@ -0,0 +1,10 @@
+﻿# Texture
+A texture is generated from 2 main steps.
+## 1. set up texture's content
+This is done by calling `glTexImage2D()`, `glTexImage1D()` etc.
+Generally we uses a `Bitmap` object to provide image data.
+## 2. set up texture's configurations
+This is done by calling 'glTexParameteri()' etc.
+We can ask OpenGL for a `sampler' object to reduce OpenGL commands.
+# To Be Continued
+The concept of mipmap is still not applied.
