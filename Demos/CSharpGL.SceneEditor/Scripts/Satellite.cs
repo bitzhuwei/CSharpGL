65a0b57a833cbb88a23676cba8524f816bd294c3
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-15 10:45:43 +0800

    clean

--- Demos/CSharpGL.SceneEditor/Scripts/Satellite.cs
+++ Demos/CSharpGL.SceneEditor/Scripts/Satellite.cs
@@ -9,7 +9,6 @@ namespace CSharpGL.SceneEditor.Scripts
     class Satellite : ScriptComponent
     {
 
-        private TransformScript transform;
         private TransformScript planetTransform;
         private double currentAngle;
         private BuildInRenderer renderer;
@@ -26,27 +25,21 @@ namespace CSharpGL.SceneEditor.Scripts
 
         protected override void DoInitialize()
         {
-            {
-                this.transform = this.BindingObject.GetScript<TransformScript>();
-                this.planetTransform = this.BindingObject.Parent.GetScript<TransformScript>(); ;
-            }
-            {
-                this.renderer = this.BindingObject.Renderer as BuildInRenderer;
-            }
+            this.renderer = this.BindingObject.Renderer as BuildInRenderer;
         }
 
         protected override void DoUpdate(double elapsedTime)
         {
             double deltaAngle = elapsedTime * Math.PI * 2 / this.RevolutionPeriod;
             double newAngle = this.currentAngle + deltaAngle;
-            this.transform.Position = new vec3(
+            var position = new vec3(
                 (float)(this.RevolutionRadius * Math.Cos(newAngle)),
                 0,
                 (float)(this.RevolutionRadius * Math.Sin(newAngle)))
                 + this.planetTransform.Position;
             this.currentAngle = newAngle;
 
-            this.renderer.ModelMatrix = this.transform.GetModelMatrix();
+            this.renderer.ModelMatrix = glm.translate(mat4.identity(), position);
         }
 
     }
