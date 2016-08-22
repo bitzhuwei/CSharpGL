6d420a265b1466e19a985dd6c62226f740686f47
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-22 18:30:17 +0800

    ModelScaleScript

---
+++ Demos/GridViewer/Scripts/ModelScaleScript.cs
@@ -0,0 +1,46 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GridViewer
+{
+    public class ModelScaleScript : ScriptComponent
+    {
+        public bool Scale(vec3 factor)
+        {
+            bool updated = false;
+
+            var stack = new Stack<SceneObject>();
+            vec3 rootPosition;
+            {
+                SceneObject obj = this.BindingObject;
+                if (obj == null) { throw new Exception(); }
+                var transform = obj.Renderer as IModelTransform;
+                if (transform == null) { throw new Exception(); }
+                rootPosition = transform.ModelMatrix.GetTranslate();
+                foreach (var item in obj.Children) { stack.Push(item); }
+            }
+            while (stack.Count > 0)
+            {
+                SceneObject obj = stack.Pop();
+                var transform = obj.Renderer as IModelTransform;
+                if (transform != null)
+                {
+                    vec3 position = transform.ModelMatrix.GetTranslate();
+                    vec3 distance = position - rootPosition;
+                    mat4 model = glm.translate(mat4.identity(), distance);
+                    model = glm.scale(model, factor);
+                    transform.ModelMatrix = model;
+                    updated = true;
+                }
+
+                foreach (var item in obj.Children) { stack.Push(item); }
+            }
+
+            return updated;
+        }
+    }
+}
