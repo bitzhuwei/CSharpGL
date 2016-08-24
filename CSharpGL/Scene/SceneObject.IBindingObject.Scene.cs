9a794f2e3233b370b857192c2aaa883635f4f4ee
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-24 20:24:40 +0800

    SceneRootObject

---
+++ CSharpGL/Scene/SceneObject.IBindingObject.Scene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace CSharpGL
+{
+    public partial class SceneObject
+    {
+
+        #region IBindingObject<Scene>
+
+        /// <summary>
+        /// Which scene is this object in?
+        /// </summary>
+        public Scene BindingObject { get; set; }
+
+        #endregion IBindingObject<Scene>
+    }
+}
---
+++ CSharpGL/Scene/SceneRootObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Root object to be rendered in a scene.
+    /// </summary>
+    public partial class SceneRootObject : SceneObject
+    {
+
+        /// <summary>
+        /// an object to be rendered in a scene.
+        /// </summary>
+        public SceneRootObject(Scene bindingScene)
+        {
+            this.BindingScene = bindingScene;
+        }
+
+        /// <summary>
+        /// Which scene is this object in?
+        /// </summary>
+        public Scene BindingScene { get; set; }
+    }
+}
