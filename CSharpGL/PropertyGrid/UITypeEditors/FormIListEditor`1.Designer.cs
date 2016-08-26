1186ae14b37f7d9f2a693043ecd080865dde37f3
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-26 16:29:55 +0800

    clean

--- CSharpGL/PropertyGrid/UITypeEditors/FormIListEditor`1.Designer.cs
+++ CSharpGL/PropertyGrid/UITypeEditors/FormIListEditor`1.Designer.cs
@@ -37,13 +37,17 @@
             this.lblSelectedType = new System.Windows.Forms.Label();
             this.propertyGrid = new System.Windows.Forms.PropertyGrid();
             this.btnClose = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 12F);
-            this.label1.Location = new System.Drawing.Point(11, 11);
+            this.label1.Location = new System.Drawing.Point(2, 3);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 16);
@@ -52,15 +56,16 @@
             // 
             // lstMember
             // 
-            this.lstMember.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstMember.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lstMember.Font = new System.Drawing.Font("宋体", 12F);
             this.lstMember.FormattingEnabled = true;
             this.lstMember.ItemHeight = 16;
-            this.lstMember.Location = new System.Drawing.Point(11, 30);
+            this.lstMember.Location = new System.Drawing.Point(2, 22);
             this.lstMember.Margin = new System.Windows.Forms.Padding(2);
             this.lstMember.Name = "lstMember";
-            this.lstMember.Size = new System.Drawing.Size(201, 308);
+            this.lstMember.Size = new System.Drawing.Size(252, 324);
             this.lstMember.TabIndex = 1;
             this.lstMember.SelectedIndexChanged += new System.EventHandler(this.lstMember_SelectedIndexChanged);
             // 
@@ -68,7 +73,7 @@
             // 
             this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnAdd.Font = new System.Drawing.Font("宋体", 12F);
-            this.btnAdd.Location = new System.Drawing.Point(11, 341);
+            this.btnAdd.Location = new System.Drawing.Point(11, 374);
             this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(68, 30);
@@ -81,7 +86,7 @@
             // 
             this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnRemove.Font = new System.Drawing.Font("宋体", 12F);
-            this.btnRemove.Location = new System.Drawing.Point(83, 341);
+            this.btnRemove.Location = new System.Drawing.Point(83, 374);
             this.btnRemove.Margin = new System.Windows.Forms.Padding(2);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(68, 30);
@@ -92,8 +97,9 @@
             // 
             // btnMoveUp
             // 
+            this.btnMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnMoveUp.Font = new System.Drawing.Font("宋体", 12F);
-            this.btnMoveUp.Location = new System.Drawing.Point(216, 30);
+            this.btnMoveUp.Location = new System.Drawing.Point(258, 22);
             this.btnMoveUp.Margin = new System.Windows.Forms.Padding(2);
             this.btnMoveUp.Name = "btnMoveUp";
             this.btnMoveUp.Size = new System.Drawing.Size(27, 28);
@@ -104,8 +110,9 @@
             // 
             // btnMoveDown
             // 
+            this.btnMoveDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnMoveDown.Font = new System.Drawing.Font("宋体", 12F);
-            this.btnMoveDown.Location = new System.Drawing.Point(216, 62);
+            this.btnMoveDown.Location = new System.Drawing.Point(258, 54);
             this.btnMoveDown.Margin = new System.Windows.Forms.Padding(2);
             this.btnMoveDown.Name = "btnMoveDown";
             this.btnMoveDown.Size = new System.Drawing.Size(27, 28);
@@ -118,7 +125,7 @@
             // 
             this.lblSelectedType.AutoSize = true;
             this.lblSelectedType.Font = new System.Drawing.Font("宋体", 12F);
-            this.lblSelectedType.Location = new System.Drawing.Point(248, 7);
+            this.lblSelectedType.Location = new System.Drawing.Point(2, 3);
             this.lblSelectedType.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSelectedType.Name = "lblSelectedType";
             this.lblSelectedType.Size = new System.Drawing.Size(112, 16);
@@ -130,10 +137,10 @@
             this.propertyGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.propertyGrid.Location = new System.Drawing.Point(248, 30);
+            this.propertyGrid.Location = new System.Drawing.Point(5, 34);
             this.propertyGrid.Margin = new System.Windows.Forms.Padding(2);
             this.propertyGrid.Name = "propertyGrid";
-            this.propertyGrid.Size = new System.Drawing.Size(252, 307);
+            this.propertyGrid.Size = new System.Drawing.Size(325, 321);
             this.propertyGrid.TabIndex = 3;
             // 
             // btnClose
@@ -141,7 +148,7 @@
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnClose.Font = new System.Drawing.Font("宋体", 12F);
-            this.btnClose.Location = new System.Drawing.Point(429, 341);
+            this.btnClose.Location = new System.Drawing.Point(567, 374);
             this.btnClose.Margin = new System.Windows.Forms.Padding(2);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(68, 30);
@@ -150,27 +157,49 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnOK_Click);
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(11, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.lstMember);
+            this.splitContainer1.Panel1.Controls.Add(this.btnMoveUp);
+            this.splitContainer1.Panel1.Controls.Add(this.btnMoveDown);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lblSelectedType);
+            this.splitContainer1.Panel2.Controls.Add(this.propertyGrid);
+            this.splitContainer1.Size = new System.Drawing.Size(623, 357);
+            this.splitContainer1.SplitterDistance = 287;
+            this.splitContainer1.TabIndex = 4;
+            // 
             // FormIListEditor
             // 
             this.AcceptButton = this.btnClose;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnClose;
-            this.ClientSize = new System.Drawing.Size(508, 382);
-            this.Controls.Add(this.propertyGrid);
+            this.ClientSize = new System.Drawing.Size(646, 415);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnRemove);
-            this.Controls.Add(this.btnMoveDown);
-            this.Controls.Add(this.btnMoveUp);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.lstMember);
-            this.Controls.Add(this.lblSelectedType);
-            this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormIListEditor";
             this.Text = "{0} List Editor";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -185,5 +214,6 @@
         private System.Windows.Forms.Label lblSelectedType;
         private System.Windows.Forms.PropertyGrid propertyGrid;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
