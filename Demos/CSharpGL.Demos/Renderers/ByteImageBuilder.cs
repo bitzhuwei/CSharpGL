1ae5d860f438998cb691351c311f9eb022744967
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-28 22:05:14 +0800

    ByteImageBuilder

---
+++ Demos/CSharpGL.Demos/Renderers/ByteImageBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    class ByteImageBuilder : ImageBuilder
+    {
+        private byte[] content;
+        private int width;
+
+        /// <summary>
+        /// build texture's content with Bitmap.
+        /// </summary>
+        /// <param name="bitmap"></param>
+        public ByteImageBuilder(byte[] content, int width)
+        {
+            this.content = content;
+            this.width = width;
+        }
+
+        /// <summary>
+        /// build texture's content with Bitmap.
+        /// </summary>
+        public override void Build()
+        {
+            OpenGL.PixelStorei(OpenGL.GL_UNPACK_ALIGNMENT, 1);
+            OpenGL.TexImage1D((uint)BindTextureTarget.Texture1D, 0, OpenGL.GL_RGBA8, this.width, 0, OpenGL.GL_BGRA, OpenGL.GL_UNSIGNED_BYTE, content);
+        }
+    }
+}
