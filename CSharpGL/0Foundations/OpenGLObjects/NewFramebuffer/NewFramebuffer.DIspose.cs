e5038cfa651f08627bc6a22bfd3526e65d95cffe
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-28 23:34:27 +0800

    newframebuffer

---
+++ CSharpGL/0Foundations/OpenGLObjects/NewFramebuffer/NewFramebuffer.DIspose.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace CSharpGL
+{
+    //TODO: IDisposable
+    public partial class NewFramebuffer : IDisposable
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        ~NewFramebuffer()
+        {
+            this.Dispose(false);
+        }
+
+        private bool disposedValue = false;
+
+        private void Dispose(bool disposing)
+        {
+            if (this.disposedValue == false)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources.
+
+                }
+
+                //// Dispose unmanaged resources.
+                glDeleteFramebuffers(1, this.id);
+            }
+
+            this.disposedValue = true;
+        }
+    }
+}
---
+++ CSharpGL/0Foundations/OpenGLObjects/NewFramebuffer/NewFramebuffer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Create, update and delete a framebuffer object.
+    /// </summary>
+    public partial class NewFramebuffer : IDisposable
+    {
+        private static OpenGL.glGenFramebuffersEXT glGenFramebuffers;
+        private static OpenGL.glBindFramebufferEXT glBindFramebuffer;
+        private static OpenGL.glFramebufferTexture2DEXT glFramebufferTexture2D;
+        private static OpenGL.glCheckFramebufferStatusEXT glCheckFramebufferStatus;
+        private static OpenGL.glDeleteFramebuffersEXT glDeleteFramebuffers;
+        private static OpenGL.glDrawBuffers glDrawBuffers;
+
+        /// <summary>
+        /// Framebuffer Id.
+        /// </summary>
+        public uint Id { get; private set; }
+
+        public NewFramebuffer()
+        {
+            if (glGenFramebuffers == null)
+            {
+                glGenFramebuffers = OpenGL.GetDelegateFor<OpenGL.glGenFramebuffersEXT>();
+                glBindFramebuffer = OpenGL.GetDelegateFor<OpenGL.glBindFramebufferEXT>();
+                glFramebufferTexture2D = OpenGL.GetDelegateFor<OpenGL.glFramebufferTexture2DEXT>();
+                glCheckFramebufferStatus = OpenGL.GetDelegateFor<OpenGL.glCheckFramebufferStatusEXT>();
+                glDeleteFramebuffers = OpenGL.GetDelegateFor<OpenGL.glDeleteFramebuffersEXT>();
+            }
+
+            var frameBuffer = new uint[1];
+            glGenFramebuffers(1, frameBuffer);
+        }
+
+    }
+}
