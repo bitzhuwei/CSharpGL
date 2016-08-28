6bad3b8416ea72341a1107a9fb88620a4186e90d
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-28 19:17:03 +0800

    clean

--- Demos/CSharpGL.Demos/08AnalyzedPointSprite/Form08AnalyzedPointSprite.Load.cs
+++ Demos/CSharpGL.Demos/08AnalyzedPointSprite/Form08AnalyzedPointSprite.Load.cs
@@ -39,8 +39,10 @@ namespace CSharpGL.Demos
                 this.scene.UIRoot.Children.Add(uiAxis);
             }
             {
-                var frmPropertyGrid = new FormProperyGrid(this.scene);
-                frmPropertyGrid.Show();
+                var builder = new StringBuilder();
+                builder.AppendLine("S: Scene's property grid.");
+                builder.AppendLine("C: Canvas' property grid.");
+                MessageBox.Show(builder.ToString());
             }
         }
     }
