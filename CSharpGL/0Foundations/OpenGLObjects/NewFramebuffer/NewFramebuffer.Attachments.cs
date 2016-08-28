f842e47b75c57a88971aabd38b09cf306856c2fd
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-29 00:36:09 +0800

    attach Texture images

--- CSharpGL/0Foundations/OpenGLObjects/NewFramebuffer/NewFramebuffer.Attachments.cs
+++ CSharpGL/0Foundations/OpenGLObjects/NewFramebuffer/NewFramebuffer.Attachments.cs
@@ -10,7 +10,42 @@ namespace CSharpGL
     {
         #region attach Texture images
 
+        private static readonly uint[] attachment_id =
+        {
+			OpenGL.GL_COLOR_ATTACHMENT0,
+			OpenGL.GL_COLOR_ATTACHMENT1,
+			OpenGL.GL_COLOR_ATTACHMENT2,
+			OpenGL.GL_COLOR_ATTACHMENT3,
+			OpenGL.GL_COLOR_ATTACHMENT4,
+			OpenGL.GL_COLOR_ATTACHMENT5,
+			OpenGL.GL_COLOR_ATTACHMENT6,
+			OpenGL.GL_COLOR_ATTACHMENT7,
+			OpenGL.GL_COLOR_ATTACHMENT8,
+			OpenGL.GL_COLOR_ATTACHMENT9,
+			OpenGL.GL_COLOR_ATTACHMENT10,
+			OpenGL.GL_COLOR_ATTACHMENT11,
+			OpenGL.GL_COLOR_ATTACHMENT12,
+			OpenGL.GL_COLOR_ATTACHMENT13,
+			OpenGL.GL_COLOR_ATTACHMENT14,
+			OpenGL.GL_COLOR_ATTACHMENT15,
+        };
+        private int nextColorAttachmentIndex = 0;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <returns></returns>
+        public bool Attach(Texture texture)
+        {
+            if (nextColorAttachmentIndex >= attachment_id.Length)
+            { throw new IndexOutOfRangeException("Not enough color attach points!"); }
+
+            glFramebufferTexture2D(
+    OpenGL.GL_FRAMEBUFFER, attachment_id[nextColorAttachmentIndex++], OpenGL.GL_TEXTURE_2D, texture.Id, 0);
+
+            return CheckCompleteness();
+        }
 
         #endregion attach Texture images
     }
--- CSharpGL/0Foundations/OpenGLObjects/NewFramebuffer/NullImageBuilder.cs
+++ CSharpGL/0Foundations/OpenGLObjects/NewFramebuffer/NullImageBuilder.cs
@@ -7,17 +7,24 @@ using System.Text;
 namespace CSharpGL
 {
     /// <summary>
-    /// Create, update, use and delete a framebuffer object.
     /// </summary>
     public class NullImageBuilder : ImageBuilder
     {
         private int width;
         private int height;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
         public NullImageBuilder(int width, int height)
         {
             this.width = width;
             this.height = height;
         }
+        /// <summary>
+        /// 
+        /// </summary>
         public override void Build()
         {
             OpenGL.TexImage2D((uint)BindTextureTarget.Texture2D, 0, (int)OpenGL.GL_RGBA, width, height, 0, OpenGL.GL_RGBA, OpenGL.GL_UNSIGNED_BYTE, IntPtr.Zero);
