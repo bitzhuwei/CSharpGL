372fd2d79ff5293ec8d7d5af7b326092aaf4a9ef
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-29 18:45:56 +0800

    clean

--- CSharpGL/0Foundations/OpenGLAPI/OpenGL.Extensions.cs
+++ CSharpGL/0Foundations/OpenGLAPI/OpenGL.Extensions.cs
@@ -9,42 +9,6 @@ namespace CSharpGL
     public static partial class OpenGL
     {
 
-        #region GL_EXT_texture3D
-
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="target"></param>
-        ///// <param name="level"></param>
-        ///// <param name="internalformat"></param>
-        ///// <param name="width"></param>
-        ///// <param name="height"></param>
-        ///// <param name="depth"></param>
-        ///// <param name="border"></param>
-        ///// <param name="format"></param>
-        ///// <param name="type"></param>
-        ///// <param name="pixels"></param>
-        //public delegate void glTexImage3DEXT(uint target, int level, uint internalformat, uint width,
-        //    uint height, uint depth, int border, uint format, uint type, IntPtr pixels);
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="target"></param>
-        ///// <param name="level"></param>
-        ///// <param name="xoffset"></param>
-        ///// <param name="yoffset"></param>
-        ///// <param name="zoffset"></param>
-        ///// <param name="width"></param>
-        ///// <param name="height"></param>
-        ///// <param name="depth"></param>
-        ///// <param name="format"></param>
-        ///// <param name="type"></param>
-        ///// <param name="pixels"></param>
-        //public delegate void glTexSubImage3DEXT(uint target, int level, int xoffset, int yoffset, int zoffset,
-        //    uint width, uint height, uint depth, uint format, uint type, IntPtr pixels);
-
-        #endregion
-
         #region GL_EXT_rescale_normal
 
         ///// <summary>
