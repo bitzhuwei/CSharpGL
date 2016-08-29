cc8c9acbb948ac578f3eabf0f3d2134eb7168211
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-30 00:00:14 +0800

    clean

--- CSharpGL/Canvas/BoundedClockRenderer.cs
+++ CSharpGL/Canvas/BoundedClockRenderer.cs
@@ -10,16 +10,14 @@ namespace CSharpGL
     /// </summary>
     class BoundedClockRenderer : RendererBase
     {
-        private float angle;
         public LegacyBoundingBoxRenderer BoxRenderer { get; set; }
         public ClockRenderer ClockRenderer { get; set; }
 
         public BoundedClockRenderer()
         {
-            //this.BoxRenderer = new LegacyBoundingBoxRenderer(new vec3(1, 1, 1), new vec3(-1, -1, -1), Color.Aqua);
-            const float factor = 0.3f;
-            this.BoxRenderer = new LegacyBoundingBoxRenderer(factor * new vec3(1, 1, 1), factor * new vec3(-1, -1, -1), Color.Aqua);
-            this.ClockRenderer = new ClockRenderer();
+            this.BoxRenderer = new LegacyBoundingBoxRenderer();
+            this.BoxRenderer.BoxColor = Color.Aqua;
+            this.ClockRenderer = new ClockRenderer(new vec3(1, 0.8f, 0));
         }
 
         protected override void DoInitialize()
@@ -30,12 +28,8 @@ namespace CSharpGL
 
         protected override void DoRender(RenderEventArgs arg)
         {
-            this.ClockRenderer.Render(arg);
-
-            OpenGL.LoadIdentity();
-            OpenGL.Rotatef(angle, 0, 1, 0);
             this.BoxRenderer.Render(arg);
-            angle += 3.0f;
+            this.ClockRenderer.Render(arg);
         }
     }
 }
--- Demos/CSharpGL.Demos/01Renderer/Form01Renderer.Designer.cs
+++ Demos/CSharpGL.Demos/01Renderer/Form01Renderer.Designer.cs
@@ -31,8 +31,8 @@
             this.dlgSaveFile = new System.Windows.Forms.SaveFileDialog();
             this.lblReadColor = new System.Windows.Forms.Label();
             this.lblColor = new System.Windows.Forms.Label();
-            this.glCanvas1 = new CSharpGL.GLCanvas();
             this.lblDrawText = new System.Windows.Forms.Label();
+            this.glCanvas1 = new CSharpGL.GLCanvas();
             ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,9 +47,10 @@
             // 
             this.lblReadColor.AutoSize = true;
             this.lblReadColor.Font = new System.Drawing.Font("宋体", 12F);
-            this.lblReadColor.Location = new System.Drawing.Point(82, 9);
+            this.lblReadColor.Location = new System.Drawing.Point(62, 7);
+            this.lblReadColor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblReadColor.Name = "lblReadColor";
-            this.lblReadColor.Size = new System.Drawing.Size(149, 20);
+            this.lblReadColor.Size = new System.Drawing.Size(120, 16);
             this.lblReadColor.TabIndex = 1;
             this.lblReadColor.Text = "Color At Mouse";
             // 
@@ -57,45 +58,48 @@
             // 
             this.lblColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.lblColor.Font = new System.Drawing.Font("宋体", 12F);
-            this.lblColor.Location = new System.Drawing.Point(12, 9);
+            this.lblColor.Location = new System.Drawing.Point(9, 7);
+            this.lblColor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblColor.Name = "lblColor";
-            this.lblColor.Size = new System.Drawing.Size(64, 20);
+            this.lblColor.Size = new System.Drawing.Size(48, 16);
             this.lblColor.TabIndex = 1;
+            // 
+            // lblDrawText
+            // 
+            this.lblDrawText.AutoSize = true;
+            this.lblDrawText.Font = new System.Drawing.Font("宋体", 12F);
+            this.lblDrawText.Location = new System.Drawing.Point(9, 26);
+            this.lblDrawText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDrawText.Name = "lblDrawText";
+            this.lblDrawText.Size = new System.Drawing.Size(72, 16);
+            this.lblDrawText.TabIndex = 1;
+            this.lblDrawText.Text = "DrawText";
             // 
             // glCanvas1
             // 
             this.glCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.glCanvas1.Location = new System.Drawing.Point(13, 56);
-            this.glCanvas1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glCanvas1.Location = new System.Drawing.Point(10, 45);
             this.glCanvas1.Name = "glCanvas1";
             this.glCanvas1.OpenGLVersion = CSharpGL.GLVersion.OpenGL2_1;
             this.glCanvas1.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
-            this.glCanvas1.Size = new System.Drawing.Size(720, 416);
+            this.glCanvas1.ShowSystemCursor = true;
+            this.glCanvas1.Size = new System.Drawing.Size(540, 333);
             this.glCanvas1.TabIndex = 0;
             this.glCanvas1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.glCanvas1_KeyPress);
             this.glCanvas1.Resize += new System.EventHandler(this.glCanvas1_Resize);
             // 
-            // lblDrawText
-            // 
-            this.lblDrawText.AutoSize = true;
-            this.lblDrawText.Font = new System.Drawing.Font("宋体", 12F);
-            this.lblDrawText.Location = new System.Drawing.Point(12, 32);
-            this.lblDrawText.Name = "lblDrawText";
-            this.lblDrawText.Size = new System.Drawing.Size(89, 20);
-            this.lblDrawText.TabIndex = 1;
-            this.lblDrawText.Text = "DrawText";
-            // 
             // Form01Renderer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(746, 485);
+            this.ClientSize = new System.Drawing.Size(560, 388);
             this.Controls.Add(this.lblColor);
             this.Controls.Add(this.lblDrawText);
             this.Controls.Add(this.lblReadColor);
             this.Controls.Add(this.glCanvas1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form01Renderer";
             this.Text = "Form01Renderer";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form01Renderer_FormClosed);
