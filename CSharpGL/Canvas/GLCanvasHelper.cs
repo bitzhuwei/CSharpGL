00fa74fa767def6d7d7bf471f2affc3afdaa69b7
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-29 22:27:16 +0800

    clean

--- CSharpGL/Canvas/GLCanvasHelper.cs
+++ CSharpGL/Canvas/GLCanvasHelper.cs
@@ -13,7 +13,7 @@ namespace CSharpGL
 
         ////  Use the 'look at' helper function to position and aim the camera.
         //OpenGL.gluLookAt(-2, 2, -2, 0, 0, 0, 0, 1, 0);
-        static readonly mat4 viewMatrix = glm.lookAt(new vec3(0, 0, 2), new vec3(0, 0, 0), new vec3(0, 1, 0));
+        static readonly mat4 viewMatrix = glm.lookAt(new vec3(0, 0, 3), new vec3(0, 0, 0), new vec3(0, 1, 0));
         public static void ResizeGL(double width, double height)
         {
             //  Set the projection matrix.
@@ -31,7 +31,7 @@ namespace CSharpGL
         }
 
         static readonly BoundedClockRenderer clockRenderer = new BoundedClockRenderer();
-     
+
         public static void DrawClock()
         {
             clockRenderer.Render(null);
