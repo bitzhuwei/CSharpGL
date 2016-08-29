00b076b08402edcb8f27626588c4de4b3308176a
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-29 18:44:31 +0800

    clean

--- CSharpGL/0Foundations/OpenGLAPI/OpenGL.Extensions.cs
+++ CSharpGL/0Foundations/OpenGLAPI/OpenGL.Extensions.cs
@@ -9,24 +9,6 @@ namespace CSharpGL
     public static partial class OpenGL
     {
 
-        #region OpenGL 3.3
-
-        ////  Delegates
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="index"></param>
-        ///// <param name="divisor"></param>
-        //public delegate void glVertexAttribDivisor(uint index, uint divisor);
-
-        ////  Constants
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //public const uint GL_VERTEX_ATTRIB_ARRAY_DIVISOR = 0x88FE;
-
-        #endregion
-
         #region OpenGL 4.0
 
         ////  Delegates
