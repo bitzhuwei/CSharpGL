6676cab6fbbe359ed30292f098923f7616ae218e
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-29 18:41:44 +0800

    clean

---
+++ CSharpGL/0Foundations/OpenGLAPI/OpenGL.2.1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace CSharpGL
+{
+    public static partial class OpenGL
+    {
+
+        #region OpenGL 2.1
+
+        //  Delegates
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="location"></param>
+        ///// <param name="count"></param>
+        ///// <param name="transpose"></param>
+        ///// <param name="value"></param>
+        //public delegate void glUniformMatrix2x3fv(int location, int count, bool transpose, float[] value);
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="location"></param>
+        ///// <param name="count"></param>
+        ///// <param name="transpose"></param>
+        ///// <param name="value"></param>
+        //public delegate void glUniformMatrix3x2fv(int location, int count, bool transpose, float[] value);
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="location"></param>
+        ///// <param name="count"></param>
+        ///// <param name="transpose"></param>
+        ///// <param name="value"></param>
+        //public delegate void glUniformMatrix2x4fv(int location, int count, bool transpose, float[] value);
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="location"></param>
+        ///// <param name="count"></param>
+        ///// <param name="transpose"></param>
+        ///// <param name="value"></param>
+        //public delegate void glUniformMatrix4x2fv(int location, int count, bool transpose, float[] value);
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="location"></param>
+        ///// <param name="count"></param>
+        ///// <param name="transpose"></param>
+        ///// <param name="value"></param>
+        //public delegate void glUniformMatrix3x4fv(int location, int count, bool transpose, float[] value);
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="location"></param>
+        ///// <param name="count"></param>
+        ///// <param name="transpose"></param>
+        ///// <param name="value"></param>
+        //public delegate void glUniformMatrix4x3fv(int location, int count, bool transpose, float[] value);
+
+        //  Constants
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_PIXEL_PACK_BUFFER = 0x88EB;
+        /// <summary>
+        /// 
+        /// </summary>
+        public const uint GL_PIXEL_UNPACK_BUFFER = 0x88EC;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_PIXEL_PACK_BUFFER_BINDING = 0x88ED;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_PIXEL_UNPACK_BUFFER_BINDING = 0x88EF;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_FLOAT_MAT2x3 = 0x8B65;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_FLOAT_MAT2x4 = 0x8B66;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_FLOAT_MAT3x2 = 0x8B67;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_FLOAT_MAT3x4 = 0x8B68;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_FLOAT_MAT4x2 = 0x8B69;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_FLOAT_MAT4x3 = 0x8B6A;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_SRGB = 0x8C40;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_SRGB8 = 0x8C41;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_SRGB_ALPHA = 0x8C42;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_SRGB8_ALPHA8 = 0x8C43;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_COMPRESSED_SRGB = 0x8C48;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public const uint GL_COMPRESSED_SRGB_ALPHA = 0x8C49;
+
+        #endregion
+
+    }
+}
