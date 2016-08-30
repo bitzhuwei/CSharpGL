b00e864d475c131e02db04add9e3f8b1161ba2f2
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-30 15:09:45 +0800

    NullImageBuilder

--- CSharpGL/0Foundations/OpenGLObjects/Framebuffer/NullImageBuilder.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Framebuffer/NullImageBuilder.cs
@@ -13,23 +13,37 @@ namespace CSharpGL
     {
         private int width;
         private int height;
+        private uint internalFormat;
+        private uint format;
+        private uint type;
 
         /// <summary>
         /// build texture's content with IntPtr.Zero.
         /// </summary>
         /// <param name="width"></param>
         /// <param name="height"></param>
-        public NullImageBuilder(int width, int height)
+        /// <param name="internalFormat"></param>
+        /// <param name="format"></param>
+        /// <param name="type"></param>
+        public NullImageBuilder(int width, int height, uint internalFormat, uint format, uint type)
         {
             this.width = width;
             this.height = height;
+            this.internalFormat = internalFormat;
+            this.format = format;
+            this.type = type;
         }
         /// <summary>
         /// 
         /// </summary>
         public override void Build()
         {
-            OpenGL.TexImage2D((uint)BindTextureTarget.Texture2D, 0, (int)OpenGL.GL_RGBA, width, height, 0, OpenGL.GL_RGBA, OpenGL.GL_UNSIGNED_BYTE, IntPtr.Zero);
+            OpenGL.TexImage2D((uint)BindTextureTarget.Texture2D, 0,
+                internalFormat,// OpenGL.GL_RGBA, 
+                width, height, 0,
+                this.format,// OpenGL.GL_RGBA,
+                this.type,// OpenGL.GL_UNSIGNED_BYTE, 
+                IntPtr.Zero);
         }
     }
 }
