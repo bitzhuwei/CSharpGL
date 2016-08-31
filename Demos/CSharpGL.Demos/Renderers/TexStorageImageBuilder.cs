cf64a59a54d159addeeb4be039511fcf500821f5
祝威 <bitzhuwei.cnblogs@qq.com>
2016-08-31 13:18:21 +0800

    clean

--- Demos/CSharpGL.Demos/Renderers/TexStorageImageBuilder.cs
+++ Demos/CSharpGL.Demos/Renderers/TexStorageImageBuilder.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace CSharpGL.Demos.Renderers
+namespace CSharpGL.Demos
 {
-    class TexStorageImageBuilder : ImageBuilder
+    public class TexStorageImageBuilder : ImageBuilder
     {
         private int levels;
         private uint internalFormat;
