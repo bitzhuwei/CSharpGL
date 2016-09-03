f4ff80a3e538ee0f4864eedba58f48c73a9b9070
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-03 12:38:41 +0800

    clean

--- CSharpGL/Renderers/SimpleRenderer/SimpleRenderer.cs
+++ CSharpGL/Renderers/SimpleRenderer/SimpleRenderer.cs
@@ -3,7 +3,7 @@
 namespace CSharpGL
 {
     /// <summary>
-    /// Renders a label(a single line of text) that always faces camera in 3D space.
+    /// Renders a simple model embeded in CSharpGL.
     /// </summary>
     public partial class SimpleRenderer : Renderer
     {
@@ -88,6 +88,25 @@ namespace CSharpGL
             PropertyNameMap propertyNameMap, params GLSwitch[] switches)
             : base(model, shaderCodes, propertyNameMap, switches)
         {
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="arg"></param>
+        protected override void DoRender(RenderEventArgs arg)
+        {
+            if (this.modelMatrixRecord.IsMarked())
+            {
+                this.SetUniform("modelMatrix", this.WorldPosition);
+                this.modelMatrixRecord.CancelMark();
+            }
+            mat4 projection = arg.Camera.GetProjectionMatrix();
+            mat4 view = arg.Camera.GetViewMatrix();
+            this.SetUniform("projectionMatrix", projection);
+            this.SetUniform("viewMatrix", view);
+
+            base.DoRender(arg);
         }
     }
 }
