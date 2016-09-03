2435af63000ace5924435cb39468015a63a626ff
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-03 12:46:29 +0800

    clean

--- Demos/CSharpGL.Demos/18PickingInScene/Form18PickingInScene.Load.cs
+++ Demos/CSharpGL.Demos/18PickingInScene/Form18PickingInScene.Load.cs
@@ -11,6 +11,9 @@ namespace CSharpGL.Demos
         private void Form_Load(object sender, EventArgs e)
         {
             {
+                this.glCanvas1.ShowSystemCursor = false;
+            }
+            {
                 var camera = new Camera(
                     new vec3(0, 0, 1), new vec3(0, 0, 0), new vec3(0, 1, 0),
                     CameraType.Perspecitive, this.glCanvas1.Width, this.glCanvas1.Height);
@@ -18,14 +21,6 @@ namespace CSharpGL.Demos
                 rotator.Bind(camera, this.glCanvas1);
                 this.scene = new Scene(camera, this.glCanvas1);
                 this.glCanvas1.Resize += this.scene.Resize;
-            }
-            {
-                LabelRenderer labelRenderer = LabelRenderer.Create(1000, 640);
-                labelRenderer.Text = "LABEL 2";
-                labelRenderer.Initialize();
-                labelRenderer.WorldPosition = new vec3(-1, 0, 0);
-                SceneObject obj = labelRenderer.WrapToSceneObject("label 2");
-                this.scene.RootObject.Children.Add(obj);
             }
             {
                 var ground = GroundRenderer.Create(new GroundModel(10));
