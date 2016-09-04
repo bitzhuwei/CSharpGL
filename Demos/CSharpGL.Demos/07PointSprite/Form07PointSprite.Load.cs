93eaaa058ee5141b6233198201dbced7180cada7
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-04 22:32:57 +0800

    clean

--- Demos/CSharpGL.Demos/07PointSprite/Form07PointSprite.Load.cs
+++ Demos/CSharpGL.Demos/07PointSprite/Form07PointSprite.Load.cs
@@ -14,7 +14,7 @@ namespace CSharpGL.Demos
         {
             {
                 var camera = new Camera(
-                    new vec3(0, 0, 1), new vec3(0, 0, 0), new vec3(0, 1, 0),
+                    new vec3(5, 4, 3) * 0.5f, new vec3(0, 0, 0), new vec3(0, 1, 0),
                     CameraType.Perspecitive, this.glCanvas1.Width, this.glCanvas1.Height);
                 var rotator = new SatelliteManipulater();
                 rotator.Bind(camera, this.glCanvas1);
