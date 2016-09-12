8154a16e5cfbdc71aec3c42900b33448829ce3d8
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-12 21:41:37 +0800

    clean

--- CSharpGL/0Foundations/OpenGLObjects/Buffers/VertexBuffers/VertexAttributeBuffer.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/VertexBuffers/VertexAttributeBuffer.cs
@@ -3,9 +3,10 @@
     /// <summary>
     /// 顶点属性Buffer。描述顶点的位置或颜色或UV等各种属性。
     /// <para>每个<see cref="VertexAttributeBuffer&lt;T&gt;"/>仅描述其中一个属性。</para>
-    /// <para>Vertex Buffer Object that describes vertex' property(position, color, uv coordinate, ect.).</para>
+    /// <para>Vertex Buffer Object that describes vertex' property(position, color, uv coordinate, etc.).</para>
     /// <para>Each <see cref="VertexAttributeBuffer&lt;T&gt;"/> describes only 1 property.</para>
-    /// <para>Note: If <typeparamref name="T"/> matches one of this.Config's value, then (this.ByteLength / (this.DataSize * this.DataTypeByteLength)) equals (this.Length).</para>
+    /// <para>Note: If <typeparamref name="T"/> matches one of this.Config's value, then (Ptr.ByteLength / (Ptr.DataSize * Ptr.DataTypeByteLength)) equals (Ptr.Length).</para>
+    /// <para><typeparamref name="T"/> is type of element of this array in application level.</para>
     /// </summary>
     /// <typeparam name="T">element type in this array in application level.</typeparam>
     public class VertexAttributeBuffer<T> : Buffer where T : struct
@@ -13,8 +14,10 @@
         /// <summary>
         /// 顶点属性Buffer。描述顶点的位置或颜色或UV等各种属性。
         /// <para>每个<see cref="VertexAttributeBuffer&lt;T&gt;"/>仅描述其中一个属性。</para>
-        /// <para>Vertex Buffer Object that describes vertex' property(position, color, uv coordinate, ect.).</para>
+        /// <para>Vertex Buffer Object that describes vertex' property(position, color, uv coordinate, etc.).</para>
         /// <para>Each <see cref="VertexAttributeBuffer&lt;T&gt;"/> describes only 1 property.</para>
+        /// <para>Note: If <typeparamref name="T"/> matches one of this.Config's value, then (Ptr.ByteLength / (Ptr.DataSize * Ptr.DataTypeByteLength)) equals (Ptr.Length).</para>
+        /// <para><typeparamref name="T"/> is type of element of this array in application level.</para>
         /// </summary>
         /// <param name="varNameInVertexShader">此顶点属性VBO对应于vertex shader中的哪个in变量？<para>Mapping variable's name in vertex shader.</para></param>
         /// <param name="config">This <paramref name="config"/> decides parameters' values in glVertexAttribPointer(attributeLocation, size, type, false, 0, IntPtr.Zero);
