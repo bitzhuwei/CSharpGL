844f695d200ed68201922b42ea2f61e5655038a0
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-13 00:49:42 +0800

    clean

---
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/BufferPtr.BufferTexture.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class BufferPtrHelper
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="bufferPtr"></param>
+        /// <param name="internalFormat"></param>
+        /// <param name="autoDispose">Dispose <paramref name="bufferPtr"/> when disposing returned texture.</param>
+        /// <returns></returns>
+        public static Texture DumpBufferTexture(this BufferPtr bufferPtr, uint internalFormat, bool autoDispose)
+        {
+            return Texture.CreateBufferTexture(internalFormat, bufferPtr, autoDispose);
+        }
+    }
+}
