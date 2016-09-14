cbc9d30ae38a24034489eeed1e2c241675ed9c13
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-14 09:55:20 +0800

    ConditionalRenderer

---
+++ Demos/CSharpGL.Demos/Renderers/ConditionalRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace CSharpGL.Demos
+{
+    /// <summary>
+    /// demostrates how to perform conditional rendering.
+    /// </summary>
+    internal class ConditionalRenderer : RendererBase
+    {
+
+        public static ConditionalRenderer Create()
+        {
+            throw new System.NotFiniteNumberException();
+        }
+
+        private ConditionalRenderer()
+        { }
+
+        protected override void DoInitialize()
+        {
+        }
+
+        protected override void DoRender(RenderEventArgs arg)
+        {
+            mat4 projection = arg.Camera.GetProjectionMatrix();
+            mat4 view = arg.Camera.GetViewMatrix();
+        }
+    }
+}
