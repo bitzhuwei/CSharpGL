5587bef1a27bb2f0a90d2fbd05d715f9b143afd1
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-16 16:56:32 +0800

    update

---
+++ Demos/WebGL.Demo/demo.js
@@ -0,0 +1,100 @@
+﻿//
+// demo.js
+//
+var canvas;
+var gl;
+var texture;
+var uFrame; // 订单着色器的uniform变量
+
+window.onload = init;
+
+function CheckError(msg) {
+    var error = gl.getError();
+    if (error != 0) {
+        var errMsg = "OpenGL error: " + error.toString(16);
+        if (msg) { errMsg = msg + "\n" + errMsg; }
+        alert(errMsg);
+    }
+}
+
+function configureTexture(iamge) {
+    texture = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, iamge);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+}
+
+function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available!"); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.4, 0.8, 1.0, 1.0);
+
+    // 
+    // 读取着色器并初始化属性数组
+    //
+    var program = InitShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var verteices = {};
+    vertices.data = new Float32Array([
+        -0.5, -0.5,
+        0.5, -0.5,
+        0.5, 0.5,
+        -0.5, 0.5
+    ]);
+    vertices.bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, verteices.bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, verteices.data, gl.STATIC_DRAW);
+    var vPos = gl.getAttribLocation(program, "vPos");
+    gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPos);
+
+    var texCoords = {};
+    texCoords.data = new Float32Array([
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0
+    ]);
+    texCoords.bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoords.bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, texCoords.data, gl.STATIC_DRAW);
+    var vTexCoord = gl.getAttribLocation(program, "vTexCoord");
+    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vTexCoord);
+
+    //
+    // 初始化纹理
+    //
+    var image = new Image();
+    iamge.onload = function () {
+        configureTexture(image);
+        render();
+    }
+    iamge.src = "box.bmp";
+
+    gl.activeTexture(gl.TEXTURE0);
+    var uTexture = gl.getUniformLocation(program, "uTexture");
+    gl.uniform1i(uTexture, 0);
+
+    uFrame = gl.getUniformLocation(program, "uFrame");
+}
+
+var frameNumber = 0;
+
+function render() {
+    gl.uniform1f(uFrame, frameNumber++);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+
+    window.requestAnimationFrame(render, canvas);
+}
