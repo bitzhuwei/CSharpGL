7e2c546d6cee56e9fc58116460057951938592f3
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-18 16:22:58 +0800

    update

--- Demos/CSharpGL.Demos/23SingleRenderer/Form23SingleRenderer.Designer.cs
+++ Demos/CSharpGL.Demos/23SingleRenderer/Form23SingleRenderer.Designer.cs
@@ -29,68 +29,39 @@
         private void InitializeComponent()
         {
             this.glCanvas1 = new CSharpGL.GLCanvas();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblState = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).BeginInit();
             this.SuspendLayout();
             // 
             // glCanvas1
             // 
-            this.glCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.glCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.glCanvas1.Location = new System.Drawing.Point(10, 58);
+            this.glCanvas1.Location = new System.Drawing.Point(10, 12);
             this.glCanvas1.Name = "glCanvas1";
             this.glCanvas1.OpenGLVersion = CSharpGL.GLVersion.OpenGL2_1;
             this.glCanvas1.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
             this.glCanvas1.ShowSystemCursor = true;
-            this.glCanvas1.Size = new System.Drawing.Size(568, 368);
+            this.glCanvas1.Size = new System.Drawing.Size(568, 414);
             this.glCanvas1.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 12F);
-            this.label1.Location = new System.Drawing.Point(9, 7);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(392, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "\'b\': Box, \'t\':Target, \'r\': Conditional Rendering";
-            // 
-            // lblState
-            // 
-            this.lblState.AutoSize = true;
-            this.lblState.Font = new System.Drawing.Font("宋体", 12F);
-            this.lblState.Location = new System.Drawing.Point(10, 27);
-            this.lblState.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblState.Name = "lblState";
-            this.lblState.Size = new System.Drawing.Size(392, 16);
-            this.lblState.TabIndex = 2;
-            this.lblState.Text = "\'b\': Box, \'t\':Target, \'r\': Conditional Rendering";
-            // 
-            // Form21ConditionalRendering
+            // Form23SingleRenderer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(587, 438);
-            this.Controls.Add(this.lblState);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.glCanvas1);
             this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "Form21ConditionalRendering";
+            this.Name = "Form23SingleRenderer";
             this.Text = "Form21ConditionalRendering";
             this.Load += new System.EventHandler(this.Form_Load);
             ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private GLCanvas glCanvas1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblState;
     }
 }
