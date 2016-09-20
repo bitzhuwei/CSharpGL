240c4dbd87764328c7b27886f606b3775d731be4
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-21 00:17:56 +0800

    WaterGroundRenderer

---
+++ Demos/CSharpGL.Demos/Renderers/Water/WaterGroundRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpGL.Demos
+{
+    internal partial class WaterGroundRenderer : Renderer
+    {
+        public static WaterGroundRenderer Create(int sideLength)
+        {
+            var model = new PlaneModel(sideLength / 2);
+            var shaderCodes = new ShaderCode[2];
+            shaderCodes[0] = new ShaderCode(File.ReadAllText(@"shaders\water\WaterTexture.vert.glsl"), ShaderType.VertexShader);
+            shaderCodes[1] = new ShaderCode(File.ReadAllText(@"shaders\water\WaterTexture.frag.glsl"), ShaderType.FragmentShader);
+            var map = new PropertyNameMap();
+            map.Add("a_vertex", PlaneModel.strPosition);
+            map.Add("a_texCoord", PlaneModel.strTexCoord);
+            var renderer = new WaterGroundRenderer(model, shaderCodes, map, new PointSpriteSwitch());
+            renderer.Lengths = new vec3(sideLength, 0, sideLength);
+
+            return renderer;
+        }
+
+        private WaterGroundRenderer(
+            IBufferable bufferable, ShaderCode[] shaderCodes,
+            PropertyNameMap propertyNameMap, params GLSwitch[] switches)
+            : base(bufferable, shaderCodes, propertyNameMap, switches)
+        {
+        }
+
+    }
+}
