316868f2d910e02ca4ae698012c3a2e0368f585e
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-21 00:01:18 +0800

    WaterTextureRenderer

--- Demos/CSharpGL.Demos/Renderers/Water/WaterTextureRenderer.cs
+++ Demos/CSharpGL.Demos/Renderers/Water/WaterTextureRenderer.cs
@@ -7,12 +7,13 @@ namespace CSharpGL.Demos
     {
         public static WaterTextureRenderer Create(int sideLength)
         {
-            var model = new WaterPlaneModel(sideLength);
+            var model = new PlaneModel(sideLength / 2);
             var shaderCodes = new ShaderCode[2];
             shaderCodes[0] = new ShaderCode(File.ReadAllText(@"shaders\water\WaterTexture.vert.glsl"), ShaderType.VertexShader);
             shaderCodes[1] = new ShaderCode(File.ReadAllText(@"shaders\water\WaterTexture.frag.glsl"), ShaderType.FragmentShader);
             var map = new PropertyNameMap();
-            map.Add("position", WaterPlaneModel.strPosition);
+            map.Add("a_vertex", PlaneModel.strPosition);
+            map.Add("a_texCoord", PlaneModel.strTexCoord);
             var renderer = new WaterTextureRenderer(model, shaderCodes, map, new PointSpriteSwitch());
             renderer.Lengths = new vec3(sideLength, 0, sideLength);
 
