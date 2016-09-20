4df9f9cb6d9aa6d3cf8715231ff8955a536d9c3f
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-20 15:15:30 +0800

    update

--- Demos/CSharpGL.Demos/17UpdateTexture/Form17UpdateTexture.Load.cs
+++ Demos/CSharpGL.Demos/17UpdateTexture/Form17UpdateTexture.Load.cs
@@ -55,7 +55,6 @@ namespace CSharpGL.Demos
             }
             {
                 var builder = new StringBuilder();
-                builder.AppendLine("1: Scene's property grid.");
                 builder.AppendLine("2: Canvas' property grid.");
                 MessageBox.Show(builder.ToString());
             }
