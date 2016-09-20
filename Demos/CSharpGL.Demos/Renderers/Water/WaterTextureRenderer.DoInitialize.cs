3defab65aa7e6e1625701fd6451031fa32b5ed81
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-20 22:28:10 +0800

    update

---
+++ Demos/CSharpGL.Demos/Renderers/Water/WaterTextureRenderer.DoInitialize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace CSharpGL.Demos
+{
+    internal partial class WaterTextureRenderer
+    {
+
+        protected override void DoInitialize()
+        {
+            base.DoInitialize();
+        }
+    }
+}
---
+++ Demos/CSharpGL.Demos/Renderers/Water/WaterTextureRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpGL.Demos
+{
+    internal partial class WaterTextureRenderer : Renderer
+    {
+        public static WaterTextureRenderer Create(int sideLength)
+        {
+            var model = new WaterPlaneModel(sideLength);
+            var shaderCodes = new ShaderCode[2];
+            shaderCodes[0] = new ShaderCode(File.ReadAllText(@"shaders\water\Water.vert.glsl"), ShaderType.VertexShader);
+            shaderCodes[1] = new ShaderCode(File.ReadAllText(@"shaders\water\Water.frag.glsl"), ShaderType.FragmentShader);
+            var map = new PropertyNameMap();
+            map.Add("position", WaterPlaneModel.strPosition);
+            var renderer = new WaterTextureRenderer(model, shaderCodes, map, new PointSpriteSwitch());
+            renderer.Lengths = new vec3(sideLength, 0, sideLength);
+
+            return renderer;
+        }
+
+        private WaterTextureRenderer(
+            IBufferable bufferable, ShaderCode[] shaderCodes,
+            PropertyNameMap propertyNameMap, params GLSwitch[] switches)
+            : base(bufferable, shaderCodes, propertyNameMap, switches)
+        {
+        }
+
+    }
+}
