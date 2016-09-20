577baaf27d212a33ce7738c800c1ab7fd8ed7d7d
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-21 00:03:59 +0800

    update

--- Demos/CSharpGL.Demos/Renderers/Water/WaterRenderer.cs
+++ Demos/CSharpGL.Demos/Renderers/Water/WaterRenderer.cs
@@ -13,7 +13,7 @@ namespace CSharpGL.Demos
             shaderCodes[0] = new ShaderCode(File.ReadAllText(@"shaders\water\Water.vert.glsl"), ShaderType.VertexShader);
             shaderCodes[1] = new ShaderCode(File.ReadAllText(@"shaders\water\Water.frag.glsl"), ShaderType.FragmentShader);
             var map = new PropertyNameMap();
-            map.Add("position", WaterPlaneModel.strPosition);
+            map.Add("a_vertex", WaterPlaneModel.strPosition);
             var renderer = new WaterRenderer(model, shaderCodes, map, new PointSpriteSwitch());
             renderer.Lengths = new vec3(sideLength, 0, sideLength);
 
