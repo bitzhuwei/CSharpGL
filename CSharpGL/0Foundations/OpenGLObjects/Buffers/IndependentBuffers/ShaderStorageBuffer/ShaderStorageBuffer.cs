cab5c2090c813e97c81e5756e037d370fae7acf4
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-22 01:20:20 +0800

    udpate

--- CSharpGL/0Foundations/OpenGLObjects/Buffers/IndependentBuffers/ShaderStorageBuffer/ShaderStorageBuffer.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/IndependentBuffers/ShaderStorageBuffer/ShaderStorageBuffer.cs
@@ -7,10 +7,6 @@
     /// <typeparam name="T">此buffer存储的是哪种struct的数据？<para>type of index value.</para></typeparam>
     public class ShaderStorageBuffer<T> : IndependentBuffer<T> where T : struct
     {
-        internal static OpenGL.glGetUniformBlockIndex glGetUniformBlockIndex;
-        internal static OpenGL.glUniformBlockBinding glUniformBlockBinding;
-        internal static OpenGL.glBindBufferBase glBindBufferBase;
-
         /// <summary>
         ///
         /// </summary>
@@ -27,13 +23,6 @@
         /// <returns></returns>
         protected override IndependentBufferPtr Upload2GPU()
         {
-            if (glGetUniformBlockIndex == null)
-            {
-                glGetUniformBlockIndex = OpenGL.GetDelegateFor<OpenGL.glGetUniformBlockIndex>();
-                glUniformBlockBinding = OpenGL.GetDelegateFor<OpenGL.glUniformBlockBinding>();
-                glBindBufferBase = OpenGL.GetDelegateFor<OpenGL.glBindBufferBase>();
-            }
-
             uint[] buffers = new uint[1];
             glGenBuffers(1, buffers);
             const uint target = OpenGL.GL_SHADER_STORAGE_BUFFER;
