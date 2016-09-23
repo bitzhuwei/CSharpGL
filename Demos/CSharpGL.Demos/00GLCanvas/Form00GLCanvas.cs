6f19d1331d8a9e01717375aa643a0d2406d18f68
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-23 10:20:52 +0800

    update

--- Demos/CSharpGL.Demos/00GLCanvas/Form00GLCanvas.cs
+++ Demos/CSharpGL.Demos/00GLCanvas/Form00GLCanvas.cs
@@ -9,14 +9,10 @@ namespace CSharpGL.Demos
 {
     public partial class Form00GLCanvas : Form
     {
-        private Point mousePosition;
-        private List<string> content = new List<string>();
-
         public Form00GLCanvas()
         {
             InitializeComponent();
 
-            this.glCanvas1.MouseMove += glCanvas1_MouseMove;
             this.glCanvas1.KeyPress += glCanvas1_KeyPress;
 
             Application.Idle += Application_Idle;
@@ -47,35 +43,31 @@ namespace CSharpGL.Demos
             this.Text = string.Format("{0} - FPS: {1}", this.GetType().Name, this.glCanvas1.FPS.ToShortString());
         }
 
-        private void UpdateContent()
+        private List<string> UpdateContent()
         {
-            this.content.Clear();
+            var content = new List<string>();
             var builder = new StringBuilder();
             int[] maxTextureSize = new int[1];
             OpenGL.GetInteger(GetTarget.MaxTextureSize, maxTextureSize);
-            this.content.Add(string.Format("FPS: {0}", this.glCanvas1.FPS));
-            this.content.Add(string.Format("Max Texture Width: {0}", maxTextureSize[0]));
-            this.content.Add(string.Format("Max Color Attachments: {0}", Framebuffer.MaxColorAttachments()));
-            this.content.Add(string.Format("Max Framebuffer Width: {0}", Framebuffer.MaxFramebufferWidth()));
-            this.content.Add(string.Format("Max Framebuffer Height: {0}", Framebuffer.MaxFramebufferHeight()));
-            this.content.Add(string.Format("Max Framebuffer Layers: {0}", Framebuffer.MaxFramebufferLayers()));
-            this.content.Add(string.Format("Max Framebuffer Samples: {0}", Framebuffer.MaxFramebufferSamples()));
-            //this.content.Add(string.Format("Framebuffer Default Width: {0}", Framebuffer.DefaultWidth()));
-            //this.content.Add(string.Format("Framebuffer Default Height: {0}", Framebuffer.DefaultHeight()));
-            //this.content.Add(string.Format("Framebuffer Default Layers: {0}", Framebuffer.DefaultLayers()));
-            //this.content.Add(string.Format("Framebuffer Default Samples: {0}", Framebuffer.DefaultSamples()));
-            //this.content.Add(string.Format("Framebuffer Default Fixed Sample Locations: {0}", Framebuffer.DefaultFixedSampleLocations()));
-        }
-
-        private void glCanvas1_MouseMove(object sender, MouseEventArgs e)
-        {
-            this.mousePosition = e.Location;
+            content.Add(string.Format("FPS: {0}", this.glCanvas1.FPS.ToShortString()));
+            content.Add(string.Format("Max Texture Width: {0}", maxTextureSize[0]));
+            content.Add(string.Format("Max Color Attachments: {0}", Framebuffer.MaxColorAttachments()));
+            content.Add(string.Format("Max Framebuffer Width: {0}", Framebuffer.MaxFramebufferWidth()));
+            content.Add(string.Format("Max Framebuffer Height: {0}", Framebuffer.MaxFramebufferHeight()));
+            content.Add(string.Format("Max Framebuffer Layers: {0}", Framebuffer.MaxFramebufferLayers()));
+            content.Add(string.Format("Max Framebuffer Samples: {0}", Framebuffer.MaxFramebufferSamples()));
+            //content.Add(string.Format("Framebuffer Default Width: {0}", Framebuffer.DefaultWidth()));
+            //content.Add(string.Format("Framebuffer Default Height: {0}", Framebuffer.DefaultHeight()));
+            //content.Add(string.Format("Framebuffer Default Layers: {0}", Framebuffer.DefaultLayers()));
+            //content.Add(string.Format("Framebuffer Default Samples: {0}", Framebuffer.DefaultSamples()));
+            //content.Add(string.Format("Framebuffer Default Fixed Sample Locations: {0}", Framebuffer.DefaultFixedSampleLocations()));
+            return content;
         }
 
         private void glCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
         {
             // 天蓝色背景
-            OpenGL.ClearColor(0x87 / 255.0f, 0xce / 255.0f, 0xeb / 255.0f, 0xff / 255.0f);
+            //OpenGL.ClearColor(0x87 / 255.0f, 0xce / 255.0f, 0xeb / 255.0f, 0xff / 255.0f);
             OpenGL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_STENCIL_BUFFER_BIT);
 
             var result = new int[1];
@@ -83,17 +75,18 @@ namespace CSharpGL.Demos
             OpenGL.GetInteger(GetTarget.StencilBackWritemask, result);
             OpenGL.GetInteger(GetTarget.StencilWritemask, result);
             //OpenGL.StencilMask()
-            UpdateContent();
+            List<string> content = UpdateContent();
 
-            for (int i = 0; i < this.content.Count; i++)
+            for (int i = 0; i < content.Count; i++)
             {
                 OpenGL.DrawText(1, this.glCanvas1.Height - 1 - 14 * (i + 1),
                     Color.Red, "Courier New", 14.0f, content[i]);
             }
 
-            OpenGL.DrawText(this.mousePosition.X,
-                this.glCanvas1.Height - this.mousePosition.Y - 1, Color.Red, "Courier New",
-                14.0f, string.Format("Mouse Position: {0}", this.mousePosition));
+            Point mousePosition = this.glCanvas1.PointToClient(Control.MousePosition);
+            OpenGL.DrawText(mousePosition.X,
+                this.glCanvas1.Height - mousePosition.Y - 1, Color.Red, "Courier New",
+                14.0f, string.Format("Mouse Position: {0}", mousePosition));
         }
     }
 }
