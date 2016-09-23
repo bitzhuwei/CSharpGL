32cfe1d33330dd28d09cf128af1f24b0a452c3e4
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-23 13:42:02 +0800

    Revert "update"
    This reverts commit 7b8c29feb0e18864a6222e54a5a6e53ad19ae757.

--- CSharpGL/0Foundations/OpenGLObjects/Texture/ImageFillers/TexBufferImageFiller.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Texture/ImageFillers/TexBufferImageFiller.cs
@@ -1,9 +1,11 @@
-﻿namespace CSharpGL
+﻿using System;
+
+namespace CSharpGL
 {
     /// <summary>
     /// fill texture's content with a buffer.
     /// </summary>
-    public class TexBufferImageFiller : ImageFiller
+    public class TexBufferImageFiller : ImageFiller, IDisposable
     {
         private uint internalformat;
         private BufferPtr bufferPtr;
@@ -28,11 +30,52 @@
         public override void Fill()
         {
             OpenGL.GetDelegateFor<OpenGL.glTexBuffer>()(OpenGL.GL_TEXTURE_BUFFER, internalformat, bufferPtr.BufferId);
-            if (this.autoDispose)
-            {
-                bufferPtr.Dispose();
-                bufferPtr = null;
-            }
         }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        } // end sub
+
+        /// <summary>
+        /// Destruct instance of the class.
+        /// </summary>
+        ~TexBufferImageFiller()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// Backing field to track whether Dispose has been called.
+        /// </summary>
+        private bool disposedValue = false;
+
+        /// <summary>
+        /// Dispose managed and unmanaged resources of this instance.
+        /// </summary>
+        /// <param name="disposing">If disposing equals true, managed and unmanaged resources can be disposed. If disposing equals false, only unmanaged resources can be disposed. </param>
+        private void Dispose(bool disposing)
+        {
+            if (this.disposedValue == false)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                } // end if
+
+                // Dispose unmanaged resources.
+                if (this.autoDispose)
+                {
+                    var disp = this.bufferPtr as IDisposable;
+                    if (disp != null) { disp.Dispose(); }
+                }
+            } // end if
+
+            this.disposedValue = true;
+        } // end sub
     }
 }
