172c52e4977a1b2deaa716a2055b134e1a2f5f33
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-23 11:31:47 +0800

    update

--- Demos/CSharpGL.Demos/00GLCanvas/Form00GLCanvas.cs
+++ Demos/CSharpGL.Demos/00GLCanvas/Form00GLCanvas.cs
@@ -13,55 +13,12 @@ namespace CSharpGL.Demos
         {
             InitializeComponent();
 
-            this.glCanvas1.KeyPress += glCanvas1_KeyPress;
-
             Application.Idle += Application_Idle;
-        }
-
-        private void glCanvas1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == 't')
-            {
-                switch (this.glCanvas1.RenderTrigger)
-                {
-                    case RenderTrigger.TimerBased:
-                        this.glCanvas1.RenderTrigger = RenderTrigger.Manual;
-                        break;
-
-                    case RenderTrigger.Manual:
-                        this.glCanvas1.RenderTrigger = RenderTrigger.TimerBased;
-                        break;
-
-                    default:
-                        break;
-                }
-            }
         }
 
         private void Application_Idle(object sender, EventArgs e)
         {
             this.Text = string.Format("{0} - FPS: {1}", this.GetType().Name, this.glCanvas1.FPS.ToShortString());
-        }
-
-        private List<string> UpdateContent()
-        {
-            var content = new List<string>();
-            var builder = new StringBuilder();
-            int[] maxTextureSize = new int[1];
-            OpenGL.GetInteger(GetTarget.MaxTextureSize, maxTextureSize);
-            content.Add(string.Format("FPS: {0}", this.glCanvas1.FPS.ToShortString()));
-            content.Add(string.Format("Max Texture Width: {0}", maxTextureSize[0]));
-            content.Add(string.Format("Max Color Attachments: {0}", Framebuffer.MaxColorAttachments()));
-            content.Add(string.Format("Max Framebuffer Width: {0}", Framebuffer.MaxFramebufferWidth()));
-            content.Add(string.Format("Max Framebuffer Height: {0}", Framebuffer.MaxFramebufferHeight()));
-            content.Add(string.Format("Max Framebuffer Layers: {0}", Framebuffer.MaxFramebufferLayers()));
-            content.Add(string.Format("Max Framebuffer Samples: {0}", Framebuffer.MaxFramebufferSamples()));
-            //content.Add(string.Format("Framebuffer Default Width: {0}", Framebuffer.DefaultWidth()));
-            //content.Add(string.Format("Framebuffer Default Height: {0}", Framebuffer.DefaultHeight()));
-            //content.Add(string.Format("Framebuffer Default Layers: {0}", Framebuffer.DefaultLayers()));
-            //content.Add(string.Format("Framebuffer Default Samples: {0}", Framebuffer.DefaultSamples()));
-            //content.Add(string.Format("Framebuffer Default Fixed Sample Locations: {0}", Framebuffer.DefaultFixedSampleLocations()));
-            return content;
         }
 
         private void glCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
@@ -87,6 +44,27 @@ namespace CSharpGL.Demos
             OpenGL.DrawText(mousePosition.X,
                 this.glCanvas1.Height - mousePosition.Y - 1, Color.Red, "Courier New",
                 14.0f, string.Format("Mouse Position: {0}", mousePosition));
+        }
+
+        private List<string> UpdateContent()
+        {
+            var content = new List<string>();
+            var builder = new StringBuilder();
+            int[] maxTextureSize = new int[1];
+            OpenGL.GetInteger(GetTarget.MaxTextureSize, maxTextureSize);
+            content.Add(string.Format("FPS: {0}", this.glCanvas1.FPS.ToShortString()));
+            content.Add(string.Format("Max Texture Width: {0}", maxTextureSize[0]));
+            content.Add(string.Format("Max Color Attachments: {0}", Framebuffer.MaxColorAttachments()));
+            content.Add(string.Format("Max Framebuffer Width: {0}", Framebuffer.MaxFramebufferWidth()));
+            content.Add(string.Format("Max Framebuffer Height: {0}", Framebuffer.MaxFramebufferHeight()));
+            content.Add(string.Format("Max Framebuffer Layers: {0}", Framebuffer.MaxFramebufferLayers()));
+            content.Add(string.Format("Max Framebuffer Samples: {0}", Framebuffer.MaxFramebufferSamples()));
+            //content.Add(string.Format("Framebuffer Default Width: {0}", Framebuffer.DefaultWidth()));
+            //content.Add(string.Format("Framebuffer Default Height: {0}", Framebuffer.DefaultHeight()));
+            //content.Add(string.Format("Framebuffer Default Layers: {0}", Framebuffer.DefaultLayers()));
+            //content.Add(string.Format("Framebuffer Default Samples: {0}", Framebuffer.DefaultSamples()));
+            //content.Add(string.Format("Framebuffer Default Fixed Sample Locations: {0}", Framebuffer.DefaultFixedSampleLocations()));
+            return content;
         }
     }
 }
