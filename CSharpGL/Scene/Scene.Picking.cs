4cedf519974e72c5a7d29c461d9d14608d7dd1d3
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-24 00:23:45 +0800

    update

---
+++ CSharpGL/Scene/Scene.Picking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace CSharpGL
+{
+    public partial class Scene
+    {
+        /// <summary>
+        /// Pick
+        /// </summary>
+        /// <param name="clientRectangle">viewport.</param>
+        /// <param name="mousePosition">mouse position.</param>
+        /// <param name="pickingGeometryType">target's geometry type.</param>
+        /// <returns></returns>
+        public PickedGeometry ColorCodedPicking(Rectangle clientRectangle, Point mousePosition, GeometryType pickingGeometryType)
+        {
+            PickedGeometry result = null;
+            lock (this.synObj)
+            {
+                var renderers = (from item in this.RootObject
+                                 where (item != null && item.Enabled && item.Renderer is IColorCodedPicking && item.Renderer.Enabled)
+                                 select item.Renderer as IColorCodedPicking).ToArray();
+                result = CSharpGL.ColorCodedPicking.Pick(new RenderEventArgs(
+                         RenderModes.ColorCodedPicking, clientRectangle, this.Camera, pickingGeometryType),
+                         mousePosition.X, mousePosition.Y, renderers);
+            }
+
+            return result;
+        }
+    }
+}
--- CSharpGL/Scene/Scene.Rendering.cs
+++ CSharpGL/Scene/Scene.Rendering.cs
@@ -8,29 +8,6 @@ namespace CSharpGL
     {
         private object synObj = new object();
 
-        /// <summary>
-        /// Pick
-        /// </summary>
-        /// <param name="clientRectangle">viewport.</param>
-        /// <param name="mousePosition">mouse position.</param>
-        /// <param name="pickingGeometryType">target's geometry type.</param>
-        /// <returns></returns>
-        public PickedGeometry ColorCodedPicking(Rectangle clientRectangle, Point mousePosition, GeometryType pickingGeometryType)
-        {
-            PickedGeometry result = null;
-            lock (this.synObj)
-            {
-                var renderers = (from item in this.RootObject
-                                 where (item != null && item.Enabled && item.Renderer is IColorCodedPicking && item.Renderer.Enabled)
-                                 select item.Renderer as IColorCodedPicking).ToArray();
-                result = CSharpGL.ColorCodedPicking.Pick(new RenderEventArgs(
-                         RenderModes.ColorCodedPicking, clientRectangle, this.Camera, pickingGeometryType),
-                         mousePosition.X, mousePosition.Y, renderers);
-            }
-
-            return result;
-        }
-
         // <param name="mousePosition">mouse position in window coordinate system.</param>
         /// <summary>
         ///
