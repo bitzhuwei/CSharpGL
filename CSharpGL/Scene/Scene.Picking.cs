9459a3f79339865f13d4e9d16e9d392a324fddbc
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-25 01:01:48 +0800

    update

--- CSharpGL/Scene/Scene.Picking.cs
+++ CSharpGL/Scene/Scene.Picking.cs
@@ -33,14 +33,14 @@ namespace CSharpGL
                 {
                     int x = tuple.Item1.X;
                     int y = tuple.Item1.Y;
-                    if (x < 0 || arg.CanvasRect.Width <= x || y < 0 || arg.CanvasRect.Height <= y) { continue; }
+                    //if (x < 0 || clientRectangle.Width <= x || y < 0 || clientRectangle.Height <= y) { continue; }
 
                     uint stageVertexId = tuple.Item2;
                     PickedGeometry pickedGeometry = GetPickGeometry(arg,
                         x, y, stageVertexId, pickedRendererList);
                     if (pickedGeometry != null)
                     {
-                        result.Add(new Tuple<Point, PickedGeometry>(new Point(x, y), pickedGeometry));
+                        result.Add(new Tuple<Point, PickedGeometry>(tuple.Item1, pickedGeometry));
                     }
                 }
 
@@ -48,53 +48,6 @@ namespace CSharpGL
             }
 
             return null;
-        }
-
-        /// <summary>
-        /// get picked geometry.
-        /// </summary>
-        /// <param name="arg"></param>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="stageVertexId"></param>
-        /// <param name="pickableRendererList"></param>
-        /// <returns></returns>
-        private static PickedGeometry GetPickGeometry(RenderEventArgs arg,
-            int x, int y,
-            uint stageVertexId,
-            List<IColorCodedPicking> pickableRendererList)
-        {
-            PickedGeometry pickedGeometry = null;
-            foreach (var item in pickableRendererList)
-            {
-                pickedGeometry = item.GetPickedGeometry(arg, stageVertexId, x, y);
-
-                if (pickedGeometry != null) { break; }
-            }
-
-            return pickedGeometry;
-        }
-
-        private List<IColorCodedPicking> Render4Picking(RenderEventArgs arg)
-        {
-            // record clear color
-            var originalClearColor = new float[4];
-            OpenGL.GetFloat(GetTarget.ColorClearValue, originalClearColor);
-
-            // white color means nothing picked.
-            OpenGL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-            OpenGL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_STENCIL_BUFFER_BIT);
-
-            // restore clear color
-            OpenGL.ClearColor(originalClearColor[0], originalClearColor[1], originalClearColor[2], originalClearColor[3]);
-
-            uint renderedVertexCount = 0;
-            var pickedRendererList = new List<IColorCodedPicking>();
-            RenderPickableObject(this.rootObject, arg, ref renderedVertexCount, pickedRendererList);
-
-            OpenGL.Flush();
-
-            return pickedRendererList;
         }
 
         private static unsafe bool PickedSomething(Rectangle rect, Rectangle rectangle)
@@ -119,6 +72,28 @@ namespace CSharpGL
             }
 
             return result;
+        }
+
+        private List<IColorCodedPicking> Render4Picking(RenderEventArgs arg)
+        {
+            // record clear color
+            var originalClearColor = new float[4];
+            OpenGL.GetFloat(GetTarget.ColorClearValue, originalClearColor);
+
+            // white color means nothing picked.
+            OpenGL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            OpenGL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_STENCIL_BUFFER_BIT);
+
+            // restore clear color
+            OpenGL.ClearColor(originalClearColor[0], originalClearColor[1], originalClearColor[2], originalClearColor[3]);
+
+            uint renderedVertexCount = 0;
+            var pickedRendererList = new List<IColorCodedPicking>();
+            RenderPickableObject(this.rootObject, arg, ref renderedVertexCount, pickedRendererList);
+
+            OpenGL.Flush();
+
+            return pickedRendererList;
         }
 
         private void RenderPickableObject(SceneObject sceneObject, RenderEventArgs arg, ref  uint renderedVertexCount, List<IColorCodedPicking> pickedRendererList)
@@ -167,29 +142,53 @@ namespace CSharpGL
         }
 
         /// <summary>
+        /// get picked geometry.
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <param name="x">mouse position</param>
+        /// <param name="y">mouse position</param>
+        /// <param name="stageVertexId"></param>
+        /// <param name="pickableRendererList"></param>
+        /// <returns></returns>
+        private static PickedGeometry GetPickGeometry(RenderEventArgs arg,
+            int x, int y,
+            uint stageVertexId,
+            List<IColorCodedPicking> pickableRendererList)
+        {
+            PickedGeometry pickedGeometry = null;
+            foreach (var item in pickableRendererList)
+            {
+                pickedGeometry = item.GetPickedGeometry(arg, stageVertexId, x, y);
+
+                if (pickedGeometry != null) { break; }
+            }
+
+            return pickedGeometry;
+        }
+
+        /// <summary>
         /// <para>Read pixels in specified rect and get the VertexIds they represent.</para>
         /// </summary>
-        /// <param name="rect"></param>
+        /// <param name="target"></param>
         /// <param name="canvasHeight"></param>
         /// <returns></returns>
-        public static unsafe List<Tuple<Point, uint>> ReadPixels(
-            Rectangle rect, int canvasHeight)
+        private static unsafe List<Tuple<Point, uint>> ReadPixels(
+            Rectangle target, int canvasHeight)
         {
             var result = new List<Tuple<Point, uint>>();
-            if (rect.Width <= 0 || rect.Height <= 0) { return result; }
 
             // get coded color.
-            using (var codedColor = new UnmanagedArray<Pixel>(rect.Width * rect.Height))
+            using (var codedColor = new UnmanagedArray<Pixel>(target.Width * target.Height))
             {
-                OpenGL.ReadPixels(rect.X, canvasHeight - rect.Y - 1, rect.Width, rect.Height,
+                OpenGL.ReadPixels(target.X, canvasHeight - target.Y - 1, target.Width, target.Height,
                     OpenGL.GL_RGBA, OpenGL.GL_UNSIGNED_BYTE, codedColor.Header);
 
                 var array = (Pixel*)codedColor.Header.ToPointer();
                 int index = 0;
                 var vertexIdList = new List<uint>();
-                for (int yOffset = rect.Height - 1; yOffset >= 0; yOffset--)
+                for (int yOffset = target.Height - 1; yOffset >= 0; yOffset--)
                 {
-                    for (int xOffset = 0; xOffset < rect.Width; xOffset++)
+                    for (int xOffset = 0; xOffset < target.Width; xOffset++)
                     {
                         Pixel pixel = array[index++];
                         // This is when (x, y) is not on background and some primitive is picked.
@@ -215,7 +214,7 @@ namespace CSharpGL
                             if (!vertexIdList.Contains(vertexId))
                             {
                                 result.Add(new Tuple<Point, uint>(
-                                    new Point(rect.X + xOffset, rect.Y + yOffset), vertexId));
+                                    new Point(target.X + xOffset, target.Y + yOffset), vertexId));
                                 vertexIdList.Add(vertexId);
                             }
                         }
