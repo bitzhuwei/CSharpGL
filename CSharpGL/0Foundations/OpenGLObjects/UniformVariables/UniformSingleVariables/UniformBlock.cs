006717583552484a0fd244e5b8699d484a7a8db1
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-26 02:24:56 +0800

    update

--- CSharpGL/0Foundations/OpenGLObjects/UniformVariables/UniformSingleVariables/UniformBlock.cs
+++ CSharpGL/0Foundations/OpenGLObjects/UniformVariables/UniformSingleVariables/UniformBlock.cs
@@ -12,33 +12,12 @@ namespace CSharpGL
     /// A uiform block in shader.
     /// <para>https://www.opengl.org/registry/specs/ARB/uniform_buffer_object.txt</para>
     /// </summary>
-    public class UniformBlock<T> : UniformSingleVariableBase where T : struct, IEquatable<T>
+    public class UniformBlock<T> : UniformSingleVariable<T> where T : struct, IEquatable<T>
     {
         internal static OpenGL.glGetUniformBlockIndex glGetUniformBlockIndex;
         internal static OpenGL.glGetActiveUniformBlockiv glGetActiveUniformBlockiv;
         //internal static OpenGL.glUniformBlockBinding glUniformBlockBinding;
         internal static OpenGL.glBindBufferBase glBindBufferBase;
-
-        /// <summary>
-        ///
-        /// </summary>
-        protected T value;
-
-        /// <summary>
-        /// Don't rename this property because its used in Renderer.GetVariable&lt;T&gt;(T value, string varNameInShader).
-        /// </summary>
-        public T Value
-        {
-            get { return this.value; }
-            set
-            {
-                if (!value.Equals(this.value))
-                {
-                    this.value = value;
-                    this.Updated = true;
-                }
-            }
-        }
 
         /// <summary>
         /// A uiform block in shader.
@@ -52,15 +31,6 @@ namespace CSharpGL
         /// <param name="blockName"></param>
         /// <param name="value"></param>
         public UniformBlock(string blockName, T value) : base(blockName) { this.Value = value; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return string.Format("{0} {1}: [{2}]", this.GetType().Name, this.VarName, this.value);
-        }
 
         /// <summary>
         ///
