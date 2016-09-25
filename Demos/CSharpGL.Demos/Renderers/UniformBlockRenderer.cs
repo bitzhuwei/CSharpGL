d4df141d61a6859b19ff931114f4221a0a96fd54
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-25 22:03:13 +0800

    update

---
+++ Demos/CSharpGL.Demos/Renderers/UniformBlockRenderer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace CSharpGL.Demos
+{
+    /// <summary>
+    /// Demo of how to use uniform block and uniform buffer object.
+    /// </summary>
+    [DemoRenderer]
+    internal class UniformBlockRenderer : Renderer
+    {
+        public static UniformBlockRenderer Create()
+        {
+            var model = new Teapot();
+            var shaderCodes = new ShaderCode[2];
+            shaderCodes[0] = new ShaderCode(File.ReadAllText(@"shaders\UniformBlockRenderer\UniformBlock.vert"), ShaderType.VertexShader);
+            shaderCodes[1] = new ShaderCode(File.ReadAllText(@"shaders\UniformBlockRenderer\UniformBlock.frag"), ShaderType.FragmentShader);
+            var map = new AttributeNameMap();
+            map.Add("vPos", Teapot.strPosition);
+            map.Add("vColor", Teapot.strColor);
+            var renderer = new UniformBlockRenderer(model, shaderCodes, map);
+            return renderer;
+        }
+
+        private UniformBlockRenderer(IBufferable model, ShaderCode[] shaderCodes,
+            AttributeNameMap attributeNameMap, params GLSwitch[] switches)
+            : base(model, shaderCodes, attributeNameMap, switches)
+        {
+        }
+
+        protected override void DoInitialize()
+        {
+            base.DoInitialize();
+        }
+
+        protected override void DoRender(RenderEventArgs arg)
+        {
+            mat4 projection = arg.Camera.GetProjectionMatrix();
+            mat4 view = arg.Camera.GetViewMatrix();
+            this.SetUniform("projectionMatrix", projection);
+            this.SetUniform("viewMatrix", view);
+            this.SetUniformBlock("Uniforms", new Uniforms(this.WorldPosition, this.Scale.x));
+
+            base.DoRender(arg);
+        }
+
+        struct Uniforms : IEquatable<Uniforms>
+        {
+            public vec3 translation;
+            public float scale;
+
+            public Uniforms(vec3 translation, float scale)
+            {
+                this.translation = translation;
+                this.scale = scale;
+            }
+
+            public bool Equals(Uniforms other)
+            {
+                return this.translation == other.translation && this.scale == other.scale;
+            }
+        }
+    }
+}
---
+++ Demos/CSharpGL.Demos/shaders/UniformBlockRenderer/UniformBlock.vert
@@ -0,0 +1,26 @@
+﻿#version 330 core
+
+// vert and frag shader share a block of uniforms named 'Uniforms'
+uniform Uniforms {
+    vec3 translation;
+	float scale;
+};
+
+in vec2 vPos;
+in vec3 vColor;
+out vec3 fColor;
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+//uniform mat4 modelMatrix;
+
+void main(void) {
+ 
+    mat4 modelMatrix = mat4(1.0f);
+	modelMatrix[3].xyz = translation;
+	modelMatrix[0].x = scale;
+	modelMatrix[1].y = scale;
+	modelMatrix[2].z = scale;
+	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vPos, 1.0);
+
+	fColor = vColor;
+}
