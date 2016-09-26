c2bca56ca9c82da96bb8f8c96d764f2eb6c63ff9
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-26 20:20:50 +0800

    DTMModel

---
+++ Demos/ArmadaTank/DTMModel.cs
@@ -0,0 +1,36 @@
+﻿using CSharpGL;
+using System;
+
+namespace ArmadaTank
+{
+    /// <summary>
+    /// Represents a model in a *.dtm file.
+    /// </summary>
+    internal class DTMModel : IBufferable
+    {
+        /// <summary>
+        /// Load model from dtm file.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static DTMModel Load(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VertexAttributeBufferPtr GetVertexAttributeBufferPtr(string bufferName, string varNameInShader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IndexBufferPtr GetIndexBufferPtr()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UsesZeroIndexBuffer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
