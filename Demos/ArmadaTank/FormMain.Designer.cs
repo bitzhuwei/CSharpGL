b2132fea1678d5904042cbb5c81dd50df384456a
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-26 20:12:33 +0800

    armada tank

---
+++ Demos/ArmadaTank/FormMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ArmadaTank
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glCanvas1 = new CSharpGL.GLCanvas();
+            ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glCanvas1
+            // 
+            this.glCanvas1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glCanvas1.Location = new System.Drawing.Point(0, 0);
+            this.glCanvas1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glCanvas1.Name = "glCanvas1";
+            this.glCanvas1.OpenGLVersion = CSharpGL.GLVersion.OpenGL2_1;
+            this.glCanvas1.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
+            this.glCanvas1.Size = new System.Drawing.Size(751, 551);
+            this.glCanvas1.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 551);
+            this.Controls.Add(this.glCanvas1);
+            this.Name = "FormMain";
+            this.Text = "Armada Tank";
+            ((System.ComponentModel.ISupportInitialize)(this.glCanvas1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CSharpGL.GLCanvas glCanvas1;
+    }
+}
+
---
+++ Demos/ArmadaTank/FormMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSharpGL;
+
+namespace ArmadaTank
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+            Application.Idle += Application_Idle;
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            this.Text = string.Format("Armada Tank FPS: {0}", this.glCanvas1.FPS.ToShortString());
+        }
+    }
+}
