1c050751439e63f85ac5d06d456d3be6e998022f
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-27 00:39:40 +0800

    OBJFile project is a *.obj file parser.

---
+++ Infrastructure/CSharpGL.OBJFile/OBJModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpGL.OBJFile
+{
+    public class OBJModel
+    {
+        public static OBJModel Load(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
