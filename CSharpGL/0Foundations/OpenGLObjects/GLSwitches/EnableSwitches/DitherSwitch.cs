b408d7cb303c6798ec9d1b28321e3fc3fb4937e4
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-28 22:37:11 +0800

    DitherSwitch

---
+++ CSharpGL/0Foundations/OpenGLObjects/GLSwitches/EnableSwitches/DitherSwitch.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpGL
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class DitherSwitch : EnableSwitch
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public DitherSwitch()
+            : base(OpenGL.GL_DITHER, true)
+        { }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="enableCapacity">true for enable, false for disable</param>
+        public DitherSwitch(bool enableCapacity)
+            : base(OpenGL.GL_DITHER, enableCapacity)
+        { }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (this.EnableCapacity)
+            { return "OpenGL.Enable(GL_DITHER);"; }
+            else
+            { return "OpenGL.Disable(GL_DITHER);"; }
+        }
+    }
+}
