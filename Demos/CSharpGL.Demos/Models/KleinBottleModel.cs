1ad1d8d2605ed043a3c70760518ecc65e839228a
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-28 13:40:19 +0800

    update

--- Demos/CSharpGL.Demos/Models/KleinBottleModel.cs
+++ Demos/CSharpGL.Demos/Models/KleinBottleModel.cs
@@ -40,6 +40,9 @@ namespace CSharpGL.Demos
             using (var buffer = new VertexAttributeBuffer<vec3>(
                 varNameInShader, VertexAttributeConfig.Vec3, BufferUsage.StaticDraw))
             {
+                bool initialized = false;
+                vec3 max = new vec3();
+                vec3 min = new vec3();
                 int uCount = (int)(Math.PI / interval);
                 int vCount = (int)(Math.PI * 2 / interval);
                 buffer.Create(uCount * vCount);
@@ -47,17 +50,40 @@ namespace CSharpGL.Demos
                 {
                     int index = 0;
                     var array = (vec3*)buffer.Header.ToPointer();
-                    for (double u = 0; u < Math.PI; u += interval)
+                    for (int uIndex = 0; uIndex < uCount; uIndex++)
                     {
-                        for (double v = 0; v < Math.PI * 2; v += interval)
+                        for (int vIndex = 0; vIndex < vCount; vIndex++)
                         {
-                            double sinU = Math.Sin(u), cosU = Math.Cos(u);
-                            double sinV = Math.Sin(v), cosV = Math.Cos(v);
-                            double x = -2.0 / 15.0 * cosU * (3 * cosV - 30 * sinU + 90 * Math.Pow(cosU, 4) * sinU - 60 * Math.Pow(cosU, 6) * sinU + 5 * cosU * cosV * sinU);
-                            double y = -1.0 / 15.0 * sinU * (3 * cosV - 3 * Math.Pow(cosU, 2) * cosV - 48 * Math.Pow(cosU, 4) * cosV + 48 * Math.Pow(cosU, 6) * cosV - 60 * sinU + 5 * cosU * cosV * sinU - 5 * Math.Pow(cosU, 3) * cosV * sinU - 80 * Math.Pow(cosU, 5) * cosV * sinU + 80 * Math.Pow(cosU, 7) * cosV * sinU);
-                            double z = 2.0 / 15.0 * (3.0 + 5 * cosU * sinU) * sinV;
-                            array[index++] = new vec3((float)x, (float)y, (float)z);
+                            if (index >= 49298)
+                            {
+                                Console.WriteLine("asdf");
+                            }
+                            else
+                            {
+                                double u = interval * uIndex / uCount * Math.PI;
+                                double v = interval * vIndex / vCount * Math.PI * 2;
+                                var position = GetPosition(u, v);
+
+                                if (!initialized)
+                                {
+                                    max = position;
+                                    min = position;
+                                    initialized = true;
+                                }
+                                else
+                                {
+                                    position.UpdateMax(ref max);
+                                    position.UpdateMin(ref min);
+                                }
+                                array[index++] = position;
+                            }
                         }
+                    }
+                    this.Lengths = max - min;
+                    vec3 worldPosition = max / 2.0f + min / 2.0f;
+                    for (int i = 0; i < index; i++)
+                    {
+                        array[i] = array[i] - worldPosition;
                     }
                 }
 
@@ -65,6 +91,19 @@ namespace CSharpGL.Demos
 
                 return positionBufferPtr;
             }
+        }
+
+        public vec3 Lengths { get; private set; }
+
+        public vec3 GetPosition(double u, double v)
+        {
+            double sinU = Math.Sin(u), cosU = Math.Cos(u);
+            double sinV = Math.Sin(v), cosV = Math.Cos(v);
+            double x = -2.0 * cosU * (3 * cosV - 30 * sinU + 90 * Math.Pow(cosU, 4) * sinU - 60 * Math.Pow(cosU, 6) * sinU + 5 * cosU * cosV * sinU);
+            double y = -1.0 * sinU * (3 * cosV - 3 * Math.Pow(cosU, 2) * cosV - 48 * Math.Pow(cosU, 4) * cosV + 48 * Math.Pow(cosU, 6) * cosV - 60 * sinU + 5 * cosU * cosV * sinU - 5 * Math.Pow(cosU, 3) * cosV * sinU - 80 * Math.Pow(cosU, 5) * cosV * sinU + 80 * Math.Pow(cosU, 7) * cosV * sinU);
+            double z = 2.0 * (3.0 + 5 * cosU * sinU) * sinV;
+
+            return new vec3((float)x, (float)y, (float)z);
         }
 
         public IndexBufferPtr GetIndexBufferPtr()
