870910adb22c63d876d7cdbaf13ef8b40d7a74c5
祝威 <bitzhuwei.cnblogs@qq.com>
2016-09-30 20:25:45 +0800

    update

--- CSharpGL/0Foundations/OpenGLObjects/Shader/ShaderProgram.SetUniforms.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Shader/ShaderProgram.SetUniforms.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
+﻿using System.ComponentModel;
 using System.Drawing.Design;
-using System.Text;
 
 namespace CSharpGL
 {
