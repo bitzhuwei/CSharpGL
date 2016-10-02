6eb89fb469dee3baf19fddc069c683e5a45650a1
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-02 18:59:10 +0800

    update

--- CSharpGL/Renderers/PickableRenderer/ColorCodedPicking/PrimivitiveRecognizers/RecognizedPrimitiveInfo.cs
+++ CSharpGL/Renderers/PickableRenderer/ColorCodedPicking/PrimivitiveRecognizers/RecognizedPrimitiveInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace CSharpGL
@@ -13,31 +14,35 @@ namespace CSharpGL
         /// <summary>
         /// All vertexs' ids of picked geometry and their indexes in <see cref="IndexBuffer"/>.
         /// </summary>
-        /// <param name="lastVertexId">last vertex' id of picked geometry.</param>
-        /// <param name="index">index of <paramref name="lastVertexId"/> in the <see cref="IndexBuffer"/>.</param>
-        /// <param name="vertexIds">All vertexs' ids of picked geometry.
-        /// <para>Note: <paramref name="vertexIds"/>.Last() must equal to <paramref name="lastVertexId"/>.</para></param>
-        public RecognizedPrimitiveInfo(uint lastVertexId, uint index, params uint[] vertexIds)
+        /// <param name="index">index of LastVertexId in the <see cref="IndexBuffer"/>.</param>
+        /// <param name="vertexIds">All vertexs' ids of picked geometry.</param>
+        public RecognizedPrimitiveInfo(uint index, params uint[] vertexIds)
         {
-            this.LastVertexId = lastVertexId;
+            if (vertexIds.Length < 1) { throw new Exception("This should not happen!"); }
+
             this.index = index;
-            this.VertexIdList = new List<uint>();
-            this.VertexIdList.AddRange(vertexIds);
+            this.VertexIdList = vertexIds;
         }
 
-        public uint LastVertexId { get; set; }
+        /// <summary>
+        /// last vertex' id of picked geometry.
+        /// </summary>
+        public uint LastVertexId { get { return this.VertexIdList[this.VertexIdList.Length - 1]; } }
 
-        public List<uint> VertexIdList { get; set; }
+        /// <summary>
+        /// All vertexs' ids of picked geometry.
+        /// </summary>
+        public uint[] VertexIdList { get; private set; }
 
         public override string ToString()
         {
             StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < this.VertexIdList.Count - 1; i++)
+            for (int i = 0; i < this.VertexIdList.Length - 1; i++)
             {
                 builder.Append(this.VertexIdList[i]); builder.Append(", ");
             }
 
-            builder.Append(this.VertexIdList[this.VertexIdList.Count - 1]);
+            builder.Append(this.VertexIdList[this.VertexIdList.Length - 1]);
             builder.AppendFormat(" | index buffer[{0}] is <{1}>", index, LastVertexId);
 
             return builder.ToString();
