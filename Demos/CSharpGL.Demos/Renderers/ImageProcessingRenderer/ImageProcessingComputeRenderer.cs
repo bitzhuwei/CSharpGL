70e28bde88f17085560ff568d5ec8a2b1dc8cd33
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-02 14:05:34 +0800

    update

--- Demos/CSharpGL.Demos/Renderers/ImageProcessingRenderer/ImageProcessingComputeRenderer.cs
+++ Demos/CSharpGL.Demos/Renderers/ImageProcessingRenderer/ImageProcessingComputeRenderer.cs
@@ -36,14 +36,16 @@ namespace CSharpGL.Demos
             OpenGL.BindImageTexture((uint)computeProgram.GetUniformLocation("output_image"), this.innerRenderer.IntermediateTexture.Id, 0, false, 0, OpenGL.GL_READ_WRITE, OpenGL.GL_RGBA32F);
             // Dispatch
             OpenGL.GetDelegateFor<OpenGL.glDispatchCompute>()(1, 512, 1);
-
             OpenGL.GetDelegateFor<OpenGL.glMemoryBarrier>()(OpenGL.GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+            computeProgram.Unbind();
 
+            computeProgram.Bind();
             OpenGL.BindImageTexture((uint)computeProgram.GetUniformLocation("input_image"), this.innerRenderer.IntermediateTexture.Id, 0, false, 0, OpenGL.GL_READ_WRITE, OpenGL.GL_RGBA32F);
             OpenGL.BindImageTexture((uint)computeProgram.GetUniformLocation("output_image"), this.innerRenderer.OutputTexture.Id, 0, false, 0, OpenGL.GL_READ_WRITE, OpenGL.GL_RGBA32F);
             // Dispatch
             OpenGL.GetDelegateFor<OpenGL.glDispatchCompute>()(1, 512, 1);
             OpenGL.GetDelegateFor<OpenGL.glMemoryBarrier>()(OpenGL.GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+            computeProgram.Unbind();
         }
     }
 }
