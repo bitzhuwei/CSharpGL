92e82ec4169001ea820ca1857ff29aadbabf8f9c
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-17 01:41:39 +0800

    cascade GetModelMatrix

--- CSharpGL/0Foundations/OpenGLObjects/Rendering/RendererBase.IModelSpace.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Rendering/RendererBase.IModelSpace.cs
@@ -107,7 +107,7 @@ namespace CSharpGL
         #endregion IModelSpace
 
         private MarkableStruct<mat4> modelMatrix = new MarkableStruct<mat4>(mat4.identity());
-        private mat4 cascadeModelMatrix = mat4.identity();
+        //private mat4 cascadeModelMatrix = mat4.identity();
 
         /// <summary>
         /// Should this model matrix be updated?
@@ -143,16 +143,52 @@ namespace CSharpGL
         //    return result;
         //}
 
+        private long parentModelMatrixTicks;
+
         /// <summary>
         /// Get model matrix that transform model from model space to world space.
         /// </summary>
         /// <returns></returns>
         public MarkableStruct<mat4> GetModelMatrix()
         {
-            if (this.modelMatrixRecord.IsMarked())
+            if (this.modelMatrixRecord.IsMarked())// this model matrix is updated.
             {
-                this.modelMatrix.Value = IModelSpaceHelper.GetModelMatrix(this);
+                mat4 thisModel = IModelSpaceHelper.GetModelMatrix(this);
                 this.modelMatrixRecord.CancelMark();
+                // cascade parent's model matrix.
+                SceneObject obj = this.BindingSceneObject;
+                if (obj != null)
+                {
+                    RendererBase renderer = obj.Renderer;
+                    if (renderer != null)
+                    {
+                        // this requires that parent scene object be rendered first, then chidren.
+                        thisModel = renderer.modelMatrix.Value * thisModel;
+                    }
+                }
+                // total model matrix.
+                this.modelMatrix.Value = thisModel;
+            }
+            else // this model matrix is not updated.
+            {
+                SceneObject obj = this.BindingSceneObject;
+                if (obj != null)
+                {
+                    SceneObject parent = obj.Parent;
+                    if (parent != null)
+                    {
+                        RendererBase parentRenderer = parent.Renderer;
+                        if (parentRenderer != null)
+                        {
+                            long ticks = parentRenderer.modelMatrix.UpdateTicks;
+                            if (this.parentModelMatrixTicks != ticks) // parent's model matrix is updated.
+                            {
+                                this.modelMatrix.Value = parentRenderer.modelMatrix.Value * this.modelMatrix.Value;
+                                this.parentModelMatrixTicks = ticks;
+                            }
+                        }
+                    }
+                }
             }
 
             return this.modelMatrix;
