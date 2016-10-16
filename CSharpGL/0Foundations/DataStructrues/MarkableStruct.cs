b06882fd40e02be50c180351b30b8dfb31ef731d
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-16 16:55:23 +0800

    MarkableStruct

---
+++ CSharpGL/0Foundations/DataStructrues/MarkableStruct.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Keeps a struct value and records the time when is it's udpated.
+    /// </summary>
+    public class MarkableStruct<T> where T : struct,IEquatable<T>
+    {
+        private T value;
+
+        public T Value
+        {
+            get { return value; }
+            set
+            {
+                if (!value.Equals(this.value))
+                {
+                    this.value = value;
+                    this.UpdateTicks = DateTime.Now.Ticks;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The last time when the value is updated.
+        /// </summary>
+        public long UpdateTicks { get; private set; }
+
+        /// <summary>
+        /// Records time when is a property is updated and uploaded.
+        /// </summary>
+        /// <param name="value">value.</param>
+        public MarkableStruct(T value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("[{0}]({1})", this.value, this.UpdateTicks);
+        }
+    }
+}
