714eb7e0c9cba18ea0796fea7ada60a00fd0c0a2
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-22 02:45:15 +0800

    update

---
+++ CSharpGL/0Foundations/Utilities/Sorting/SortingHelper.Comparer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Helper class for sorting unmanaged array.
+    /// </summary>
+    public static partial class SortingHelper
+    {
+        //static void Test()
+        //{
+        //    IntPtr pointer = IntPtr.Zero;// map, new, etc...
+        //    unsafe
+        //    {
+        //        var array = (vec3*)pointer.ToPointer();
+        //        int start = 0;
+        //        int length = 100;
+        //        Func<vec3, vec3, int> comparer = null;
+        //        Sort(pointer, start, length, comparer);
+        //    }
+        //}
+
+        /// <summary>
+        /// Sort unmanaged array specified with <paramref name="array"/> at specified area.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="start">index of first value to be sorted.</param>
+        /// <param name="length">length of <paramref name="array"/> to bo sorted.</param>
+        /// <param name="comparer">
+        /// If you want descending sort, make it returns -1 when <paramref name="array"/>[left] &lt; <paramref name="array"/>[right].
+        /// <para>Otherwise, make it returns -1 when <paramref name="array"/>[left] &gt; <paramref name="array"/>[right].</para></param>
+        public static void Sort<T>(this UnmanagedArray<T> array, int start, int length, Func<T, T, int> comparer) where T : struct
+        {
+            QuickSort(array, start, start + length - 1, comparer);
+        }
+
+        private static void QuickSort<T>(UnmanagedArray<T> array, int start, int end, Func<T, T, int> comparer) where T : struct
+        {
+            if (start >= end) { return; }
+
+            var stack = new Stack<int>();
+            stack.Push(end);
+            stack.Push(start);
+            QuickSort(array, comparer, stack);
+        }
+
+        private static void QuickSort<T>(UnmanagedArray<T> array, Func<T, T, int> comparer, Stack<int> stack) where T : struct
+        {
+            IntPtr pointer = array.Header;
+            Type type = typeof(T);
+            int elementSize = Marshal.SizeOf(type);
+
+            while (stack.Count > 0)
+            {
+                int start = stack.Pop();
+                int end = stack.Pop();
+                int index = QuickSortPartion(pointer, start, end, comparer, type, elementSize);
+                if (start < index - 1)
+                {
+                    stack.Push(index - 1); stack.Push(start);
+                }
+                if (index + 1 < end)
+                {
+                    stack.Push(end); stack.Push(index + 1);
+                }
+            }
+        }
+
+        private static int QuickSortPartion<T>(IntPtr pointer, int start, int end, Func<T, T, int> comparer, Type type, int elementSize) where T : struct
+        {
+            IntPtr pivotIndex, startIndex, endIndex;
+            T pivot, startValue, endValue;
+            pivotIndex = new IntPtr((int)pointer + start * elementSize);
+            pivot = (T)Marshal.PtrToStructure(pivotIndex, type);
+            while (start < end)
+            {
+                startIndex = new IntPtr((int)pointer + start * elementSize);
+                startValue = (T)Marshal.PtrToStructure(startIndex, type);
+                while (start < end && comparer(startValue, pivot) < 0)
+                {
+                    start++;
+                    startIndex = new IntPtr((int)pointer + start * elementSize);
+                    startValue = (T)Marshal.PtrToStructure(startIndex, type);
+                }
+
+                endIndex = new IntPtr((int)pointer + end * elementSize);
+                endValue = (T)Marshal.PtrToStructure(endIndex, type);
+                while (start < end && comparer(endValue, pivot) > 0)
+                {
+                    end--;
+                    endIndex = new IntPtr((int)pointer + end * elementSize);
+                    endValue = (T)Marshal.PtrToStructure(endIndex, type);
+                }
+                if (start < end)
+                {
+                    Marshal.StructureToPtr(endValue, startIndex, true);
+                    Marshal.StructureToPtr(startValue, endIndex, true);
+                }
+            }
+
+            return start;
+        }
+
+    }
+}
---
+++ CSharpGL/0Foundations/Utilities/Sorting/SortingHelper.Order.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Helper class for sorting unmanaged array.
+    /// </summary>
+    public static partial class SortingHelper
+    {
+        //static void Test()
+        //{
+        //    IntPtr pointer = IntPtr.Zero;// map, new, etc...
+        //    unsafe
+        //    {
+        //        var array = (vec3*)pointer.ToPointer();
+        //        int start = 0;
+        //        int length = 100;
+        //        Func<vec3, vec3, int> comparer = null;
+        //        Sort(pointer, start, length, comparer);
+        //    }
+        //}
+
+        /// <summary>
+        /// Sort unmanaged array specified with <paramref name="array"/> at specified area.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="start">index of first value to be sorted.</param>
+        /// <param name="length">length of <paramref name="array"/> to bo sorted.</param>
+        /// <param name="descending">true for descending sort; otherwise false.</param>
+        public static void Sort<T>(this UnmanagedArray<T> array, int start, int length, bool descending) where T : struct, IComparable<T>
+        {
+            QuickSort(array, start, start + length - 1, descending);
+        }
+
+        private static void QuickSort<T>(UnmanagedArray<T> array, int start, int end, bool descending) where T : struct, IComparable<T>
+        {
+            if (start >= end) { return; }
+
+            var stack = new Stack<int>();
+            stack.Push(end);
+            stack.Push(start);
+            QuickSort(array, descending, stack);
+        }
+
+        private static void QuickSort<T>(UnmanagedArray<T> array, bool descending, Stack<int> stack) where T : struct, IComparable<T>
+        {
+            IntPtr pointer = array.Header;
+            Type type = typeof(T);
+            int elementSize = Marshal.SizeOf(type);
+
+            while (stack.Count > 0)
+            {
+                int start = stack.Pop();
+                int end = stack.Pop();
+                int index = QuickSortPartion(array, start, end, descending, type, elementSize);
+                if (start < index - 1)
+                {
+                    stack.Push(index - 1); stack.Push(start);
+                }
+                if (index + 1 < end)
+                {
+                    stack.Push(end); stack.Push(index + 1);
+                }
+            }
+        }
+
+        private static int QuickSortPartion<T>(UnmanagedArray<T> array, int start, int end, bool descending, Type type, int elementSize) where T : struct, IComparable<T>
+        {
+            IntPtr pointer = array.Header;
+            IntPtr pivotIndex, startIndex, endIndex;
+            T pivot, startValue, endValue;
+            pivotIndex = new IntPtr((int)pointer + start * elementSize);
+            pivot = (T)Marshal.PtrToStructure(pivotIndex, type);
+            while (start < end)
+            {
+                startIndex = new IntPtr((int)pointer + start * elementSize);
+                startValue = (T)Marshal.PtrToStructure(startIndex, type);
+                if (descending)
+                {
+                    while (start < end && startValue.CompareTo(pivot) < 0)
+                    {
+                        start++;
+                        startIndex = new IntPtr((int)pointer + start * elementSize);
+                        startValue = (T)Marshal.PtrToStructure(startIndex, type);
+                    }
+                }
+                else
+                {
+                    while (start < end && startValue.CompareTo(pivot) > 0)
+                    {
+                        start++;
+                        startIndex = new IntPtr((int)pointer + start * elementSize);
+                        startValue = (T)Marshal.PtrToStructure(startIndex, type);
+                    }
+                }
+
+                endIndex = new IntPtr((int)pointer + end * elementSize);
+                endValue = (T)Marshal.PtrToStructure(endIndex, type);
+                if (descending)
+                {
+                    while (start < end && endValue.CompareTo(pivot) > 0)
+                    {
+                        end--;
+                        endIndex = new IntPtr((int)pointer + end * elementSize);
+                        endValue = (T)Marshal.PtrToStructure(endIndex, type);
+                    }
+                }
+                else
+                {
+                    while (start < end && endValue.CompareTo(pivot) < 0)
+                    {
+                        end--;
+                        endIndex = new IntPtr((int)pointer + end * elementSize);
+                        endValue = (T)Marshal.PtrToStructure(endIndex, type);
+                    }
+                }
+
+                if (start < end)
+                {
+                    Marshal.StructureToPtr(endValue, startIndex, true);
+                    Marshal.StructureToPtr(startValue, endIndex, true);
+                }
+            }
+
+            return start;
+        }
+    }
+}
