f4c05dc03f8010687b5fca7505c52e653956a5f2
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-21 22:12:04 +0800

    update

---
+++ CSharpGL/0Foundations/Utilities/Sorting/Sorting.vec3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Helper class for sorting unmanaged array.
+    /// </summary>
+    public static partial class SortingHelper
+    {
+        /// <summary>
+        /// Sort unmanaged array specified with <paramref name="pointer"/> at specified area.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        /// <param name="comparer"></param>
+        public static unsafe void Sort(vec3* array, int start, int length, Func<vec3, vec3, int> comparer)
+        {
+            bool exchanges;
+            do
+            {
+                exchanges = false;
+                for (int i = 0; i < length - 1; i++)
+                {
+                    if (comparer(array[start + i], array[start + i + 1]) < 0)
+                    {
+                        vec3 temp = array[start + i];
+                        array[start + i] = array[start + i + 1];
+                        array[start + i + 1] = temp;
+                        //当某轮比较没有发生移动时，就可以确定排序完成了
+                        //否则应该继续排序
+                        exchanges = true;
+                    }
+                }
+            } while (exchanges);
+        }
+    }
+}
--- CSharpGL/0Foundations/Utilities/Sorting/SortingHelper.cs
+++ CSharpGL/0Foundations/Utilities/Sorting/SortingHelper.cs
@@ -7,20 +7,20 @@ namespace CSharpGL
     /// <summary>
     /// Helper class for sorting unmanaged array.
     /// </summary>
-    public static class SortingHelper
+    public static partial class SortingHelper
     {
-        static void Test()
-        {
-            IntPtr pointer = IntPtr.Zero;// map, new, etc...
-            unsafe
-            {
-                var array = (vec3*)pointer.ToPointer();
-                int start = 0;
-                int length = 100;
-                Func<vec3, vec3, int> comparer = null;
-                Sort(pointer, start, length, comparer);
-            }
-        }
+        //static void Test()
+        //{
+        //    IntPtr pointer = IntPtr.Zero;// map, new, etc...
+        //    unsafe
+        //    {
+        //        var array = (vec3*)pointer.ToPointer();
+        //        int start = 0;
+        //        int length = 100;
+        //        Func<vec3, vec3, int> comparer = null;
+        //        Sort(pointer, start, length, comparer);
+        //    }
+        //}
 
         /// <summary>
         /// Sort unmanaged array specified with <paramref name="pointer"/> at specified area.
@@ -56,34 +56,5 @@ namespace CSharpGL
                 }
             } while (exchanges);
         }
-
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <typeparam name="T"></typeparam>
-        ///// <param name="array"></param>
-        ///// <param name="start"></param>
-        ///// <param name="length"></param>
-        ///// <param name="comparer"></param>
-        //public static unsafe void Sort(vec3* array, int start, int length, Func<vec3, vec3, int> comparer)
-        //{
-        //    bool exchanges;
-        //    do
-        //    {
-        //        exchanges = false;
-        //        for (int i = 0; i < length - 1; i++)
-        //        {
-        //            if (comparer(array[start + i], array[start + i + 1]) < 0)
-        //            {
-        //                vec3 temp = array[start + i];
-        //                array[start + i] = array[start + i + 1];
-        //                array[start + i + 1] = temp;
-        //                //当某轮比较没有发生移动时，就可以确定排序完成了
-        //                //否则应该继续排序
-        //                exchanges = true;
-        //            }
-        //        }
-        //    } while (exchanges);
-        //}
     }
 }
