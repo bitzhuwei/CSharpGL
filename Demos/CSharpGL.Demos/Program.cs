7b59a3c72864f7e7dd4a50faa323cb0772e17dbb
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-21 22:46:11 +0800

    QuickSort

--- Demos/CSharpGL.Demos/Program.cs
+++ Demos/CSharpGL.Demos/Program.cs
@@ -12,6 +12,17 @@ namespace CSharpGL.Demos
         [STAThread]
         private static void Main()
         {
+            var array = new UnmanagedArray<int>(10);
+            unsafe
+            {
+                Func<int, int, int> comparer = (x, y) => x - y;
+                array.Header.Sort(0, 10, comparer);
+                var p = (int*)array.Header.ToPointer();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(p[i]);
+                }
+            }
             string filename = string.Format("CSharpGL{0:yyyy-MM-dd_HH-mm-ss.ff}.log", DateTime.Now);
             Debug.Listeners.Add(new TextWriterTraceListener(filename));
             Debug.AutoFlush = true;
