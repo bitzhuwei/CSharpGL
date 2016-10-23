483e0b25aaaf5de09fd0f7d063269d96375c5fa0
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-23 16:05:17 +0800

    update

---
+++ CSharpGL/0Foundations/Utilities/Sorting/SortingHelper.Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    class SortingHelper
+    {
+        static void Test()
+        {
+            const int length = 17;
+            var array = new UnmanagedArray<float>(length);
+            unsafe
+            {
+                var p = (float*)array.Header.ToPointer();
+                for (int i = 0; i < length; i++)
+                {
+                    p[i] = i + length;
+                }
+
+
+                array.Sort(Comparer<float>.Default);
+                //array.Sort(true);
+
+                float[] p2 = new float[length];
+                for (int i = 0; i < length; i++)
+                {
+                    p2[i] = p[i];
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
