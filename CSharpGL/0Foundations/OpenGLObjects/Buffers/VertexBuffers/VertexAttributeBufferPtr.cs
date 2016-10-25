8a05f8eefc5637e6c0097867f20bc852b97fa9c1
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-25 14:34:41 +0800

    update

--- CSharpGL/0Foundations/OpenGLObjects/Buffers/VertexBuffers/VertexAttributeBufferPtr.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/VertexBuffers/VertexAttributeBufferPtr.cs
@@ -102,104 +102,7 @@ namespace CSharpGL
         {
             get
             {
-                int result = 0;
-                switch (this.Config)
-                {
-                    case VertexAttributeConfig.Byte:
-                        result = sizeof(byte);
-                        break;
-
-                    case VertexAttributeConfig.BVec2:
-                        result = sizeof(byte);
-                        break;
-
-                    case VertexAttributeConfig.BVec3:
-                        result = sizeof(byte);
-                        break;
-
-                    case VertexAttributeConfig.BVec4:
-                        result = sizeof(byte);
-                        break;
-
-                    case VertexAttributeConfig.Int:
-                        result = sizeof(int);
-                        break;
-
-                    case VertexAttributeConfig.IVec2:
-                        result = sizeof(int);
-                        break;
-
-                    case VertexAttributeConfig.IVec3:
-                        result = sizeof(int);
-                        break;
-
-                    case VertexAttributeConfig.IVec4:
-                        result = sizeof(int);
-                        break;
-
-                    case VertexAttributeConfig.UInt:
-                        result = sizeof(uint);
-                        break;
-
-                    case VertexAttributeConfig.UVec2:
-                        result = sizeof(uint);
-                        break;
-
-                    case VertexAttributeConfig.UVec3:
-                        result = sizeof(uint);
-                        break;
-
-                    case VertexAttributeConfig.UVec4:
-                        result = sizeof(uint);
-                        break;
-
-                    case VertexAttributeConfig.Float:
-                        result = sizeof(float);
-                        break;
-
-                    case VertexAttributeConfig.Vec2:
-                        result = sizeof(float);
-                        break;
-
-                    case VertexAttributeConfig.Vec3:
-                        result = sizeof(float);
-                        break;
-
-                    case VertexAttributeConfig.Vec4:
-                        result = sizeof(float);
-                        break;
-
-                    case VertexAttributeConfig.Double:
-                        result = sizeof(double);
-                        break;
-
-                    case VertexAttributeConfig.DVec2:
-                        result = sizeof(double);
-                        break;
-
-                    case VertexAttributeConfig.DVec3:
-                        result = sizeof(double);
-                        break;
-
-                    case VertexAttributeConfig.DVec4:
-                        result = sizeof(double);
-                        break;
-
-                    case VertexAttributeConfig.Mat2:
-                        result = sizeof(float);
-                        break;
-
-                    case VertexAttributeConfig.Mat3:
-                        result = sizeof(float);
-                        break;
-
-                    case VertexAttributeConfig.Mat4:
-                        result = sizeof(float);
-                        break;
-
-                    default:
-                        throw new System.NotImplementedException();
-                }
+                int result = GetDataTypeByteLength();
 
                 return result;
             }
@@ -275,7 +178,7 @@ namespace CSharpGL
                         break;
 
                     default:
-                        break;
+                        throw new NotImplementedException();
                 }
 
                 if (patchVertexes > 0)// tessellation shading.
@@ -384,6 +287,111 @@ namespace CSharpGL
 
                 default:
                     throw new NotImplementedException();
+            }
+
+            return result;
+        }
+
+        private int GetDataTypeByteLength()
+        {
+            int result = 0;
+
+            switch (this.Config)
+            {
+                case VertexAttributeConfig.Byte:
+                    result = sizeof(byte);
+                    break;
+
+                case VertexAttributeConfig.BVec2:
+                    result = sizeof(byte);
+                    break;
+
+                case VertexAttributeConfig.BVec3:
+                    result = sizeof(byte);
+                    break;
+
+                case VertexAttributeConfig.BVec4:
+                    result = sizeof(byte);
+                    break;
+
+                case VertexAttributeConfig.Int:
+                    result = sizeof(int);
+                    break;
+
+                case VertexAttributeConfig.IVec2:
+                    result = sizeof(int);
+                    break;
+
+                case VertexAttributeConfig.IVec3:
+                    result = sizeof(int);
+                    break;
+
+                case VertexAttributeConfig.IVec4:
+                    result = sizeof(int);
+                    break;
+
+                case VertexAttributeConfig.UInt:
+                    result = sizeof(uint);
+                    break;
+
+                case VertexAttributeConfig.UVec2:
+                    result = sizeof(uint);
+                    break;
+
+                case VertexAttributeConfig.UVec3:
+                    result = sizeof(uint);
+                    break;
+
+                case VertexAttributeConfig.UVec4:
+                    result = sizeof(uint);
+                    break;
+
+                case VertexAttributeConfig.Float:
+                    result = sizeof(float);
+                    break;
+
+                case VertexAttributeConfig.Vec2:
+                    result = sizeof(float);
+                    break;
+
+                case VertexAttributeConfig.Vec3:
+                    result = sizeof(float);
+                    break;
+
+                case VertexAttributeConfig.Vec4:
+                    result = sizeof(float);
+                    break;
+
+                case VertexAttributeConfig.Double:
+                    result = sizeof(double);
+                    break;
+
+                case VertexAttributeConfig.DVec2:
+                    result = sizeof(double);
+                    break;
+
+                case VertexAttributeConfig.DVec3:
+                    result = sizeof(double);
+                    break;
+
+                case VertexAttributeConfig.DVec4:
+                    result = sizeof(double);
+                    break;
+
+                case VertexAttributeConfig.Mat2:
+                    result = sizeof(float);
+                    break;
+
+                case VertexAttributeConfig.Mat3:
+                    result = sizeof(float);
+                    break;
+
+                case VertexAttributeConfig.Mat4:
+                    result = sizeof(float);
+                    break;
+
+                default:
+                    throw new System.NotImplementedException();
             }
 
             return result;
