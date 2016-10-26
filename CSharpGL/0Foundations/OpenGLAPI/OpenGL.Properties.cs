a2a667b67289b100dccab625a586f6d4bb9d6088
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-27 00:35:19 +0800

    ClearColor

---
+++ CSharpGL/0Foundations/OpenGLAPI/OpenGL.Properties.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// </summary>
+    public static partial class OpenGL
+    {
+        /// <summary>
+        /// Gets or sets OpenGL's clear color.
+        /// </summary>
+        public static Color ClearColor
+        {
+            get
+            {
+                var color = new float[4];
+                OpenGL.GetFloat(GetTarget.ColorClearValue, color);
+                return (new vec4(color[0], color[1], color[2], color[3])).ToColor();
+            }
+            set
+            {
+                vec4 color = value.ToVec4();
+                OpenGL.ClearColor(color.x, color.y, color.z, color.w);
+            }
+        }
+    }
+}
