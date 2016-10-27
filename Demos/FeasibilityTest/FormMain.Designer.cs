9bf6cc7be2cf64404fc5c6e5bd215eae9d76d68b
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-28 02:22:01 +0800

    update

--- Demos/FeasibilityTest/FormMain.Designer.cs
+++ Demos/FeasibilityTest/FormMain.Designer.cs
@@ -33,6 +33,8 @@
             this.btnSetColor = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.openFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.drvSimuCanvas1 = new DrvSimu.DrvSimuCanvas();
+            ((System.ComponentModel.ISupportInitialize)(this.drvSimuCanvas1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAddPoint
@@ -57,15 +59,29 @@
             // 
             this.openFileDlg.Filter = "*.txt|*.txt";
             // 
+            // drvSimuCanvas1
+            // 
+            this.drvSimuCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.drvSimuCanvas1.Location = new System.Drawing.Point(12, 42);
+            this.drvSimuCanvas1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.drvSimuCanvas1.Name = "drvSimuCanvas1";
+            this.drvSimuCanvas1.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
+            this.drvSimuCanvas1.Size = new System.Drawing.Size(713, 498);
+            this.drvSimuCanvas1.TabIndex = 2;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(738, 553);
+            this.Controls.Add(this.drvSimuCanvas1);
             this.Controls.Add(this.btnSetColor);
             this.Controls.Add(this.btnAddPoint);
             this.Name = "FormMain";
             this.Text = "FormMain";
+            ((System.ComponentModel.ISupportInitialize)(this.drvSimuCanvas1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -76,6 +92,7 @@
         private System.Windows.Forms.Button btnSetColor;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.OpenFileDialog openFileDlg;
+        private DrvSimu.DrvSimuCanvas drvSimuCanvas1;
 
     }
 }
