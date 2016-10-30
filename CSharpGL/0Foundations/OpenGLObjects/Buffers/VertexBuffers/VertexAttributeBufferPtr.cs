5485893eebbedefe3fb101e231cabf3d8fc3dfd0
祝威 <bitzhuwei.cnblogs@qq.com>
2016-10-30 23:29:09 +0800

    update

--- CSharpGL/0Foundations/OpenGLObjects/Buffers/VertexBuffers/VertexAttributeBufferPtr.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/VertexBuffers/VertexAttributeBufferPtr.cs
@@ -163,7 +163,7 @@ namespace CSharpGL
             {
                 // 指定格式
                 // set up data format.
-                switch (this.GetVertexAttribPointerType(this.Config))
+                switch (this.Config.GetVertexAttribPointerType())
                 {
                     case VertexAttribPointerType.Default:
                         glVertexAttribPointer(loc + i, dataSize, dataType, false, stride, new IntPtr(i * startOffsetUnit));
@@ -201,104 +201,6 @@ namespace CSharpGL
                 }
             }
             glBindBuffer(OpenGL.GL_ARRAY_BUFFER, 0);
-        }
-
-        private VertexAttribPointerType GetVertexAttribPointerType(VertexAttributeConfig config)
-        {
-            var result = VertexAttribPointerType.Default;
-
-            switch (config)
-            {
-                case VertexAttributeConfig.Byte:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.BVec2:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.BVec3:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.BVec4:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.Int:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.IVec2:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.IVec3:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.IVec4:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.UInt:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.UVec2:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.UVec3:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.UVec4:
-                    result = VertexAttribPointerType.Integer;
-                    break;
-
-                case VertexAttributeConfig.Float:
-                    break;
-
-                case VertexAttributeConfig.Vec2:
-                    break;
-
-                case VertexAttributeConfig.Vec3:
-                    break;
-
-                case VertexAttributeConfig.Vec4:
-                    break;
-
-                case VertexAttributeConfig.Double:
-                    result = VertexAttribPointerType.Long;
-                    break;
-
-                case VertexAttributeConfig.DVec2:
-                    result = VertexAttribPointerType.Long;
-                    break;
-
-                case VertexAttributeConfig.DVec3:
-                    result = VertexAttribPointerType.Long;
-                    break;
-
-                case VertexAttributeConfig.DVec4:
-                    result = VertexAttribPointerType.Long;
-                    break;
-
-                case VertexAttributeConfig.Mat2:
-                    break;
-
-                case VertexAttributeConfig.Mat3:
-                    break;
-
-                case VertexAttributeConfig.Mat4:
-                    break;
-
-                default:
-                    throw new NotImplementedException();
-            }
-
-            return result;
         }
 
         private int GetDataTypeByteLength()
--- CSharpGL/0Foundations/OpenGLObjects/Buffers/VertexBuffers/VertexAttributeConfig.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/VertexBuffers/VertexAttributeConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace CSharpGL
 {
@@ -271,8 +272,107 @@ namespace CSharpGL
                     break;
 
                 default:
-                    throw new System.NotImplementedException();
+                    throw new NotImplementedException();
             }
         }
+
+        internal static VertexAttribPointerType GetVertexAttribPointerType(this VertexAttributeConfig config)
+        {
+            var result = VertexAttribPointerType.Default;
+
+            switch (config)
+            {
+                case VertexAttributeConfig.Byte:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.BVec2:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.BVec3:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.BVec4:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.Int:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.IVec2:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.IVec3:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.IVec4:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.UInt:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.UVec2:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.UVec3:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.UVec4:
+                    result = VertexAttribPointerType.Integer;
+                    break;
+
+                case VertexAttributeConfig.Float:
+                    break;
+
+                case VertexAttributeConfig.Vec2:
+                    break;
+
+                case VertexAttributeConfig.Vec3:
+                    break;
+
+                case VertexAttributeConfig.Vec4:
+                    break;
+
+                case VertexAttributeConfig.Double:
+                    result = VertexAttribPointerType.Long;
+                    break;
+
+                case VertexAttributeConfig.DVec2:
+                    result = VertexAttribPointerType.Long;
+                    break;
+
+                case VertexAttributeConfig.DVec3:
+                    result = VertexAttribPointerType.Long;
+                    break;
+
+                case VertexAttributeConfig.DVec4:
+                    result = VertexAttribPointerType.Long;
+                    break;
+
+                case VertexAttributeConfig.Mat2:
+                    break;
+
+                case VertexAttributeConfig.Mat3:
+                    break;
+
+                case VertexAttributeConfig.Mat4:
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return result;
+        }
+
     }
 }
