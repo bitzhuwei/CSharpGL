d163a420f66e9d62a668213fbc27621bf2975784
祝威 <bitzhuwei.cnblogs@qq.com>
2016-11-07 13:49:31 +0800

    BitmapFiller supports multiple levels.

--- CSharpGL/0Foundations/OpenGLObjects/Texture/ImageFillers/BitmapFiller.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Texture/ImageFillers/BitmapFiller.cs
@@ -10,7 +10,7 @@ namespace CSharpGL
     public class BitmapFiller : ImageFiller
     {
         private System.Drawing.Bitmap bitmap;
-        private int level;
+        private int maxLevel;
         private uint internalformat;
         private int border;
         private uint format;
@@ -21,17 +21,19 @@ namespace CSharpGL
         /// build texture's content with Bitmap.
         /// </summary>
         /// <param name="bitmap"></param>
-        /// <param name="level">0</param>
+        /// <param name="maxLevel">0 as default.</param>
         /// <param name="internalformat">OpenGL.GL_RGBA etc.</param>
-        /// <param name="border">0</param>
+        /// <param name="border">max level.</param>
         /// <param name="format">OpenGL.GL_BGRA etc.</param>
         /// <param name="type">OpenGL.GL_UNSIGNED_BYTE etc.</param>
         /// <param name="target2d">true for 2D; false for 1D.</param>
         public BitmapFiller(System.Drawing.Bitmap bitmap,
-            int level, uint internalformat, int border, uint format, uint type, bool target2d = true)
+            int maxLevel, uint internalformat, int border, uint format, uint type, bool target2d = true)
         {
+            if (maxLevel < 0) { throw new ArgumentException("texture level must be no less than 0!"); }
+
             this.bitmap = bitmap;
-            this.level = level;
+            this.maxLevel = maxLevel;
             this.internalformat = internalformat;
             this.border = border;
             this.format = format;
@@ -45,22 +47,56 @@ namespace CSharpGL
         public override void Fill()
         {
             // generate texture.
-            //  Lock the image bits (so that we can pass them to OGL).
-            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-                ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
             if (target == TextureTarget.Texture1D)
             {
-                OpenGL.TexImage1D((uint)target, 0, this.internalformat, bitmap.Width, 0, this.format, this.type, bitmapData.Scan0);
+                Bitmap bmp = null;
+                for (int level = 0; level <= this.maxLevel; level++)
+                {
+                    if (level == 0) { bmp = this.bitmap; }
+                    else
+                    {
+                        bmp.Dispose();
+                        int width = bmp.Width / 2;
+                        if (width < 1) { width = 1; }
+                        bmp = new Bitmap(bmp, width, 1);
+                    }
+                    //  Lock the image bits (so that we can pass them to OGL).
+                    BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                        ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                    OpenGL.TexImage1D((uint)target, level, this.internalformat, bitmap.Width, 0, this.format, this.type, bitmapData.Scan0);
+                    //  Unlock the image.
+                    bmp.UnlockBits(bitmapData);
+                }
+
+                if (bmp != this.bitmap) { bmp.Dispose(); }
             }
             else if (target == TextureTarget.Texture2D)
             {
-                OpenGL.TexImage2D((uint)target, 0, this.internalformat, bitmap.Width, bitmap.Height, 0, this.format, this.type, bitmapData.Scan0);
+                Bitmap bmp = null;
+                for (int level = 0; level <= this.maxLevel; level++)
+                {
+                    if (level == 0) { bmp = this.bitmap; }
+                    else
+                    {
+                        bmp.Dispose();
+                        int width = bmp.Width / 2, height = bmp.Height / 2;
+                        if (width < 1) { width = 1; }
+                        if (height < 1) { height = 1; }
+                        bmp = new Bitmap(bmp, width, height);
+                    }
+                    //  Lock the image bits (so that we can pass them to OGL).
+                    BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                        ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                    OpenGL.TexImage2D((uint)target, level, this.internalformat, bmp.Width, bmp.Height, 0, this.format, this.type, bitmapData.Scan0);
+                    //  Unlock the image.
+                    bmp.UnlockBits(bitmapData);
+                }
+
+                if (bmp != this.bitmap) { bmp.Dispose(); }
             }
             else
             { throw new NotImplementedException(); }
 
-            //  Unlock the image.
-            bitmap.UnlockBits(bitmapData);
         }
     }
 }
