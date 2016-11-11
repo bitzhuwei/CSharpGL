05de419fb1ae9b49280e09d33b21637e2c65b86c
祝威 <bitzhuwei.cnblogs@qq.com>
2016-11-12 01:30:14 +0800

    update

--- CSharpGL/0Foundations/OpenGLObjects/Buffers/Buffer.BufferTexture.cs
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/Buffer.BufferTexture.cs
@@ -1,18 +1,17 @@
 ﻿namespace CSharpGL
 {
-    public static class BufferPtrHelper
+    public abstract partial class Buffer
     {
         /// <summary>
         /// Dump a <see cref="Texture"/> filled with this <see cref="Buffer"/>.
         /// </summary>
-        /// <param name="bufferPtr"></param>
         /// <param name="internalFormat"></param>
-        /// <param name="autoDispose">Dispose <paramref name="bufferPtr"/> when disposing returned texture.</param>
+        /// <param name="autoDispose">Dispose this buffer when disposing returned texture.</param>
         /// <returns></returns>
-        public static Texture DumpBufferTexture(this Buffer bufferPtr, uint internalFormat, bool autoDispose)
+        public Texture DumpBufferTexture(uint internalFormat, bool autoDispose)
         {
             var texture = new Texture(TextureTarget.TextureBuffer,
-                new TexBufferImageFiller(internalFormat, bufferPtr, autoDispose),
+                new TexBufferImageFiller(internalFormat, this, autoDispose),
                 new NullSampler());
             texture.Initialize();
             return texture;
