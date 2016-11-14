8cc52fba4956337332c1d2b0d866b1c362aae7af
祝威 <bitzhuwei.cnblogs@qq.com>
2016-11-14 11:10:11 +0800

    update

---
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/Data2Buffer/GenOtherBuffers.T[].cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSharpGL
+{
+    public static partial class Data2Buffer
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static AtomicCounterBuffer GenAtomicCounterBuffer<T>(this T[] array, BufferUsage usage) where T : struct
+        {
+            return GenIndependentBuffer(array, IndependentBufferTarget.AtomicCounterBuffer, usage) as AtomicCounterBuffer;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static PixelPackBuffer GenPixelPackBuffer<T>(this T[] array, BufferUsage usage) where T : struct
+        {
+            return GenIndependentBuffer(array, IndependentBufferTarget.PixelPackBuffer, usage) as PixelPackBuffer;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static PixelUnpackBuffer GenPixelUnpackBuffer<T>(this T[] array, BufferUsage usage) where T : struct
+        {
+            return GenIndependentBuffer(array, IndependentBufferTarget.PixelUnpackBuffer, usage) as PixelUnpackBuffer;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static ShaderStorageBuffer GenShaderStorageBuffer<T>(this T[] array, BufferUsage usage) where T : struct
+        {
+            return GenIndependentBuffer(array, IndependentBufferTarget.ShaderStorageBuffer, usage) as ShaderStorageBuffer;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static TextureBuffer GenTextureBuffer<T>(this T[] array, BufferUsage usage) where T : struct
+        {
+            return GenIndependentBuffer(array, IndependentBufferTarget.TextureBuffer, usage) as TextureBuffer;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static UniformBuffer GenUniformBuffer<T>(this T[] array, BufferUsage usage) where T : struct
+        {
+            return GenIndependentBuffer(array, IndependentBufferTarget.UniformBuffer, usage) as UniformBuffer;
+        }
+
+        private static Buffer GenIndependentBuffer<T>(this T[] array, IndependentBufferTarget target, BufferUsage usage) where T : struct
+        {
+            GCHandle pinned = GCHandle.Alloc(array, GCHandleType.Pinned);
+            IntPtr header = Marshal.UnsafeAddrOfPinnedArrayElement(array, 0);
+            var unmanagedArray = new UnmanagedArray<T>(header, array.Length);// It's not neecessary to call Dispose() for this unmanaged array.
+            Buffer buffer = GenIndependentBuffer(unmanagedArray, target, usage);
+            pinned.Free();
+
+            return buffer;
+        }
+
+    }
+}
---
+++ CSharpGL/0Foundations/OpenGLObjects/Buffers/Data2Buffer/GenOtherBuffers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSharpGL
+{
+    public static partial class Data2Buffer
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static AtomicCounterBuffer GenAtomicCounterBuffer<T>(this T data, BufferUsage usage) where T : struct
+        {
+            var array = new T[] { data };
+            return GenAtomicCounterBuffer(array, usage);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static PixelPackBuffer GenPixelPackBuffer<T>(this T data, BufferUsage usage) where T : struct
+        {
+            var array = new T[] { data };
+            return GenPixelPackBuffer(array, usage);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static PixelUnpackBuffer GenPixelUnpackBuffer<T>(this T data, BufferUsage usage) where T : struct
+        {
+            var array = new T[] { data };
+            return GenPixelUnpackBuffer(array, usage);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static ShaderStorageBuffer GenShaderStorageBuffer<T>(this T data, BufferUsage usage) where T : struct
+        {
+            var array = new T[] { data };
+            return GenShaderStorageBuffer(array, usage);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static TextureBuffer GenTextureBuffer<T>(this T data, BufferUsage usage) where T : struct
+        {
+            var array = new T[] { data };
+            return GenTextureBuffer(array, usage);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="usage"></param>
+        /// <returns></returns>
+        public static UniformBuffer GenUniformBuffer<T>(this T data, BufferUsage usage) where T : struct
+        {
+            var array = new T[] { data };
+            return GenUniformBuffer(array, usage);
+        }
+    }
+}
