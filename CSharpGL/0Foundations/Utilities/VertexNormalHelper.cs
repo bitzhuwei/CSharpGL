f58daf4459f5ae18e1297ed6a7f7238dc91e12c9
祝威 <bitzhuwei.cnblogs@qq.com>
2017-02-26 00:34:58 +0800

    update

--- CSharpGL/0Foundations/Utilities/VertexNormalHelper.cs
+++ CSharpGL/0Foundations/Utilities/VertexNormalHelper.cs
@@ -130,3 +130,38 @@
         }
     }
 }
+// TODO: update this algorithm with following codes:
+/*
+            var faceNormals = new vec3[faces.Length];
+            var normals = new vec3[vertexPositions.Length];
+            var counts = new int[vertexPositions.Length];
+            for (int i = 0; i < faceNormals.Length; i++)
+            {
+                int vertexId1 = faces[i].Num1;
+                int vertexId2 = faces[i].Num2;
+                int vertexId3 = faces[i].Num3;
+                vec3 vertex1 = vertexPositions[vertexId1];
+                vec3 vertex2 = vertexPositions[vertexId2];
+                vec3 vertex3 = vertexPositions[vertexId3];
+                vec3 v12 = vertex2 - vertex1;
+                vec3 v23 = vertex3 - vertex2;
+                vec3 normal = v23.cross(v12).normalize();
+                faceNormals[i] = normal;
+                normals[vertexId1] += normal;
+                normals[vertexId2] += normal;
+                normals[vertexId3] += normal;
+                counts[vertexId1]++;
+                counts[vertexId2]++;
+                counts[vertexId3]++;
+            }
+
+            for (int i = 0; i < normals.Length; i++)
+            {
+                if (counts[i] > 0)
+                {
+                    normals[i] = normals[i] / counts[i];
+                }
+            }
+
+            return normals;
+*/
