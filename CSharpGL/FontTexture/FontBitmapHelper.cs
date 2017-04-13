d3f03cfd703cdd8de0bc62fdd09c7328f23dcd0a
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-04-14 03:18:09 +0800

    update

--- CSharpGL/FontTexture/FontBitmapHelper.cs
+++ CSharpGL/FontTexture/FontBitmapHelper.cs
@@ -156,41 +156,6 @@ namespace CSharpGL
 
             return dict;
         }
-        ///// <summary>
-        ///// Gets a <see cref="FontBitmap"/>'s intance.
-        ///// </summary>
-        ///// <param name="font"></param>
-        ///// <param name="charSet"></param>
-        ///// <param name="drawBoundary"></param>
-        ///// <returns></returns>
-        //public static FontBitmap GetFontBitmap(this Font font, string charSet, bool drawBoundary = false)
-        //{
-        //    var fontBitmap = new FontBitmap();
-        //    fontBitmap.GlyphFont = font;
 
-        //    // 以下几步，不能调换先后顺序。
-        //    // Don't change the order in which these functions invoked.
-        //    int singleCharWidth, singleCharHeight;
-        //    PrepareInitialGlyphDict(fontBitmap, charSet, out singleCharWidth, out singleCharHeight);
-        //    int width, height;
-        //    PrepareFinalBitmapSize(fontBitmap, out width, out height);
-        //    PrintBitmap(fontBitmap, singleCharWidth, singleCharHeight, width, height);
-        //    if (drawBoundary)
-        //    {
-        //        using (var graphics = Graphics.FromImage(fontBitmap.GlyphBitmap))
-        //        {
-        //            bool odd = false;
-        //            foreach (GlyphInfo item in fontBitmap.GlyphInfoDictionary.Values)
-        //            {
-        //                graphics.DrawRectangle(
-        //                    odd ? Pens.Green : Pens.Blue,
-        //                    item.ToRectangle(1, 1, -2, -1));
-        //                odd = !odd;
-        //            }
-        //            graphics.DrawRectangle(Pens.Red, 0, 0, fontBitmap.GlyphBitmap.Width - 1, fontBitmap.GlyphBitmap.Height - 1);
-        //        }
-        //    }
-        //    return fontBitmap;
-        //}
     }
 }
