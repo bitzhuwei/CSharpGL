98a47c5c01b4b95c25538456ae58af2306f6e7be
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-04-28 19:43:44 +0800

    GLStateNode

---
+++ CSharpGL3/GLScene/GLNodes/GLStateNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// shader program.
+    /// </summary>
+    public class GLStateNode : GLNode
+    {
+        private GLState glState;
+
+        private static readonly Type type = typeof(GLStateNode);
+        internal override Type ThisTypeCache
+        {
+            get { return type; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shaderCodes"></param>
+        public GLStateNode(GLState glState)
+        {
+            this.glState = glState;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public GLState GetState()
+        {
+            return this.glState;
+        }
+    }
+}
---
+++ CSharpGL3/GLScene/GLSnippets/RenderActionSnippets/Render_State.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Render_State : GLSnippet
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="glAction"></param>
+        /// <param name="glNode"></param>
+        public override void DoAction(GLAction glAction, GLNode glNode)
+        {
+            var action = glAction as RenderAction;
+            var node = glNode as GLStateNode;
+            Debug.Assert(action != null);
+            Debug.Assert(node != null);
+
+            GLState state = node.GetState();
+            action.Context.glStateList.Add(state);
+        }
+
+    }
+}
