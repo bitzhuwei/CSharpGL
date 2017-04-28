e5d470102244c7ced966759f6ab4fa1503673f82
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-04-28 20:21:50 +0800

    update

---
+++ CSharpGL3/GLScene/GLNodes/GLTriangleStripNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// complex node. commonly used shape.
+    /// </summary>
+    public class GLTriangleStripNode : GLShaderNode
+    {
+        private static readonly Type type = typeof(GLTriangleStripNode);
+        internal override Type ThisTypeCache
+        {
+            get { return type; }
+        }
+
+
+
+        internal VertexArrayObject GetVertexArrayObject(RenderActionContext renderActionContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharpGL3/GLScene/GLSnippets/RenderActionSnippets/Render_TriangleStrip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Render_TriangleStrip : GLSnippet
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="glAction"></param>
+        /// <param name="glNode"></param>
+        public override void DoAction(GLAction glAction, GLNode glNode)
+        {
+            var action = glAction as RenderAction;
+            var node = glNode as GLTriangleStripNode;
+            Debug.Assert(action != null);
+            Debug.Assert(node != null);
+
+            VertexArrayObject vao = node.GetVertexArrayObject(action.Context);
+            action.Context.vertexArrayObject = vao;
+        }
+
+    }
+}
