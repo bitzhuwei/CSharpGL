833a09aa65a13398ea1c619d423226b1742f8cd5
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-04-29 14:12:55 +0800

    GLCameraNode

---
+++ CSharpGL3/GLScene/GLNodes/GLCameraNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// map to 'in vec3 inPosition;' in vertex shader.
+    /// </summary>
+    public sealed class GLCameraNode : GLNode
+    {
+
+        private static readonly Type type = typeof(GLCameraNode);
+        internal override Type ThisTypeCache
+        {
+            get { return type; }
+        }
+
+        public GLCameraNode() { }
+
+    }
+}
