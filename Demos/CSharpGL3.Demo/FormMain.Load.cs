41878266617604bb77d78369d27efe21ba518636
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-05-05 20:10:34 +0800

    update

--- Demos/CSharpGL3.Demo/FormMain.Load.cs
+++ Demos/CSharpGL3.Demo/FormMain.Load.cs
@@ -22,19 +22,25 @@ namespace CSharpGL3.Demo
             shaderCodes[1] = new ShaderCode(System.IO.File.ReadAllText(@"Demo.frag"), ShaderType.FragmentShader);
             var program = new GLProgramNode(shaderCodes);
 
-            var positions = new vec3[3];
+            var positions = new vec3[6]; var diff = new vec3(1, -1, 1) * 0.5f;
             positions[0] = new vec3(-1, -1, 0) * 0.5f;
             positions[1] = new vec3(-1, 1, 0) * 0.5f;
             positions[2] = new vec3(1, 1, 0) * 0.5f;
+            positions[3] = (new vec3(-1, -1, 0) + diff) * 0.5f;
+            positions[4] = (new vec3(-1, 1, 0) + diff) * 0.5f;
+            positions[5] = (new vec3(1, 1, 0) + diff) * 0.5f;
             var positionNode = GLVertexNode.Create(positions, VBOConfig.Vec3, "in_Position", BufferUsage.StaticDraw);
 
-            var colors = new vec3[3];
+            var colors = new vec3[6];
             colors[0] = Color.Red.ToVec3();
             colors[1] = Color.Green.ToVec3();
             colors[2] = Color.Blue.ToVec3();
+            colors[3] = Color.Orange.ToVec3();
+            colors[4] = Color.LightGreen.ToVec3();
+            colors[5] = Color.DarkBlue.ToVec3();
             var colorNode = GLVertexNode.Create(colors, VBOConfig.Vec3, "in_Color", BufferUsage.StaticDraw);
 
-            var indexNode = new GLZeroIndexNode(CSharpGL.DrawMode.Triangles, 0, 3);
+            var indexNode = new GLZeroIndexNode(CSharpGL.DrawMode.Triangles, 0, positions.Length);
 
             var shapeNode = new GLTriangleStripNode();
 
