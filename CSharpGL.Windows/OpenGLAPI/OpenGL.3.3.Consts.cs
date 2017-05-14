2284d25b33cb337e7dec07ddfd6005ff7fd8debc
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-05-14 19:02:58 +0800

    gl3.3 consts

--- CSharpGL.Windows/OpenGLAPI/OpenGL.3.3.Consts.cs
+++ CSharpGL.Windows/OpenGLAPI/OpenGL.3.3.Consts.cs
@@ -4,13 +4,11 @@
     {
         #region OpenGL 3.3
 
-        ////  Delegates
+        ////  Constants
         ///// <summary>
         /////
         ///// </summary>
-        ///// <param name="index"></param>
-        ///// <param name="divisor"></param>
-        //internal delegate void glVertexAttribDivisor(uint index, uint divisor);
+        //public const uint GL_VERTEX_ATTRIB_ARRAY_DIVISOR = 0x88FE;
 
         #endregion OpenGL 3.3
     }
