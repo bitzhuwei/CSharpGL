c7c14a1a6b3183310f9f44fc4823976c0a476d5b
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-05-14 19:01:57 +0800

    gl3.2 consts

---
+++ CSharpGL.Windows/OpenGLAPI/OpenGL.3.2.Consts.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace CSharpGL
+{
+    public static partial class OpenGL
+    {
+        #region OpenGL 3.2
+
+        //  Constants
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_CONTEXT_CORE_PROFILE_BIT = 0x00000001;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_CONTEXT_COMPATIBILITY_PROFILE_BIT = 0x00000002;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_LINES_ADJACENCY = 0x000A;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_LINE_STRIP_ADJACENCY = 0x000B;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_TRIANGLES_ADJACENCY = 0x000C;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_PATCHES = 0xE;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_TRIANGLE_STRIP_ADJACENCY = 0x000D;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_PROGRAM_POINT_SIZE = 0x8642;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_MAX_GEOMETRY_TEXTURE_IMAGE_UNITS = 0x8C29;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_FRAMEBUFFER_ATTACHMENT_LAYERED = 0x8DA7;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS = 0x8DA8;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_GEOMETRY_SHADER = 0x8DD9;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_GEOMETRY_VERTICES_OUT = 0x8916;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_GEOMETRY_INPUT_TYPE = 0x8917;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_GEOMETRY_OUTPUT_TYPE = 0x8918;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_MAX_GEOMETRY_UNIFORM_COMPONENTS = 0x8DDF;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_MAX_GEOMETRY_OUTPUT_VERTICES = 0x8DE0;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_MAX_GEOMETRY_TOTAL_OUTPUT_COMPONENTS = 0x8DE1;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_MAX_VERTEX_OUTPUT_COMPONENTS = 0x9122;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_MAX_GEOMETRY_INPUT_COMPONENTS = 0x9123;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_MAX_GEOMETRY_OUTPUT_COMPONENTS = 0x9124;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_MAX_FRAGMENT_INPUT_COMPONENTS = 0x9125;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public const uint GL_CONTEXT_PROFILE_MASK = 0x9126;
+
+        #endregion OpenGL 3.2
+    }
+}
