35010d35923feb7ff076249ab5322d12498bb7a6
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-05-14 16:02:55 +0800

    gl1.2 cons

--- CSharpGL.Windows/OpenGLAPI/OpenGL.1.2.cs
+++ CSharpGL.Windows/OpenGLAPI/OpenGL.1.2.cs
@@ -20,61 +20,61 @@ namespace CSharpGL
         ///// </summary>
         ///// <param name="mode"></param>
         //public delegate void glBlendEquation(uint mode);
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="mode"></param>
-        /// <param name="start"></param>
-        /// <param name="end"></param>
-        /// <param name="count"></param>
-        /// <param name="type"></param>
-        /// <param name="indices"></param>
-        public delegate void glDrawRangeElements(uint mode, uint start, uint end, int count, uint type, IntPtr indices);
+        ///// <summary>
+        /////
+        ///// </summary>
+        ///// <param name="mode"></param>
+        ///// <param name="start"></param>
+        ///// <param name="end"></param>
+        ///// <param name="count"></param>
+        ///// <param name="type"></param>
+        ///// <param name="indices"></param>
+        //public delegate void glDrawRangeElements(uint mode, uint start, uint end, int count, uint type, IntPtr indices);
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="target"></param>
-        /// <param name="level"></param>
-        /// <param name="internalformat"></param>
-        /// <param name="width"></param>
-        /// <param name="height"></param>
-        /// <param name="depth"></param>
-        /// <param name="border"></param>
-        /// <param name="format"></param>
-        /// <param name="type"></param>
-        /// <param name="pixels"></param>
-        public delegate void glTexImage3D(uint target, int level, int internalformat, int width, int height, int depth, int border, uint format, uint type, IntPtr pixels);
+        ///// <summary>
+        /////
+        ///// </summary>
+        ///// <param name="target"></param>
+        ///// <param name="level"></param>
+        ///// <param name="internalformat"></param>
+        ///// <param name="width"></param>
+        ///// <param name="height"></param>
+        ///// <param name="depth"></param>
+        ///// <param name="border"></param>
+        ///// <param name="format"></param>
+        ///// <param name="type"></param>
+        ///// <param name="pixels"></param>
+        //public delegate void glTexImage3D(uint target, int level, int internalformat, int width, int height, int depth, int border, uint format, uint type, IntPtr pixels);
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="target"></param>
-        /// <param name="level"></param>
-        /// <param name="xoffset"></param>
-        /// <param name="yoffset"></param>
-        /// <param name="width"></param>
-        /// <param name="height"></param>
-        /// <param name="format"></param>
-        /// <param name="type"></param>
-        /// <param name="pixels"></param>
-        public delegate void glTexSubImage2D(uint target, int level, int xoffset, int yoffset, int width, int height, uint format, uint type, IntPtr pixels);
+        ///// <summary>
+        /////
+        ///// </summary>
+        ///// <param name="target"></param>
+        ///// <param name="level"></param>
+        ///// <param name="xoffset"></param>
+        ///// <param name="yoffset"></param>
+        ///// <param name="width"></param>
+        ///// <param name="height"></param>
+        ///// <param name="format"></param>
+        ///// <param name="type"></param>
+        ///// <param name="pixels"></param>
+        //public delegate void glTexSubImage2D(uint target, int level, int xoffset, int yoffset, int width, int height, uint format, uint type, IntPtr pixels);
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="target"></param>
-        /// <param name="level"></param>
-        /// <param name="xoffset"></param>
-        /// <param name="yoffset"></param>
-        /// <param name="zoffset"></param>
-        /// <param name="width"></param>
-        /// <param name="height"></param>
-        /// <param name="depth"></param>
-        /// <param name="format"></param>
-        /// <param name="type"></param>
-        /// <param name="pixels"></param>
-        public delegate void glTexSubImage3D(uint target, int level, int xoffset, int yoffset, int zoffset, int width, int height, int depth, uint format, uint type, IntPtr pixels);
+        ///// <summary>
+        /////
+        ///// </summary>
+        ///// <param name="target"></param>
+        ///// <param name="level"></param>
+        ///// <param name="xoffset"></param>
+        ///// <param name="yoffset"></param>
+        ///// <param name="zoffset"></param>
+        ///// <param name="width"></param>
+        ///// <param name="height"></param>
+        ///// <param name="depth"></param>
+        ///// <param name="format"></param>
+        ///// <param name="type"></param>
+        ///// <param name="pixels"></param>
+        //public delegate void glTexSubImage3D(uint target, int level, int xoffset, int yoffset, int zoffset, int width, int height, int depth, uint format, uint type, IntPtr pixels);
 
         ///// <summary>
         /////
@@ -346,187 +346,6 @@ namespace CSharpGL
         ///// </summary>
         ///// <param name="target"></param>
         //public delegate void glResetMinmax(uint target);
-
-        //  Constants
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_BYTE_3_3_2 = 0x8032;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_SHORT_4_4_4_4 = 0x8033;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_SHORT_5_5_5_1 = 0x8034;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_INT_8_8_8_8 = 0x8035;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_INT_10_10_10_2 = 0x8036;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_TEXTURE_BINDING_3D = 0x806A;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_PACK_SKIP_IMAGES = 0x806B;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_PACK_IMAGE_HEIGHT = 0x806C;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNPACK_SKIP_IMAGES = 0x806D;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNPACK_IMAGE_HEIGHT = 0x806E;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_TEXTURE_3D = 0x806F;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_PROXY_TEXTURE_3D = 0x8070;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_TEXTURE_DEPTH = 0x8071;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_TEXTURE_WRAP_R = 0x8072;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_MAX_3D_TEXTURE_SIZE = 0x8073;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_BYTE_2_3_3_REV = 0x8362;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_SHORT_5_6_5 = 0x8363;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_SHORT_5_6_5_REV = 0x8364;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_SHORT_4_4_4_4_REV = 0x8365;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_SHORT_1_5_5_5_REV = 0x8366;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_INT_8_8_8_8_REV = 0x8367;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_UNSIGNED_INT_2_10_10_10_REV = 0x8368;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_BGR = 0x80E0;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_BGRA = 0x80E1;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_MAX_ELEMENTS_VERTICES = 0x80E8;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_MAX_ELEMENTS_INDICES = 0x80E9;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_CLAMP_TO_EDGE = 0x812F;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_TEXTURE_MIN_LOD = 0x813A;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_TEXTURE_MAX_LOD = 0x813B;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_TEXTURE_BASE_LEVEL = 0x813C;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_TEXTURE_MAX_LEVEL = 0x813D;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_SMOOTH_POINT_SIZE_RANGE = 0x0B12;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_SMOOTH_POINT_SIZE_GRANULARITY = 0x0B13;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_SMOOTH_LINE_WIDTH_RANGE = 0x0B22;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_SMOOTH_LINE_WIDTH_GRANULARITY = 0x0B23;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public const uint GL_ALIASED_LINE_WIDTH_RANGE = 0x846E;
 
         #endregion OpenGL 1.2
     }
