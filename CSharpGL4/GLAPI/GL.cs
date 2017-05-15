e42c0c3ea344b0efe0385848c30b9f3cbb16ed2d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-05-15 14:05:43 +0800

    update

--- CSharpGL4/GLAPI/GL.cs
+++ CSharpGL4/GLAPI/GL.cs
@@ -8,6 +8,9 @@ namespace CSharpGL
 {
     /// <summary>
     /// OpenGL is a specification of 3D graphics library.
+    /// <para>
+    /// <see cref="GL"/> represents functions suppproted in OpenGL32.dll/openGL32.so/... and all extended functions.
+    /// </para>
     /// </summary>
     public abstract partial class GL
     {
