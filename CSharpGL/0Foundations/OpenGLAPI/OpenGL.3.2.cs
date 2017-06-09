e7b336831fa85b7d5cee597f05789aac48533b22
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-10 00:46:52 +0800

    opengl3.2

--- CSharpGL/0Foundations/OpenGLAPI/OpenGL.3.2.cs
+++ CSharpGL/0Foundations/OpenGLAPI/OpenGL.3.2.cs
@@ -7,30 +7,30 @@ namespace CSharpGL
         #region OpenGL 3.2
 
         //  Delegates
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="target"></param>
-        /// <param name="index"></param>
-        /// <param name="data"></param>
-        public delegate void glGetInteger64i_v(uint target, uint index, Int64[] data);
+        ///// <summary>
+        /////
+        ///// </summary>
+        ///// <param name="target"></param>
+        ///// <param name="index"></param>
+        ///// <param name="data"></param>
+        //public delegate void glGetInteger64i_v(uint target, uint index, Int64[] data);
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="target"></param>
-        /// <param name="pname"></param>
-        /// <param name="parameters"></param>
-        public delegate void glGetBufferParameteri64v(uint target, uint pname, Int64[] parameters);
+        ///// <summary>
+        /////
+        ///// </summary>
+        ///// <param name="target"></param>
+        ///// <param name="pname"></param>
+        ///// <param name="parameters"></param>
+        //public delegate void glGetBufferParameteri64v(uint target, uint pname, Int64[] parameters);
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="target"></param>
-        /// <param name="attachment"></param>
-        /// <param name="texture"></param>
-        /// <param name="level"></param>
-        public delegate void glFramebufferTexture(uint target, uint attachment, uint texture, int level);
+        ///// <summary>
+        /////
+        ///// </summary>
+        ///// <param name="target"></param>
+        ///// <param name="attachment"></param>
+        ///// <param name="texture"></param>
+        ///// <param name="level"></param>
+        //public delegate void glFramebufferTexture(uint target, uint attachment, uint texture, int level);
 
         //  Constants
         /// <summary>
