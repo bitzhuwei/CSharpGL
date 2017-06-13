bed99aa3b67c9d8069f121282c78cd7fdeb88cd1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-13 20:50:02 +0800

    PickableRenderer

--- CSharpGL4/ModernRendering/PickableRenderer/PickableRenderer.Property.cs
+++ CSharpGL4/ModernRendering/PickableRenderer/PickableRenderer.Property.cs
@@ -6,40 +6,40 @@ namespace CSharpGL
 {
     public partial class PickableRenderer
     {
-        private const string strRenderer = "Renderer";
+        private const string strPickableRenderer = "PickableRenderer";
 
         /// <summary>
         /// Shader Program that does the rendering algorithm.
         /// </summary>
-        [Category(strRenderer)]
+        [Category(strPickableRenderer)]
         [Description("Shader Program that does the rendering algorithm.")]
         public ShaderProgram Program { get; protected set; }
 
         /// <summary>
         /// model data that can be transfermed into OpenGL Buffer Objects.
         /// </summary>
-        [Category(strRenderer)]
+        [Category(strPickableRenderer)]
         [Description("model data that can be transfermed into OpenGL Buffer Objects.")]
         public IBufferable DataSource { get; private set; }
 
         /// <summary>
         ///
         /// </summary>
-        [Category(strRenderer)]
+        [Category(strPickableRenderer)]
         [Description("OpenGL switches.")]
         public GLStateList StateList { get { return this.stateList; } }
 
         /// <summary>
         ///
         /// </summary>
-        [Category(strRenderer)]
+        [Category(strPickableRenderer)]
         [Description("index buffer(glDrawArrays or glDrawElements).")]
         public IndexBuffer IndexBuffer { get { return this.indexBuffer; } }
 
         /// <summary>
         ///  Vertex attribute buffers.
         /// </summary>
-        [Category(strRenderer)]
+        [Category(strPickableRenderer)]
         [Description("Vertex attribute buffers.")]
         //[Editor(typeof(PropertyGridEditor), typeof(UITypeEditor))]
         public IEnumerable<VertexShaderAttribute> VertexAttributeBuffers
@@ -53,7 +53,7 @@ namespace CSharpGL
         /// <summary>
         /// Vertex Array Object.
         /// </summary>
-        [Category(strRenderer)]
+        [Category(strPickableRenderer)]
         [Description("Vertex Array Object.")]
         public VertexArrayObject VertexArrayObject { get { return this.vertexArrayObject; } }
     }
