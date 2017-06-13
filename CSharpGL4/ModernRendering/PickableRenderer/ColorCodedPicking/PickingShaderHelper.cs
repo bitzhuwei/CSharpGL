f05aa699f12724e0d2a259052a953db618ebd72d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-13 23:56:01 +0800

    PickableRenderer.ctor

---
+++ CSharpGL4/ModernRendering/PickableRenderer/ColorCodedPicking/PickingShaderHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CSharpGL
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class PickingShaderHelper //: IDisposable
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        public static IShaderProgramProvider GetPickingShaderProgramProvider()
+        {
+            var vertexShader = new VertexShader(pickVertexShader, "in_Position");
+            var fragmentShader = new FragmentShader(pickFragmentShader);
+            var provider = new ShaderArray(vertexShader, fragmentShader);
+            return provider;
+        }
+
+        private static readonly string pickVertexShader =
+@"#version 150 core
+
+in vec3 in_Position;
+flat out vec4 pass_Color; // glShadeMode(GL_FLAT); in legacy opengl.
+uniform mat4 MVP;
+uniform int pickingBaseId; // how many vertices have been coded so far?
+
+void main(void) {
+	gl_Position = MVP * vec4(in_Position, 1.0);
+
+	int objectID = pickingBaseId + gl_VertexID;
+	pass_Color = vec4(
+		float(objectID & 0xFF) / 255.0, 
+		float((objectID >> 8) & 0xFF) / 255.0, 
+		float((objectID >> 16) & 0xFF) / 255.0, 
+		float((objectID >> 24) & 0xFF) / 255.0);
+}
+";
+
+        private static readonly string pickFragmentShader =
+@"#version 150 core
+flat in vec4 pass_Color; // glShadeMode(GL_FLAT); in legacy opengl.
+out vec4 out_Color;
+
+void main(void) {
+	out_Color = pass_Color;
+}
+";
+
+    }
+}
--- CSharpGL4/ModernRendering/PickableRenderer/PickableRenderer.cs
+++ CSharpGL4/ModernRendering/PickableRenderer/PickableRenderer.cs
@@ -55,6 +55,21 @@ namespace CSharpGL
         public PickableRenderer(IBufferable model, IShaderProgramProvider shaderProgramProvider,
             AttributeMap attributeMap, string positionNameInIBufferable,
             params GLState[] switches)
+            : this(model, shaderProgramProvider, attributeMap, positionNameInIBufferable, PickingShaderHelper.GetPickingShaderProgramProvider(), switches)
+        {
+        }
+
+        /// <summary>
+        /// 支持"拾取"的渲染器
+        /// </summary>
+        /// <param name="model">一种渲染方式</param>
+        /// <param name="shaderProgramProvider">各种类型的shader代码</param>
+        /// <param name="attributeMap">关联<paramref name="model"/>和<paramref name="shaderProgramProvider"/>中的属性</param>
+        /// <param name="positionNameInIBufferable">描述顶点位置信息的buffer的名字</param>
+        ///<param name="switches"></param>
+        public PickableRenderer(IBufferable model, IShaderProgramProvider shaderProgramProvider,
+            AttributeMap attributeMap, string positionNameInIBufferable, IShaderProgramProvider pickingProgramProvider,
+            params GLState[] switches)
         {
             this.PositionNameInIBufferable = positionNameInIBufferable;
 
