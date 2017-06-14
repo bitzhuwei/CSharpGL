51d828b664c5adb08ecdc9a5eff6a077a4393690
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-14 18:28:04 +0800

    demo: Hello CSharpGL

---
+++ Demos/HelloCSharpGL/FormMain.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HelloCSharpGL
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.winGLCanvas1 = new CSharpGL.WinGLCanvas();
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // winGLCanvas1
+            // 
+            this.winGLCanvas1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.winGLCanvas1.Location = new System.Drawing.Point(0, 0);
+            this.winGLCanvas1.Name = "winGLCanvas1";
+            this.winGLCanvas1.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
+            this.winGLCanvas1.Size = new System.Drawing.Size(830, 573);
+            this.winGLCanvas1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 573);
+            this.Controls.Add(this.winGLCanvas1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CSharpGL.WinGLCanvas winGLCanvas1;
+    }
+}
