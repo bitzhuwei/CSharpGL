e43da52da3936640a25c56562bb27333b5bf9a23
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-14 14:53:34 +0800

    OneIndexPicker.Searcher

--- CSharpGL4/ModernRendering/PickableRenderer/PickableRenderer.IPickable/Pickers/OneIndexPicker/OneIndexPicker.Searcher.cs
+++ CSharpGL4/ModernRendering/PickableRenderer/PickableRenderer.IPickable/Pickers/OneIndexPicker/OneIndexPicker.Searcher.cs
@@ -7,61 +7,112 @@ namespace CSharpGL
 {
     partial class OneIndexPicker
     {
-        private static Dictionary<DrawMode, OneIndexLineSearcher> lineSearcherDict;
-        private static Dictionary<DrawMode, OneIndexPointSearcher> pointSearcherDict;
+        private static readonly OneIndexLineSearcher lineInTriangle = new OneIndexLineInTriangleSearcher();
+        private static readonly OneIndexLineSearcher lineInQuad = new OneIndexLineInQuadSearcher();
+        private static readonly OneIndexLineSearcher lineInPolygon = new OneIndexLineInPolygonSearcher();
 
         private static OneIndexLineSearcher GetLineSearcher(DrawMode mode)
         {
-            if (lineSearcherDict == null)
+            OneIndexLineSearcher result = null;
+            switch (mode)
             {
-                var triangle = new OneIndexLineInTriangleSearcher();
-                var quad = new OneIndexLineInQuadSearcher();
-                var polygon = new OneIndexLineInPolygonSearcher();
-                var dict = new Dictionary<DrawMode, OneIndexLineSearcher>();
-                dict.Add(DrawMode.Triangles, triangle);
-                dict.Add(DrawMode.TrianglesAdjacency, triangle);
-                dict.Add(DrawMode.TriangleStrip, triangle);
-                dict.Add(DrawMode.TriangleStripAdjacency, triangle);
-                dict.Add(DrawMode.TriangleFan, triangle);
-                dict.Add(DrawMode.Quads, quad);
-                dict.Add(DrawMode.QuadStrip, quad);
-                dict.Add(DrawMode.Polygon, polygon);
-
-                lineSearcherDict = dict;
+                case DrawMode.Points:
+                    break;
+                case DrawMode.Lines:
+                    break;
+                case DrawMode.LineLoop:
+                    break;
+                case DrawMode.LineStrip:
+                    break;
+                case DrawMode.Triangles:
+                    result = lineInTriangle;
+                    break;
+                case DrawMode.TriangleStrip:
+                    result = lineInTriangle;
+                    break;
+                case DrawMode.TriangleFan:
+                    result = lineInTriangle;
+                    break;
+                case DrawMode.Quads:
+                    result = lineInQuad;
+                    break;
+                case DrawMode.QuadStrip:
+                    result = lineInQuad;
+                    break;
+                case DrawMode.Polygon:
+                    result = lineInPolygon;
+                    break;
+                case DrawMode.LinesAdjacency:
+                    break;
+                case DrawMode.LineStripAdjacency:
+                    break;
+                case DrawMode.TrianglesAdjacency:
+                    result = lineInTriangle;
+                    break;
+                case DrawMode.TriangleStripAdjacency:
+                    result = lineInTriangle;
+                    break;
+                case DrawMode.Patches:
+                    break;
+                default:
+                    break;
             }
 
-            OneIndexLineSearcher result = null;
-            if (lineSearcherDict.TryGetValue(mode, out result))
-            { return result; }
-            else
-            { return null; }
+            return result;
         }
+
+        private static readonly OneIndexPointSearcher pointInTriangle = new OneIndexPointInTriangleSearcher();
+        private static readonly OneIndexPointSearcher pointInQuad = new OneIndexPointInQuadSearcher();
+        private static readonly OneIndexPointSearcher pointInPolygon = new OneIndexPointInPolygonSearcher();
 
         private static OneIndexPointSearcher GetPointSearcher(DrawMode mode)
         {
-            if (pointSearcherDict == null)
+            OneIndexPointSearcher result = null;
+            switch (mode)
             {
-                var triangle = new OneIndexPointInTriangleSearcher();
-                var quad = new OneIndexPointInQuadSearcher();
-                var polygon = new OneIndexPointInPolygonSearcher();
-                var dict = new Dictionary<DrawMode, OneIndexPointSearcher>();
-                dict.Add(DrawMode.Triangles, triangle);
-                dict.Add(DrawMode.TrianglesAdjacency, triangle);
-                dict.Add(DrawMode.TriangleStrip, triangle);
-                dict.Add(DrawMode.TriangleStripAdjacency, triangle);
-                dict.Add(DrawMode.TriangleFan, triangle);
-                dict.Add(DrawMode.Quads, quad);
-                dict.Add(DrawMode.QuadStrip, quad);
-                dict.Add(DrawMode.Polygon, polygon);
-
-                pointSearcherDict = dict;
+                case DrawMode.Points:
+                    break;
+                case DrawMode.Lines:
+                    break;
+                case DrawMode.LineLoop:
+                    break;
+                case DrawMode.LineStrip:
+                    break;
+                case DrawMode.Triangles:
+                    result = pointInTriangle;
+                    break;
+                case DrawMode.TriangleStrip:
+                    result = pointInTriangle;
+                    break;
+                case DrawMode.TriangleFan:
+                    result = pointInTriangle;
+                    break;
+                case DrawMode.Quads:
+                    result = pointInQuad;
+                    break;
+                case DrawMode.QuadStrip:
+                    result = pointInQuad;
+                    break;
+                case DrawMode.Polygon:
+                    result = pointInPolygon;
+                    break;
+                case DrawMode.LinesAdjacency:
+                    break;
+                case DrawMode.LineStripAdjacency:
+                    break;
+                case DrawMode.TrianglesAdjacency:
+                    result = pointInTriangle;
+                    break;
+                case DrawMode.TriangleStripAdjacency:
+                    result = pointInTriangle;
+                    break;
+                case DrawMode.Patches:
+                    break;
+                default:
+                    break;
             }
 
-            OneIndexPointSearcher result = null;
-            if (pointSearcherDict.TryGetValue(mode, out result))
-            { return result; }
-            else
-            { return null; }
+            return result;
         }
     }
 }
