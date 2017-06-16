bf0a68d99b2d7aaa507210a902a1703434f3f230
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-16 17:03:49 +0800

    cledan

--- CSharpGL4/Scene/Scene.PickRendererBases.cs
+++ CSharpGL4/Scene/Scene.PickRendererBases.cs
@@ -20,40 +20,31 @@ namespace CSharpGL
         /// <returns></returns>
         public List<RendererBase> Pick(int x, int y)
         {
-            var viewport = new int[4];
-            //	Get the viewport, then convert the mouse point to an opengl point.
-            GL.Instance.GetIntegerv((uint)GetTarget.Viewport, viewport);
-
             //	Create a select buffer.
             var selectBuffer = new uint[512];
             GL.Instance.SelectBuffer(selectBuffer.Length, selectBuffer);
+
             //	Enter select mode.
             GL.Instance.RenderMode(GL.GL_SELECT);
             //	Initialise the names, and add the first name.
             GL.Instance.InitNames();
             GL.Instance.PushName(0);
 
+            var viewport = new int[4];
+            //	Get the viewport, then convert the mouse point to an opengl point.
+            GL.Instance.GetIntegerv((uint)GetTarget.Viewport, viewport);
             ////	Push matrix, set up projection, then load matrix.
             mat4 pickMatrix = glm.pickMatrix(new vec2(x, y), new vec2(4, 4), new ivec4(viewport[0], viewport[1], viewport[2], viewport[3]));
             var arg = new LegacyPickEventArgs(pickMatrix, this, x, y);
-
-            {
-                //const float one = 1.0f;
-                //GL.Instance.ClearColor(one, one, one, one);
-                //GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
-
-                uint currentName = 1;
-                this.RenderForPicking(this.RootElement, arg, ref currentName);
-            }
-
+            uint currentName = 1;
+            this.RenderForPicking(this.RootElement, arg, ref currentName);
             //	Flush commands.
             GL.Instance.Flush();
 
-            //  Create  result set.
-            var pickedRenderer = new List<RendererBase>();
-
             //	End selection.
             int hits = GL.Instance.RenderMode(GL.GL_RENDER);
+            //  Create  result set.
+            var pickedRenderer = new List<RendererBase>();
             uint posinarray = 0;
             //  Go through each name.
             for (int hit = 0; hit < hits; hit++)
