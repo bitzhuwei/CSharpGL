159657ca005284c98dcc19d9f389a86af702c6a5
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-16 19:18:33 +0800

    update

--- Demos/HelloCSharpGL/FormMain.cs
+++ Demos/HelloCSharpGL/FormMain.cs
@@ -14,7 +14,6 @@ namespace HelloCSharpGL
     public partial class FormMain : Form
     {
         Scene scene;
-        private ProperllerRenderer properller;
 
         public FormMain()
         {
@@ -28,19 +27,15 @@ namespace HelloCSharpGL
 
         private void FormMain_Load(object sender, EventArgs e)
         {
-            var position = new vec3(0, 0, 4);
+            var position = new vec3(5, 3, 4);
             var center = new vec3(0, 0, 0);
             var up = new vec3(0, 1, 0);
             var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
-            var clock = new ClockRenderer();
-            var properller = new ProperllerRenderer() { WorldPosition = new vec3(0, -1f, 0) };
-            var group = new RendererGroup(clock, properller);
+            var properller = new ProperllerRenderer();
             this.scene = new Scene(camera, this.winGLCanvas1)
             {
-                RootElement = group,
+                RootElement = properller,
             };
-
-            this.properller = properller;
         }
 
         private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
@@ -75,7 +70,7 @@ namespace HelloCSharpGL
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            IWorldSpace renderer = this.properller;
+            IWorldSpace renderer = this.scene.RootElement;
             if (renderer != null)
             {
                 renderer.RotationAngle += 7;
