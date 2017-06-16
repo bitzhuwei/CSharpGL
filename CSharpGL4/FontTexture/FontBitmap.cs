1764bd4053d4f4e8cb99e6f035633b6b7cd1bd05
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-17 00:56:56 +0800

    FontTexture

---
+++ CSharpGL4/FontTexture/FontBitmap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// font, bitmap and texture coordiante.
+    /// </summary>
+    public partial class FontBitmap : IDisposable
+    {
+        /// <summary>
+        /// font of glyphs in <see cref="GlyphBitmap"/>.
+        /// </summary>
+        public Font GlyphFont { get; internal set; }
+
+        /// <summary>
+        /// bitmap in which glyphs is printed.
+        /// </summary>
+        public Bitmap GlyphBitmap { get; internal set; }
+
+        /// <summary>
+        /// glyph's height.
+        /// </summary>
+        public int GlyphHeight { get; internal set; }
+
+        /// <summary>
+        /// glyph information dictionary.
+        /// </summary>
+        public FullDictionary<char, GlyphInfo> GlyphInfoDictionary { get; internal set; }
+
+        internal FontBitmap()
+        {
+            this.GlyphInfoDictionary = new FullDictionary<char, GlyphInfo>(GlyphInfo.Default);
+        }
+    }
+}
