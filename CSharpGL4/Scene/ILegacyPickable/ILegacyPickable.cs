e22d706f985e8690b137eca1cda8a2b689af6896
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-17 13:02:58 +0800

    update

--- CSharpGL4/Scene/ILegacyPickable/ILegacyPickable.cs
+++ CSharpGL4/Scene/ILegacyPickable/ILegacyPickable.cs
@@ -6,12 +6,12 @@ using System.Text;
 namespace CSharpGL
 {
     /// <summary>
-    /// 
+    /// Supports picking in legacy OpenGL.
     /// </summary>
     public interface ILegacyPickable
     {
         /// <summary>
-        /// 
+        /// Render this model in legacy OpenGL.
         /// </summary>
         /// <param name="arg"></param>
         void RenderForLegacyPicking(LegacyPickEventArgs arg);
