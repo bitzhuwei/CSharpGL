12eacb867b17c3d4cbdc0eba302b972292f614b3
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-20 19:34:19 +0800

    update

---
+++ Demos/Transformers/FormMain.cs
@@ -0,0 +1,52 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Transformers
+{
+    public partial class FormMain : Form
+    {
+        private Scene scene;
+        private TeapotRenderer teapot;
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Load += FormMain_Load;
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            var position = new vec3(5, 3, 4);
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+            this.teapot = TeapotRenderer.Create();
+            teapot.Children.Add(new LegacyBoundingBoxRenderer(teapot.ModelSize));
+
+            this.scene = new Scene(camera, this.winGLCanvas1)
+            {
+                RootElement = teapot,
+            };
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            this.scene.Render();
+        }
+
+        void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
+        }
+    }
+}
