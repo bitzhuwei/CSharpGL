898fbfaeae28673107e602e13b019b23a7e8b66a
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-21 23:39:46 +0800

    OpenGLHardwareDescription

---
+++ Demos/OpenGLHardwareDescription/FormMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace OpenGLHardwareDescription
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.winGLCanvas1 = new CSharpGL.WinGLCanvas();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // winGLCanvas1
+            // 
+            this.winGLCanvas1.Location = new System.Drawing.Point(490, 301);
+            this.winGLCanvas1.Name = "winGLCanvas1";
+            this.winGLCanvas1.RenderTrigger = CSharpGL.RenderTrigger.Manual;
+            this.winGLCanvas1.Size = new System.Drawing.Size(11, 10);
+            this.winGLCanvas1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(814, 596);
+            this.textBox1.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 596);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.winGLCanvas1);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CSharpGL.WinGLCanvas winGLCanvas1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
