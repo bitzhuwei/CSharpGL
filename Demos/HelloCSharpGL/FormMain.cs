487be8463e2a9ea0032d63fe635130b92af1ed34
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-22 10:09:04 +0800

    update

--- Demos/HelloCSharpGL/FormMain.cs
+++ Demos/HelloCSharpGL/FormMain.cs
@@ -30,12 +30,12 @@ namespace HelloCSharpGL
             var center = new vec3(0, 0, 0);
             var up = new vec3(0, 1, 0);
             var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
-            //var propeller = GetLegacyPropellerLegacyFlabellum();
+            var propeller = GetLegacyPropellerLegacyFlabellum();
             //var propeller = GetLegacyPropellerFlabellum();
             //var propeller = GetPropellerLegacyFlabellum();
             //var propeller = GetPropellerFlabellum();
-            var propeller = new LegacyRectangleRenderer();
-            propeller.Children.Add(new LegacyBoundingBoxRenderer(propeller.ModelSize));
+            //var propeller = new LegacyRectangleRenderer();
+            //propeller.Children.Add(new LegacyBoundingBoxRenderer(propeller.ModelSize));
             this.scene = new Scene(camera, this.winGLCanvas1)
             {
                 RootElement = propeller,
