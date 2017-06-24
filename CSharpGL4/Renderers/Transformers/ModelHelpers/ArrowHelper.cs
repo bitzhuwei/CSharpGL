cd47fc544b7e2bb0089b8203828d9e7db99cd171
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-06-24 22:56:40 +0800

    update

---
+++ CSharpGL4/Renderers/Transformers/ModelHelpers/ArrowHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    ///// <summary>
+    ///// 
+    ///// </summary>
+    //static class ArrayHelper
+    //{
+    //    //public static vec3[] 
+    //}
+
+    //class ddd
+    //{
+    //    public readonly vec3[] positions;
+    //    public readonly vec3[] colors;
+    //    public readonly vec3[] normals;
+
+    //    public readonly DrawMode
+    //}
+}
