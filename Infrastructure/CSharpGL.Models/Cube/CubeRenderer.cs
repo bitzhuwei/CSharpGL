068cae7f14159827679a8c192bd2151b5511fe4a
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-07-13 11:36:14 +0800

    CubeRenderer

---
+++ Infrastructure/CSharpGL.Models/Cube/CubeRenderer.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// </summary>
+    public class CubeRenderer : PickableRenderer
+    {
+        private const string inPosition = "inPosition";
+        private const string inUV = "inUV";
+        private const string projectionMatrix = "projectionMatrix";
+        private const string viewMatrix = "viewMatrix";
+        private const string modelMatrix = "modelMatrix";
+        private const string color = "color";
+        private const string vertexCode =
+            @"#version 330 core
+
+in vec3 " + inPosition + @";
+in vec2 " + inUV + @";
+
+uniform mat4 " + projectionMatrix + @";
+uniform mat4 " + viewMatrix + @";
+uniform mat4 " + modelMatrix + @";
+
+out vec2 passUV;
+
+void main(void) {
+	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(inPosition, 1.0);
+	passUV = inUV;
+}
+";
+        private const string fragmentCode =
+            @"#version 330 core
+
+in vec2 passUV;
+
+uniform vec4 " + color + @";
+
+layout(location = 0) out vec4 out_Color;
+//out vec4 out_Color;
+
+void main(void) {
+    out_Color = color;
+}
+";
+        /// <summary>
+        /// 
+        /// </summary>
+        public vec4 Color { get; set; }
+
+        /// <summary>
+        /// Render propeller in modern opengl.
+        /// </summary>
+        /// <returns></returns>
+        public static CubeRenderer Create()
+        {
+            var vertexShader = new VertexShader(vertexCode, inPosition, inUV);
+            var fragmentShader = new FragmentShader(fragmentCode);
+            var provider = new ShaderArray(vertexShader, fragmentShader);
+            var map = new AttributeMap();
+            map.Add(inPosition, RectangleModel.strPosition);
+            map.Add(inUV, RectangleModel.strUV);
+            var renderer = new CubeRenderer(new CubeModel(), provider, map, inPosition);
+            renderer.Initialize();
+
+            return renderer;
+        }
+
+        /// <summary>
+        /// Render propeller in legacy opengl.
+        /// </summary>
+        private CubeRenderer(CubeModel model, IShaderProgramProvider renderProgramProvider,
+            AttributeMap attributeMap, string positionNameInVertexShader,
+            params GLState[] switches)
+            : base(model, renderProgramProvider, attributeMap, positionNameInVertexShader, switches)
+        {
+            this.ModelSize = model.ModelSize;
+            this.Color = new vec4(1, 1, 1, 1);
+        }
+
+        protected override void DoRender(RenderEventArgs arg)
+        {
+            ICamera camera = arg.CameraStack.Peek();
+            mat4 projection = camera.GetProjectionMatrix();
+            mat4 view = camera.GetViewMatrix();
+            mat4 model = this.GetModelMatrix();
+            this.SetUniform(projectionMatrix, projection);
+            this.SetUniform(viewMatrix, view);
+            this.SetUniform(modelMatrix, model);
+            this.SetUniform(color, this.Color);
+
+            base.DoRender(arg);
+        }
+
+    }
+
+    class CubeModel : IBufferable
+    {
+        public vec3 ModelSize { get; private set; }
+
+        public CubeModel()
+        {
+            this.ModelSize = new vec3(xLength * 2, yLength * 2, (xLength + yLength) * 0.02f);
+        }
+
+        public const string strPosition = "position";
+        private VertexBuffer positionBuffer;
+
+        private IndexBuffer indexBuffer;
+
+        #region IBufferable 成员
+
+        public VertexBuffer GetVertexAttributeBuffer(string bufferName, string varNameInShader)
+        {
+            if (bufferName == strPosition)
+            {
+                if (this.positionBuffer == null)
+                {
+                    this.positionBuffer = positions.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+
+                return this.positionBuffer;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public IndexBuffer GetIndexBuffer()
+        {
+            if (this.indexBuffer == null)
+            {
+                this.indexBuffer = ZeroIndexBuffer.Create(DrawMode.TriangleStrip, 0, positions.Length);
+            }
+
+            return this.indexBuffer;
+        }
+
+        #endregion
+
+        private const float xLength = 0.5f;
+        private const float yLength = 0.5f;
+        private const float zLength = 0.5f;
+        /// <summary>
+        /// four vertexes.
+        /// </summary>
+        private static readonly vec3[] positions = new vec3[]
+        {
+            new vec3(+xLength, +yLength, +zLength),//  0
+            new vec3(+xLength, -yLength, +zLength),//  1
+            new vec3(+xLength, +yLength, -zLength),//  2
+            new vec3(+xLength, -yLength, -zLength),//  3
+            new vec3(-xLength, -yLength, -zLength),//  4
+            new vec3(+xLength, -yLength, +zLength),//  5
+            new vec3(-xLength, -yLength, +zLength),//  6
+            new vec3(+xLength, +yLength, +zLength),//  7
+            new vec3(-xLength, +yLength, +zLength),//  8
+            new vec3(+xLength, +yLength, -zLength),//  9
+            new vec3(-xLength, +yLength, -zLength),// 10
+            new vec3(-xLength, -yLength, -zLength),// 11
+            new vec3(-xLength, +yLength, +zLength),// 12
+            new vec3(-xLength, -yLength, +zLength),// 13
+        };
+    }
+}
