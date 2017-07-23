0dee6382211e5035b607ce6e4fd4a30c7a3439c1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-07-24 00:42:19 +0800

    clean

--- CSharpGL4/Scene/Actions/RenderAction.cs
+++ CSharpGL4/Scene/Actions/RenderAction.cs
@@ -21,34 +21,6 @@ namespace CSharpGL
         /// <summary>
         /// Render <see cref="IRenderable"/> objects.
         /// </summary>
-        /// <param name="clear"></param>
-        /// <param name="clearColor"></param>
-        /// <param name="rootElement"></param>
-        /// <param name="camera"></param>
-        /// <param name="firstPass"></param>
-        public static void Render(bool clear, vec4 clearColor, SceneNodeBase rootElement, ICamera camera, bool firstPass)
-        {
-            int[] value = null;
-            if (clear)
-            {
-                value = new int[4];
-                GL.Instance.GetIntegerv((uint)GetTarget.ColorClearValue, value);
-                GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
-                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
-            }
-
-            var arg = new RenderEventArgs(camera);
-            RenderAction.Render(rootElement, arg, firstPass, true);
-
-            if (clear)
-            {
-                GL.Instance.ClearColor(value[0], value[1], value[2], value[3]);
-            }
-        }
-
-        /// <summary>
-        /// Render <see cref="IRenderable"/> objects.
-        /// </summary>
         /// <param name="rootElement"></param>
         /// <param name="camera"></param>
         public RenderAction(SceneNodeBase rootElement, ICamera camera)
@@ -96,6 +68,13 @@ namespace CSharpGL
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sceneElement"></param>
+        /// <param name="arg"></param>
+        /// <param name="firstPass"></param>
+        /// <param name="renderThis"></param>
         public static void Render(SceneNodeBase sceneElement, RenderEventArgs arg, bool firstPass, bool renderThis)
         {
             if (sceneElement != null)
