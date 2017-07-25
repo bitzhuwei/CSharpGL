77f115db9f0919fb8bbdfe07cc327773137dd6b2
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-07-25 20:19:53 +0800

    no need to update IWorldSpace when picking

--- CSharpGL4/Scene/Scene.PickGeometry.cs
+++ CSharpGL4/Scene/Scene.PickGeometry.cs
@@ -140,9 +140,6 @@ namespace CSharpGL
         {
             if (sceneElement != null)
             {
-                mat4 parentCascadeModelMatrix = arg.ModelMatrixStack.Peek();
-                sceneElement.cascadeModelMatrix = sceneElement.GetModelMatrix(parentCascadeModelMatrix);
-
                 var pickable = sceneElement as IPickable;
                 TwoFlags flags = (pickable != null) ? pickable.EnablePicking : TwoFlags.None;
                 bool before = (pickable != null) && ((flags & TwoFlags.BeforeChildren) == TwoFlags.BeforeChildren);
@@ -157,12 +154,10 @@ namespace CSharpGL
 
                 if (children)
                 {
-                    arg.ModelMatrixStack.Push(sceneElement.cascadeModelMatrix);
                     foreach (var item in sceneElement.Children)
                     {
                         this.RenderForPicking(item, arg);
                     }
-                    arg.ModelMatrixStack.Pop();
                 }
             }
         }
