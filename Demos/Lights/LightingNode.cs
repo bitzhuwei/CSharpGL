f6b73cbf7dd82cce9a32371ee43624708eb0b793
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-07-31 14:39:56 +0800

    lighting node

--- Demos/Lights/LightingNode.cs
+++ Demos/Lights/LightingNode.cs
@@ -6,11 +6,39 @@ using CSharpGL;
 
 namespace Lights
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public partial class LightingNode : PickableNode
     {
-        public static LightingNode Create()
+        const int pointLightIndex = 0;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="position"></param>
+        /// <param name="normal"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static LightingNode Create(IBufferSource model, string position, string normal, vec3 size)
         {
-            throw new NotImplementedException();
+            var builders = new List<RenderUnitBuilder>();
+            {
+                var vs = new VertexShader(pointLightVert, vPosition, vNormal);
+                var fs = new FragmentShader(pointLightFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add(vPosition, position);
+                map.Add(vNormal, normal);
+                builders.Add(new RenderUnitBuilder(provider, map));
+            }
+
+            var node = new LightingNode(model, position, builders.ToArray());
+            node.ModelSize = size;
+
+            node.Initialize();
+
+            return node;
         }
 
         private LightingNode(IBufferSource model, string positionNameInIBufferSource, params RenderUnitBuilder[] builders)
