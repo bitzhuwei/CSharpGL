a378d723079d4dc6bc4359deada2f4458385c173
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-03 01:33:34 +0800

    OrderIndependentTransparency

--- Demos/OrderIndependentTransparency/OITNode.cs
+++ Demos/OrderIndependentTransparency/OITNode.cs
@@ -8,10 +8,39 @@ namespace OrderIndependentTransparency
 {
     public partial class OITNode : PickableNode
     {
+        private const string vPosition = "vPosition";
+        private const string vNormal = "vNormal";
 
-        public static OITNode Create()
+        const int buildLists = 0;
+        const int resolveLists = 1;
+        public static OITNode Create(IBufferSource model, string position, string normal, vec3 size)
         {
-            throw new NotImplementedException();
+            var builders = new RenderUnitBuilder[2];
+            {
+                var vs = new VertexShader(buildListsVert, vPosition, vNormal);
+                var fs = new FragmentShader(buildListsFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add(vPosition, position);
+                map.Add(vNormal, normal);
+                builders[buildLists] = new RenderUnitBuilder(provider, map);
+            }
+            {
+                var vs = new VertexShader(resolveListsVert, vPosition, vNormal);
+                var fs = new FragmentShader(resolveListsFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add(vPosition, position);
+                map.Add(vNormal, normal);
+                builders[resolveLists] = new RenderUnitBuilder(provider, map);
+            }
+            var node = new OITNode(model, position, builders);
+            node.ModelSize = size;
+            node.Children.Add(new LegacyBoundingBoxNode(size));
+
+            node.Initialize();
+
+            return node;
         }
 
         private OITNode(IBufferSource model, string positionNameInIBufferSource, params RenderUnitBuilder[] builders)
