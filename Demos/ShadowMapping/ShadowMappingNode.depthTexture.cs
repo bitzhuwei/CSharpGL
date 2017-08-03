4724211750f9c948769dc8cfffe68ee407af13c8
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-03 14:23:53 +0800

    update

---
+++ Demos/ShadowMapping/ShadowMappingNode.depthTexture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace ShadowMapping
+{
+    /// <summary>
+    /// render a teapot with shadow.
+    /// </summary>
+    public partial class ShadowMappingNode : PickableNode, IShadowMapping
+    {
+        private const string shadowVertexCode =
+    @"#version 330
+
+uniform mat4 " + mvpMatrix + @";
+
+layout (location = 0) in vec4 " + inPosition + @";;
+
+void main(void)
+{
+	gl_Position = mvpMatrix * position;
+}
+";
+        // this fragment shader is not needed.
+        //        private const string shadowFragmentCode =
+        //            @"#version 330 core
+        //
+        //layout(location = 0) out float fragmentdepth;
+        ////out vec4 out_Color;
+        //
+        //void main(void) {
+        //    fragmentdepth = gl_FragCoord.z;
+        //
+        //}
+        //";
+
+    }
+}
---
+++ Demos/ShadowMapping/ShadowMappingNode.shadowMapping.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace ShadowMapping
+{
+    /// <summary>
+    /// render a teapot with shadow.
+    /// </summary>
+    public partial class ShadowMappingNode : PickableNode, IShadowMapping
+    {
+        private const string lightVertexCode =
+    @"#version 330
+
+uniform mat4 " + model_matrix + @";
+uniform mat4 " + view_matrix + @";
+uniform mat4 " + projection_matrix + @";
+
+uniform mat4 " + shadow_matrix + @";
+
+layout (location = 0) in vec4 " + inPosition + @";
+layout (location = 1) in vec3 " + inNormal + @";
+
+out VS_FS_INTERFACE
+{
+	vec4 shadow_coord;
+	vec3 world_coord;
+	vec3 eye_coord;
+	vec3 normal;
+} vertex;
+
+void main(void)
+{
+	vec4 world_pos = model_matrix * position;
+	vec4 eye_pos = view_matrix * world_pos;
+	vec4 clip_pos = projection_matrix * eye_pos;
+	
+	vertex.world_coord = world_pos.xyz;
+	vertex.eye_coord = eye_pos.xyz;
+	vertex.shadow_coord = shadow_matrix * world_pos;
+	vertex.normal = normalize(mat3(view_matrix * model_matrix) * normal);
+	
+	gl_Position = clip_pos;
+}
+";
+        private const string lightFragmentCode =
+            @"#version 330
+
+uniform sampler2DShadow " + depth_texture + @";
+uniform vec3 " + light_position + @";
+
+uniform vec3 " + material_ambient + @";
+uniform vec3 " + material_diffuse + @";
+uniform vec3 " + material_specular + @";
+uniform float " + material_specular_power + @";
+
+layout (location = 0) out vec4 color;
+
+in VS_FS_INTERFACE
+{
+	vec4 shadow_coord;
+	vec3 world_coord;
+	vec3 eye_coord;
+	vec3 normal;
+} fragment;
+
+void main(void)
+{
+	vec3 N = normalize(fragment.normal);
+	vec3 L = normalize(light_position - fragment.eye_coord);
+	vec3 R = reflect(L, N);
+	vec3 E = normalize(fragment.eye_coord);
+	float NdotL = dot(N, L);
+	float EdotR = dot(E, R);
+	float diffuse = max(NdotL, 0.0);
+	float specular = max(pow(EdotR, material_specular_power), 0.0);
+	float f = textureProj(depth_texture, fragment.shadow_coord);
+	
+	color = vec4(material_ambient + f * (material_diffuse * diffuse + material_specular * specular), 1.0);
+}
+";
+
+    }
+}
