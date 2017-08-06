1b66b3896334119deb38b6ba2ec298053460d48c
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-06 13:19:54 +0800

    update

--- Demos/HowTransformFeedbackWorks/SimpleTransformFeedBackNode.render.cs
+++ Demos/HowTransformFeedbackWorks/SimpleTransformFeedBackNode.render.cs
@@ -8,6 +8,27 @@ namespace HowTransformFeedbackWorks
 {
     partial class SimpleTransformFeedBackNode
     {
+        private const string renderVert = @"#version 330
 
+in vec3 inPosition; // world space
+
+uniform mat4 mvpMatrix;
+
+void main()
+{
+    gl_Position = mapMatrix * vec4(inPosition, 1);
+}
+";
+        private const string renderFrag = @"#version 330
+
+uniform vec3 color = vec3(1, 0, 0);
+
+out vec4 vFragColor;
+
+void main()
+{
+    vFragColor = vec4(color, 1);
+}
+";
     }
 }
