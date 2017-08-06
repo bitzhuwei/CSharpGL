2c34cf03079f5260eb2746182dbb8109ee5d0bd7
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-06 13:17:40 +0800

    update

---
+++ Demos/HowTransformFeedbackWorks/SimpleTransformFeedBackNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace HowTransformFeedbackWorks
+{
+    partial class SimpleTransformFeedBackNode : SceneNodeBase, IRenderable
+    {
+
+        #region IRenderable 成员
+
+        public ThreeFlags EnableRendering { get { return ThreeFlags.BeforeChildren | ThreeFlags.Children; } set { } }
+
+        public void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderAfterChildren(RenderEventArgs arg)
+        {
+        }
+
+        #endregion
+    }
+}
