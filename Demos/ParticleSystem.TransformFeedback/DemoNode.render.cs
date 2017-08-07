c6824063630bfa88758a054f02b9cfbbb475a2fa
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-07 17:27:14 +0800

    update

--- Demos/ParticleSystem.TransformFeedback/DemoNode.render.cs
+++ Demos/ParticleSystem.TransformFeedback/DemoNode.render.cs
@@ -9,29 +9,42 @@ namespace ParticleSystem.TransformFeedback
     partial class DemoNode
     {
         private const string renderVert = @"#version 330
-
-in vec3 inPosition; // world space
-in vec3 inVelocity;
-
-uniform mat4 mvpMatrix;
-
-out vec3 passColor;
-
+layout(location = 0) in vec4 vPosition;
+void main() 
+{
+    gl_Position = vPosition;
+}
+";
+        private const string renerGeom = @"#version 330
+uniform mat4 View;
+uniform mat4 Projection;
+layout (points) in;
+layout (triangle_strip, max_vertices = 4) out;
+out vec2 txcoord;
 void main()
 {
-    gl_Position = mvpMatrix * vec4(inPosition, 1);
-    passColor = inVelocity;
+    vec4 pos = View*gl_in[0].gl_Position;
+    txcoord = vec2(-1,-1);
+    gl_Position = Projection*(pos+0.2*vec4(txcoord,0,0));
+    EmitVertex();
+    txcoord = vec2( 1,-1);
+    gl_Position = Projection*(pos+0.2*vec4(txcoord,0,0));
+    EmitVertex();
+    txcoord = vec2(-1, 1);
+    gl_Position = Projection*(pos+0.2*vec4(txcoord,0,0));
+    EmitVertex();
+    txcoord = vec2( 1, 1);
+    gl_Position = Projection*(pos+0.2*vec4(txcoord,0,0));
+    EmitVertex();
 }
 ";
         private const string renderFrag = @"#version 330
-
-in vec3 passColor;
-
-out vec4 vFragColor;
-
+in vec2 txcoord;
+layout(location = 0) out vec4 vFragColor;
 void main()
 {
-    vFragColor = vec4(passColor, 1);
+    float s = 0.2*(1/(1+15.*dot(txcoord, txcoord))-1/16.);
+    vFragColor = s*vec4(0.3,0.3,1.0,1);
 }
 ";
     }
--- Demos/ParticleSystem.TransformFeedback/DemoNode.update.cs
+++ Demos/ParticleSystem.TransformFeedback/DemoNode.update.cs
@@ -9,27 +9,40 @@ namespace ParticleSystem.TransformFeedback
     partial class DemoNode
     {
         private const string updateVert = @"#version 330
+uniform vec3 center[3];
+uniform float radius[3];
+uniform vec3 g;
+uniform float dt;
+uniform float bounce;
+uniform int seed;
+layout(location = 0) in vec3 inposition;
+layout(location = 1) in vec3 invelocity;
+out vec3 outposition;
+out vec3 outvelocity;
 
-in vec3 inPosition;// world space.
-in vec3 inVelocity;// world space.
+float hash(int x) {
+    x = x*1235167 + gl_VertexID*948737 + seed*9284365;
+    x = (x >> 13) ^ x;
+    return ((x * (x * x * 60493 + 19990303) + 1376312589) & 0x7fffffff)/float(0x7fffffff-1);
+}
 
-out vec3 outPosition;// world space
-out vec3 outVelocity;// world space
-
-uniform float time = 0.01;
-
-void main()
-{
-    vec3 pos = inPosition + inVelocity * time;
-    if (pos.x >3 || pos.y > 3 || pos.z > 3)
-    {
-        pos = vec3(0, 0, 0);
+void main() {
+    outvelocity = invelocity;
+    for(int j = 0;j<3;++j) {
+        vec3 diff = inposition-center[j];
+        float dist = length(diff);
+        float vdot = dot(diff, invelocity);
+        if(dist<radius[j] && vdot<0.0)
+            outvelocity -= bounce*diff*vdot/(dist*dist);
     }
-    
-    outPosition = pos;
-    outVelocity = inVelocity;
-
-    gl_Position = vec4(pos, 1); 
+    outvelocity += dt*g;
+    outposition = inposition + dt*outvelocity;
+    if(outposition.y < -30.0)
+    {
+        outvelocity = vec3(0,0,0);
+        outposition = 0.5-vec3(hash(3*gl_VertexID+0),hash(3*gl_VertexID+1),hash(3*gl_VertexID+2));
+        outposition = vec3(0,20,0) + 5.0*outposition;
+    }
 }
 ";
     }
