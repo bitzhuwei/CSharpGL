6d3fbd280da2c1f181173251a307ef1d9c14e5b8
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-07 13:58:54 +0800

    update

--- Demos/HowTransformFeedbackWorks/DemoNode.cs
+++ Demos/HowTransformFeedbackWorks/DemoNode.cs
@@ -14,83 +14,58 @@ namespace HowTransformFeedbackWorks
         private const string outVelocity = "outVelocity";
         private const string mvpMatrix = "mvpMatrix";
         private TransformFeedbackObject transformFeedbackObject;
-        private ShaderProgram updateProgram;
-        private ShaderProgram renderProgram;
-        private VertexBuffer[] positionBuffers = new VertexBuffer[2];
-        private VertexBuffer[] velocityBuffers = new VertexBuffer[2];
-        private IndexBuffer[] indexBuffers = new IndexBuffer[2];
-        private VertexArrayObject[] updateVAOs = new VertexArrayObject[2];
-        private VertexArrayObject[] renderVAOs = new VertexArrayObject[2];
         private int currentIndex = 0;
 
-        private static readonly vec3[] positions = new vec3[3];
-        //{
-        //new vec3(1, 0, 0), new vec3(0, 0, 1), new vec3(-1, 0, 0),
-        //new vec3(0, 0, -1),
-        //};
-        private static readonly vec3[] velocitys = new vec3[3]
+        public static DemoNode Create()
         {
-            new vec3(1, 0, 0), new vec3(0, 1, 0), new vec3(0, 0, 1), 
-            //new vec3(0, 0, -1),
-        };
-
-        public DemoNode()
-            : base(null)
-        {
-            var tf = new TransformFeedbackObject();
-            this.transformFeedbackObject = tf;
+            IShaderProgramProvider updateProvider, renderProvider;
             {
                 var vs = new VertexShader(updateVert, inPosition, inVelocity);
-                var program = new ShaderProgram();
-                var capture = new string[] { outPosition, outVelocity };
-                program.Initialize(capture, ShaderProgram.BufferMode.Separate, vs);
-                this.updateProgram = program;
-
+                var feedbackVaryings = new string[] { outPosition, outVelocity };
+                updateProvider = new ShaderArray(feedbackVaryings, ShaderProgram.BufferMode.Separate, vs);
             }
             {
                 var vs = new VertexShader(renderVert, inPosition);
                 var fs = new FragmentShader(renderFrag);
-                var program = new ShaderProgram();
-                program.Initialize(vs, fs);
-                this.renderProgram = program;
+                renderProvider = new ShaderArray(vs, fs);
+            }
+            RenderUnitBuilder updateBuilder, updateBuilder2, renderBuilder, renderBuilder2;
+            {
+                var map = new AttributeMap();
+                map.Add(inPosition, DemoModel.inPosition);
+                map.Add(inVelocity, DemoModel.inVelocity);
+                updateBuilder = new RenderUnitBuilder(updateProvider, map);
             }
             {
-                for (int i = 0; i < 2; i++)
-                {
-                    VertexBuffer buffer = positions.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.DynamicCopy);
-                    this.positionBuffers[i] = buffer;
-                }
-                for (int i = 0; i < 2; i++)
-                {
-                    VertexBuffer buffer = velocitys.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.DynamicCopy);
-                    this.velocityBuffers[i] = buffer;
-                }
-                for (int i = 0; i < 2; i++)
-                {
-                    IndexBuffer buffer = ZeroIndexBuffer.Create(DrawMode.Triangles, 0, positions.Length);
-                    this.indexBuffers[i] = buffer;
-                }
+                var map = new AttributeMap();
+                map.Add(inPosition, DemoModel.inPosition2);
+                map.Add(inVelocity, DemoModel.inVelocity2);
+                updateBuilder2 = new RenderUnitBuilder(updateProvider, map);
             }
             {
-                for (int i = 0; i < 2; i++)
-                {
-                    var attributes = new VertexShaderAttribute[2];
-                    attributes[0] = new VertexShaderAttribute(this.positionBuffers[i], inPosition);
-                    attributes[1] = new VertexShaderAttribute(this.velocityBuffers[i], inVelocity);
-                    var vao = new VertexArrayObject(this.indexBuffers[i], attributes);
-                    vao.Initialize(this.updateProgram);
-                    this.updateVAOs[i] = vao;
-                }
-                for (int i = 0; i < 2; i++)
-                {
-                    var attributes = new VertexShaderAttribute[2];
-                    attributes[0] = new VertexShaderAttribute(this.positionBuffers[i], inPosition);
-                    attributes[1] = new VertexShaderAttribute(this.velocityBuffers[i], inVelocity);
-                    var vao = new VertexArrayObject(this.indexBuffers[i], attributes);
-                    vao.Initialize(this.renderProgram);
-                    this.renderVAOs[i] = vao;
-                }
+                var map = new AttributeMap();
+                map.Add(inPosition, DemoModel.inPosition);
+                map.Add(inVelocity, DemoModel.inVelocity);
+                renderBuilder = new RenderUnitBuilder(renderProvider, map);
             }
+            {
+                var map = new AttributeMap();
+                map.Add(inPosition, DemoModel.inPosition2);
+                map.Add(inVelocity, DemoModel.inVelocity2);
+                renderBuilder2 = new RenderUnitBuilder(renderProvider, map);
+            }
+
+            var model = new DemoModel();
+            var node = new DemoNode(model, updateBuilder, updateBuilder2, renderBuilder, renderBuilder2);
+            node.Initialize();
+
+            return node;
+        }
+
+        private DemoNode(IBufferSource model, params RenderUnitBuilder[] builders)
+            : base(model, builders)
+        {
+            this.transformFeedbackObject = new TransformFeedbackObject();
         }
 
         #region IRenderable 成员
@@ -101,54 +76,45 @@ namespace HowTransformFeedbackWorks
             {
                 GL.Instance.Enable(GL.GL_RASTERIZER_DISCARD);
 
-
-                ShaderProgram program = this.updateProgram;
-                VertexArrayObject vao = this.updateVAOs[currentIndex];
                 TransformFeedbackObject tf = this.transformFeedbackObject;
-
-                tf.BindBuffer(0, this.positionBuffers[(currentIndex + 1) % 2].BufferId);
-                tf.BindBuffer(1, this.velocityBuffers[(currentIndex + 1) % 2].BufferId);
-
-                tf.Bind();
-                program.Bind();
-                program.PushUniforms();
                 {
-                    tf.Begin(DrawMode.Triangles);
-                    vao.Render();
-                    tf.End();
+                    RenderUnit unit = this.RenderUnits[(currentIndex + 1) % 2];
+                    var attributes = unit.VertexArrayObject.VertexAttributes;
+                    for (uint i = 0; i < attributes.Length; i++)
+                    {
+                        tf.BindBuffer(i, attributes[i].Buffer.BufferId);
+                    }
                 }
-                program.Unbind();
-                tf.Unbind();
+                {
+
+                    RenderUnit unit = this.RenderUnits[currentIndex];
+                    ShaderProgram program = unit.Program;
+                    //program.SetUniform("xxx", value);
+                    unit.Render(tf);
+                }
                 GL.Instance.Disable(GL.GL_RASTERIZER_DISCARD);
             }
-            unsafe
-            {
-                var array = (vec3*)this.positionBuffers[(currentIndex + 1) % 2].MapBuffer(MapBufferAccess.ReadOnly);
-                var data = new vec3[this.positionBuffers[(currentIndex + 1) % 2].Length];
-                for (int i = 0; i < data.Length; i++)
-                {
-                    data[i] = array[i];
-                }
-                this.positionBuffers[(currentIndex + 1) % 2].UnmapBuffer();
-            }
+            //unsafe
+            //{
+            //    var array = (vec3*)this.positionBuffers[(currentIndex + 1) % 2].MapBuffer(MapBufferAccess.ReadOnly);
+            //    var data = new vec3[this.positionBuffers[(currentIndex + 1) % 2].Length];
+            //    for (int i = 0; i < data.Length; i++)
+            //    {
+            //        data[i] = array[i];
+            //    }
+            //    this.positionBuffers[(currentIndex + 1) % 2].UnmapBuffer();
+            //}
             // render
             {
-                ShaderProgram program = this.renderProgram;
-                VertexArrayObject vao = this.renderVAOs[(currentIndex + 1) % 2];
-                {
-                    ICamera camera = arg.CameraStack.Peek();
-                    mat4 projection = camera.GetProjectionMatrix();
-                    mat4 view = camera.GetViewMatrix();
-                    mat4 model = this.GetModelMatrix();
+                RenderUnit unit = this.RenderUnits[(currentIndex + 1) % 2 + 2];
+                ShaderProgram program = unit.Program;
+                ICamera camera = arg.CameraStack.Peek();
+                mat4 projection = camera.GetProjectionMatrix();
+                mat4 view = camera.GetViewMatrix();
+                mat4 model = this.GetModelMatrix();
 
-                    program.SetUniform(mvpMatrix, projection * view * model);
-
-                    program.Bind();
-                    program.PushUniforms();
-
-                    vao.Render();
-                    program.Unbind();
-                }
+                program.SetUniform(mvpMatrix, projection * view * model);
+                unit.Render();
             }
             // exchange
             {
