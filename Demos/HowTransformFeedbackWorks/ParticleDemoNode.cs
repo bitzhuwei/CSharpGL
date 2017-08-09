e879566f9116974fbd5775da8340599e9cd39250
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-09 16:53:50 +0800

    update

---
+++ Demos/HowTransformFeedbackWorks/ParticleDemoNode.cs
@@ -0,0 +1,128 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HowTransformFeedbackWorks
+{
+    partial class ParticleDemoNode : ModernNode
+    {
+        private const string inPosition = "inPosition";
+        private const string inVelocity = "inVelocity";
+        private const string outPosition = "outPosition";
+        private const string outVelocity = "outVelocity";
+        private const string mvpMatrix = "mvpMatrix";
+        private TransformFeedbackObject[] transformFeedbackObjects = new TransformFeedbackObject[2];
+        private int currentIndex = 0;
+
+        public static ParticleDemoNode Create()
+        {
+            IShaderProgramProvider updateProvider, renderProvider;
+            {
+                var vs = new VertexShader(updateVert, inPosition, inVelocity);
+                var feedbackVaryings = new string[] { "outposition", "outvelocity" };
+                updateProvider = new ShaderArray(feedbackVaryings, ShaderProgram.BufferMode.Separate, vs);
+            }
+            {
+                var vs = new VertexShader(renderVert, inPosition);
+                var fs = new FragmentShader(renderFrag);
+                renderProvider = new ShaderArray(vs, fs);
+            }
+            RenderUnitBuilder updateBuilder, updateBuilder2, renderBuilder, renderBuilder2;
+            {
+                var map = new AttributeMap();
+                map.Add(inPosition, DemoModel.inPosition);
+                map.Add(inVelocity, DemoModel.inVelocity);
+                updateBuilder = new RenderUnitBuilder(updateProvider, map);
+            }
+            {
+                var map = new AttributeMap();
+                map.Add(inPosition, DemoModel.inPosition2);
+                map.Add(inVelocity, DemoModel.inVelocity2);
+                updateBuilder2 = new RenderUnitBuilder(updateProvider, map);
+            }
+            {
+                var map = new AttributeMap();
+                map.Add(inPosition, DemoModel.inPosition);
+                map.Add(inVelocity, DemoModel.inVelocity);
+                renderBuilder = new RenderUnitBuilder(renderProvider, map);
+            }
+            {
+                var map = new AttributeMap();
+                map.Add(inPosition, DemoModel.inPosition2);
+                map.Add(inVelocity, DemoModel.inVelocity2);
+                renderBuilder2 = new RenderUnitBuilder(renderProvider, map);
+            }
+
+            var model = new DemoModel();
+            var node = new ParticleDemoNode(model, updateBuilder, updateBuilder2, renderBuilder, renderBuilder2);
+            node.Initialize();
+
+            return node;
+        }
+
+        private ParticleDemoNode(IBufferSource model, params RenderUnitBuilder[] builders)
+            : base(model, builders)
+        {
+        }
+
+        protected override void DoInitialize()
+        {
+            base.DoInitialize();
+
+            for (int i = 0; i < 2; i++)
+            {
+                var tf = new TransformFeedbackObject();
+                RenderUnit unit = this.RenderUnits[i];
+                VertexShaderAttribute[] attributes = unit.VertexArrayObject.VertexAttributes;
+                for (uint t = 0; t < attributes.Length; t++)
+                {
+                    tf.BindBuffer(t, attributes[t].Buffer);
+                }
+                this.transformFeedbackObjects[i] = tf;
+            }
+        }
+        #region IRenderable 成员
+
+        public override void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            TransformFeedbackObject tf = transformFeedbackObjects[(currentIndex + 1) % 2];
+            // update
+            {
+                GL.Instance.Enable(GL.GL_RASTERIZER_DISCARD);
+
+                RenderUnit unit = this.RenderUnits[currentIndex];
+                ShaderProgram program = unit.Program;
+                //program.SetUniform("xxx", value);
+                unit.Render(tf); // update buffers and record output to tf's binding.
+
+                GL.Instance.Disable(GL.GL_RASTERIZER_DISCARD);
+            }
+            // render
+            {
+                RenderUnit unit = this.RenderUnits[(currentIndex + 1) % 2 + 2];
+                ShaderProgram program = unit.Program;
+                ICamera camera = arg.CameraStack.Peek();
+                mat4 projection = camera.GetProjectionMatrix();
+                mat4 view = camera.GetViewMatrix();
+                mat4 model = this.GetModelMatrix();
+
+                program.SetUniform(mvpMatrix, projection * view * model);
+                //unit.Render(); // this methos must specify vertes count.
+                tf.Draw(unit); // render updated buffersi without specifying vertex count.
+            }
+            // exchange
+            {
+                currentIndex = (currentIndex + 1) % 2;
+            }
+        }
+
+        public override void RenderAfterChildren(RenderEventArgs arg)
+        {
+        }
+
+        #endregion
+
+    }
+}
---
+++ Demos/HowTransformFeedbackWorks/ParticleDemoNode.update.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace HowTransformFeedbackWorks
+{
+    partial class ParticleDemoNode
+    {
+        private const string updateVert =
+         @"#version 330
+uniform vec3 center[3];
+uniform float radius[3];
+uniform vec3 g;
+uniform float dt;
+uniform float bounce;
+uniform int seed;
+layout(location = 0) in vec3 inposition;
+layout(location = 1) in vec3 invelocity;
+out vec3 outposition;
+out vec3 outvelocity;
+
+float hash(int x) {
+   x = x*1235167 + gl_VertexID*948737 + seed*9284365;
+   x = (x >> 13) ^ x;
+   return ((x * (x * x * 60493 + 19990303) + 1376312589) & 0x7fffffff)/float(0x7fffffff-1);
+}
+
+void main() {
+   outvelocity = invelocity;
+   for(int j = 0;j<3;++j) {
+       vec3 diff = inposition-center[j];
+       float dist = length(diff);
+       float vdot = dot(diff, invelocity);
+       if(dist<radius[j] && vdot<0.0)
+           outvelocity -= bounce*diff*vdot/(dist*dist);
+   }
+   outvelocity += dt*g;
+   outposition = inposition + dt*outvelocity;
+   if(outposition.y < -30.0)
+   {
+       outvelocity = vec3(0,0,0);
+       outposition = 0.5-vec3(hash(3*gl_VertexID+0),hash(3*gl_VertexID+1),hash(3*gl_VertexID+2));
+       outposition = vec3(0,20,0) + 5.0*outposition;
+   }
+}
+";
+    }
+}
