d7b146196a5eaa63f76a074bcb403200f5f04fc7
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-11 14:02:06 +0800

    Oh, no, it's not glBitmap()

--- CSharpGL.Windows/GLAPI/WinGL.DrawText.cs
+++ CSharpGL.Windows/GLAPI/WinGL.DrawText.cs
@@ -7,6 +7,15 @@ namespace CSharpGL
 {
     public partial class WinGL
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="color"></param>
+        /// <param name="faceName"></param>
+        /// <param name="fontSize"></param>
+        /// <param name="text"></param>
         public override void DrawText(int x, int y, System.Drawing.Color color, string faceName, float fontSize, string text)
         {
             FontBitmaps.DrawText(x, y, color, faceName, fontSize, text);
