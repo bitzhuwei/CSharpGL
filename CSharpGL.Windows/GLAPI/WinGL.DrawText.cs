5c7ffa8e2e9cccbc4419b7163b765ee902cb8946
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-11 14:00:37 +0800

    DrawText with glBitmap()

---
+++ CSharpGL.Windows/GLAPI/WinGL.DrawText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace CSharpGL
+{
+    public partial class WinGL
+    {
+        public override void DrawText(int x, int y, System.Drawing.Color color, string faceName, float fontSize, string text)
+        {
+            FontBitmaps.DrawText(x, y, color, faceName, fontSize, text);
+        }
+    }
+}
---
+++ CSharpGL4/GLAPI/GL.DrawText.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    public partial class GL
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="color"></param>
+        /// <param name="faceName"></param>
+        /// <param name="fontSize"></param>
+        /// <param name="text"></param>
+        public abstract void DrawText(int x, int y, Color color, string faceName, float fontSize, string text);
+    }
+}
