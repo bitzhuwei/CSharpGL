8aeb4333360aef5e66f5fc08af82ebcc9387ac8b
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-13 01:18:44 +0800

    update

---
+++ Demos/RaycastVolumeRendering/RaycastModel.cs
@@ -0,0 +1,150 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaycastVolumeRendering
+{
+    internal class RaycastModel : IBufferSource
+    {
+        public const string strposition = "position";
+        public const string strcolor = "color";
+        private VertexBuffer positionBuffer;
+        private VertexBuffer colorBuffer;
+
+        private IndexBuffer indexBuffer = null;
+
+
+        // draw the six faces of the boundbox by drawwing triangles
+        // draw it contra-clockwise
+        // front: 1 5 7 3
+        // back:  0 2 6 4
+        // left： 0 1 3 2
+        // right: 7 5 4 6
+        // up:    2 3 7 6
+        // down:  1 0 4 5
+        private static readonly float[] boundingBox =
+        {
+			0.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f,
+			0.0f, 1.0f, 1.0f,
+			1.0f, 0.0f, 0.0f,
+			1.0f, 0.0f, 1.0f,
+			1.0f, 1.0f, 0.0f,
+			1.0f, 1.0f, 1.0f,
+        };
+
+        private static readonly float[] boundingBoxColor =
+        {
+			0.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f,
+			0.0f, 1.0f, 1.0f,
+			1.0f, 0.0f, 0.0f,
+			1.0f, 0.0f, 1.0f,
+			1.0f, 1.0f, 0.0f,
+			1.0f, 1.0f, 1.0f,
+        };
+
+        private static readonly uint[] indices =
+        {
+			1,5,7,
+			7,3,1,
+			0,2,6,
+			6,4,0,
+			0,1,3,
+			3,2,0,
+			7,5,4,
+			4,6,7,
+			2,3,7,
+			7,6,2,
+			1,0,4,
+			4,5,1,
+        };
+
+        static RaycastModel()
+        {
+            for (int i = 0; i < boundingBox.Length; i++)
+            {
+                boundingBox[i] = boundingBox[i] - 0.5f;
+            }
+        }
+        public VertexBuffer GetVertexAttributeBuffer(string bufferName)
+        {
+            if (bufferName == strposition)
+            {
+                if (this.positionBuffer == null)
+                {
+                    //int length = boundingBox.Length;
+                    //VertexBuffer buffer = VertexBuffer.Create(typeof(float), length, VBOConfig.Vec3, varNameInShader, BufferUsage.StaticDraw);
+                    //unsafe
+                    //{
+                    //    IntPtr pointer = buffer.MapBuffer(MapBufferAccess.WriteOnly);
+                    //    var array = (float*)pointer;
+                    //    for (int i = 0; i < boundingBox.Length; i++)
+                    //    {
+                    //        array[i] = boundingBox[i] - 0.5f;
+                    //    }
+                    //    buffer.UnmapBuffer();
+                    //}
+
+                    //this.positionBuffer = buffer;
+                    this.positionBuffer = boundingBox.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+                return this.positionBuffer;
+            }
+            else if (bufferName == strcolor)
+            {
+                if (this.colorBuffer == null)
+                {
+                    //int length = boundingBox.Length;
+                    //VertexBuffer buffer = VertexBuffer.Create(typeof(float), length, VBOConfig.Vec3, varNameInShader, BufferUsage.StaticDraw);
+                    //unsafe
+                    //{
+                    //    IntPtr pointer = buffer.MapBuffer(MapBufferAccess.WriteOnly);
+                    //    var array = (float*)pointer;
+                    //    for (int i = 0; i < boundingBox.Length; i++)
+                    //    {
+                    //        array[i] = boundingBox[i];
+                    //    }
+                    //    buffer.UnmapBuffer();
+                    //}
+
+                    //this.colorBuffer = buffer;
+                    this.colorBuffer = boundingBoxColor.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+                return this.colorBuffer;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IndexBuffer GetIndexBuffer()
+        {
+            if (indexBuffer == null)
+            {
+                //int length = indices.Length;
+                //OneIndexBuffer buffer = CSharpGL.Buffer.Create(IndexElementType.UInt, length, DrawMode.Triangles, BufferUsage.StaticDraw);
+                //unsafe
+                //{
+                //    IntPtr pointer = buffer.MapBuffer(MapBufferAccess.WriteOnly);
+                //    var array = (uint*)pointer;
+                //    for (int i = 0; i < indices.Length; i++)
+                //    {
+                //        array[i] = indices[i];
+                //    }
+                //    buffer.UnmapBuffer();
+                //}
+                //this.indexBuffer = buffer;
+                this.indexBuffer = indices.GenIndexBuffer(DrawMode.Triangles, BufferUsage.StaticDraw);
+            }
+
+            return indexBuffer;
+        }
+
+    }
+}
--- Demos/RaycastVolumeRendering/RaycastNode.cs
+++ Demos/RaycastVolumeRendering/RaycastNode.cs
@@ -8,10 +8,37 @@ namespace RaycastVolumeRendering
 {
     public partial class RaycastNode : PickableNode
     {
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public static RaycastNode Create()
         {
-            throw new NotImplementedException();
+            var model = new RaycastModel();
+            RenderUnitBuilder backfaceBuilder, raycastingBuilder;
+            {
+                var vs = new VertexShader(backfaceVert, "position", "boundingBox");
+                var fs = new FragmentShader(backfaceFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("position", RaycastModel.strposition);
+                map.Add("boundingBox", RaycastModel.strcolor);
+                backfaceBuilder = new RenderUnitBuilder(provider, map, new CullFaceState(CullFaceMode.Front, true));
+            }
+            {
+                var vs = new VertexShader(raycastingVert, "position", "boundingBox");
+                var fs = new FragmentShader(raycastingFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("position", RaycastModel.strposition);
+                map.Add("boundingBox", RaycastModel.strcolor);
+                raycastingBuilder = new RenderUnitBuilder(provider, map, new CullFaceState(CullFaceMode.Back, true));
+            }
+
+            var node = new RaycastNode(model, RaycastModel.strposition, backfaceBuilder, raycastingBuilder);
+            node.Initialize();
+
+            return node;
         }
 
         private RaycastNode(IBufferSource model, string positionNameInIBufferSource, params RenderUnitBuilder[] builders)
