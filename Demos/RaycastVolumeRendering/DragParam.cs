1f471e800958734ab511d9c832240b50f3874387
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-13 00:47:56 +0800

    clean

---
+++ Demos/RaycastVolumeRendering/DragParam.cs
@@ -0,0 +1,42 @@
+﻿using CSharpGL;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace RaycastVolumeRendering
+{
+    internal class DragParam
+    {
+        public List<uint> pickedVertexIds = new List<uint>();
+        public mat4 projectionMatrix;
+        public mat4 viewMatrix;
+        public ivec2 lastMousePositionOnScreen;
+        public vec4 viewport;
+
+        public DragParam(mat4 projectionMatrix, mat4 viewMatrix, vec4 viewport, ivec2 lastMousePositionOnScreen)
+        {
+            this.projectionMatrix = projectionMatrix;
+            this.viewMatrix = viewMatrix;
+            this.lastMousePositionOnScreen = lastMousePositionOnScreen;
+            this.viewport = viewport;
+        }
+
+        public DragParam(mat4 projectionMatrix, mat4 viewMatrix, vec4 viewport, ivec2 lastMousePositionOnScreen,
+           IEnumerable<uint> indexes)
+            : this(projectionMatrix, viewMatrix, viewport, lastMousePositionOnScreen)
+        {
+            this.pickedVertexIds.AddRange(indexes);
+        }
+
+        public DragParam(mat4 projectionMatrix, mat4 viewMatrix, vec4 viewport, ivec2 lastMousePositionOnScreen,
+            params uint[] indexes)
+            : this(projectionMatrix, viewMatrix, viewport, lastMousePositionOnScreen)
+        {
+            this.pickedVertexIds.AddRange(indexes);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Last Mouse Position On Screen: [{0}]", this.lastMousePositionOnScreen);
+        }
+    }
+}
---
+++ Demos/RaycastVolumeRendering/FormMain.cs
@@ -0,0 +1,67 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RaycastVolumeRendering
+{
+    public partial class FormMain : Form
+    {
+        private Scene scene;
+        private ActionList actionList;
+
+        private PickingAction pickingAction;
+        private LegacyTriangleNode triangleTip;
+        private LegacyQuadNode quadTip;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Load += FormMain_Load;
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+            this.winGLCanvas1.MouseDown += glCanvas1_MouseDown;
+            this.winGLCanvas1.MouseMove += glCanvas1_MouseMove;
+            this.winGLCanvas1.MouseUp += glCanvas1_MouseUp;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            var position = new vec3(5, 3, 4);
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+
+            this.scene = new Scene(camera, this.winGLCanvas1);
+
+            var list = new ActionList();
+            var transformAction = new TransformAction(scene);
+            list.Add(transformAction);
+            var renderAction = new RenderAction(scene);
+            list.Add(renderAction);
+            this.actionList = list;
+
+            this.pickingAction = new PickingAction(scene);
+
+            this.triangleTip = new LegacyTriangleNode();
+            this.quadTip = new LegacyQuadNode();
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            this.actionList.Act();
+        }
+
+        void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
+        }
+
+    }
+}
