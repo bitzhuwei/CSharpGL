a748c38f45a89f2e380cead8981eec97f9b2d990
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-08-13 01:02:05 +0800

    update

---
+++ Demos/RaycastVolumeRendering/RaycastNode.cs
@@ -0,0 +1,32 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaycastVolumeRendering
+{
+    public partial class RaycastNode : PickableNode
+    {
+
+        public static RaycastNode Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        private RaycastNode(IBufferSource model, string positionNameInIBufferSource, params RenderUnitBuilder[] builders)
+            : base(model, positionNameInIBufferSource, builders)
+        {
+        }
+
+        public override void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RenderAfterChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
