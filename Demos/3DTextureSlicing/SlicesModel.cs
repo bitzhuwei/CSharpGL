60590ae1591df39d1974de7f6f9f058011343f2a
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-01 00:34:55 +0800

    3d texture slicing

---
+++ Demos/3DTextureSlicing/SlicesModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace _3DTextureSlicing
+{
+    class SlicesModel : IBufferSource
+    {
+        public const string position = "position";
+        private VertexBuffer slicesBuffer;
+
+        private IndexBuffer indexBuffer;
+
+        #region IBufferSource 成员
+
+        public VertexBuffer GetVertexAttributeBuffer(string bufferName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IndexBuffer GetIndexBuffer()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Demos/3DTextureSlicing/SlicesNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace _3DTextureSlicing
+{
+    class SlicesNode : ModernNode
+    {
+        public static SlicesNode Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        private SlicesNode(IBufferSource model, params RenderUnitBuilder[] builders)
+            : base(model, builders)
+        {
+        }
+
+        public override void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RenderAfterChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
