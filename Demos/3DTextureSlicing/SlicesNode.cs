ddb5ea84460de6d5203a77423d8d355a96ae1eb1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-01 12:33:38 +0800

    update

--- Demos/3DTextureSlicing/SlicesNode.cs
+++ Demos/3DTextureSlicing/SlicesNode.cs
@@ -6,11 +6,29 @@ using CSharpGL;
 
 namespace _3DTextureSlicing
 {
-    class SlicesNode : ModernNode
+    partial class SlicesNode : ModernNode
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public static SlicesNode Create()
         {
-            throw new NotImplementedException();
+            var model = new SlicesModel();
+            RenderUnitBuilder textureSlicerBuilder;
+            {
+                var vs = new VertexShader(textureSlicerVert, "vVertex");
+                var fs = new FragmentShader(textureSlicerFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("vVertex", SlicesModel.position);
+                textureSlicerBuilder = new RenderUnitBuilder(provider, map);
+            }
+
+            var node = new SlicesNode(model, textureSlicerBuilder);
+            node.Initialize();
+
+            return node;
         }
 
         private SlicesNode(IBufferSource model, params RenderUnitBuilder[] builders)
