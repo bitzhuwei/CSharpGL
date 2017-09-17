79dfcfba7af6d7155d4ab0385125efd08c0edae5
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-18 04:23:14 +0800

    update

---
+++ Demos/FrontToBackPeeling/QuadModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace FrontToBackPeeling
+{
+    class QuadModel : IBufferSource
+    {
+
+        private static readonly vec2[] quadVerts = new vec2[4] { new vec2(0, 0), new vec2(1, 0), new vec2(1, 1), new vec2(0, 1), };
+        private static readonly ushort[] quadIndices = new ushort[] { 0, 1, 2, 0, 2, 3 };
+
+        #region IBufferSource 成员
+
+        public VertexBuffer GetVertexAttributeBuffer(string bufferName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IndexBuffer GetIndexBuffer()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
