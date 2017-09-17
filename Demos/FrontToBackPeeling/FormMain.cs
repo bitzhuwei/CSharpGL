4e31cc08370a3d7c8c3fa5df3269cd32ae004068
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-18 03:46:26 +0800

    update

---
+++ Demos/FrontToBackPeeling/FormMain.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CSharpGL;
+
+namespace FrontToBackPeeling
+{
+    public partial class FormMain : Form
+    {
+        private Scene scene;
+        private ActionList actionList;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Load += FormMain_Load;
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            var rootElement = GetTree();
+
+            var position = new vec3(5, 3, 4) * 0.2f;
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+            this.scene = new Scene(camera, this.winGLCanvas1)
+            {
+                RootElement = rootElement,
+                ClearColor = Color.SkyBlue.ToVec4(),
+            };
+
+            var list = new ActionList();
+            var transformAction = new TransformAction(scene);
+            list.Add(transformAction);
+            var renderAction = new RenderAction(scene);
+            list.Add(renderAction);
+            this.actionList = list;
+
+            Match(this.trvScene, scene.RootElement);
+            this.trvScene.ExpandAll();
+
+            var manipulater = new FirstPerspectiveManipulater();
+            manipulater.StepLength = 0.1f;
+            manipulater.Bind(camera, this.winGLCanvas1);
+        }
+
+        private void Match(TreeView treeView, SceneNodeBase nodeBase)
+        {
+            treeView.Nodes.Clear();
+            var node = new TreeNode(nodeBase.ToString()) { Tag = nodeBase };
+            treeView.Nodes.Add(node);
+            Match(node, nodeBase);
+        }
+
+        private void Match(TreeNode node, SceneNodeBase nodeBase)
+        {
+            foreach (var item in nodeBase.Children)
+            {
+                var child = new TreeNode(item.ToString()) { Tag = item };
+                node.Nodes.Add(child);
+                Match(child, item);
+            }
+        }
+
+        private SceneNodeBase GetTree()
+        {
+            throw new NotImplementedException();
+            //var node = SlicesNode.Create();
+            //return node;
+            //var group = new GroupNode();
+
+            //return group;
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            this.actionList.Act();
+        }
+
+        void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
+        }
+
+        private void trvScene_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            this.propGrid.SelectedObject = e.Node.Tag;
+
+            this.lblState.Text = string.Format("{0} objects selected.", 1);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IWorldSpace node = this.scene.RootElement;
+            if (node != null)
+            {
+                node.RotationAngle += 1.3f;
+            }
+        }
+    }
+
+}
