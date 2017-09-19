519a6ceb55342b2e65316077d98c490096ff0edd
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-19 20:36:50 +0800

    update

--- Demos/FrontToBackPeeling/QuadNode.cs
+++ Demos/FrontToBackPeeling/QuadNode.cs
@@ -8,11 +8,38 @@ namespace FrontToBackPeeling
 {
     class QuadNode : ModernNode
     {
+        public enum RenderMode { Blend = 0, FInal = 1 };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public RenderMode Mode { get; set; }
+
         public static QuadNode Create()
         {
-            var model = new QuadModel();
+            RenderUnitBuilder blendBuilder, finalBuilder;
+            {
+                var vs = new VertexShader(Shaders.blendVert, "vVertex");
+                var fs = new FragmentShader(Shaders.blendFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("vVertex", QuadModel.positions);
+                blendBuilder = new RenderUnitBuilder(provider, map);
+            }
+            {
+                var vs = new VertexShader(Shaders.blendVert, "vVertex");// reuse blend vertex shader.
+                var fs = new FragmentShader(Shaders.finalFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("vVertex", QuadModel.positions);
+                finalBuilder = new RenderUnitBuilder(provider, map);
+            }
 
-            throw new NotImplementedException();
+            var model = new QuadModel();
+            var node = new QuadNode(model, blendBuilder, finalBuilder);
+            node.Initialize();
+
+            return node;
         }
 
         private QuadNode(IBufferSource model, params RenderUnitBuilder[] builders)
