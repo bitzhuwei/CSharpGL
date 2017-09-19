8f9d139849e62b89dfa2d049b0e4bb066707cc07
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-19 22:34:22 +0800

    update

---
+++ Demos/FrontToBackPeeling/PeelingNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace FrontToBackPeeling
+{
+    class PeelingNode : SceneNodeBase, IRenderable
+    {
+        #region IRenderable 成员
+
+        public ThreeFlags EnableRendering
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderAfterChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
