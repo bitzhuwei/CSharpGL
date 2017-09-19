d88ef6af7a9d0613f70aeffc48f53178b015a3b1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-19 21:18:28 +0800

    update

---
+++ Demos/FrontToBackPeeling/CubeMode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace FrontToBackPeeling
+{
+    class CubeMode : IBufferSource
+    {
+        private static vec3[] vertices = new vec3[8] { new vec3(-0.5f, -0.5f, -0.5f), new vec3(0.5f, -0.5f, -0.5f), new vec3(0.5f, 0.5f, -0.5f), new vec3(-0.5f, 0.5f, -0.5f), new vec3(-0.5f, -0.5f, 0.5f), new vec3(0.5f, -0.5f, 0.5f), new vec3(0.5f, 0.5f, 0.5f), new vec3(-0.5f, 0.5f, 0.5f) };
+
+        private static ushort[] cubeIndices = new ushort[36] { 0, 5, 4, 5, 0, 1, 3, 7, 6, 3, 6, 2, 7, 4, 6, 6, 4, 5, 2, 1, 3, 3, 1, 0, 3, 0, 7, 7, 0, 4, 6, 5, 2, 2, 5, 1 };
+
+        public const string positions = "positions";
+        private VertexBuffer positionBuffer;
+
+        private IndexBuffer indexBuffer;
+
+        #region IBufferSource 成员
+
+        public VertexBuffer GetVertexAttributeBuffer(string bufferName)
+        {
+            if (positions == bufferName)
+            {
+                if (this.positionBuffer == null)
+                {
+                    this.positionBuffer = vertices.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+
+                return this.positionBuffer;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public IndexBuffer GetIndexBuffer()
+        {
+            if (this.indexBuffer == null)
+            {
+                this.indexBuffer = cubeIndices.GenIndexBuffer(DrawMode.Triangles, BufferUsage.StaticDraw);
+            }
+
+            return this.indexBuffer;
+        }
+
+        #endregion
+    }
+}
