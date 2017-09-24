d5067eb0e3c3396e4b85ec30a873b6d87cacbad1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-24 18:31:02 +0800

    update

--- Demos/Texture2D/CrateTextureSource.cs
+++ Demos/Texture2D/CrateTextureSource.cs
@@ -12,6 +12,7 @@ namespace Texture2D
         public CrateTextureSource(string filename)
         {
             var bmp = new System.Drawing.Bitmap(filename);
+            bmp.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipX);
             texture = new Texture(TextureTarget.Texture2D,
                 new TexImage2D(TexImage2D.Target.Texture2D, 0, (int)GL.GL_RGBA, bmp.Width, bmp.Height, 0, GL.GL_BGRA, GL.GL_UNSIGNED_BYTE, new ImageDataProvider(bmp)));
             texture.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureWrapS, (int)GL.GL_CLAMP_TO_EDGE));
--- Demos/Texture2D/FormMain.cs
+++ Demos/Texture2D/FormMain.cs
@@ -51,23 +51,23 @@ namespace Texture2D
 
         private SceneNodeBase GetRootElement()
         {
-
             var rectangle = RectangleNode.Create();
             rectangle.Scale *= 3;
             rectangle.TextureSource = new CrateTextureSource(@"Crate.bmp");
 
             var blend = RectangleNode.Create();
-            blend.Scale *= 3;
-            blend.WorldPosition = new vec3(1, 0, 0.1f);
+            blend.Scale *= 1.5f;
+            blend.WorldPosition = new vec3(-0.5f, 0, 0.1f);
             blend.RenderUnits[0].StateList.Add(new BlendState(BlendingSourceFactor.SourceAlpha, BlendingDestinationFactor.OneMinusSourceAlpha));
             blend.TextureSource = new CrateTextureSource(@"particle.png");
 
             var blend2 = RectangleNode.Create();
-            blend2.Scale *= 3;
-            blend2.WorldPosition = new vec3(-1, 0, 0.2f);
+            blend2.Scale *= 1.5f;
+            blend2.WorldPosition = new vec3(0.5f, 0, 0.2f);
             blend2.RenderUnits[0].StateList.Add(new BlendState(BlendingSourceFactor.SourceAlpha, BlendingDestinationFactor.OneMinusSourceAlpha));
             blend2.TextureSource = new CrateTextureSource(@"particle.png");
 
+            // note: this tells us that the right way is to render the nearest transparenct object last.
             var group = new GroupNode(rectangle, blend, blend2);
 
             return group;
