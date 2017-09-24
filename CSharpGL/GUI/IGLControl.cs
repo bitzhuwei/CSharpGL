9ac17c4ec7edbf390244fb56081e30be34a6be41
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-24 18:36:31 +0800

    GUI

---
+++ CSharpGL/GUI/IGLControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IGLControl
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void Layout();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="renderer"></param>
+        void Render(IControlRenderer renderer);
+    }
+}
