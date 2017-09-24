8a09fef063f41e4fabf54ac20c2c5448b958c54f
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-24 12:32:46 +0800

    update

--- Demos/SimpleBlending/Program.cs
+++ Demos/SimpleBlending/Program.cs
@@ -15,7 +15,7 @@ namespace SimpleBlending
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
