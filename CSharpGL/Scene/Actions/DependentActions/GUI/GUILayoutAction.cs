8166853ee0d4831f8ec5a1d641e02ea5e8dab5d4
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-09-27 14:11:04 +0800

    update

---
+++ CSharpGL/Scene/Actions/DependentActions/GUI/GUILayoutAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Render <see cref="IRenderable"/> objects.
+    /// </summary>
+    public class GUILayoutAction : DependentActionBase
+    {
+        /// <summary>
+        /// Render <see cref="IRenderable"/> objects.
+        /// </summary>
+        /// <param name="scene"></param>
+        public GUILayoutAction(Scene scene) : base(scene) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Act()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharpGL/Scene/Actions/DependentActions/GUI/GUIRenderAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Render <see cref="IGLControl"/> objects.
+    /// </summary>
+    public class GUIRenderAction : DependentActionBase
+    {
+        /// <summary>
+        /// Render <see cref="IRenderable"/> objects.
+        /// </summary>
+        /// <param name="scene"></param>
+        public GUIRenderAction(Scene scene) : base(scene) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Act()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
