31be2450a18ca9701af2f58c4ae38b0dd8399f80
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-10-04 13:41:00 +0800

    PickableNode is a ModernNode

--- CSharpGL/Scene/SceneNodes/PickableNode/PickableNode.IRenderable.cs
+++ CSharpGL/Scene/SceneNodes/PickableNode/PickableNode.IRenderable.cs
@@ -1,34 +1,34 @@
-﻿using System.ComponentModel;
-namespace CSharpGL
-{
-    public partial class PickableNode
-    {
-        #region IRenderable 成员
+﻿//using System.ComponentModel;
+//namespace CSharpGL
+//{
+//    public partial class PickableNode
+//    {
+//        #region IRenderable 成员
 
-        private ThreeFlags enableRendering = ThreeFlags.BeforeChildren | ThreeFlags.Children;
-        /// <summary>
-        /// 
-        /// </summary>
-        [Category(strPickableRenderer)]
-        [Description("Render before/after children? Render children?")]
-        public ThreeFlags EnableRendering
-        {
-            get { return this.enableRendering; }
-            set { this.enableRendering = value; }
-        }
+//        private ThreeFlags enableRendering = ThreeFlags.BeforeChildren | ThreeFlags.Children;
+//        /// <summary>
+//        /// 
+//        /// </summary>
+//        [Category(strPickableRenderer)]
+//        [Description("Render before/after children? Render children?")]
+//        public ThreeFlags EnableRendering
+//        {
+//            get { return this.enableRendering; }
+//            set { this.enableRendering = value; }
+//        }
 
-        /// <summary>
-        /// Render something before renddering children.
-        /// </summary>
-        /// <param name="arg"></param>
-        public abstract void RenderBeforeChildren(RenderEventArgs arg);
+//        /// <summary>
+//        /// Render something before renddering children.
+//        /// </summary>
+//        /// <param name="arg"></param>
+//        public abstract void RenderBeforeChildren(RenderEventArgs arg);
 
-        /// <summary>
-        /// Render something after renddering children.
-        /// </summary>
-        /// <param name="arg"></param>
-        public abstract void RenderAfterChildren(RenderEventArgs arg);
+//        /// <summary>
+//        /// Render something after renddering children.
+//        /// </summary>
+//        /// <param name="arg"></param>
+//        public abstract void RenderAfterChildren(RenderEventArgs arg);
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
--- CSharpGL/Scene/SceneNodes/PickableNode/PickableNode.UniformArrays.cs
+++ CSharpGL/Scene/SceneNodes/PickableNode/PickableNode.UniformArrays.cs
@@ -1,210 +1,210 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
 
-namespace CSharpGL
-{
-    public partial class PickableNode
-    {
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformBoolArrayValue(string varNameInShader, out bool[] value)
-        //{
-        //    return this.RenderProgram.GetUniformBoolArrayValue(varNameInShader, out value);
-        //}
+//namespace CSharpGL
+//{
+//    public partial class PickableNode
+//    {
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformBoolArrayValue(string varNameInShader, out bool[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformBoolArrayValue(varNameInShader, out value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformFloatArrayValue(string varNameInShader, out float[] value)
-        //{
-        //    return this.RenderProgram.GetUniformFloatArrayValue(varNameInShader, out value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformFloatArrayValue(string varNameInShader, out float[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformFloatArrayValue(varNameInShader, out value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformVec2ArrayValue(string varNameInShader, out vec2[] value)
-        //{
-        //    return this.RenderProgram.GetUniformVec2ArrayValue(varNameInShader, out value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformVec2ArrayValue(string varNameInShader, out vec2[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformVec2ArrayValue(varNameInShader, out value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformVec3ArrayValue(string varNameInShader, out vec3[] value)
-        //{
-        //    return this.RenderProgram.GetUniformVec3ArrayValue(varNameInShader, out value);
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformVec3ArrayValue(string varNameInShader, out vec3[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformVec3ArrayValue(varNameInShader, out value);
 
-        //}
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformVec4ArrayValue(string varNameInShader, out vec4[] value)
-        //{
-        //    return this.RenderProgram.GetUniformVec4ArrayValue(varNameInShader, out value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformVec4ArrayValue(string varNameInShader, out vec4[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformVec4ArrayValue(varNameInShader, out value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformMat2ArrayValue(string varNameInShader, out mat2[] value)
-        //{
-        //    return this.RenderProgram.GetUniformMat2ArrayValue(varNameInShader, out value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformMat2ArrayValue(string varNameInShader, out mat2[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformMat2ArrayValue(varNameInShader, out value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformMat3ArrayValue(string varNameInShader, out mat3[] value)
-        //{
-        //    return this.RenderProgram.GetUniformMat3ArrayValue(varNameInShader, out value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformMat3ArrayValue(string varNameInShader, out mat3[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformMat3ArrayValue(varNameInShader, out value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformMat4ArrayValue(string varNameInShader, out mat4[] value)
-        //{
-        //    return this.RenderProgram.GetUniformMat4ArrayValue(varNameInShader, out value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformMat4ArrayValue(string varNameInShader, out mat4[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformMat4ArrayValue(varNameInShader, out value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool GetUniformSamplerArrayValue(string varNameInShader, out samplerValue[] value)
-        //{
-        //    return this.RenderProgram.GetUniformSamplerArrayValue(varNameInShader, out value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool GetUniformSamplerArrayValue(string varNameInShader, out samplerValue[] value)
+//        //{
+//        //    return this.RenderProgram.GetUniformSamplerArrayValue(varNameInShader, out value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, bool[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, bool[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, float[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, float[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, vec2[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, vec2[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, vec3[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, vec3[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, vec4[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, vec4[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, mat2[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, mat2[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, mat3[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, mat3[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, mat4[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, mat4[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-        ///// <summary>
-        /////
-        ///// </summary>
-        ///// <param name="varNameInShader"></param>
-        ///// <param name="value"></param>
-        ///// <returns></returns>
-        //public bool SetUniform(string varNameInShader, samplerValue[] value)
-        //{
-        //    return this.RenderProgram.SetUniform(varNameInShader, value);
-        //}
+//        ///// <summary>
+//        /////
+//        ///// </summary>
+//        ///// <param name="varNameInShader"></param>
+//        ///// <param name="value"></param>
+//        ///// <returns></returns>
+//        //public bool SetUniform(string varNameInShader, samplerValue[] value)
+//        //{
+//        //    return this.RenderProgram.SetUniform(varNameInShader, value);
+//        //}
 
-    }
-}
+//    }
+//}
