8135f3be2e1c69d2e37197dc16ce6ef47cdad17e
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-11-15 19:41:47 +0800

    GetAbsolute Position in world/view space

---
+++ CSharpGL/Scene/SceneNodes/SceneNodeBase.AbsoluteTransformInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace CSharpGL
+{
+    public abstract partial class SceneNodeBase
+    {
+        /// <summary>
+        /// Gets this node's abolute position in world space.
+        /// </summary>
+        /// <returns></returns>
+        public vec3 GetAbsoluteWorldPosition()
+        {
+            var position = new vec4(this.worldPosition, 1.0f);
+            var worldPosition = this.cascadeModelMatrix * position;
+            return new vec3(worldPosition);
+        }
+
+        /// <summary>
+        /// Gets this node's abolute position in specified view space.
+        /// </summary>
+        /// <param name="viewMatrix">specifies the view space transform.</param>
+        /// <returns></returns>
+        public vec3 GetAbsoluteViewPosition(mat4 viewMatrix)
+        {
+            var position = new vec4(this.worldPosition, 1.0f);
+            var viewPosition = viewMatrix * this.cascadeModelMatrix * position;
+            return new vec3(viewPosition);
+        }
+    }
+}
