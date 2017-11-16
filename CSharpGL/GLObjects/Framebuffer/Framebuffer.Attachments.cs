185274286e8e0114aae745249d323a945f127e84
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-11-17 01:07:51 +0800

    separate file for TextureAttachment

--- CSharpGL/GLObjects/Framebuffer/Framebuffer.Attachments.cs
+++ CSharpGL/GLObjects/Framebuffer/Framebuffer.Attachments.cs
@@ -3,31 +3,6 @@ using System.Collections.Generic;
 
 namespace CSharpGL
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public enum TextureAttachment : uint
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        ColorAttachment = GL.GL_COLOR_ATTACHMENT0,
-
-        /// <summary>
-        ///
-        /// </summary>
-        DepthAttachment = GL.GL_DEPTH_ATTACHMENT,
-
-        /// <summary>
-        ///
-        /// </summary>
-        StencilAttachment = GL.GL_STENCIL_ATTACHMENT,
-
-        /// <summary>
-        ///
-        /// </summary>
-        DepthStencilAttachment = GL.GL_DEPTH_STENCIL_ATTACHMENT,
-    }
     public partial class Framebuffer
     {
         private static readonly uint[] attachment_id =
---
+++ CSharpGL/GLObjects/Framebuffer/TextureAttachment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum TextureAttachment : uint
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        ColorAttachment = GL.GL_COLOR_ATTACHMENT0,
+
+        /// <summary>
+        ///
+        /// </summary>
+        DepthAttachment = GL.GL_DEPTH_ATTACHMENT,
+
+        /// <summary>
+        ///
+        /// </summary>
+        StencilAttachment = GL.GL_STENCIL_ATTACHMENT,
+
+        /// <summary>
+        ///
+        /// </summary>
+        DepthStencilAttachment = GL.GL_DEPTH_STENCIL_ATTACHMENT,
+    }
+}
