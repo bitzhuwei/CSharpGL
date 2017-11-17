4e55f3cf3bae70c76eccdbbd1deb1b2578964245
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-11-17 15:52:11 +0800

    add mipmap leve las input parameter

--- CSharpGL/GLObjects/Framebuffer/Framebuffer.Attach.Texture.cs
+++ CSharpGL/GLObjects/Framebuffer/Framebuffer.Attach.Texture.cs
@@ -10,13 +10,14 @@ namespace CSharpGL
         /// <para>Bind() this framebuffer before invoking this method.</para>
         /// </summary>
         /// <param name="texture"></param>
+        /// <param name="mipmapLevel">which mipmap level should be attached to this framebuffer?</param>
         /// <returns></returns>
-        public void Attach(Texture texture)
+        public void Attach(Texture texture, int mipmapLevel = 0)
         {
             if (nextColorAttachmentIndex >= attachment_id.Length)
             { throw new IndexOutOfRangeException("Not enough color attach points!"); }
 
-            glFramebufferTexture(GL.GL_FRAMEBUFFER, attachment_id[nextColorAttachmentIndex++], texture.Id, 0);
+            glFramebufferTexture(GL.GL_FRAMEBUFFER, attachment_id[nextColorAttachmentIndex++], texture.Id, mipmapLevel);
         }
 
         /// <summary>
@@ -25,19 +26,20 @@ namespace CSharpGL
         /// </summary>
         /// <param name="texture"></param>
         /// <param name="depthAttachment">true for depth attachment; otherwise, color attachment.</param>
+        /// <param name="mipmapLevel">which mipmap level should be attached to this framebuffer?</param>
         /// <returns></returns>
-        public void Attach(Texture texture, bool depthAttachment)
+        public void Attach(Texture texture, bool depthAttachment, int mipmapLevel = 0)
         {
             if (depthAttachment)
             {
-                glFramebufferTexture2D(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_ATTACHMENT, (uint)texture.Target, texture.Id, 0);
+                glFramebufferTexture2D(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_ATTACHMENT, (uint)texture.Target, texture.Id, mipmapLevel);
             }
             else
             {
                 if (nextColorAttachmentIndex >= attachment_id.Length)
                 { throw new IndexOutOfRangeException("Not enough color attach points!"); }
 
-                glFramebufferTexture2D(GL.GL_FRAMEBUFFER, attachment_id[nextColorAttachmentIndex++], (uint)texture.Target, texture.Id, 0);
+                glFramebufferTexture2D(GL.GL_FRAMEBUFFER, attachment_id[nextColorAttachmentIndex++], (uint)texture.Target, texture.Id, mipmapLevel);
             }
         }
 
@@ -49,7 +51,7 @@ namespace CSharpGL
         /// <returns></returns>
         public Texture Attach(TextureAttachment type)
         {
-            const int level = 0;
+            const int mipmapLevel = 0;
             Texture result = null;
             switch (type)
             {
@@ -61,7 +63,7 @@ namespace CSharpGL
                     result.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureMinFilter, (int)GL.GL_LINEAR));
                     result.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureMagFilter, (int)GL.GL_LINEAR));
                     result.Initialize();
-                    glFramebufferTexture(GL.GL_FRAMEBUFFER, attachment_id[nextColorAttachmentIndex++], result.Id, level);
+                    glFramebufferTexture(GL.GL_FRAMEBUFFER, attachment_id[nextColorAttachmentIndex++], result.Id, mipmapLevel);
                     break;
                 case TextureAttachment.DepthAttachment:
                     result = new Texture(TextureTarget.Texture2D,
@@ -76,10 +78,10 @@ namespace CSharpGL
                     result.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureWrapS, (int)GL.GL_CLAMP_TO_EDGE));
                     result.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureWrapT, (int)GL.GL_CLAMP_TO_EDGE));
                     result.Initialize();
-                    glFramebufferTexture(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_ATTACHMENT, result.Id, level);
+                    glFramebufferTexture(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_ATTACHMENT, result.Id, mipmapLevel);
                     //glFramebufferTexture(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_STENCIL_ATTACHMENT, result.Id, level);
-                    //glFramebufferTexture2D(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_ATTACHMENT, GL.GL_TEXTURE_2D, result.Id, level);
-                    //glFramebufferTexture2D(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_STENCIL_ATTACHMENT, GL.GL_TEXTURE_2D, result.Id, level);// error 
+                    //glFramebufferTexture2D(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_ATTACHMENT, GL.GL_TEXTURE_2D, result.Id, mipmapLevel);
+                    //glFramebufferTexture2D(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_STENCIL_ATTACHMENT, GL.GL_TEXTURE_2D, result.Id, mipmapLevel);// error 
                     break;
                 case TextureAttachment.StencilAttachment:
                     throw new NotImplementedException();
