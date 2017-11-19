aa118617e892a539cbb1e3b0a270a09d8fa24431
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-11-19 22:19:44 +0800

    attach cubemapArrayTexture

--- CSharpGL/GLObjects/Texture/Storages/TexImages/TexImageDataProvider/DataProviders/CubemapData.cs
+++ CSharpGL/GLObjects/Texture/Storages/TexImages/TexImageDataProvider/DataProviders/CubemapData.cs
@@ -14,42 +14,11 @@ namespace CSharpGL
         /// <summary>
         /// 
         /// </summary>
-        public enum Target : uint
-        {
-            /// <summary>
-            /// 
-            /// </summary>
-            PositiveX = GL.GL_TEXTURE_CUBE_MAP_POSITIVE_X,
-            /// <summary>
-            /// 
-            /// </summary>
-            NegtiveX = GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-            /// <summary>
-            /// 
-            /// </summary>
-            PositiveY = GL.GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-            /// <summary>
-            /// 
-            /// </summary>
-            NegtiveY = GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-            /// <summary>
-            /// 
-            /// </summary>
-            PositiveZ = GL.GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-            /// <summary>
-            /// 
-            /// </summary>
-            NegtiveZ = GL.GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
         public readonly Bitmap bitmap;
         /// <summary>
         /// 
         /// </summary>
-        public readonly Target target;
+        public readonly CubemapFace target;
         //private readonly int level;
         private readonly bool autoDispose;
 
@@ -61,7 +30,7 @@ namespace CSharpGL
         /// <param name="bitmap"></param>
         /// <param name="target"></param>
         /// <param name="autoDispose"></param>
-        public CubemapData(Bitmap bitmap, Target target, bool autoDispose)
+        public CubemapData(Bitmap bitmap, CubemapFace target, bool autoDispose)
         {
             this.bitmap = bitmap;
             this.target = target;
