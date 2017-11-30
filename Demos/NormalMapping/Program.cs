7070a5bc3faa5349141b6f788769eba5506a60ed
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-01 00:58:54 +0800

    new project for normal mapping

--- Demos/NormalMapping/Program.cs
+++ Demos/NormalMapping/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace WorldSpaceBillboard
+namespace NormalMapping
 {
     static class Program
     {
@@ -15,7 +15,7 @@ namespace WorldSpaceBillboard
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new Form1());
         }
     }
 }
