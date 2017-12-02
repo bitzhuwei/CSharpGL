c8be6a1d3d3fef13c1c213a7111207e9fe67c6f6
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-03 00:21:34 +0800

    init directional light, color texture and normal texture

--- Demos/NormalMapping/NormalMappingNode.cs
+++ Demos/NormalMapping/NormalMappingNode.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CSharpGL;
+using System.Drawing;
 
 namespace NormalMapping
 {
@@ -32,6 +33,60 @@ namespace NormalMapping
         {
         }
 
+        private DirectionalLight__ m_dirLight;
+        private Texture m_pTexture;
+        private Texture m_pNormalMap;
+
+        protected override void DoInitialize()
+        {
+            base.DoInitialize();
+
+            RenderMethod method = this.RenderUnit.Methods[0];
+            ShaderProgram program = method.Program;
+            {
+                var dirLight = new DirectionalLight__();
+                dirLight.AmbientIntensity = 0.2f;
+                dirLight.DiffuseIntensity = 0.8f;
+                dirLight.Color = new vec3(1.0f, 1.0f, 1.0f);
+                dirLight.Direction = new vec3(1.0f, 0.0f, 0.0f);
+                program.SetUniform("gDirectionalLight.Base.Color", dirLight.Color);
+                program.SetUniform("gDirectionalLight.Base.AmbientIntensity", dirLight.AmbientIntensity);
+                program.SetUniform("gDirectionalLight.Base.DiffuseIntensity", dirLight.DiffuseIntensity);
+                program.SetUniform("gDirectionalLight.Direction", dirLight.Direction.normalize());
+
+                this.m_dirLight = dirLight;
+            }
+            {
+                var bmp = new Bitmap(@"bricks.jpg");
+                var storage = new TexImageBitmap(bmp);
+                var texture = new Texture(storage,
+                    new TexParameteri(TexParameter.PropertyName.TextureWrapS, (int)GL.GL_CLAMP_TO_EDGE),
+                    new TexParameteri(TexParameter.PropertyName.TextureWrapT, (int)GL.GL_CLAMP_TO_EDGE),
+                    new TexParameteri(TexParameter.PropertyName.TextureWrapR, (int)GL.GL_CLAMP_TO_EDGE),
+                    new TexParameteri(TexParameter.PropertyName.TextureMinFilter, (int)GL.GL_LINEAR),
+                    new TexParameteri(TexParameter.PropertyName.TextureMagFilter, (int)GL.GL_LINEAR));
+                texture.TextureUnitIndex = 0;
+                texture.Initialize();
+                bmp.Dispose();
+                program.SetUniform("gColorMap", texture);
+                this.m_pTexture = texture;
+            }
+            {
+                var bmp = new Bitmap(@"normal_map.jpg");
+                var storage = new TexImageBitmap(bmp);
+                var texture = new Texture(storage,
+                    new TexParameteri(TexParameter.PropertyName.TextureWrapS, (int)GL.GL_CLAMP_TO_EDGE),
+                    new TexParameteri(TexParameter.PropertyName.TextureWrapT, (int)GL.GL_CLAMP_TO_EDGE),
+                    new TexParameteri(TexParameter.PropertyName.TextureWrapR, (int)GL.GL_CLAMP_TO_EDGE),
+                    new TexParameteri(TexParameter.PropertyName.TextureMinFilter, (int)GL.GL_LINEAR),
+                    new TexParameteri(TexParameter.PropertyName.TextureMagFilter, (int)GL.GL_LINEAR));
+                texture.TextureUnitIndex = 2;
+                texture.Initialize();
+                bmp.Dispose();
+                program.SetUniform("gNormalMap", texture);
+                this.m_pNormalMap = texture;
+            }
+        }
         public override void RenderBeforeChildren(RenderEventArgs arg)
         {
             ICamera camera = arg.CameraStack.Peek();
