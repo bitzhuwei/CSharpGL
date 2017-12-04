c7cfc8460c5a641b3623628fa68e6a0779d31bdf
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-04 11:48:04 +0800

    Fields connections (#47)
    * all lights in the scene
    
    * ConnectionField: When Someone chhanged its value, I will know.
    
    * generic ConnectionField
    
    * clean
    
    * add comments.
    
    * update

---
+++ CSharpGL/BasicDataStructures/Connection/ConnectionField.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// When Someone has changed its value, I will be notified.
+    /// <para>If my value have changed, I will notify others.</para>
+    /// </summary>
+    public class ConnectionField<T> : ConnectionFieldBase
+    {
+        private T _value;
+        /// <summary>
+        /// The value.
+        /// </summary>
+        public T Value
+        {
+            get { return _value; }
+            set
+            {
+                if (!value.Equals(_value))
+                {
+                    _value = value;
+                    // Notify all fields in 'to' that my value has changed.
+                    foreach (var item in this.to)
+                    {
+                        RaiseNotifyEvent(item);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// When Someone chhanged its value, I will know.
+        /// </summary>
+        /// <param name="value">Initial value. This will not notify anyone.</param>
+        public ConnectionField(T value)
+        {
+            this._value = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("value:{0}", this._value);
+        }
+    }
+}
---
+++ CSharpGL/BasicDataStructures/Connection/ConnectionFieldBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// When Someone has changed its value, I will be notified.
+    /// <para>If my value have changed, I will notify others.</para>
+    /// </summary>
+    public class ConnectionFieldBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        protected List<ConnectionFieldBase> to = new List<ConnectionFieldBase>();
+
+        /// <summary>
+        /// Send a connection request to <paramref name="field"/>.
+        /// <para>If <paramref name="field"/>'s value has changed, I will be notified.</para>
+        /// </summary>
+        /// <param name="field">The field that I want to connect to.</param>
+        public void ConnectTo(ConnectionFieldBase field)
+        {
+            if (field == null) { throw new ArgumentNullException("field"); }
+
+            field.to.Add(this);
+        }
+
+        ///// <summary>
+        ///// There is a connection request from <paramref name="field"/>.
+        ///// <para>If my value have changed, I will notify <paramref name="field"/>.</para>
+        ///// </summary>
+        ///// <param name="field">The field that wants to connect to me(my value's change).</param>
+        //public void ConnectFrom(ConnectionFieldBase field)
+        //{
+        //    this.to.Add(field);
+        //}
+
+        /// <summary>
+        /// Raise notify event for specified <param name="field"></param>.
+        /// </summary>
+        protected static void RaiseNotifyEvent(ConnectionFieldBase field)
+        {
+            var notified = field.Notified;
+            if (notified != null)
+            {
+                notified(field, EventArgs.Empty);
+            }
+        }
+
+        /// <summary>
+        /// Raised when one of the fields that I connected to has changed its value.
+        /// </summary>
+        public event EventHandler Notified;
+
+    }
+}
