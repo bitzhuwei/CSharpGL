ed01162eb43fb0b9b985482018719ee62a823f81
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-05 01:31:48 +0800

    add demo project: how stencil test works

---
+++ Demos/StencilTest/HowStencilWorkNode.cs
@@ -0,0 +1,21 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StencilTest
+{
+    partial class HowStencilWorkNode : ModernNode
+    {
+        public override void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RenderAfterChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demos/StencilTest/HowStencilWorkNode.shader.cs
@@ -0,0 +1,41 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StencilTest
+{
+    partial class HowStencilWorkNode
+    {
+        private const string vertexCode = @"#version 150 core
+    in vec3 position;
+    in vec3 color;
+    in vec2 texcoord;
+    out vec3 Color;
+    out vec2 Texcoord;
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 proj;
+    uniform vec3 overrideColor;
+    void main()
+    {
+        Color = overrideColor * color;
+        Texcoord = texcoord;
+        gl_Position = proj * view * model * vec4(position, 1.0);
+    }
+";
+
+        private const string fragmentCode = @"#version 150 core
+    in vec3 Color;
+    in vec2 Texcoord;
+    out vec4 outColor;
+    uniform sampler2D texKitten;
+    uniform sampler2D texPuppy;
+    void main()
+    {
+        outColor = vec4(Color, 1.0) * mix(texture(texKitten, Texcoord), texture(texPuppy, Texcoord), 0.5);
+    }
+";
+    }
+}
