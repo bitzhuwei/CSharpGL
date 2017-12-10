21ce58d4711a2edc9f54ce99314456806298382d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-10 23:54:18 +0800

    stencil test node

---
+++ Demos/StencilTest/StencilTestModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace StencilTest
+{
+    //
+    /// <summary>
+    /// 
+    /// </summary>
+    class StencilTestModel : IBufferSource
+    {
+        static readonly vec3[] positions = new vec3[] 
+        {
+            new vec3(1, -1, 0), new vec3(1, 1, 0), new vec3(-1, 1, 0), 
+            new vec3(1, -1, 0), new vec3(-1, 1, 0), new vec3(-1, -1, 0) 
+        };
+
+        public const string strPosition = "position";
+        private VertexBuffer positionBuffer;
+
+        private IndexBuffer indexBuffer;
+
+        #region IBufferSource 成员
+
+        public VertexBuffer GetVertexAttributeBuffer(string bufferName)
+        {
+            if (bufferName == strPosition)
+            {
+                if (this.positionBuffer == null)
+                {
+                    this.positionBuffer = positions.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+
+                return this.positionBuffer;
+            }
+            else
+            {
+                throw new ArgumentException("bufferName");
+            }
+        }
+
+        public IndexBuffer GetIndexBuffer()
+        {
+            if (this.indexBuffer == null)
+            {
+                this.indexBuffer = ZeroIndexBuffer.Create(DrawMode.Triangles, 0, positions.Length);
+            }
+
+            return this.indexBuffer;
+        }
+
+        #endregion
+    }
+}
---
+++ Demos/StencilTest/StencilTestNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace StencilTest
+{
+    partial class StencilTestNode : ModernNode
+    {
+        public static StencilTestNode Create()
+        {
+            var model = new StencilTestModel();
+            var vs = new VertexShader(vertexCode);
+            var fs = new FragmentShader(fragmentCode);
+            var provider = new ShaderArray(vs, fs);
+            var map = new AttributeMap();
+            map.Add(inPosition, StencilTestModel.strPosition);
+            var builder = new RenderMethodBuilder(provider, map);
+            var node = new StencilTestNode(model, builder);
+            node.Initialize();
+
+            return node;
+        }
+
+        private StencilTestNode(IBufferSource model, params RenderMethodBuilder[] builders)
+            : base(model, builders)
+        {
+            this.Color = new vec4(1, 1, 1, 1);
+        }
+
+        public override void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            if (!this.IsInitialized) { this.Initialize(); }
+
+            ICamera camera = arg.CameraStack.Peek();
+            mat4 projection = camera.GetProjectionMatrix();
+            mat4 view = camera.GetViewMatrix();
+            mat4 model = this.GetModelMatrix();
+
+            var method = this.RenderUnit.Methods[0]; // the only render unit in this node.
+            ShaderProgram program = method.Program;
+            program.SetUniform(projectionMatrix, projection);
+            program.SetUniform(viewMatrix, view);
+            program.SetUniform(modelMatrix, model);
+            program.SetUniform(color, this.Color);
+
+            method.Render();
+        }
+
+        public override void RenderAfterChildren(RenderEventArgs arg)
+        {
+        }
+
+        public vec4 Color { get; set; }
+    }
+}
