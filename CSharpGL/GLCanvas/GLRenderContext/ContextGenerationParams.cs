7d29784899361a29724da7ea9d8b131ba2b5e136
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-11 13:53:27 +0800

    ContextGenerationParams

---
+++ CSharpGL/GLCanvas/GLRenderContext/ContextGenerationParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    public class ContextGenerationParams
+    {
+        public int width;
+        public int height;
+        public byte colorBitDepth = 32;
+    }
+}
