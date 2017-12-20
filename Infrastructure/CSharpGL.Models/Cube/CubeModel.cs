8e0627078f6aa47188af4ff98ccc8db4ba77d8d6
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-20 20:37:44 +0800

    separate file for cube model.

---
+++ Infrastructure/CSharpGL.Models/Cube/CubeModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CubeModel : IBufferSource
+    {
+        public vec3 ModelSize { get; private set; }
+
+        public CubeModel()
+        {
+            this.ModelSize = new vec3(xLength * 2, yLength * 2, zLength * 2);
+        }
+
+        public const string strPosition = "position";
+        private VertexBuffer positionBuffer;
+
+        private IndexBuffer indexBuffer;
+
+        #region IBufferable 成员
+
+        public VertexBuffer GetVertexAttributeBuffer(string bufferName)
+        {
+            if (bufferName == strPosition)
+            {
+                if (this.positionBuffer == null)
+                {
+                    this.positionBuffer = positions.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+
+                return this.positionBuffer;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public IndexBuffer GetIndexBuffer()
+        {
+            if (this.indexBuffer == null)
+            {
+                this.indexBuffer = ZeroIndexBuffer.Create(DrawMode.TriangleStrip, 0, positions.Length);
+            }
+
+            return this.indexBuffer;
+        }
+
+        #endregion
+
+        private const float xLength = 0.5f;
+        private const float yLength = 0.5f;
+        private const float zLength = 0.5f;
+        /// <summary>
+        /// four vertexes.
+        /// </summary>
+        private static readonly vec3[] positions = new vec3[]
+        {
+            new vec3(+xLength, +yLength, +zLength),//  0
+            new vec3(+xLength, -yLength, +zLength),//  1
+            new vec3(+xLength, +yLength, -zLength),//  2
+            new vec3(+xLength, -yLength, -zLength),//  3
+            new vec3(-xLength, -yLength, -zLength),//  4
+            new vec3(+xLength, -yLength, +zLength),//  5
+            new vec3(-xLength, -yLength, +zLength),//  6
+            new vec3(+xLength, +yLength, +zLength),//  7
+            new vec3(-xLength, +yLength, +zLength),//  8
+            new vec3(+xLength, +yLength, -zLength),//  9
+            new vec3(-xLength, +yLength, -zLength),// 10
+            new vec3(-xLength, -yLength, -zLength),// 11
+            new vec3(-xLength, +yLength, +zLength),// 12
+            new vec3(-xLength, -yLength, +zLength),// 13
+        };
+    }
+}
