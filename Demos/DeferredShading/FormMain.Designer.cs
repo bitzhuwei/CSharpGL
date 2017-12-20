7e274719a999120c88a4857952ca5c7026316dac
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-20 20:37:33 +0800

    deferred shading demo project

---
+++ Demos/DeferredShading/FormMain.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace DeferredShading
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.winGLCanvas1 = new CSharpGL.WinGLCanvas();
+            this.btnOpenImage = new System.Windows.Forms.Button();
+            this.openImageDlg = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // winGLCanvas1
+            // 
+            this.winGLCanvas1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.winGLCanvas1.Location = new System.Drawing.Point(12, 41);
+            this.winGLCanvas1.Name = "winGLCanvas1";
+            this.winGLCanvas1.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
+            this.winGLCanvas1.Size = new System.Drawing.Size(679, 631);
+            this.winGLCanvas1.TabIndex = 0;
+            this.winGLCanvas1.TimerTriggerInterval = 40;
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(110, 23);
+            this.btnOpenImage.TabIndex = 1;
+            this.btnOpenImage.Text = "Open Image ...";
+            this.btnOpenImage.UseVisualStyleBackColor = true;
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // openImageDlg
+            // 
+            this.openImageDlg.Filter = "*.*|*.*";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 684);
+            this.Controls.Add(this.btnOpenImage);
+            this.Controls.Add(this.winGLCanvas1);
+            this.Name = "FormMain";
+            this.Text = "Compute Shader - Edge Detection - CSharpGL";
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CSharpGL.WinGLCanvas winGLCanvas1;
+        private System.Windows.Forms.Button btnOpenImage;
+        private System.Windows.Forms.OpenFileDialog openImageDlg;
+    }
+}
