dd805124b0fcf26ea6bd4f22f041b797b6d1feb5
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2017-12-27 23:19:36 +0800

    node's shader.

---
+++ Demos/DeferredShading/ManyCubesNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace DeferredShading
+{
+    partial class ManyCubesNode : ModernNode
+    {
+
+        public static ManyCubesNode Create(int lengthX, int lengthY, int lengthZ)
+        {
+            var model = new ManyCubesModel(lengthX, lengthY, lengthZ);
+
+        }
+
+        private ManyCubesNode(IBufferSource model, params RenderMethodBuilder[] builders)
+            : base(model, builders) { }
+
+        public override void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RenderAfterChildren(RenderEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demos/DeferredShading/ManyCubesNode.shaders.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace DeferredShading
+{
+    partial class ManyCubesNode
+    {
+        private const string firstPassVert = @"#version 330 core
+
+in vec3 vPosition; // per-vertex position
+in vec3 vColor; // per-vertex normal
+
+uniform mat4 MVP; // combined model view projection matrix
+
+out vec3 passColor;
+
+void main()
+{
+	gl_Position = MVP * vec4(vPosition, 1);
+    passColor = vColor;
+}
+";
+        private const string firstPassFrag = @"#version 330 core
+
+in vec3 passColor;
+
+layout (location = 0) out vec3 vFragColor;
+
+void main()
+{
+    vFragColor = passColor;
+}
+";
+
+        private const string secondPassVert = @"#version 330 core
+
+in vec2 texCoord;
+
+out vec2 passTexCoord;
+
+const float value = 1;
+
+void main(void) {
+	vec2 vertexes[4] = vec2[4](vec2(value, value), vec2(-value, value), vec2(-value, -value), vec2(value, -value));
+	vec2 texCoord[4] = vec2[4](vec2(1.0, 1.0), vec2(0.0, 1.0), vec2(0.0, 0.0), vec2(1.0, 0.0));
+    gl_Position = vec4(vertexes[gl_VertexID], 0, 1);
+
+    passTexCoord = texCoord;
+}
+";
+
+
+        private const string secondPassFrag = @"#version 330 core
+
+uniform sampler2D colorSampler;
+
+in vec2 passTexCoord;
+
+out vec4 vFragColor;
+
+void main(void) {
+    vFragColor = texture(colorSampler, passTexCoord);
+}
+";
+
+    }
+}
