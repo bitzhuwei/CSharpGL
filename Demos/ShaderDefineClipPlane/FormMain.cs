427e1c275b4c11856a67cb8114f8cb6003a93189
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-01-17 11:32:41 +0800

    new demo project: ShaderDefineClipPlane

---
+++ Demos/ShaderDefineClipPlane/FormMain.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CSharpGL;
+
+namespace ShaderDefineClipPlane
+{
+    public partial class FormMain : Form
+    {
+        private Scene scene;
+        private ActionList actionList;
+        private LegacyPickingAction legacyPickingAction;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Load += FormMain_Load;
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+            this.winGLCanvas1.MouseClick += winGLCanvas1_MouseClick;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //var rootElement = GetLegacyPropellerLegacyFlabellum();
+            //var rootElement = GetLegacyPropellerFlabellum();
+            //var rootElement = GetPropellerLegacyFlabellum();
+            //var rootElement = GetPropellerFlabellum();
+            var rootElement = GetPropellerRTT();
+
+            var position = new vec3(5, 3, 4);
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+            this.scene = new Scene(camera)
+            {
+                RootElement = rootElement,
+                ClearColor = Color.SkyBlue.ToVec4(),
+            };
+
+            var list = new ActionList();
+            var transformAction = new TransformAction(scene);
+            list.Add(transformAction);
+            var renderAction = new RenderAction(scene);
+            list.Add(renderAction);
+            this.actionList = list;
+
+            this.legacyPickingAction = new LegacyPickingAction(scene);
+
+            Match(this.trvScene, scene.RootElement);
+            this.trvScene.ExpandAll();
+        }
+
+        private void Match(TreeView treeView, SceneNodeBase nodeBase)
+        {
+            treeView.Nodes.Clear();
+            var node = new TreeNode(nodeBase.ToString()) { Tag = nodeBase };
+            treeView.Nodes.Add(node);
+            Match(node, nodeBase);
+        }
+
+        private void Match(TreeNode node, SceneNodeBase nodeBase)
+        {
+            foreach (var item in nodeBase.Children)
+            {
+                var child = new TreeNode(item.ToString()) { Tag = item };
+                node.Nodes.Add(child);
+                Match(child, item);
+            }
+        }
+
+        private SceneNodeBase GetPropellerRTT()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            ActionList list = this.actionList;
+            if (list != null)
+            {
+                list.Act(new ActionParams(Viewport.GetCurrent()));
+            }
+        }
+
+        void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
+        }
+
+        /// <summary>
+        /// click to pick and toggle the render wireframe state.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void winGLCanvas1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = this.winGLCanvas1.Height - e.Y - 1;
+            List<HitTarget> list = this.legacyPickingAction.Pick(x, y);
+            //foreach (var item in list)
+            //{
+            //    var parent = item.node.Parent;
+            //    if (parent != null)
+            //    {
+            //        var node = parent as IRenderable;
+            //        if (node != null)
+            //        {
+            //            node.RenderingEnabled = !node.RenderingEnabled;
+            //        }
+            //    }
+            //}
+
+            if (list.Count == 0)
+            {
+                this.propGrid.SelectedObject = null;
+            }
+            else if (list.Count == 1)
+            {
+                this.propGrid.SelectedObject = list[0].node;
+            }
+            else
+            {
+                this.propGrid.SelectedObjects = (from item in list select item.node).ToArray();
+            }
+
+            this.lblState.Text = string.Format("{0} objects selected.", list.Count);
+        }
+
+        private void trvScene_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            this.propGrid.SelectedObject = e.Node.Tag;
+
+            this.lblState.Text = string.Format("{0} objects selected.", 1);
+        }
+    }
+}
