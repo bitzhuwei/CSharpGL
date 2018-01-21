6c6d9037ffd0a1924eb13c1c971a4a154d2d0576
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-01-21 22:58:52 +0800

    AdjacentFace

--- Infrastructure/CSharpGL.Models/Teapot/Teapot.Faces.cs
+++ Infrastructure/CSharpGL.Models/Teapot/Teapot.Faces.cs
@@ -1,5 +1,22 @@
 ﻿namespace CSharpGL
 {
+    internal struct AdjacentFace
+    {
+        public ushort vertexId1;
+        public ushort adjacentId1;
+        public ushort vertexId2;
+        public ushort adjacentId2;
+        public ushort vertexId3;
+        public ushort adjacentId3;
+
+        public AdjacentFace(ushort v1, ushort a1, ushort v2, ushort a2, ushort v3, ushort a3)
+        {
+            this.vertexId1 = v1; this.adjacentId1 = a1;
+            this.vertexId2 = v2; this.adjacentId2 = a2;
+            this.vertexId3 = v3; this.adjacentId3 = a3;
+        }
+    }
+
     internal struct Face
     {
         public ushort vertexId1;
