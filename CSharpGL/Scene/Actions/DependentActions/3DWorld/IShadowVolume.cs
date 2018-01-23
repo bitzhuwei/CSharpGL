74511b1019cf5cd8f0e5bcdeeb71115fd7dc05c1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-01-23 13:55:35 +0800

    IShadowVolume

---
+++ CSharpGL/Scene/Actions/DependentActions/3DWorld/IShadowVolume.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Design;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Supports shadow volume.
+    /// </summary>
+    [Editor(typeof(PropertyGridEditor), typeof(UITypeEditor))]
+    public interface IShadowVolume
+    {
+        /// <summary>
+        /// Is casting shadow for this object enabled?
+        /// </summary>
+        bool EnableShadowVolume { get; set; }
+
+        /// <summary>
+        /// Cast shadow to specified texture in framebuffer, or Prepare for its children to cast shadow.
+        /// </summary>
+        /// <param name="arg"></param>
+        void ExtrudeShadow(ShdowVolumeEventArgs arg);
+    }
+
+    /// <summary>
+    /// Render event argument.
+    /// </summary>
+    public class ShdowVolumeEventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RenderEventArgs"/> class.
+        /// </summary>
+        public ShdowVolumeEventArgs()
+        {
+            this.ModelMatrixStack = new Stack<mat4>();
+            this.ModelMatrixStack.Push(mat4.identity());
+
+            //this.LightStack = new Stack<LightBase>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal Stack<mat4> ModelMatrixStack { get; private set; }
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <returns></returns>
+        //public virtual mat4 GetViewMatrix()
+        //{
+        //    return this.Scene.Camera.GetViewMatrix();
+        //}
+
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <returns></returns>
+        //public virtual mat4 GetProjectionMatrix()
+        //{
+        //    return this.Scene.Camera.GetProjectionMatrix();
+        ////}
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //internal Stack<LightBase> LightStack { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public LightBase CurrentLight { get; set; }
+    }
+}
