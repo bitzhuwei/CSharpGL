78a84b570b6c0c72e4d576c4435d6dc603fbc930
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-01-25 16:42:12 +0800

    AdjacentCubeModel

---
+++ Demos/StencilShadowVolume/AjdacentTrianglesNode.shaders.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StencilShadowVolume
+{
+    partial class AjdacentTrianglesNode
+    {
+
+        private const string vertexCode = @"#version 330
+
+in vec3 inPosition;
+in vec3 inColor;
+
+uniform mat4 mvpMat;
+
+out vec3 passColor;
+
+void main(void) {
+	gl_Position = mvpMat * vec4(inPosition, 1.0);
+    passColor = inColor;
+}
+";
+        private const string fragmentCode = @"#version 330
+
+in vec3 passColor;
+
+out vec4 out_Color;
+
+void main(void) {
+	out_Color = vec4(passColor, 1.0);
+}
+";
+    }
+}
--- Infrastructure/CSharpGL.Models/Cube/AdjacentCubeModel.cs
+++ Infrastructure/CSharpGL.Models/Cube/AdjacentCubeModel.cs
@@ -18,6 +18,17 @@ namespace CSharpGL
             new vec3(-1,1,1),
             new vec3(-1,1,-1),
         };
+        private static readonly vec3[] colors = new vec3[]
+        {
+            new vec3(1,0,0),
+            new vec3(1,1,1) * 0.5f,
+            new vec3(0,0,1),
+            new vec3(1,1,1) * 0.5f,
+            new vec3(1,1,1) * 0.5f,
+            new vec3(1,1,1),
+            new vec3(1,1,1) * 0.5f,
+            new vec3(0,1,0),
+        };
 
         private static readonly Face[] indexes = new Face[] { new Face(0, 1, 2), new Face(0, 2, 3), new Face(4, 7, 6), new Face(4, 6, 5), new Face(0, 4, 5), new Face(0, 5, 1), new Face(1, 5, 6), new Face(1, 6, 2), new Face(2, 6, 7), new Face(2, 7, 3), new Face(4, 0, 3), new Face(4, 3, 7), };
 
@@ -28,18 +39,59 @@ namespace CSharpGL
             adjacentIndexes = FaceHelper.CalculateAdjacentFaces(indexes);
         }
 
+        public const string strPosition = "position";
+        private VertexBuffer positionBuffer;
+        public const string strColor = "color";
+        private VertexBuffer colorBuffer;
+
+        private IDrawCommand command;
+
         #region IBufferSource 成员
 
         public IEnumerable<VertexBuffer> GetVertexAttributeBuffer(string bufferName)
         {
-            throw new NotImplementedException();
+            if (strPosition == bufferName)
+            {
+                if (this.positionBuffer == null)
+                {
+                    this.positionBuffer = positions.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+
+                yield return this.positionBuffer;
+            }
+            else if (strColor == bufferName)
+            {
+                if (this.colorBuffer == null)
+                {
+                    this.colorBuffer = colors.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+
+                yield return this.colorBuffer;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
 
         public IEnumerable<IDrawCommand> GetDrawCommand()
         {
-            throw new NotImplementedException();
+            if (this.command == null)
+            {
+                //IndexBuffer indexBuffer = indexes.GenIndexBuffer(IndexBufferElementType.UShort, BufferUsage.StaticDraw);
+                //this.command = new DrawElementsCmd(indexBuffer, DrawMode.Triangles);
+                IndexBuffer indexBuffer = adjacentIndexes.GenIndexBuffer(IndexBufferElementType.UShort, BufferUsage.StaticDraw);
+                this.command = new DrawElementsCmd(indexBuffer, DrawMode.TrianglesAdjacency);
+            }
+
+            yield return this.command;
         }
 
         #endregion
+
+        public vec3 GetSize()
+        {
+            return new vec3(2, 2, 2);
+        }
     }
 }
