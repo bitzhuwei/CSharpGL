c7244a5d3ae93d79b478a171feb8c1212f1ae80b
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-01-30 12:15:02 +0800

    __LightSetUniformsHelper

---
+++ CSharpGL/Scene/NewLights/__LightSetUniformsHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class __LightSetUniformsHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="light"></param>
+        public static void SetUniforms(this LightBase light)
+        {
+
+        }
+
+    }
+}
