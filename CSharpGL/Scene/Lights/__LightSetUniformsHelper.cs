35aaef3f8f058f1ff2663942b9bb39b7ebc43217
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-02-01 01:57:00 +0800

    SetUniforms(this PointLight light, ShaderProgram program)

--- CSharpGL/Scene/Lights/__LightSetUniformsHelper.cs
+++ CSharpGL/Scene/Lights/__LightSetUniformsHelper.cs
@@ -23,9 +23,9 @@ namespace CSharpGL
         /// <param name="program"></param>
         public static void SetUniforms(this LightBase light, ShaderProgram program)
         {
-            if (light is TSpotLight)
+            if (light is PointLight)
             {
-                SetUniforms(light as TSpotLight, program);
+                SetUniforms(light as PointLight, program);
             }
             else if (light is DirectionalLight)
             {
@@ -35,13 +35,17 @@ namespace CSharpGL
             {
                 SetUniforms(light as SpotLight, program);
             }
+            else if (light is TSpotLight)
+            {
+                SetUniforms(light as TSpotLight, program);
+            }
             else
             {
                 throw new System.Exception(string.Format("Not expected light type:[{0}].", light.GetType()));
             }
         }
 
-        private static void SetUniforms(this TSpotLight light, ShaderProgram program)
+        private static void SetUniforms(this PointLight light, ShaderProgram program)
         {
             program.SetUniform("light.position", light.Position);
             program.SetUniform("light.diffuse", light.Diffuse);
@@ -49,12 +53,11 @@ namespace CSharpGL
             program.SetUniform("light.constant", light.Attenuation.Constant);
             program.SetUniform("light.linear", light.Attenuation.Linear);
             program.SetUniform("light.quadratic", light.Attenuation.Exp);
-            program.SetUniform("light.direction", light.Direction.ToVec3());
-            //program.SetUniform("light.cutOff", light.CutOff);
+            //program.SetUniform("light.direction", );
+            //program.SetUniform("light.cutOff", );
 
-            int lightUpRoutine = (int)light.Direction;
+            int lightUpRoutine = 0;
             program.SetUniform("lightUpRoutine", lightUpRoutine);
-
         }
 
         private static void SetUniforms(this DirectionalLight light, ShaderProgram program)
@@ -70,7 +73,6 @@ namespace CSharpGL
 
             const int lightUpRoutine = 1;
             program.SetUniform("lightUpRoutine", lightUpRoutine);
-
         }
 
         private static void SetUniforms(this SpotLight light, ShaderProgram program)
@@ -86,7 +88,22 @@ namespace CSharpGL
 
             const int lightUpRoutine = 2;
             program.SetUniform("lightUpRoutine", lightUpRoutine);
-
         }
+
+        private static void SetUniforms(this TSpotLight light, ShaderProgram program)
+        {
+            program.SetUniform("light.position", light.Position);
+            program.SetUniform("light.diffuse", light.Diffuse);
+            program.SetUniform("light.specular", light.Specular);
+            program.SetUniform("light.constant", light.Attenuation.Constant);
+            program.SetUniform("light.linear", light.Attenuation.Linear);
+            program.SetUniform("light.quadratic", light.Attenuation.Exp);
+            program.SetUniform("light.direction", light.Direction.ToVec3());
+            //program.SetUniform("light.cutOff", light.CutOff);
+
+            int lightUpRoutine = (int)light.Direction;
+            program.SetUniform("lightUpRoutine", lightUpRoutine);
+        }
+
     }
 }
