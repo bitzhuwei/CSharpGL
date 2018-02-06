0883bc292169e0eab4a33fce7094d267e1b47394
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-02-06 22:50:15 +0800

    TrianglesAdjacencyRecognizer

---
+++ CSharpGL/Scene/SceneNodes/PickableNode/PickableNode.IPickable/ColorCodedPicking/PrimivitiveRecognizers/TrianglesAdjacencyRecognizer.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpGL
+{
+    internal class TrianglesAdjacencyRecognizer : PrimitiveRecognizer
+    {
+        protected override void RecognizeByte(uint lastVertexId, DrawElementsCmd cmd, List<RecognizedPrimitiveInfo> primitiveInfoList)
+        {
+            IndexBuffer indexBuffer = cmd.IndexBufferObject;
+            int length = indexBuffer.Length;
+            IntPtr pointer = indexBuffer.MapBuffer(MapBufferAccess.ReadOnly);
+            unsafe
+            {
+                var array = (byte*)pointer.ToPointer();
+                //long nearestRestartIndex = -1;
+                uint i = 0;
+                for (i = i + 4; i < length; i += 6)
+                {
+                    var value = array[i];
+                    if (value == lastVertexId)
+                    //&& (i - nearestRestartIndex) % 3 == 0)
+                    {
+                        var item = new RecognizedPrimitiveInfo(i, array[i - 4], array[i - 2], lastVertexId);
+                        primitiveInfoList.Add(item);
+                    }
+                }
+            }
+            indexBuffer.UnmapBuffer();
+        }
+
+        protected override void RecognizeUShort(uint lastVertexId, DrawElementsCmd cmd, List<RecognizedPrimitiveInfo> primitiveInfoList)
+        {
+            IndexBuffer indexBuffer = cmd.IndexBufferObject;
+            int length = indexBuffer.Length;
+            IntPtr pointer = indexBuffer.MapBuffer(MapBufferAccess.ReadOnly);
+            unsafe
+            {
+                var array = (ushort*)pointer.ToPointer();
+                //long nearestRestartIndex = -1;
+                uint i = 0;
+                for (i = i + 2; i < length; i += 3)
+                {
+                    var value = array[i];
+                    if (value == lastVertexId)
+                    //&& (i - nearestRestartIndex) % 3 == 0)
+                    {
+                        var item = new RecognizedPrimitiveInfo(i, array[i - 2], array[i - 1], lastVertexId);
+                        primitiveInfoList.Add(item);
+                    }
+                }
+            }
+            indexBuffer.UnmapBuffer();
+        }
+
+        protected override void RecognizeUInt(uint lastVertexId, DrawElementsCmd cmd, List<RecognizedPrimitiveInfo> primitiveInfoList)
+        {
+            IndexBuffer indexBuffer = cmd.IndexBufferObject;
+            int length = indexBuffer.Length;
+            IntPtr pointer = indexBuffer.MapBuffer(MapBufferAccess.ReadOnly);
+            unsafe
+            {
+                var array = (uint*)pointer.ToPointer();
+                //long nearestRestartIndex = -1;
+                uint i = 0;
+                for (i = i + 2; i < length; i += 3)
+                {
+                    var value = array[i];
+                    if (value == lastVertexId)
+                    //&& (i - nearestRestartIndex) % 3 == 0)
+                    {
+                        var item = new RecognizedPrimitiveInfo(i, array[i - 2], array[i - 1], lastVertexId);
+                        primitiveInfoList.Add(item);
+                    }
+                }
+            }
+            indexBuffer.UnmapBuffer();
+        }
+
+        protected override void RecognizeByte(uint lastVertexId, DrawElementsCmd cmd, List<RecognizedPrimitiveInfo> primitiveInfoList, uint primitiveRestartIndex)
+        {
+            IndexBuffer indexBuffer = cmd.IndexBufferObject;
+            int length = indexBuffer.Length;
+            IntPtr pointer = indexBuffer.MapBuffer(MapBufferAccess.ReadOnly);
+            unsafe
+            {
+                var array = (byte*)pointer.ToPointer();
+                long nearestRestartIndex = -1;
+                uint i = 0;
+                while (i < length && array[i] == primitiveRestartIndex)
+                { nearestRestartIndex = i; i++; }
+                for (i = i + 2; i < length; i++)
+                {
+                    var value = array[i];
+                    if (value == primitiveRestartIndex)
+                    {
+                        nearestRestartIndex = i;
+                    }
+                    else if (value == lastVertexId
+                        && array[i - 1] != primitiveRestartIndex
+                        && array[i - 2] != primitiveRestartIndex
+                        && (i - nearestRestartIndex) % 3 == 0)
+                    {
+                        var item = new RecognizedPrimitiveInfo(i, array[i - 2], array[i - 1], lastVertexId);
+                        primitiveInfoList.Add(item);
+                    }
+                }
+            }
+            indexBuffer.UnmapBuffer();
+        }
+
+        protected override void RecognizeUShort(uint lastVertexId, DrawElementsCmd cmd, List<RecognizedPrimitiveInfo> primitiveInfoList, uint primitiveRestartIndex)
+        {
+            IndexBuffer indexBuffer = cmd.IndexBufferObject;
+            int length = indexBuffer.Length;
+            IntPtr pointer = indexBuffer.MapBuffer(MapBufferAccess.ReadOnly);
+            unsafe
+            {
+                var array = (ushort*)pointer.ToPointer();
+                long nearestRestartIndex = -1;
+                uint i = 0;
+                while (i < length && array[i] == primitiveRestartIndex)
+                { nearestRestartIndex = i; i++; }
+                for (i = i + 2; i < length; i++)
+                {
+                    var value = array[i];
+                    if (value == primitiveRestartIndex)
+                    {
+                        nearestRestartIndex = i;
+                    }
+                    else if (value == lastVertexId
+                        && array[i - 1] != primitiveRestartIndex
+                        && array[i - 2] != primitiveRestartIndex
+                        && (i - nearestRestartIndex) % 3 == 0)
+                    {
+                        var item = new RecognizedPrimitiveInfo(i, array[i - 2], array[i - 1], lastVertexId);
+                        primitiveInfoList.Add(item);
+                    }
+                }
+            }
+            indexBuffer.UnmapBuffer();
+        }
+
+        protected override void RecognizeUInt(uint lastVertexId, DrawElementsCmd cmd, List<RecognizedPrimitiveInfo> primitiveInfoList, uint primitiveRestartIndex)
+        {
+            IndexBuffer indexBuffer = cmd.IndexBufferObject;
+            int length = indexBuffer.Length;
+            IntPtr pointer = indexBuffer.MapBuffer(MapBufferAccess.ReadOnly);
+            unsafe
+            {
+                var array = (uint*)pointer.ToPointer();
+                long nearestRestartIndex = -1;
+                uint i = 0;
+                while (i < length && array[i] == primitiveRestartIndex)
+                { nearestRestartIndex = i; i++; }
+                for (i = i + 2; i < length; i++)
+                {
+                    var value = array[i];
+                    if (value == primitiveRestartIndex)
+                    {
+                        nearestRestartIndex = i;
+                    }
+                    else if (value == lastVertexId
+                        && array[i - 1] != primitiveRestartIndex
+                        && array[i - 2] != primitiveRestartIndex
+                        && (i - nearestRestartIndex) % 3 == 0)
+                    {
+                        var item = new RecognizedPrimitiveInfo(i, array[i - 2], array[i - 1], lastVertexId);
+                        primitiveInfoList.Add(item);
+                    }
+                }
+            }
+            indexBuffer.UnmapBuffer();
+        }
+    }
+}
