345ecae5d6397ae0a92da4c719c69ef72c2700b0
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-02-11 16:17:19 +0800

    rename project fronttobackpeeling

--- Demos/DepthPeeling.FrontToBackPeeling/FormMain.cs
+++ Demos/DepthPeeling.FrontToBackPeeling/FormMain.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using CSharpGL;
 
-namespace FrontToBackPeeling
+namespace DepthPeeling.FrontToBackPeeling
 {
     public partial class FormMain : Form
     {
--- Demos/DepthPeeling.FrontToBackPeeling/Program.cs
+++ Demos/DepthPeeling.FrontToBackPeeling/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace FrontToBackPeeling
+namespace DepthPeeling.FrontToBackPeeling
 {
     static class Program
     {
