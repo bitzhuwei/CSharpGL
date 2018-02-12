0dcab10ea0510cf884e2e040ad6f3451066da6bc
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-02-12 15:22:11 +0800

    RenderUnit is a unit that Rendering something using multiple GLSL shader programs and VBO(VAO)..

--- CSharpGL/Scene/SceneNodes/ModernNode/ModernNode.Property.cs
+++ CSharpGL/Scene/SceneNodes/ModernNode/ModernNode.Property.cs
@@ -9,10 +9,10 @@ namespace CSharpGL
         private const string strModernNode = "ModernNode";
 
         /// <summary>
-        /// 
+        /// Rendering something using multiple GLSL shader programs and VBO(VAO).
         /// </summary>
         [Category(strModernNode)]
-        [Description("rendering in multiple ways.")]
+        [Description("Rendering something using multiple GLSL shader programs and VBO(VAO).")]
         public ModernRenderUnit RenderUnit { get; private set; }
 
     }
