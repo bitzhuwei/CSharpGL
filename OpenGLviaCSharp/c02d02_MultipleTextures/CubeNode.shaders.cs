494179b21b86671bf4c1d7d312177a48b907f29a
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-03-08 23:54:41 +0800

    c02d02_MultipleTextures

---
+++ OpenGLviaCSharp/c02d02_MultipleTextures/CubeNode.shaders.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace c02d02_MultipleTextures
+{
+    partial class CubeNode
+    {
+        private const string vertexCode = @"
+#version 150
+
+in vec3 inPosition;
+in vec2 inTexCoord;
+
+uniform mat4 mvpMatrix;
+
+out vec2 passTexCoord;
+
+void main() {
+    gl_Position = mvpMatrix * vec4(inPosition, 1.0); 
+
+    passTexCoord = inTexCoord;
+}
+";
+
+        private const string fragmnetCode = @"
+#version 150
+
+//uniform vec4 color = vec4(1, 0, 0, 1); // default: red color.
+uniform sampler2D tex;
+
+in vec2 passTexCoord;
+
+out vec4 outColor;
+
+void main() {
+    //outColor = color;
+    outColor = texture(tex, passTexCoord);
+}
+";
+    }
+}
---
+++ OpenGLviaCSharp/c02d02_MultipleTextures/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace c02d02_MultipleTextures
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
