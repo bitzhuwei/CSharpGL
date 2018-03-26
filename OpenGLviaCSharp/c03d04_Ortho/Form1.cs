beab6d1aa9f30bb935248d4670141c4dff8d0d37
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-03-26 13:01:26 +0800

    c03d04_Ortho

--- OpenGLviaCSharp/c03d04_Ortho/Form1.cs
+++ OpenGLviaCSharp/c03d04_Ortho/Form1.cs
@@ -33,7 +33,7 @@ namespace c03d04_Ortho
             var position = new vec3(5, 3, 4);
             var center = new vec3(0, 0, 0);
             var up = new vec3(0, 1, 0);
-            var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+            var camera = new Camera(position, center, up, CameraType.Ortho, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
             var scene = new Scene(camera);
             var rootNode = GetRootNode();
             scene.RootNode = rootNode;
@@ -67,7 +67,7 @@ namespace c03d04_Ortho
                 rootNode.Children.Add(cameraNode);
             }
             {
-                var perspectiveNode = PerspectiveNode.Create();
+                var perspectiveNode = OrthoNode.Create();
                 rootNode.Children.Add(perspectiveNode);
             }
             {
