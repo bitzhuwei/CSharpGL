56012be4fceca714f42257ad098dda98b93baa62
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-05-01 21:59:18 +0800

    c08d02_Triangles and c08d03_flat

---
+++ OpenGLviaCSharp/c08d02_Triangles/TrianglesNode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace c08d02_Triangles
+{
+    partial class TrianglesNode : PickableNode, IRenderable
+    {
+        public enum EMethod { Random, gl_VertexID, };
+        public EMethod Method { get; set; }
+
+        public static TrianglesNode Create(IBufferSource model, string position, string color, vec3 size)
+        {
+            RenderMethodBuilder randomBuilder, gl_VertexIDBuilder;
+            {
+                var vs = new VertexShader(randomVert);
+                var fs = new FragmentShader(randomFrag);
+                var array = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("inPosition", position);
+                map.Add("inColor", color);
+                randomBuilder = new RenderMethodBuilder(array, map);
+            }
+            {
+                var vs = new VertexShader(gl_VertexIDVert);
+                var fs = new FragmentShader(gl_VertexIDFrag);
+                var array = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("inPosition", position);
+                gl_VertexIDBuilder = new RenderMethodBuilder(array, map);
+            }
+
+            var node = new TrianglesNode(model, position, randomBuilder, gl_VertexIDBuilder);
+            node.Initialize();
+            node.ModelSize = size;
+
+            return node;
+        }
+
+        private TrianglesNode(IBufferSource model, string positionNameInIBufferSource, params RenderMethodBuilder[] builders)
+            : base(model, positionNameInIBufferSource, builders)
+        {
+            this.EnableRendering = ThreeFlags.BeforeChildren | ThreeFlags.Children;
+        }
+
+        #region IRenderable 成员
+
+        public ThreeFlags EnableRendering { get; set; }
+
+        public void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            ICamera camera = arg.Camera;
+            mat4 projection = camera.GetProjectionMatrix();
+            mat4 view = camera.GetViewMatrix();
+            mat4 model = this.GetModelMatrix();
+
+            var method = this.RenderUnit.Methods[(int)this.Method];
+            ShaderProgram program = method.Program;
+            program.SetUniform("mvpMatrix", projection * view * model);
+            method.Render();
+        }
+
+        public void RenderAfterChildren(RenderEventArgs arg)
+        {
+        }
+
+        #endregion
+    }
+
+}
---
+++ OpenGLviaCSharp/c08d02_Triangles/TrianglesNode.shaders.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace c08d02_Triangles
+{
+    partial class TrianglesNode
+    {
+
+        private const string randomVert = @"#version 150 core
+
+in vec3 inPosition;
+in vec3 inColor;
+
+uniform mat4 mvpMatrix;
+
+out vec3 passColor;
+
+void main(void) {
+    // transform vertex' position from model space to clip space.
+    gl_Position = mvpMatrix * vec4(inPosition, 1.0);
+
+    passColor = inColor;
+}
+
+";
+        private const string randomFrag = @"#version 150
+
+in vec3 passColor;
+
+out vec4 out_Color;
+
+void main(void) {
+	out_Color = vec4(passColor, 1.0);
+}
+";
+        private const string gl_VertexIDVert = @"// vertex shader that gets color value according to gl_VertexID.
+#version 150 core
+
+in vec3 inPosition;
+
+uniform mat4 mvpMatrix;
+
+out vec4 passColor;
+
+void main(void) {
+    // transform vertex' position from model space to clip space.
+    gl_Position = mvpMatrix * vec4(inPosition, 1.0);
+
+    // gets color value according to gl_VertexID.
+    int index = gl_VertexID;
+    passColor = vec4(
+        float(index & 0xFF) / 255.0, 
+        float((index >> 8) & 0xFF) / 255.0, 
+        float((index >> 16) & 0xFF) / 255.0, 
+        float((index >> 24) & 0xFF) / 255.0);
+}
+";
+        private const string gl_VertexIDFrag = @"#version 150
+
+in vec4 passColor;
+
+out vec4 out_Color;
+
+void main(void) {
+	out_Color = passColor;
+}
+";
+
+    }
+
+}
