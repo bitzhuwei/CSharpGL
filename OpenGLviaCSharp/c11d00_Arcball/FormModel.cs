1838a7e6a395fdbb2f70437d623becf7ac36bf2b
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-06-02 11:52:57 +0800

    teapot

--- OpenGLviaCSharp/c11d00_Arcball/FormModel.cs
+++ OpenGLviaCSharp/c11d00_Arcball/FormModel.cs
@@ -6,14 +6,94 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using CSharpGL;
 
 namespace c11d00_Arcball
 {
     public partial class FormModel : Form
     {
+        private Scene scene;
+        private ActionList actionList;
+
         public FormModel()
         {
             InitializeComponent();
+
+            this.Load += FormMain_Load;
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            var position = new vec3(5, 3, 4);
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+            this.scene = new Scene(camera);
+            this.scene.RootNode = GetRootNode();
+
+            var list = new ActionList();
+            list.Add(new TransformAction(scene.RootNode));
+            list.Add(new RenderAction(scene));
+            this.actionList = list;
+
+            var manipulater = new FirstPerspectiveManipulater();
+            manipulater.Bind(camera, this.winGLCanvas1);
+
+        }
+
+        private SceneNodeBase GetRootNode()
+        {
+            TeapotNode node = TeapotNode.Create();
+            return node;
+        }
+        //private SceneNodeBase GetRootNode()
+        //{
+        //    var group = new GroupNode();
+        //    var filenames = new string[] { "floor.obj_", "cube.obj_", };
+        //    var colors = new Color[] { Color.Green, Color.White, };
+        //    for (int i = 0; i < filenames.Length; i++)
+        //    {
+        //        string folder = System.Windows.Forms.Application.StartupPath;
+        //        string filename = System.IO.Path.Combine(folder + @"\..\..\..\..\Infrastructure\CSharpGL.Model", filenames[i]);
+        //        var parser = new ObjVNFParser(true);
+        //        ObjVNFResult result = parser.Parse(filename);
+        //        if (result.Error != null)
+        //        {
+        //            MessageBox.Show(result.Error.ToString());
+        //        }
+        //        else
+        //        {
+        //            ObjVNFMesh mesh = result.Mesh;
+        //            var model = new AdjacentTriangleModel(mesh);
+        //            var node = PointsNode.Create(model, ObjVNF.strPosition, ObjVNF.strNormal, model.GetSize());
+        //            node.Color = colors[i].ToVec3();
+        //            node.WorldPosition = new vec3(0, i * 5, 0);
+        //            node.Name = filename;
+        //            group.Children.Add(node);
+        //        }
+        //    }
+
+        //    return group;
+        //}
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            ActionList list = this.actionList;
+            if (list != null)
+            {
+                vec4 clearColor = this.scene.ClearColor;
+                GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+
+                list.Act(new ActionParams(Viewport.GetCurrent()));
+            }
+        }
+
+        void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
         }
     }
 }
