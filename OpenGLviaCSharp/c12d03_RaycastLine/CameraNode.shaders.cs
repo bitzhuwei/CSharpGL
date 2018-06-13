63a9ffd54806e401c9b5ec92c9a9ff5812e11e7f
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-06-13 18:14:07 +0800

    c12d03_RaycastLine

---
+++ OpenGLviaCSharp/c12d03_RaycastLine/CameraNode.shaders.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace c12d03_RaycastLine
+{
+    partial class CameraNode
+    {
+        private const string vertexCode = @"
+#version 150
+
+in vec3 inPosition;
+
+uniform mat4 mvpMatrix;
+
+void main() {
+    gl_Position = mvpMatrix * vec4(inPosition, 1.0); 
+}
+";
+
+        private const string fragmentCode = @"
+#version 150
+
+uniform vec4 color = vec4(1, 1, 1, 1); // default: red color.
+
+uniform bool halfTransparent = false;
+
+out vec4 outColor;
+
+void main() {
+    if (halfTransparent) {
+        if (int(gl_FragCoord.x - 0.5) % 2 == 1 && int(gl_FragCoord.y - 0.5) % 2 == 1) discard;
+        if (int(gl_FragCoord.x - 0.5) % 2 != 1 && int(gl_FragCoord.y - 0.5) % 2 != 1) discard;
+    }
+
+    outColor = color;
+}
+";
+    }
+}
