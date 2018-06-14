fbf6614304b172057521ae51eb847b47d30b27a2
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-06-14 16:25:38 +0800

    update

--- Demos/OrderIndependentTransparency/OITNode.resolve_lists.cs
+++ Demos/OrderIndependentTransparency/OITNode.resolve_lists.cs
@@ -17,7 +17,6 @@ uniform mat4 mvpMatrix;
 void main(void)
 {
     gl_Position = mvpMatrix * vec4(vPosition, 1.0f);
-    //gl_Position = vec4(position, 1.0f);
 }
 ";
         private const string resolveListsFrag = @"#version 420 core
