b619489b5c5dd94de6b596962738205ab0b5e6d5
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-06-21 08:37:02 +0800

    query object.

---
+++ OpenGLviaCSharp/c13d01_QueryObject/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace c13d01_QueryObject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
