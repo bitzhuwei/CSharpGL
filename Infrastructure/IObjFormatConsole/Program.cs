9da8a0abb7e3c73a987b4017563fc98fbc00c5c1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-07-12 09:14:52 +0800

    vnfHanoiTower.obj_

--- Infrastructure/IObjFormatConsole/Program.cs
+++ Infrastructure/IObjFormatConsole/Program.cs
@@ -84,7 +84,71 @@ namespace IObjFormatConsole
                     model.DumpObjFile("vnf" + filename, "disk");
                 }
             }
+            {
+                Console.WriteLine("hanoiTower");
+                var list = HanoiTower.GetDataSource();
+                uint nextIndex = 0;
+                var positionList = new List<vec3>();
+                var indexList = new List<uint>();
+                foreach (var item in list)
+                {
+                    vec3[] positions = item.model.GetPositions();
+                    uint[] indexes = item.model.GetIndexes();
+                    for (int i = 0; i < positions.Length; i++)
+                    {
+                        positionList.Add(positions[i] + item.position);
+                    }
+                    for (int i = 0; i < indexes.Length; i++)
+                    {
+                        indexList.Add(indexes[i] + nextIndex);
+                    }
+
+                    nextIndex += (uint)positions.Length;
+                }
+                var hanoiTower = new TmpModel(positionList.ToArray(), indexList.ToArray());
+                hanoiTower.DumpObjFile("HanoiTower.obj", "HanoiTower");
+                var filename = "HanoiTower.obj";
+                hanoiTower.DumpObjFile(filename, "hanoiTower");
+                var parser = new ObjVNFParser(false);
+                ObjVNFResult result = parser.Parse(filename);
+                if (result.Error != null)
+                {
+                    Console.WriteLine("Error: {0}", result.Error);
+                }
+                else
+                {
+                    ObjVNFMesh mesh = result.Mesh;
+                    var model = new ObjVNF(mesh);
+                    model.DumpObjFile("vnf" + filename, "hanoiTower");
+                }
+            }
             Console.WriteLine("done");
+        }
+
+        class TmpModel : IObjFormat
+        {
+            private vec3[] positions;
+            private uint[] indexes;
+
+            public TmpModel(vec3[] positions, uint[] indexes)
+            {
+                this.positions = positions;
+                this.indexes = indexes;
+            }
+
+            #region IObjFormat 成员
+
+            public vec3[] GetPositions()
+            {
+                return this.positions;
+            }
+
+            public uint[] GetIndexes()
+            {
+                return this.indexes;
+            }
+
+            #endregion
         }
     }
 }
