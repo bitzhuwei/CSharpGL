46ecba9537caec5ac909ded5314f5efd4b77b9dc
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-07-12 17:59:39 +0800

    camera

--- CSharpGL/Scene/Scene.cs
+++ CSharpGL/Scene/Scene.cs
@@ -19,6 +19,7 @@ namespace CSharpGL
         /// <summary>
         /// 
         /// </summary>
+        [Editor(typeof(PropertyGridEditor), typeof(UITypeEditor))]
         public ICamera Camera { get; set; }
 
         ///// <summary>
@@ -29,23 +30,27 @@ namespace CSharpGL
         /// <summary>
         /// 
         /// </summary>
+        [Editor(typeof(PropertyGridEditor), typeof(UITypeEditor))]
         public SceneNodeBase RootNode { get; set; }
 
         private List<LightBase> lights = new List<LightBase>();
         /// <summary>
         /// All lights in the scene.
         /// </summary>
+        [Editor(typeof(PropertyGridEditor), typeof(UITypeEditor))]
         public List<LightBase> Lights { get { return this.lights; } }
 
         /// <summary>
         /// 
         /// </summary>
+        [Editor(typeof(PropertyGridEditor), typeof(UITypeEditor))]
         public GLControl RootControl { get; set; }
 
         private vec4 clearColor = Color.SkyBlue.ToVec4();
         /// <summary>
         /// 
         /// </summary>
+        [Editor(typeof(PropertyGridEditor), typeof(UITypeEditor))]
         public vec4 ClearColor
         {
             get { return clearColor; }
@@ -55,6 +60,7 @@ namespace CSharpGL
         /// <summary>
         /// Ambient color.
         /// </summary>
+        [Editor(typeof(PropertyGridEditor), typeof(UITypeEditor))]
         public vec3 AmbientColor { get; set; }
 
         /// <summary>
