d7151982534ea23b4c102cb3c57b55041cc5e6c1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-07-18 17:25:58 +0800

    fuluDd00_LayeredEngraving

---
+++ OpenGLviaCSharp/fuluDd00_LayeredEngraving/QuadNode.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace fuluDd00_LayeredEngraving
+{
+    class QuadNode : ModernNode, IRenderable
+    {
+        public enum RenderMode { Blend = 0, Final = 1 };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public RenderMode Mode { get; set; }
+
+
+        private bool useBackground = true;
+
+        public bool UseBackground
+        {
+            get { return useBackground; }
+            set
+            {
+                useBackground = value;
+                {
+                    RenderMethod method = this.RenderUnit.Methods[(int)RenderMode.Final];
+                    ShaderProgram program = method.Program;
+                    program.SetUniform("useBackground", value);
+                }
+            }
+        }
+
+        private Texture tempTexture;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Texture TempTexture
+        {
+            get { return this.tempTexture; }
+            set
+            {
+                this.tempTexture = value;
+                {
+                    RenderMethod method = this.RenderUnit.Methods[(int)RenderMode.Blend];
+                    ShaderProgram program = method.Program;
+                    program.SetUniform("tempTexture", value);
+                }
+                {
+                    RenderMethod method = this.RenderUnit.Methods[(int)RenderMode.Final];
+                    ShaderProgram program = method.Program;
+                    program.SetUniform("colorTexture", value);
+                }
+            }
+        }
+
+        public static QuadNode Create()
+        {
+            RenderMethodBuilder blendBuilder, finalBuilder;
+            {
+                var vs = new VertexShader(Shaders.blendVert);
+                var fs = new FragmentShader(Shaders.blendFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("vVertex", QuadModel.positions);
+                blendBuilder = new RenderMethodBuilder(provider, map);
+            }
+            {
+                var vs = new VertexShader(Shaders.finalVert);// reuse blend vertex shader.
+                var fs = new FragmentShader(Shaders.finalFrag);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add("vVertex", QuadModel.positions);
+                finalBuilder = new RenderMethodBuilder(provider, map);
+            }
+
+            var model = new QuadModel();
+            var node = new QuadNode(model, blendBuilder, finalBuilder);
+            node.Initialize();
+
+            return node;
+        }
+
+        private QuadNode(IBufferSource model, params RenderMethodBuilder[] builders)
+            : base(model, builders)
+        {
+
+        }
+
+        private ThreeFlags enableRendering = ThreeFlags.BeforeChildren | ThreeFlags.Children | ThreeFlags.AfterChildren;
+        /// <summary>
+        /// Render before/after children? Render children? 
+        /// RenderAction cares about this property. Other actions, maybe, maybe not, your choice.
+        /// </summary>
+        public ThreeFlags EnableRendering
+        {
+            get { return this.enableRendering; }
+            set { this.enableRendering = value; }
+        }
+
+        public void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            //ICamera camera = arg.CameraStack;
+            //mat4 projection = camera.GetProjectionMatrix();
+            //mat4 view = camera.GetViewMatrix();
+            //mat4 model = this.GetModelMatrix();
+
+            RenderMethod method = this.RenderUnit.Methods[(int)this.Mode];
+            if (this.Mode == RenderMode.Final)
+            {
+                ShaderProgram program = method.Program;
+                var clearColor = new float[4];
+                GL.Instance.GetFloatv((uint)GetTarget.ColorClearValue, clearColor);
+                var value = new vec4(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
+                program.SetUniform("vBackgroundColor", value);
+            }
+
+            method.Render();
+        }
+
+        public void RenderAfterChildren(RenderEventArgs arg)
+        {
+        }
+
+    }
+}
---
+++ OpenGLviaCSharp/fuluDd00_LayeredEngraving/shaders.2.peel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace fuluDd00_LayeredEngraving
+{
+    public static partial class Shaders
+    {
+        public const string peelVert = @"#version 330 core
+  
+layout(location = 0) in vec3 vVertex; //object space vertex position
+
+//uniform
+uniform mat4 MVP;  //combined modelview projection matrix
+
+void main()
+{  
+    //get the clipspace vertex position
+    gl_Position = MVP*vec4(vVertex.xyz,1);
+}
+";
+        public const string peelFrag = @"#version 330 core
+
+layout(location = 0) out vec4 vFragColor;	//fragment shader output
+
+//uniforms
+uniform vec4 vColor;						//solid colour 
+uniform sampler2DRect  depthTexture;		//depth texture 
+
+void main()
+{
+    //read the depth value from the depth texture
+    float frontDepth = texture(depthTexture, gl_FragCoord.xy).r;
+
+    //compare the current fragment depth with the depth in the depth texture
+    //if it is less, discard the current fragment
+    if(gl_FragCoord.z <= frontDepth) discard;
+	
+    //otherwise set the given color uniform as the final output
+    vFragColor = vec4(vColor.rgb * vColor.a, vColor.a);
+}
+";
+    }
+}
