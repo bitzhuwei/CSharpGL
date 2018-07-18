c480afed2fdec672aee70c2968423c4517f1065c
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-07-18 18:24:16 +0800

    Render4VolumeData

--- OpenGLviaCSharp/fuluDd00_LayeredEngraving/PeelingNode.cs
+++ OpenGLviaCSharp/fuluDd00_LayeredEngraving/PeelingNode.cs
@@ -34,8 +34,10 @@ namespace fuluDd00_LayeredEngraving
             glBlendFuncSeparate = GL.Instance.GetDelegateFor("glBlendFuncSeparate", GLDelegates.typeof_void_uint_uint_uint_uint) as GLDelegates.void_uint_uint_uint_uint;
         }
 
-        public PeelingNode(params SceneNodeBase[] children)
+        public PeelingNode(vec3 size, params SceneNodeBase[] children)
         {
+            this.ModelSize = size;
+
             this.query = new Query();
             this.Children.AddRange(children);
             {
@@ -72,6 +74,124 @@ namespace fuluDd00_LayeredEngraving
         }
 
         public void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            Render4Display(arg);
+
+            //Render4VolumeData(arg);
+        }
+
+        private void Render4VolumeData(RenderEventArgs arg)
+        {
+            var viewport = new int[4]; GL.Instance.GetIntegerv((uint)GetTarget.Viewport, viewport);
+
+            if (this.width != viewport[2] || this.height != viewport[3])
+            {
+                Resize(viewport[2], viewport[3]);
+
+                this.width = viewport[2];
+                this.height = viewport[3];
+            }
+
+            int currentStep = 0, totalStep = this.RenderStep;
+            this.resources.blenderFBO.Bind();
+            GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+            this.resources.blenderFBO.Unbind();
+            Texture targetTexture = this.resources.blenderColorTexture;
+
+            // remember clear color.
+            var clearColor = new float[4];
+            GL.Instance.GetFloatv((uint)GetTarget.ColorClearValue, clearColor);
+
+            // init.
+            if (currentStep <= totalStep)
+            {
+                currentStep++;
+                this.resources.blenderFBO.Bind();
+                GL.Instance.ClearColor(0, 0, 0, 1);
+                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+                this.DrawScene(arg, CubeNode.RenderMode.Init, null);
+                this.resources.blenderFBO.Unbind();
+                targetTexture = this.resources.blenderColorTexture;
+
+                if (firstRun) { targetTexture.GetImage(this.width, this.height).Save("0.init.png"); }
+            }
+
+            int numLayers = (NUM_PASSES - 1) * 2;
+            int finalId = 2;
+            // for each pass
+            for (int layer = 1; (bUseOQ || layer < numLayers) && (currentStep <= totalStep); layer++)
+            {
+                finalId = layer * 2 + 1;
+                int currId = layer % 2;
+                int prevId = 1 - currId;
+                bool sampled = true;
+                // peel.
+                if (currentStep <= totalStep)
+                {
+                    currentStep++;
+                    this.resources.FBOs[currId].Bind();
+                    GL.Instance.ClearColor(0, 0, 0, 0);
+                    GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+                    if (bUseOQ) { this.query.BeginQuery(QueryTarget.SamplesPassed); }
+                    this.DrawScene(arg, CubeNode.RenderMode.Peel, this.resources.depthTextures[prevId]);
+                    if (bUseOQ) { this.query.EndQuery(QueryTarget.SamplesPassed); }
+                    this.resources.FBOs[currId].Unbind();
+                    targetTexture = this.resources.colorTextures[currId];
+
+                    if (bUseOQ)
+                    {
+                        int sampleCount = this.query.SampleCount();
+                        sampled = (sampleCount > 0);
+                    }
+
+                    if (firstRun && sampled)
+                    {
+                        targetTexture.GetImage(this.width, this.height).Save(string.Format(
+                            "{0}.peel.png", layer * 2 - 1));
+                    }
+                }
+
+
+                // blend.
+                if (currentStep <= totalStep)
+                {
+                    currentStep++;
+                    this.resources.blenderFBO.Bind();
+                    this.depthTest.On();
+                    this.blend.On();
+                    this.DrawFullScreenQuad(arg, QuadNode.RenderMode.Blend, this.resources.colorTextures[currId], false);
+                    this.blend.Off();
+                    this.depthTest.Off();
+                    this.resources.blenderFBO.Unbind();
+                    targetTexture = this.resources.blenderColorTexture;
+                    if (firstRun && sampled)
+                    {
+                        targetTexture.GetImage(this.width, this.height).Save(string.Format(
+                            "{0}.blend.png", layer * 2));
+                    }
+                }
+
+                if (!sampled) { break; }
+            }
+
+            // restore clear color.
+            GL.Instance.ClearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
+            // final.
+            this.DrawFullScreenQuad(arg, QuadNode.RenderMode.Final, targetTexture, this.renderStep >= maxStep);
+            if (this.firstRun)
+            {
+                var final = new Bitmap(width, height);
+                var data = final.LockBits(new Rectangle(0, 0, width, height), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                //GL.Instance.GetTexImage((uint)texture.Target, 0, GL.GL_BGRA, GL.GL_UNSIGNED_BYTE, data.Scan0);
+                GL.Instance.ReadPixels(0, 0, width, height, GL.GL_BGRA, GL.GL_UNSIGNED_BYTE, data.Scan0);
+                final.UnlockBits(data);
+                final.RotateFlip(RotateFlipType.Rotate180FlipX);
+                final.Save(string.Format("{0}.final.png", finalId));
+            }
+            this.firstRun = false;
+        }
+
+        private void Render4Display(RenderEventArgs arg)
         {
             var viewport = new int[4]; GL.Instance.GetIntegerv((uint)GetTarget.Viewport, viewport);
 
