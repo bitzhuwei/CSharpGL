748669728a299b30af0ac522ab2feb5e3ce5f604
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-07-26 12:37:29 +0800

    update

--- Demos/NormalMapping/FormMain.cs
+++ Demos/NormalMapping/FormMain.cs
@@ -31,9 +31,7 @@ namespace NormalMapping
             var center = new vec3(0, 0, 0);
             var up = new vec3(0, 1, 0);
             var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
-            this.scene = new Scene(camera)
-;
-
+            this.scene = new Scene(camera);
             {
                 var node = NormalMappingNode.Create();
                 float max = node.ModelSize.max();
