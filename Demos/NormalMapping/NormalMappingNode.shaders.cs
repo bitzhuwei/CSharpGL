367a4c968c636924b7f782a56674ae8c0e9872e6
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-07-27 17:17:47 +0800

    update

--- Demos/NormalMapping/NormalMappingNode.shaders.cs
+++ Demos/NormalMapping/NormalMappingNode.shaders.cs
@@ -95,19 +95,6 @@ uniform vec3 gEyeWorldPos;
 uniform float gMatSpecularIntensity;                                                        
 uniform float gSpecularPower;                                                               
                                                                                             
-float CalcShadowFactor(vec4 LightSpacePos)                                                  
-{                                                                                           
-    vec3 ProjCoords = LightSpacePos.xyz / LightSpacePos.w;                                  
-    vec2 UVCoords;                                                                          
-    UVCoords.x = 0.5 * ProjCoords.x + 0.5;                                                  
-    UVCoords.y = 0.5 * ProjCoords.y + 0.5;                                                  
-    float Depth = texture(gShadowMap, UVCoords).x;                                          
-    if (Depth <= (ProjCoords.z + 0.005))                                                    
-        return 0.5;                                                                         
-    else                                                                                    
-        return 1.0;                                                                         
-}                                                                                           
-                                                                                            
 vec4 CalcLightInternal(BaseLight Light, vec3 LightDirection, vec3 Normal,                   
                        float ShadowFactor)                                                  
 {                                                                                           
@@ -136,35 +123,6 @@ vec4 CalcDirectionalLight(vec3 Normal)
 {                                                                                                
     return CalcLightInternal(gDirectionalLight.Base, gDirectionalLight.Direction, Normal, 1.0);  
 }                                                                                                
-                                                                                            
-vec4 CalcPointLight(PointLight l, vec3 Normal, vec4 LightSpacePos)                          
-{                                                                                           
-    vec3 LightDirection = WorldPos0 - l.Position;                                           
-    float Distance = length(LightDirection);                                                
-    LightDirection = normalize(LightDirection);                                             
-    float ShadowFactor = CalcShadowFactor(LightSpacePos);                                   
-                                                                                            
-    vec4 Color = CalcLightInternal(l.Base, LightDirection, Normal, ShadowFactor);           
-    float Attenuation =  l.Atten.Constant +                                                 
-                         l.Atten.Linear * Distance +                                        
-                         l.Atten.Exp * Distance * Distance;                                 
-                                                                                            
-    return Color / Attenuation;                                                             
-}                                                                                           
-                                                                                            
-vec4 CalcSpotLight(SpotLight l, vec3 Normal, vec4 LightSpacePos)                            
-{                                                                                           
-    vec3 LightToPixel = normalize(WorldPos0 - l.Base.Position);                             
-    float SpotFactor = dot(LightToPixel, l.Direction);                                      
-                                                                                            
-    if (SpotFactor > l.Cutoff) {                                                            
-        vec4 Color = CalcPointLight(l.Base, Normal, LightSpacePos);                         
-        return Color * (1.0 - (1.0 - SpotFactor) * 1.0/(1.0 - l.Cutoff));                   
-    }                                                                                       
-    else {                                                                                  
-        return vec4(0,0,0,0);                                                               
-    }                                                                                       
-}                                                                                           
                                                                                             
 vec3 CalcBumpedNormal()                                                                     
 {                                                                                           
