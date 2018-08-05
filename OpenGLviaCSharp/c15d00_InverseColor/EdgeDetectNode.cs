a29ab6064c583352212bd41c428cfab540743ebd
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-08-05 23:27:58 +0800

    inverse color

---
+++ OpenGLviaCSharp/c15d00_InverseColor/EdgeDetectNode.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+using System.Drawing;
+
+namespace c15d00_InverseColor
+{
+    partial class EdgeDetectNode : PickableNode, IRenderable
+    {
+        private static readonly GLDelegates.void_uint glMemoryBarrier;
+        private static readonly GLDelegates.void_uint_uint_int_bool_int_uint_uint glBindImageTexture;
+        private static readonly GLDelegates.void_uint_uint_uint glDispatchCompute;
+        static EdgeDetectNode()
+        {
+            glMemoryBarrier = GL.Instance.GetDelegateFor("glMemoryBarrier", GLDelegates.typeof_void_uint) as GLDelegates.void_uint;
+            glBindImageTexture = GL.Instance.GetDelegateFor("glBindImageTexture", GLDelegates.typeof_void_uint_uint_int_bool_int_uint_uint) as GLDelegates.void_uint_uint_int_bool_int_uint_uint;
+            glDispatchCompute = GL.Instance.GetDelegateFor("glDispatchCompute", GLDelegates.typeof_void_uint_uint_uint) as GLDelegates.void_uint_uint_uint;
+        }
+
+        private const int width = 512;
+        private const int height = 512;
+        private Texture inTexture;
+        private Texture outTexture;
+
+        public static EdgeDetectNode Create()
+        {
+            RenderMethodBuilder compute, render;
+            {
+                var cs = new CSharpGL.ComputeShader(inverseColorComp);
+                var provider = new ShaderArray(cs);
+                var map = new AttributeMap();
+                compute = new RenderMethodBuilder(provider, map);
+            }
+            {
+                var vs = new VertexShader(vertexCode);
+                var fs = new FragmentShader(fragmentCode);
+                var provider = new ShaderArray(vs, fs);
+                var map = new AttributeMap();
+                map.Add(inPosition, RectangleModel.strPosition);
+                map.Add(inUV, RectangleModel.strUV);
+                render = new RenderMethodBuilder(provider, map);
+            }
+
+            var node = new EdgeDetectNode(new RectangleModel(), RectangleModel.strPosition, compute, render);
+            node.Initialize();
+
+            return node;
+        }
+
+        private EdgeDetectNode(IBufferSource model, string positionNameInIBufferSource, params RenderMethodBuilder[] builders)
+            : base(model, positionNameInIBufferSource, builders)
+        {
+        }
+
+        protected override void DoInitialize()
+        {
+            base.DoInitialize();
+
+            {
+                var bitmap = new Bitmap(width, height);
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    g.Clear(Color.Red);
+                    g.DrawString("CSharpGL", new Font("宋体", 88F), new SolidBrush(Color.Gold), new PointF(0, height / 2));
+                }
+                this.inTexture = this.GetTexture(bitmap);
+                this.InitIntermediateTexture();
+
+                bitmap.Dispose();
+            }
+        }
+
+        private void InitIntermediateTexture()
+        {
+            var storage = new TexStorage2D(TexStorage2D.Target.Texture2D, GL.GL_RGBA32F, width, height, 8);
+            var texture = new Texture(storage);
+            texture.Initialize();
+
+            this.outTexture = texture;
+        }
+
+        private Texture GetTexture(Bitmap bitmap)
+        {
+            bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
+
+            var texture = new Texture(new TexImageBitmap(bitmap, GL.GL_RGBA32F));
+            texture.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureWrapS, (int)GL.GL_CLAMP_TO_EDGE));
+            texture.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureWrapT, (int)GL.GL_CLAMP_TO_EDGE));
+            texture.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureWrapR, (int)GL.GL_CLAMP_TO_EDGE));
+            texture.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureMinFilter, (int)GL.GL_LINEAR));
+            texture.BuiltInSampler.Add(new TexParameteri(TexParameter.PropertyName.TextureMagFilter, (int)GL.GL_LINEAR));
+
+            texture.Initialize();
+
+            return texture;
+        }
+
+        #region IRenderable 成员
+
+        private ThreeFlags enableRendering = ThreeFlags.BeforeChildren | ThreeFlags.Children | ThreeFlags.AfterChildren;
+        /// <summary>
+        /// Render before/after children? Render children? 
+        /// RenderAction cares about this property. Other actions, maybe, maybe not, your choice.
+        /// </summary>
+        public ThreeFlags EnableRendering
+        {
+            get { return this.enableRendering; }
+            set { this.enableRendering = value; }
+        }
+
+        public void RenderBeforeChildren(RenderEventArgs arg)
+        {
+            uint inputUnit = 0, outputUnit = 1;
+            {
+                RenderMethod method = this.RenderUnit.Methods[0];
+                ShaderProgram computeProgram = method.Program;
+                // Activate the compute program and bind the output texture image
+                computeProgram.Bind();
+                glBindImageTexture(inputUnit, this.inTexture.Id, 0, false, 0, GL.GL_READ_WRITE, GL.GL_RGBA32F);
+                glBindImageTexture(outputUnit, this.outTexture.Id, 0, false, 0, GL.GL_READ_WRITE, GL.GL_RGBA32F);
+                // Dispatch
+                glDispatchCompute(1, width, 1);
+                glMemoryBarrier(GL.GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+                computeProgram.Unbind();
+            }
+            {
+                ICamera camera = arg.Camera;
+                mat4 projection = camera.GetProjectionMatrix();
+                mat4 view = camera.GetViewMatrix();
+                mat4 model = this.GetModelMatrix();
+
+                var method = this.RenderUnit.Methods[1]; // the only render unit in this node.
+                ShaderProgram program = method.Program;
+                program.SetUniform(tex, this.outTexture);
+                program.SetUniform(projectionMatrix, projection);
+                program.SetUniform(viewMatrix, view);
+                program.SetUniform(modelMatrix, model);
+
+                method.Render();
+            }
+        }
+
+        public void RenderAfterChildren(RenderEventArgs arg)
+        {
+        }
+
+        #endregion
+
+        public void SetTexture(Bitmap bitmap)
+        {
+            var bmp = new Bitmap(bitmap, width, height);
+            this.inTexture = GetTexture(bmp);
+        }
+
+    }
+}
---
+++ OpenGLviaCSharp/c15d00_InverseColor/FormMain.cs
@@ -0,0 +1,91 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace c15d00_InverseColor
+{
+    public partial class FormMain : Form
+    {
+        private Scene scene;
+        private ActionList actionList;
+        private EdgeDetectNode edgeDetectNode;
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Load += FormMain_Load;
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            var position = new vec3(0, 0, 0.9f);
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            var camera = new Camera(position, center, up, CameraType.Perspecitive, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+            this.scene = new Scene(camera)
+;
+            {
+                this.edgeDetectNode = EdgeDetectNode.Create();
+                this.scene.RootNode = this.edgeDetectNode;
+            }
+            var list = new ActionList();
+            var transformAction = new TransformAction(scene.RootNode);
+            list.Add(transformAction);
+            var renderAction = new RenderAction(scene);
+            list.Add(renderAction);
+            this.actionList = list;
+
+            var manipulater = new FirstPerspectiveManipulater();
+            manipulater.Bind(camera, this.winGLCanvas1);
+            manipulater.StepLength = 0.1f;
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            ActionList list = this.actionList;
+            if (list != null)
+            {
+                vec4 clearColor = this.scene.ClearColor;
+                GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+
+                list.Act(new ActionParams(Viewport.GetCurrent()));
+            }
+        }
+
+        void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
+        }
+
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            if (this.openImageDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    string filename = this.openImageDlg.FileName;
+                    var bitmap = new Bitmap(filename);
+                    var node = this.edgeDetectNode;
+                    if (node != null)
+                    {
+                        node.SetTexture(bitmap);
+                    }
+                    bitmap.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
