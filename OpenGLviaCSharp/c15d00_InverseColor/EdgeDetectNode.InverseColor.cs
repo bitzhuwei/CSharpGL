a6ed528f9eb991a05506d18db7497023c5485eea
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-08-05 23:37:46 +0800

    update

--- OpenGLviaCSharp/c15d00_InverseColor/EdgeDetectNode.InverseColor.cs
+++ OpenGLviaCSharp/c15d00_InverseColor/EdgeDetectNode.InverseColor.cs
@@ -15,8 +15,6 @@ layout (local_size_x = 512) in;
 layout (rgba32f, binding = 0) uniform image2D inImage;
 layout (rgba32f, binding = 1) uniform image2D outImage;
 
-shared vec4 scanline[512];
-
 void main(void)
 {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
