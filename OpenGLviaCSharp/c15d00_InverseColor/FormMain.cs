97d46367820cacc0933b8fbd929290c6608a789c
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-08-07 00:44:20 +0800

    update

--- OpenGLviaCSharp/c15d00_InverseColor/FormMain.cs
+++ OpenGLviaCSharp/c15d00_InverseColor/FormMain.cs
@@ -45,7 +45,7 @@ namespace c15d00_InverseColor
                 this.scene.RootNode = this.computeShaderNode;
             }
             var list = new ActionList();
-            var transformAction = new TransformAction(scene.RootNode);
+            var transformAction = new TransformAction(scene);
             list.Add(transformAction);
             var renderAction = new RenderAction(scene);
             list.Add(renderAction);
