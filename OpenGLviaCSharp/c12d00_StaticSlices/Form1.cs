fb03e886c53a9d988dc350fbcd7a6a813dd285bf
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-08-13 18:04:55 +0800

    update

--- OpenGLviaCSharp/c12d00_StaticSlices/Form1.cs
+++ OpenGLviaCSharp/c12d00_StaticSlices/Form1.cs
@@ -80,7 +80,7 @@ namespace c12d00_StaticSlices
             {
                 Texture tffTexture = InitTFF1DTexture("tff.png");
                 int width = 128, height = 128, depth = 128;
-                byte[] volumeData = VolumeData.GetData(width, height, depth);
+                byte[] volumeData = VolumeDataGenerator.GetData(width, height, depth);
                 Texture volumeTexture = InitVolume3DTexture(volumeData, width, height, depth);
 
                 int sliceCount = 512;
