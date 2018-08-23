0a6b7b89feb9cb894d8a639bd54e27e61a893661
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-08-23 10:58:11 +0800

    update

--- OpenGLviaCSharp/fuluDD01_LayeredEngraving.PNG/shaders.4.final.cs
+++ OpenGLviaCSharp/fuluDD01_LayeredEngraving.PNG/shaders.4.final.cs
@@ -15,7 +15,7 @@ in vec2 inPosition; //object space vertex position
 void main()
 {  
     //get the clip space position from the object space position
-    gl_Position = vec4(inPosition.xy*2 - 1.0,0,1);
+    gl_Position = vec4(inPosition.xy * 2 - 1.0,0,1);
 }
 ";
         public const string finalFrag = @"#version 330 core
@@ -35,7 +35,7 @@ void main()
     //by multiplying the colour alpha with the background colour and adding the 
     //product to the given colour uniform
     if (useBackground) {
-	    outColor = color + backgroundColor*color.a;
+	    outColor = color + backgroundColor * color.a;
     }
     else {
         outColor = color;
