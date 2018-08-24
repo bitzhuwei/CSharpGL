467ea77256f34420fcab2e596cc09e42895e1393
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-08-24 18:56:22 +0800

    update

--- OpenGLviaCSharp/fuluDD02_LayeredEngraving.ComputeShader/RaycastNode.raycasting.cs
+++ OpenGLviaCSharp/fuluDD02_LayeredEngraving.ComputeShader/RaycastNode.raycasting.cs
@@ -40,20 +40,11 @@ out vec4 FragColor;
 
 void main()
 {
-    // ExitPointCoord is normalized device coordinate
     vec3 exitPoint = texture(texExitPoint, gl_FragCoord.st / canvasSize).xyz;
-    // that will actually give you clip-space coordinates rather than
-    // normalised device coordinates, since you're not performing the perspective
-    // division which happens during the rasterisation process (between the vertex
-    // shader and fragment shader
-    // vec2 exitFragCoord = (ExitPointCoord.xy / ExitPointCoord.w + 1.0)/2.0;
-    // vec3 exitPoint  = texture(texExitPoint, exitFragCoord).xyz;
-
-    //background need no raycasting
     if (passEntryPoint == exitPoint) { discard; }
 
     vec3 direction = exitPoint - passEntryPoint;
-    float directionLength = length(direction); // the length from front to back is calculated and used to terminate the ray
+    float directionLength = length(direction);
     vec3 deltaDirection = direction * (stepLength / directionLength);
 
     vec3 voxelCoord = passEntryPoint;
@@ -65,14 +56,10 @@ void main()
  
     for(int i = 0; i < cycle; i++)
     {
-        // get scaler value in the volume data
         intensity = texture(texVolume, voxelCoord).rgb;
-        // get mapped color from 1-D texture
         colorSample = vec4(intensity, 0.05);
-        // modulate the value of colorSample.a
-        // front-to-back integration
+        // front-to-back integration.
         if (colorSample.r > 0.0 || colorSample.g > 0.0 || colorSample.b > 0.0) {
-            // accomodate for variable sampling rates (base interval defined by mod_compositing.frag)
             colorSample.a = 1.0 - pow(1.0 - colorSample.a, stepLength * 200.0f);
             colorAccumulator += (1.0 - alphaAccumulator) * colorSample.rgb * colorSample.a;
             alphaAccumulator += (1.0 - alphaAccumulator) * colorSample.a;
@@ -83,7 +70,7 @@ void main()
         {    
             colorAccumulator = colorAccumulator * alphaAccumulator 
                 + (1 - alphaAccumulator) * backgroundColor.rgb;
-            break;  // terminate if opacity > 1 or the ray is outside the volume
+            break;
         }    
         else if (alphaAccumulator > 1.0)
         {
@@ -92,10 +79,6 @@ void main()
         }
     }
     FragColor = vec4(colorAccumulator, alphaAccumulator);
-    // for test
-    // FragColor = vec4(passEntryPoint, 1.0);
-    // FragColor = vec4(exitPoint, 1.0);
-   
 }
 ";
     }
