f2cfb2971911789debb6477a49d72b9594d576fa
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-09 23:12:14 +0800

    new project MultipleContexts.

---
+++ Demos/MultipleContexts/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MultipleContexts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.winGLCanvas1 = new CSharpGL.WinGLCanvas();
+            this.winGLCanvas2 = new CSharpGL.WinGLCanvas();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.winGLCanvas1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.winGLCanvas2);
+            this.splitContainer1.Size = new System.Drawing.Size(824, 413);
+            this.splitContainer1.SplitterDistance = 351;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // winGLCanvas1
+            // 
+            this.winGLCanvas1.AccumAlphaBits = ((byte)(0));
+            this.winGLCanvas1.AccumBits = ((byte)(0));
+            this.winGLCanvas1.AccumBlueBits = ((byte)(0));
+            this.winGLCanvas1.AccumGreenBits = ((byte)(0));
+            this.winGLCanvas1.AccumRedBits = ((byte)(0));
+            this.winGLCanvas1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.winGLCanvas1.Location = new System.Drawing.Point(0, 0);
+            this.winGLCanvas1.Name = "winGLCanvas1";
+            this.winGLCanvas1.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
+            this.winGLCanvas1.Size = new System.Drawing.Size(351, 413);
+            this.winGLCanvas1.StencilBits = ((byte)(0));
+            this.winGLCanvas1.TabIndex = 0;
+            this.winGLCanvas1.TimerTriggerInterval = 40;
+            this.winGLCanvas1.UpdateContextVersion = true;
+            // 
+            // winGLCanvas2
+            // 
+            this.winGLCanvas2.AccumAlphaBits = ((byte)(0));
+            this.winGLCanvas2.AccumBits = ((byte)(0));
+            this.winGLCanvas2.AccumBlueBits = ((byte)(0));
+            this.winGLCanvas2.AccumGreenBits = ((byte)(0));
+            this.winGLCanvas2.AccumRedBits = ((byte)(0));
+            this.winGLCanvas2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.winGLCanvas2.Location = new System.Drawing.Point(0, 0);
+            this.winGLCanvas2.Name = "winGLCanvas2";
+            this.winGLCanvas2.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
+            this.winGLCanvas2.Size = new System.Drawing.Size(469, 413);
+            this.winGLCanvas2.StencilBits = ((byte)(0));
+            this.winGLCanvas2.TabIndex = 0;
+            this.winGLCanvas2.TimerTriggerInterval = 40;
+            this.winGLCanvas2.UpdateContextVersion = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 413);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private CSharpGL.WinGLCanvas winGLCanvas1;
+        private CSharpGL.WinGLCanvas winGLCanvas2;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ Demos/MultipleContexts/Form1.cs
@@ -0,0 +1,140 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultipleContexts
+{
+    public partial class Form1 : Form
+    {
+        private CubeNode cubeNode1;
+        private ActionList actionList1;
+        private Scene scene1;
+
+        private CubeNode cubeNode2;
+        private ActionList actionList2;
+        private Scene scene2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // init resources.
+            this.Load += FormMain_Load;
+            // render event.
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            // resize event.
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+            // render event.
+            this.winGLCanvas2.OpenGLDraw += winGLCanvas2_OpenGLDraw;
+            // resize event.
+            this.winGLCanvas2.Resize += winGLCanvas2_Resize;
+        }
+
+        private void winGLCanvas2_Resize(object sender, EventArgs e)
+        {
+            this.scene2.Camera.AspectRatio = ((float)this.winGLCanvas2.Width) / ((float)this.winGLCanvas2.Height);
+        }
+
+        private void winGLCanvas2_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            var canvas = sender as WinGLCanvas;
+            canvas.MakeCurrent();
+            ActionList list = this.actionList2;
+            if (list != null)
+            {
+                vec4 clearColor = this.scene2.ClearColor;
+                GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+
+                Viewport viewport = Viewport.GetCurrent();
+                list.Act(new ActionParams(viewport));
+            }
+        }
+        private void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            this.scene1.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            var canvas = sender as WinGLCanvas;
+            canvas.MakeCurrent();
+            ActionList list = this.actionList1;
+            if (list != null)
+            {
+                vec4 clearColor = this.scene1.ClearColor;
+                GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+
+                Viewport viewport = Viewport.GetCurrent();
+                list.Act(new ActionParams(viewport));
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            var thread = System.Threading.Thread.CurrentThread;
+            {
+                this.winGLCanvas1.MakeCurrent();
+                var position = new vec3(5, 3, 4) * 0.5f;
+                var center = new vec3(0, 0, 0);
+                var up = new vec3(0, 1, 0);
+                var camera = new Camera(position, center, up, CameraType.Perspective, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+                this.cubeNode1 = CubeNode.Create();
+                var scene = new Scene(camera);
+                scene.RootNode = cubeNode1;
+                this.scene1 = scene;
+
+                var list = new ActionList();
+                var transformAction = new TransformAction(scene);
+                list.Add(transformAction);
+                var renderAction = new RenderAction(scene);
+                list.Add(renderAction);
+                this.actionList1 = list;
+
+                //// uncomment these lines to enable manipualter of camera!
+                //var manipulater = new FirstPerspectiveManipulater();
+                //manipulater.BindingMouseButtons = System.Windows.Forms.MouseButtons.Right;
+                //manipulater.Bind(camera, this.winGLCanvas1);
+            }
+            {
+                this.winGLCanvas2.MakeCurrent();
+                var position = new vec3(5, 3, 4) * 0.5f;
+                var center = new vec3(0, 0, 0);
+                var up = new vec3(0, 1, 0);
+                var camera = new Camera(position, center, up, CameraType.Perspective, this.winGLCanvas2.Width, this.winGLCanvas2.Height);
+                this.cubeNode2 = CubeNode.Create();
+                var scene = new Scene(camera);
+                scene.RootNode = cubeNode2;
+                this.scene2 = scene;
+
+                var list = new ActionList();
+                var transformAction = new TransformAction(scene);
+                list.Add(transformAction);
+                var renderAction = new RenderAction(scene);
+                list.Add(renderAction);
+                this.actionList2 = list;
+
+                //// uncomment these lines to enable manipualter of camera!
+                //var manipulater = new FirstPerspectiveManipulater();
+                //manipulater.BindingMouseButtons = System.Windows.Forms.MouseButtons.Right;
+                //manipulater.Bind(camera, this.winGLCanvas1);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.cubeNode1.RotationAxis = new vec3(0, 1, 0);
+            this.cubeNode1.RotationAngle += 7f;
+
+            this.cubeNode2.RotationAxis = new vec3(0, 1, 0);
+            this.cubeNode2.RotationAngle += 7f;
+        }
+    }
+}
