4d19a0e04d046beb0a1fd0c0c81bd4b3c09d8974
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-13 16:09:32 +0800

    node

---
+++ Demos/EZMFileViewer/EZModel.cs
@@ -0,0 +1,31 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EZMFileViewer
+{
+    class EZModel : IBufferSource
+    {
+        private EZMFile ezmFile;
+        public EZModel(EZMFile file)
+        {
+            this.ezmFile = file;
+        }
+
+        #region IBufferSource 成员
+
+        public IEnumerable<VertexBuffer> GetVertexAttribute(string bufferName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IDrawCommand> GetDrawCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
