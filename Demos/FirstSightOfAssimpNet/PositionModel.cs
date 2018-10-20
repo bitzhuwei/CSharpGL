7559df9b269f0c99cf907f5d8556ef84fbece973
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-20 20:40:06 +0800

    PositionModel

---
+++ Demos/FirstSightOfAssimpNet/PositionModel.cs
@@ -0,0 +1,63 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstSightOfAssimpNet
+{
+    class PositionModel : IBufferSource
+    {
+        private Assimp.Mesh mesh;
+        public PositionModel(Assimp.Mesh mesh)
+        {
+            this.mesh = mesh;
+        }
+
+        public const string strPosition = "position";
+        private VertexBuffer positionBuffer;
+
+        private IDrawCommand drawCommand;
+
+        #region IBufferSource 成员
+
+        public IEnumerable<VertexBuffer> GetVertexAttribute(string bufferName)
+        {
+            if (strPosition == bufferName)
+            {
+                if (this.positionBuffer == null)
+                {
+                    this.positionBuffer = this.mesh.Vertices.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+
+                yield return this.positionBuffer;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public IEnumerable<IDrawCommand> GetDrawCommand()
+        {
+            if (this.drawCommand == null)
+            {
+                int faceCount = this.mesh.FaceCount;
+                var indexes = new uint[faceCount * 3];
+                for (int i = 0; i < faceCount; i++)
+                {
+                    for (int t = 0; t < 3; t++)
+                    {
+                        indexes[i * 3 + t] = this.mesh.Faces[faceCount].Indices[t];
+                    }
+                }
+                IndexBuffer indexBuffer = indexes.GenIndexBuffer(BufferUsage.StaticDraw);
+                this.drawCommand = new DrawElementsCmd(indexBuffer, DrawMode.Triangles);
+            }
+
+            yield return this.drawCommand;
+        }
+
+        #endregion
+    }
+}
