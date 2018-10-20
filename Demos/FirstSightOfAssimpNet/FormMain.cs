3bb4d7b8816ce71fac35148f5ea977bdf86de79b
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-20 20:07:58 +0800

    FirstSightOfAssimpNet

---
+++ Demos/FirstSightOfAssimpNet/FormMain.cs
@@ -0,0 +1,77 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FirstSightOfAssimpNet
+{
+    public partial class FormMain : Form
+    {
+        private Scene scene;
+        private ActionList actionList;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Load += FormMain_Load;
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            var position = new vec3(-5, 6, 4) * 3;
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            var camera = new Camera(position, center, up, CameraType.Perspective, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+
+            this.scene = new Scene(camera);
+
+            this.scene.RootNode = new GroupNode();
+            (new FormPropertyGrid(scene)).Show();
+
+            var list = new ActionList();
+            var transformAction = new TransformAction(scene);
+            list.Add(transformAction);
+            var renderAction = new RenderAction(scene);
+            list.Add(renderAction);
+            this.actionList = list;
+
+            var manipulater = new FirstPerspectiveManipulater();
+            manipulater.Bind(camera, this.winGLCanvas1);
+
+            var rootElement = this.scene.RootNode;
+            rootElement.Children.Clear();
+            string filename = @"skull.obj_";
+            //CreateTextureNode(filename);
+            //CreateBoneNode(filename);
+            //CreateDualQuatNode(filename);
+
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            ActionList list = this.actionList;
+            if (list != null)
+            {
+                vec4 clearColor = this.scene.ClearColor;
+                GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+
+                list.Act(new ActionParams(Viewport.GetCurrent()));
+            }
+        }
+
+        void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
+        }
+
+    }
+}
