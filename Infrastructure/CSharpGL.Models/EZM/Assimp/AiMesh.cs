fc8a42ddd7503eb93859320e8d9d2e5c8e171b70
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-29 19:30:58 +0800

    update

--- Infrastructure/CSharpGL.Models/EZM/Assimp/AiMesh.cs
+++ Infrastructure/CSharpGL.Models/EZM/Assimp/AiMesh.cs
@@ -7,5 +7,17 @@ namespace CSharpGL
 {
     public class AiMesh
     {
+        public vec3[] Vertexes { get; internal set; }
+
+        public vec3[] Normals { get; internal set; }
+
+        public vec2[] TexCoords { get; internal set; }
+
+        public vec4[] boneWeights { get; internal set; }
+
+        public uvec4[] boneIndexes { get; internal set; }
+
+        public uint[] indexes { get; internal set; }
+
     }
 }
--- Infrastructure/CSharpGL.Models/EZM/Assimp/EZMFile2AiScene.AiMesh.cs
+++ Infrastructure/CSharpGL.Models/EZM/Assimp/EZMFile2AiScene.AiMesh.cs
@@ -1,15 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace CSharpGL
 {
     public static partial class EZMFile2AiScene
     {
-        private static AiMesh Parse(EZMMesh ezmMesh)
+        private static unsafe AiMesh Parse(EZMMesh ezmMesh)
         {
-            throw new NotImplementedException();
+            var aiMesh = new AiMesh();
+            EZMVertexbuffer vertexbuffer = ezmMesh.Vertexbuffer;
+            {
+                // positions.
+                Passbuffer passbuffer = vertexbuffer.GetBuffer("position");
+                GCHandle pin = GCHandle.Alloc(passbuffer.array, GCHandleType.Pinned);
+                IntPtr address = pin.AddrOfPinnedObject();
+                vec3* pointer = (vec3*)address.ToPointer();
+                int length = passbuffer.Length();
+                var array = new vec3[length];
+                for (int i = 0; i < length; i++)
+                {
+                    array[i] = pointer[i];
+                }
+                aiMesh.Vertexes = array;
+            }
+            {
+                // normals.
+                Passbuffer passbuffer = vertexbuffer.GetBuffer("normal");
+                GCHandle pin = GCHandle.Alloc(passbuffer.array, GCHandleType.Pinned);
+                IntPtr address = pin.AddrOfPinnedObject();
+                vec3* pointer = (vec3*)address.ToPointer();
+                int length = passbuffer.Length();
+                var array = new vec3[length];
+                for (int i = 0; i < length; i++)
+                {
+                    array[i] = pointer[i];
+                }
+                aiMesh.Normals = array;
+            }
+            {
+                // normals.
+                Passbuffer passbuffer = vertexbuffer.GetBuffer("texcoord1");
+                GCHandle pin = GCHandle.Alloc(passbuffer.array, GCHandleType.Pinned);
+                IntPtr address = pin.AddrOfPinnedObject();
+                vec2* pointer = (vec2*)address.ToPointer();
+                int length = passbuffer.Length();
+                var array = new vec2[length];
+                for (int i = 0; i < length; i++)
+                {
+                    array[i] = pointer[i];
+                }
+                aiMesh.TexCoords = array;
+            }
+            {
+                // bone weights.
+                Passbuffer passbuffer = vertexbuffer.GetBuffer("blendweights");
+                GCHandle pin = GCHandle.Alloc(passbuffer.array, GCHandleType.Pinned);
+                IntPtr address = pin.AddrOfPinnedObject();
+                vec4* pointer = (vec4*)address.ToPointer();
+                int length = passbuffer.Length();
+                var array = new vec4[length];
+                for (int i = 0; i < length; i++)
+                {
+                    array[i] = pointer[i];
+                }
+                aiMesh.boneWeights = array;
+            }
+            {
+                // bone indexes.
+                Passbuffer passbuffer = vertexbuffer.GetBuffer("blendindices");
+                GCHandle pin = GCHandle.Alloc(passbuffer.array, GCHandleType.Pinned);
+                IntPtr address = pin.AddrOfPinnedObject();
+                uvec4* pointer = (uvec4*)address.ToPointer();
+                int length = passbuffer.Length();
+                var array = new uvec4[length];
+                for (int i = 0; i < length; i++)
+                {
+                    array[i] = pointer[i];
+                }
+                aiMesh.boneIndexes = array;
+            }
+
+            //ezmMesh.MeshSections
+            return aiMesh;
         }
 
     }
