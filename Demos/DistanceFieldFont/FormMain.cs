a26c6443111bbaa43e4cdc05f85c3049d00878c1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-12-07 13:00:56 +0800

    distance field font

---
+++ Demos/DistanceFieldFont/FormMain.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CSharpGL;
+
+namespace DistanceFieldFont
+{
+    public partial class FormMain : Form
+    {
+        private Scene scene;
+        private ActionList actionList;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Load += FormMain_Load;
+            this.winGLCanvas1.OpenGLDraw += winGLCanvas1_OpenGLDraw;
+            this.winGLCanvas1.Resize += winGLCanvas1_Resize;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            SceneNodeBase rootElement = GetRootElement();
+
+            var position = new vec3(1, 0, 4);
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            var camera = new Camera(position, center, up, CameraType.Perspective, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+            this.scene = new Scene(camera)
+            {
+                RootNode = rootElement,
+                ClearColor = Color.SkyBlue.ToVec4(),
+            };
+            var list = new ActionList();
+
+            var transformAction = new TransformAction(scene);
+            list.Add(transformAction);
+            var renderAction = new RenderAction(scene);
+            list.Add(renderAction);
+
+            this.actionList = list;
+
+            var manipulater = new FirstPerspectiveManipulater();
+            manipulater.Bind(camera, this.winGLCanvas1);
+            manipulater.StepLength = 0.1f;
+        }
+
+        private SceneNodeBase GetRootElement()
+        {
+            var rectangle = DistanceFieldNode.Create();
+            rectangle.Scale *= 3;
+            string folder = System.Windows.Forms.Application.StartupPath;
+            rectangle.TextureSource = new TextureSource(System.IO.Path.Combine(folder, @"texture2D.png"));
+
+            var group = new GroupNode(rectangle);//, blend, blend2);
+
+            var axis = AxisNode.Create();
+            group.Children.Add(axis);
+            return group;
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            ActionList list = this.actionList;
+            if (list != null)
+            {
+                vec4 clearColor = this.scene.ClearColor;
+                GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+
+                list.Act(new ActionParams(Viewport.GetCurrent()));
+            }
+        }
+
+        void winGLCanvas1_Resize(object sender, EventArgs e)
+        {
+            if (this.scene != null)
+            {
+                this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
+            }
+        }
+    }
+}
