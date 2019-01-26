710b8137c9bc528c13e36412b464a7896ef847fe
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-01-26 14:25:52 +0800

    shaders ready

--- PhysicallyBasedRendering/AnimationNode.cs
+++ PhysicallyBasedRendering/AnimationNode.cs
@@ -11,18 +11,19 @@ namespace PhysicallyBasedRendering
         private PBRModel model;
         public static PBRNode Create(PBRModel model)
         {
-            var vs = new VertexShader(vertexCode);
-            var fs = new FragmentShader(fragmentCode);
-            var array = new ShaderArray(vs, fs);
-            var map = new AttributeMap();
-            map.Add("inPosition", PBRModel.strPosition);
-            map.Add("inNormal", PBRModel.strNormal);
-            map.Add("inTexCoord", PBRModel.strTexCoord);
-            var builder = new RenderMethodBuilder(array, map);
-            var node = new PBRNode(model, builder);
-            node.Initialize();
+            throw new NotImplementedException();
+            //var vs = new VertexShader(vertexCode);
+            //var fs = new FragmentShader(fragmentCode);
+            //var array = new ShaderArray(vs, fs);
+            //var map = new AttributeMap();
+            //map.Add("inPosition", PBRModel.strPosition);
+            //map.Add("inNormal", PBRModel.strNormal);
+            //map.Add("inTexCoord", PBRModel.strTexCoord);
+            //var builder = new RenderMethodBuilder(array, map);
+            //var node = new PBRNode(model, builder);
+            //node.Initialize();
 
-            return node;
+            //return node;
         }
 
         private PBRNode(PBRModel model, params RenderMethodBuilder[] builders) : base(model, builders) { this.model = model; }
