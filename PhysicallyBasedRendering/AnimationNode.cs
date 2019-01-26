3300a4688981565a9dc69f0c4e4f70d178203d81
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-01-26 18:07:24 +0800

    LoadTexture

--- PhysicallyBasedRendering/AnimationNode.cs
+++ PhysicallyBasedRendering/AnimationNode.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CSharpGL;
+using System.Drawing;
 
 namespace PhysicallyBasedRendering
 {
@@ -28,6 +29,17 @@ namespace PhysicallyBasedRendering
 
         private PBRNode(PBRModel model, params RenderMethodBuilder[] builders) : base(model, builders) { this.model = model; }
 
+        private Texture LoadTexture(string filename)
+        {
+            var bitmap = new Bitmap(filename);
+            var storage = new TexImageBitmap(bitmap, GL.GL_RGB);
+            var texture = new Texture(storage,
+                new TexParameteri(TexParameter.PropertyName.TextureWrapS, (int)GL.GL_REPEAT),
+                new TexParameteri(TexParameter.PropertyName.TextureWrapT, (int)GL.GL_REPEAT),
+                new TexParameteri(TexParameter.PropertyName.TextureMinFilter, (int)GL.GL_LINEAR),
+                new TexParameteri(TexParameter.PropertyName.TextureMagFilter, (int)GL.GL_LINEAR));
 
+            return texture;
+        }
     }
 }
