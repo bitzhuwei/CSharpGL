63af739d5462c250733c171fd1596458d10ccb21
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-02-19 23:28:52 +0800

    update

--- Demos/PhysicallyBasedRendering/stb_Image/rgbe.cs
+++ Demos/PhysicallyBasedRendering/stb_Image/rgbe.cs
@@ -26,6 +26,7 @@ namespace stb_Image {
     }
 
     public static class rgbe {
+
         /* flags indicating which fields in an rgbe_header_info are valid */
         public const int RGBE_VALID_PROGRAMTYPE = 0x01;
         public const int RGBE_VALID_GAMMA = 0x02;
@@ -34,6 +35,13 @@ namespace stb_Image {
         /* return codes for rgbe routines */
         public const int RGBE_RETURN_SUCCESS = 0;
         public const int RGBE_RETURN_FAILURE = -1;
+
+        /* offsets to red, green, and blue components in a data (float) pixel */
+        public const int RGBE_DATA_RED = 0;
+        public const int RGBE_DATA_GREEN = 1;
+        public const int RGBE_DATA_BLUE = 2;
+        /* number of floats per pixel */
+        public const int RGBE_DATA_SIZE = 3;
 
         /* read or write headers */
         /* you may set rgbe_header_info to null if you want to */
