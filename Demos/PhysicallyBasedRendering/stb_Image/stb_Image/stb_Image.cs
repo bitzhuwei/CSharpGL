ded32485f0ad6a958233449f0be2935ca1f6df2a
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-02-20 19:58:06 +0800

    stbi__result_info

--- Demos/PhysicallyBasedRendering/stb_Image/stb_Image/stb_Image.cs
+++ Demos/PhysicallyBasedRendering/stb_Image/stb_Image/stb_Image.cs
@@ -16,7 +16,7 @@ namespace CSharpGL {
             STBI_grey_alpha = 2,
             STBI_rgb = 3,
             STBI_rgb_alpha = 4
-        };
+        }
 
         delegate int delRead(object user, char* data, int size);
         delegate void delSkip(object user, int n);
@@ -34,21 +34,30 @@ namespace CSharpGL {
 
         // stbi__context structure is our basic context used by all images, so it
         // contains all the IO context, plus some basic image information
-        public unsafe struct stbi__context {
-            UInt32 img_x, img_y;
-            int img_n, img_out_n;
+        unsafe struct stbi__context {
+            public UInt32 img_x, img_y;
+            public int img_n, img_out_n;
 
-            stbi_io_callbacks io;
-            void* io_user_data;
+            public stbi_io_callbacks io;
+            public void* io_user_data;
 
-            int read_from_callbacks;
-            int buflen;
-            char* buffer_start;//=new char[128];
+            public int read_from_callbacks;
+            public int buflen;
+            public char* buffer_start;//=new char[128];
 
-            char* img_buffer;
-            char* img_buffer_end;
-            char* img_buffer_original;
-            char* img_buffer_original_end;
+            public char* img_buffer;
+            public char* img_buffer_end;
+            public char* img_buffer_original;
+            public char* img_buffer_original_end;
         }
+
+        struct stbi__result_info {
+            public int bits_per_channel;
+            public int num_channels;
+            public int channel_order;
+        }
+
+        static int stbi__vertically_flip_on_load = 0;
+
     }
 }
