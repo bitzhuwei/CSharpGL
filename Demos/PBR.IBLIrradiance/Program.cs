c3e010c05c2027bfff279e4b167ed0864dc763a5
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-04-30 11:03:19 +0800

    PBR.IBLIrradiance

---
+++ Demos/PBR.IBLIrradiance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PBR.IBLIrradiance {
+    static class Program {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+    }
+}
