55ac57d1145bd55bdfae1260adb6d3ef2942d03e
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-04-30 16:36:04 +0800

    PBR.IBLSpecular

---
+++ Demos/PBR.IBLSpecular/CubeModel.cs
@@ -0,0 +1,83 @@
+﻿using CSharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PBR.IBLSpecular {
+    class CubeModel : IBufferSource {
+        static readonly float[] vertices = {
+            // back face
+            -1.0f, -1.0f, -1.0f,  // bottom-left
+             1.0f,  1.0f, -1.0f,  // top-right
+             1.0f, -1.0f, -1.0f,  // bottom-right         
+             1.0f,  1.0f, -1.0f,  // top-right
+            -1.0f, -1.0f, -1.0f,  // bottom-left
+            -1.0f,  1.0f, -1.0f,  // top-left
+            // front face
+            -1.0f, -1.0f,  1.0f,  // bottom-left
+             1.0f, -1.0f,  1.0f,  // bottom-right
+             1.0f,  1.0f,  1.0f,  // top-right
+             1.0f,  1.0f,  1.0f,  // top-right
+            -1.0f,  1.0f,  1.0f,  // top-left
+            -1.0f, -1.0f,  1.0f,  // bottom-left
+            // left face
+            -1.0f,  1.0f,  1.0f,  // top-right
+            -1.0f,  1.0f, -1.0f,  // top-left
+            -1.0f, -1.0f, -1.0f,  // bottom-left
+            -1.0f, -1.0f, -1.0f,  // bottom-left
+            -1.0f, -1.0f,  1.0f,  // bottom-right
+            -1.0f,  1.0f,  1.0f,  // top-right
+            // right face
+             1.0f,  1.0f,  1.0f,  // top-left
+             1.0f, -1.0f, -1.0f,  // bottom-right
+             1.0f,  1.0f, -1.0f,  // top-right         
+             1.0f, -1.0f, -1.0f,  // bottom-right
+             1.0f,  1.0f,  1.0f,  // top-left
+             1.0f, -1.0f,  1.0f,  // bottom-left     
+            // bottom face
+            -1.0f, -1.0f, -1.0f,  // top-right
+             1.0f, -1.0f, -1.0f,  // top-left
+             1.0f, -1.0f,  1.0f,  // bottom-left
+             1.0f, -1.0f,  1.0f,  // bottom-left
+            -1.0f, -1.0f,  1.0f,  // bottom-right
+            -1.0f, -1.0f, -1.0f,  // top-right
+            // top face
+            -1.0f,  1.0f, -1.0f,  // top-left
+             1.0f,  1.0f , 1.0f,  // bottom-right
+             1.0f,  1.0f, -1.0f,  // top-right     
+             1.0f,  1.0f,  1.0f,  // bottom-right
+            -1.0f,  1.0f, -1.0f,  // top-left
+            -1.0f,  1.0f,  1.0f,  // bottom-left        
+        };
+
+        public const string strPosition = "position";
+        private VertexBuffer positionBuffer;
+        private IDrawCommand drawCmd;
+
+        #region IBufferSource 成员
+
+        public IEnumerable<VertexBuffer> GetVertexAttribute(string bufferName) {
+            if (strPosition == bufferName) {
+                if (this.positionBuffer == null) {
+                    this.positionBuffer = vertices.GenVertexBuffer(VBOConfig.Vec3, BufferUsage.StaticDraw);
+                }
+
+                yield return this.positionBuffer;
+            }
+            else {
+                throw new ArgumentException();
+            }
+        }
+
+        public IEnumerable<IDrawCommand> GetDrawCommand() {
+            if (this.drawCmd == null) {
+                this.drawCmd = new DrawArraysCmd(DrawMode.Triangles, 36);
+            }
+
+            yield return this.drawCmd;
+        }
+
+        #endregion
+    }
+}
---
+++ Demos/PBR.IBLSpecular/CubemapNode.shaders.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpGL;
+
+namespace PBR.IBLSpecular {
+    partial class CubemapNode {
+        private const string vertexCode = @"#version 330 core
+layout (location = 0) in vec3 aPos;
+
+out vec3 WorldPos;
+
+uniform mat4 projection;
+uniform mat4 view;
+
+void main()
+{
+    WorldPos = aPos;  
+    gl_Position =  projection * view * vec4(WorldPos, 1.0);
+}
+";
+
+        private const string fragmentCode = @"#version 330 core
+out vec4 FragColor;
+in vec3 WorldPos;
+
+uniform sampler2D equirectangularMap;
+
+const vec2 invAtan = vec2(0.1591, 0.3183);
+vec2 SampleSphericalMap(vec3 v)
+{
+    vec2 uv = vec2(atan(v.z, v.x), asin(v.y));
+    uv *= invAtan;
+    uv += 0.5;
+    return uv;
+}
+
+void main()
+{		
+    vec2 uv = SampleSphericalMap(normalize(WorldPos));
+    vec3 color = texture(equirectangularMap, uv).rgb;
+    
+    FragColor = vec4(color, 1.0);
+}
+";
+    }
+}
