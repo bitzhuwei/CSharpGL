68af4036f0a90d7cacdf02115d7073753ff4b120
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2019-05-01 17:28:37 +0800

    Texture info

--- Demos/SimpleObjFile/FormMain.cs
+++ Demos/SimpleObjFile/FormMain.cs
@@ -8,19 +8,17 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace SimpleObjFile
-{
-    public partial class FormMain : Form
-    {
+namespace SimpleObjFile {
+    public partial class FormMain : Form {
         private Scene scene;
         private ActionList actionList;
         //private LegacyTriangleNode triangleTip;
         //private LegacyQuadNode quadTip;
         private HighlightGeometryNode tipNode;
         private Picking pickingAction;
+        private TexturedNode targetNode;
 
-        public FormMain()
-        {
+        public FormMain() {
             InitializeComponent();
 
             this.Load += FormMain_Load;
@@ -34,8 +32,7 @@ namespace SimpleObjFile
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void winGLCanvas1_MouseMove(object sender, MouseEventArgs e)
-        {
+        void winGLCanvas1_MouseMove(object sender, MouseEventArgs e) {
             //LegacyTriangleNode triangleTip = this.triangleTip;
             //if (triangleTip == null) { return; }
             //LegacyQuadNode quadTip = this.quadTip;
@@ -45,17 +42,13 @@ namespace SimpleObjFile
             int x = e.X;
             int y = this.winGLCanvas1.Height - e.Y - 1;
             PickedGeometry pickedGeometry = null;
-            try
-            {
+            try {
                 pickedGeometry = this.pickingAction.Pick(x, y, PickingGeometryTypes.Triangle | PickingGeometryTypes.Quad, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
             }
-            catch (Exception)
-            { }
+            catch (Exception) { }
 
-            if (pickedGeometry != null)
-            {
-                switch (pickedGeometry.Type)
-                {
+            if (pickedGeometry != null) {
+                switch (pickedGeometry.Type) {
                     case GeometryType.Point:
                         tipNode.Vertex0 = pickedGeometry.Positions[0];
                         tipNode.CurrentMode = CSharpGL.DrawMode.Points;
@@ -104,16 +97,14 @@ namespace SimpleObjFile
                 }
 
             }
-            else
-            {
+            else {
                 //triangleTip.Parent = null;
                 //quadTip.Parent = null;
                 tipNode.Parent = null;
             }
         }
 
-        private void FormMain_Load(object sender, EventArgs e)
-        {
+        private void FormMain_Load(object sender, EventArgs e) {
             var position = new vec3(5, 6, 4) * 3;
             var center = new vec3(0, 0, 0);
             var up = new vec3(0, 1, 0);
@@ -147,39 +138,34 @@ namespace SimpleObjFile
             manipulater.Bind(camera, this.winGLCanvas1);
             {
                 ObjItem[] items = HanoiTower.GetDataSource();
-                for (int i = 0; i < items.Length; i++)
-                {
+                for (int i = 0; i < items.Length; i++) {
                     var item = items[i];
                     var filename = "item" + (i) + ".obj";
                     item.model.DumpObjFile(filename, "item" + (i));
                     var parser = new ObjVNFParser(false);
                     ObjVNFResult result = parser.Parse(filename);
-                    if (result.Error != null)
-                    {
+                    if (result.Error != null) {
                         //Console.WriteLine("Error: {0}", result.Error);
                     }
-                    else
-                    {
+                    else {
                         ObjVNFMesh mesh = result.Mesh;
-                        var node = ObjVNFNode.Create(mesh);
+                        var node = TexturedNode.Create(mesh);
                         node.Children.Add(new LegacyBoundingBoxNode(node.ModelSize));
                         //float max = node.ModelSize.max();
                         //node.Scale *= 7.0f / max;
                         node.WorldPosition = item.position;
-                        node.Diffuse = item.color;
                         var rootElement = this.scene.RootNode;
                         this.scene.RootNode.Children.Add(node);
+                        this.targetNode = node;
                         //if (rootElement != null) { rootElement.Dispose(); }
                     }
                 }
             }
         }
 
-        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
-        {
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e) {
             ActionList list = this.actionList;
-            if (list != null)
-            {
+            if (list != null) {
                 vec4 clearColor = this.scene.ClearColor;
                 GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
                 GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
@@ -188,57 +174,77 @@ namespace SimpleObjFile
             }
         }
 
-        void winGLCanvas1_Resize(object sender, EventArgs e)
-        {
+        void winGLCanvas1_Resize(object sender, EventArgs e) {
             this.scene.Camera.AspectRatio = ((float)this.winGLCanvas1.Width) / ((float)this.winGLCanvas1.Height);
         }
 
 
-        private void 打开OToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (this.openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                string filename = this.openFileDialog1.FileName;
+        private void 打开OToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (this.openModelDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                string filename = this.openModelDlg.FileName;
                 var parser = new ObjVNFParser(false);
                 ObjVNFResult result = parser.Parse(filename);
-                if (result.Error != null)
-                {
+                if (result.Error != null) {
                     MessageBox.Show(result.Error.ToString());
                 }
-                else
-                {
-                    var node = ObjVNFNode.Create(result.Mesh);
+                else {
+                    var node = TexturedNode.Create(result.Mesh);
                     float max = node.ModelSize.max();
                     node.Scale *= 16.0f / max;
                     node.WorldPosition = new vec3(0, 0, 0);
                     var rootElement = this.scene.RootNode;
                     this.scene.RootNode = node;
                     if (rootElement != null) { rootElement.Dispose(); }
+                    this.targetNode = node;
                 }
             }
         }
 
-        private void 退出XToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void 退出XToolStripMenuItem_Click(object sender, EventArgs e) {
             this.Close();
         }
 
-        private void 选项OToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void 选项OToolStripMenuItem_Click(object sender, EventArgs e) {
             (new FormPropertyGrid(this.scene)).Show();
         }
 
-        private void 旋转RToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void 旋转RToolStripMenuItem_Click(object sender, EventArgs e) {
             this.timer1.Enabled = !this.timer1.Enabled;
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
+        private void timer1_Tick(object sender, EventArgs e) {
             IWorldSpace node = this.scene.RootNode;
-            if (node != null)
-            {
+            if (node != null) {
                 node.RotationAngle += 1;
+            }
+        }
+
+        private void 纹理TToolStripMenuItem_Click(object sender, EventArgs e) {
+            TexturedNode node = this.targetNode;
+            if (node == null) {
+                MessageBox.Show("Please open a model first!");
+                return;
+            }
+
+            if (this.openTextureDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                try {
+                    var filename = this.openTextureDlg.FileName;
+                    var bitmap = new Bitmap(filename);
+                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
+                    var storage = new TexImageBitmap(bitmap);
+                    var texture = new Texture(storage,
+                        new TexParameteri(TexParameter.PropertyName.TextureWrapS, (int)GL.GL_CLAMP_TO_EDGE),
+                        new TexParameteri(TexParameter.PropertyName.TextureWrapT, (int)GL.GL_CLAMP_TO_EDGE),
+                        new TexParameteri(TexParameter.PropertyName.TextureMinFilter, (int)GL.GL_LINEAR),
+                        new TexParameteri(TexParameter.PropertyName.TextureMagFilter, (int)GL.GL_LINEAR));
+                    texture.Initialize();
+                    bitmap.Dispose();
+                    node.Texture = texture;
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(ex.ToString());
+                    return;
+                }
             }
         }
     }
---
+++ Infrastructure/CSharpGL.Models/TexCoordAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace CSharpGL {
+    public class TexCoordAnalyzer {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="texCoords">texture coordinates.</param>
+        /// <param name="indices">indices of triangles.</param>
+        /// <param name="length">length of returned bitmap file.</param>
+        /// <returns></returns>
+        public static Bitmap DumpLines(vec2[] texCoords, uint[] indices, int length) {
+            if (length < 1) { throw new ArgumentOutOfRangeException(); }
+
+            var bmp = new Bitmap(length, length);
+            var pens = new Pen[] { new Pen(Color.Red), new Pen(Color.Green), new Pen(Color.Blue) };
+            int penIndex = 0;
+            using (var graphics = Graphics.FromImage(bmp)) {
+                for (int i = 0; i < indices.Length; i += 3) {
+                    var uv1 = texCoords[indices[i + 0]];
+                    var uv2 = texCoords[indices[i + 1]];
+                    var uv3 = texCoords[indices[i + 2]];
+                    var p1 = new Point((int)(uv1.x * length), (int)(uv1.y * length));
+                    var p2 = new Point((int)(uv2.x * length), (int)(uv2.y * length));
+                    var p3 = new Point((int)(uv3.x * length), (int)(uv3.y * length));
+                    graphics.DrawLine(pens[penIndex], p1, p2);
+                    graphics.DrawLine(pens[penIndex], p2, p3);
+                    graphics.DrawLine(pens[penIndex], p3, p1);
+                    penIndex = (penIndex + 1 == pens.Length) ? 0 : penIndex + 1;
+                }
+            }
+
+            return bmp;
+        }
+    }
+}
